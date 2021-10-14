af4705ab122747ae01701be493e544e4edcca076
iMikul <gamer3mc@gmail.com>
2021-10-13 21:14:32 -0400

    mass deletion

--- src/main/java/net/momostudios/coldsweat/core/capabilities/ITemperatureCapability.java
+++
@@ -1,9 +0,0 @@
-package net.momostudios.coldsweat.core.capabilities;
-
-import net.momostudios.coldsweat.core.util.PlayerTemp;
-
-public interface ITemperatureCapability
-{
-    double get(PlayerTemp.Types type);
-    void set(PlayerTemp.Types type, double value);
-}
--- src/main/java/net/momostudios/coldsweat/core/util/ModEffects.java
+++
@@ -1,9 +0,0 @@
-package net.momostudios.coldsweat.core.util;
-
-import net.minecraft.potion.Effect;
-import net.momostudios.coldsweat.core.init.EffectInit;
-
-public class ModEffects
-{
-    public static Effect INSULATION = EffectInit.INSULATED_EFFECT_REGISTRY.get();
-}
