ed7cc0b12bd901b9eaca95af37d66ad093205f68
iMikul <gamer3mc@gmail.com>
2022-01-24 14:37:02 -0500

    Added ID remapping for soulfire_lamp -> nethernile_lamp

---
+++ src/main/java/net/momostudios/coldsweat/core/event/RemapMissingIDs.java
@@ -0,0 +1,22 @@
+package net.momostudios.coldsweat.core.event;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.momostudios.coldsweat.util.registrylists.ModItems;
+
+@Mod.EventBusSubscriber
+public class RemapMissingIDs
+{
+    @SubscribeEvent
+    public static void remapMissingItems(RegistryEvent.MissingMappings<Item> event)
+    {
+        event.getAllMappings().forEach(mapping -> {
+            if (mapping.key.toString().equals("cold_sweat:soulfire_lamp"))
+            {
+                mapping.remap(ModItems.NETHERNILE_LAMP);
+            }
+        });
+    }
+}
