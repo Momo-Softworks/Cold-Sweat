371aca47089346fd897100f24874bced519ab93a
iMikul <gamer3mc@gmail.com>
2022-01-28 14:48:51 -0500

    BlockEffects now have reduced effect through walls

--- src/main/java/net/momostudios/coldsweat/common/temperature/modifier/BlockTempModifier.java
+++ src/main/java/net/momostudios/coldsweat/common/temperature/modifier/BlockTempModifier.java
@@ -2,11 +2,15 @@ package net.momostudios.coldsweat.common.temperature.modifier;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.chunk.Chunk;
 import net.momostudios.coldsweat.common.temperature.Temperature;
 import net.momostudios.coldsweat.common.temperature.modifier.block.BlockEffect;
 import net.momostudios.coldsweat.common.world.BlockEffectEntries;
 import net.momostudios.coldsweat.util.CSMath;
+import net.momostudios.coldsweat.util.WorldInfo;
 
 public class BlockTempModifier extends TempModifier
 {
@@ -34,17 +38,79 @@ public class BlockTempModifier extends TempModifier
                     try
                     {
                         BlockPos blockpos = player.getPosition().add(x1, y1, z1);
+                        Chunk chunk = player.world.getChunkProvider().getChunkNow(blockpos.getX() >> 4, blockpos.getZ() >> 4);
 
-                        BlockState state = player.world.getBlockState(blockpos);
-                        BlockEffect be = BlockEffectEntries.getEntries().getEntryFor(state.getBlock());
-                        if (be != null)
+                        if (chunk == null) continue;
+
+                        // Get the BlockEffect associated with the block
+                        BlockState state = chunk.getBlockState(blockpos);
+                        BlockEffect be = BlockEffectEntries.getEntries().getEntryFor(state);
+
+                        if (be == null) continue;
+
+                        // Is totalTemp within the bounds of the BlockEffect's min/max allowed temps?
+                        if (CSMath.isBetween(totalTemp, be.minTemp(), be.maxTemp()))
                         {
-                            if (CSMath.isBetween(totalTemp, be.minTemp(), be.maxTemp()))
+                            // Get Vector positions of the centers of the source block and player
+                            Vector3d pos = new Vector3d(blockpos.getX() + 0.5, blockpos.getY() + 0.5, blockpos.getZ() + 0.5);
+                            Vector3d playerPos1 = new Vector3d(player.getPosX(), player.getPosY() + player.getHeight() * 0.25, player.getPosZ());
+                            Vector3d playerPos2 = new Vector3d(player.getPosX(), player.getPosY() + player.getEyeHeight(), player.getPosZ());
+
+                            // Get the temperature of the block given the player's distance
+                            double tempToAdd = be.getTemperature(player, state, blockpos, CSMath.getDistance(player, pos));
+
+                            // Lessen the effect with each block between the player and the block
+                            Vector3d prevPos1 = playerPos1;
+                            Vector3d prevPos2 = playerPos2;
+                            int blocksBetween = 0;
+                            for (int i = 0; i < 15; i++)
                             {
-                                totalTemp += be.getTemperature(player, state, blockpos, CSMath.getDistance(player, blockpos.add(0.5, 0.5, 0.5)));
+                                // Get the next block (sub)position
+                                Vector3d newPos1 = playerPos1.subtract(playerPos1.subtract(pos).scale(i / 15.0));
+                                Vector3d newPos2 = playerPos2.subtract(playerPos2.subtract(pos).scale(i / 15.0));
+
+                                //player.world.addParticle(ParticleTypes.FLAME, newPos1.x, newPos1.y, newPos1.z, 0, 0, 0);
+                                //player.world.addParticle(ParticleTypes.FLAME, newPos2.x, newPos2.y, newPos2.z, 0, 0, 0);
+
+                                // Check if the newPos1 is not a duplicate BlockPos or solid block
+                                BlockPos bpos1 = new BlockPos(newPos1);
+                                Vector3d facing1 = newPos1.subtract(prevPos1);
+                                Direction dir1 = Direction.getFacingFromVector(facing1.x, facing1.y, facing1.z);
+
+                                if (!bpos1.equals(new BlockPos(prevPos1)) && !bpos1.equals(blockpos)
+                                && !chunk.getBlockState(bpos1).isAir()
+                                && WorldInfo.isFullSide(chunk.getBlockState(bpos1), dir1, bpos1, player.world)
+                                && WorldInfo.isFullSide(chunk.getBlockState(bpos1), dir1.getOpposite(), bpos1, player.world))
+                                {
+                                    // Divide the added temperature by 2 for each block between the player and the block
+                                    blocksBetween++;
+                                }
+
+                                // Check if the newPos2 is not a duplicate BlockPos or solid block
+                                BlockPos bpos2 = new BlockPos(newPos2);
+                                Vector3d facing2 = newPos2.subtract(prevPos2);
+                                Direction dir2 = Direction.getFacingFromVector(facing2.x, facing2.y, facing2.z);
+
+                                if (!bpos2.equals(new BlockPos(prevPos2)) && !bpos2.equals(blockpos)
+                                && !chunk.getBlockState(bpos2).isAir()
+                                && WorldInfo.isFullSide(chunk.getBlockState(bpos2), dir2, bpos2, player.world)
+                                && WorldInfo.isFullSide(chunk.getBlockState(bpos2), dir2.getOpposite(), bpos2, player.world))
+                                {
+                                    // Divide the added temperature by 2 for each block between the player and the block
+                                    blocksBetween++;
+                                }
+
+                                prevPos1 = newPos1;
+                                prevPos2 = newPos2;
                             }
+                            double blockDampening = Math.pow(1.5, blocksBetween);
+
+                            totalTemp += tempToAdd / blockDampening;
                         }
-                    } catch (Exception e) {}
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
