f620a48b77ec63ff6c8740e8015b12a7a2091765
iMikul <gamer3mc@gmail.com>
2022-02-02 21:17:01 -0500

    Improve effectiveness & efficiency of DepthTempModifier

--- src/main/java/net/momostudios/coldsweat/common/temperature/modifier/DepthTempModifier.java
+++ src/main/java/net/momostudios/coldsweat/common/temperature/modifier/DepthTempModifier.java
@@ -1,47 +1,29 @@
 package net.momostudios.coldsweat.common.temperature.modifier;
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.LightType;
 import net.momostudios.coldsweat.common.temperature.Temperature;
 import net.momostudios.coldsweat.config.ConfigCache;
 import net.momostudios.coldsweat.util.CSMath;
 import net.momostudios.coldsweat.util.WorldInfo;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DepthTempModifier extends TempModifier
 {
     @Override
     public double getResult(Temperature temp, PlayerEntity player)
     {
-        if (player.world.getDimensionType().getHasCeiling())
-            return temp.get();
+        double midTemp = (ConfigCache.getInstance().maxTemp + ConfigCache.getInstance().minTemp) / 2;
+        double depth = Math.max(0, WorldInfo.getGroundLevel(player.getPosition(), player.world) - player.getPosition().getY());
 
-        double y = player.getPosY();
-        ConfigCache config = ConfigCache.getInstance();
-        Temperature midTemp = new Temperature((config.maxTemp + config.minTemp) / 2d);
+        Map<Double, Double> valueMap = new HashMap<>();
+        valueMap.put(temp.get(), 0.8);
+        valueMap.put(CSMath.blend(midTemp, temp.get(), player.world.getLightFor(LightType.SKY, player.getPosition()), 0, 15), 2.0);
+        valueMap.put(CSMath.blend(temp.get(), midTemp, !WorldInfo.canSeeSky(player.world, player.getPosition()) ? depth : depth / 2, 2, 20), 4.0);
 
-        // The total height across all samples
-        double weightedHeight = 0;
-        //
-        double surfaceNumber = 0.01;
-
-        for (BlockPos iterator : WorldInfo.getNearbyPositions(player.getPosition(), 100, 4))
-        {
-            // Get the surface height at the BlockPos
-            int level = WorldInfo.getGroundLevel(iterator, player.world) - 2;
-            // Makes insulation less effective if the BlockPos is exposed to air
-            if (y > level)
-                surfaceNumber++;
-
-            // Add the height to the total
-            weightedHeight += Math.max(0, level - y);
-        }
-        // Get the average depth from the samples
-        weightedHeight /= 100;
-
-        // Gets the depth
-        double factor = Math.max(0, weightedHeight - surfaceNumber);
-        double level = CSMath.clamp((-1/30d) * factor + 1, 0, 1);
-        return midTemp.get() + (temp.get() - midTemp.get()) * level;
+        return CSMath.weightedAverage(valueMap);
     }
 
     public String getID()
