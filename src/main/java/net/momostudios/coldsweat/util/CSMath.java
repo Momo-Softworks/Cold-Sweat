9e4d6c1cd3a6425756aefb3966d583218d5a7c28
iMikul <gamer3mc@gmail.com>
2022-02-02 21:03:00 -0500

    Refine blend() and add average() methods to CSMath

--- src/main/java/net/momostudios/coldsweat/util/CSMath.java
+++ src/main/java/net/momostudios/coldsweat/util/CSMath.java
@@ -4,6 +4,8 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 
+import java.util.Map;
+
 public class CSMath
 {
     /**
@@ -69,14 +71,61 @@ public class CSMath
      * @param rangeMax The maximum of the range of values over which to interpolate.
      * @return The interpolated value.
      */
-    public static double blend(double pointA, double pointB, double factor, double rangeMin, double rangeMax) {
-        return ((1 / (rangeMax - rangeMin)) * (clamp(factor, rangeMin, rangeMax) - rangeMin)) * (pointB - pointA) + pointA;
+    public static double blend(double pointA, double pointB, double factor, double rangeMin, double rangeMax)
+    {
+        if (factor <= rangeMin) return pointA;
+        if (factor >= rangeMax) return pointB;
+        return ((1 / (rangeMax - rangeMin)) * (factor - rangeMin)) * (pointB - pointA) + pointA;
     }
 
-    public static double getDistance(Entity entity, Vector3d pos) {
+    public static double getDistance(Entity entity, Vector3d pos)
+    {
         double xDistance = Math.max(0, Math.abs(entity.getPosX() - pos.x) - entity.getWidth() / 2);
         double yDistance = Math.max(0, Math.abs((entity.getPosY() + entity.getHeight() / 2) - pos.y) - entity.getHeight() / 2);
         double zDistance = Math.max(0, Math.abs(entity.getPosZ() - pos.z) - entity.getWidth() / 2);
         return Math.sqrt(xDistance * xDistance + yDistance * yDistance + zDistance * zDistance);
+    }
+
+    public static double average(double... values)
+    {
+        double sum = 0;
+        for (double value : values)
+        {
+            sum += value;
+        }
+        return sum / values.length;
+    }
+
+    /**
+     * Takes an average of the two values, with weight<br>
+     * The weight of an item should NEVER be 0.<br>
+     * @param val1 The first value.
+     * @param val2 The second value.
+     * @param weight1 The weight of the first value.
+     * @param weight2 The weight of the second value.
+     * @return The weighted average.
+     */
+    public static double weightedAverage(double val1, double val2, double weight1, double weight2)
+    {
+        return (val1 * weight1 + val2 * weight2) / (weight1 + weight2);
+    }
+
+    /**
+     * Takes an average of all the values in the given map, with weight<br>
+     * The weight of an item should NEVER be 0.<br>
+     * <br>
+     * @param values The map of values to average (value, weight).
+     * @return The average of the values in the given array.
+     */
+    public static double weightedAverage(Map<Double, Double> values)
+    {
+        double sum = 0;
+        double weightSum = 0;
+        for (Map.Entry<Double, Double> entry : values.entrySet())
+        {
+            sum += entry.getKey() * entry.getValue();
+            weightSum += entry.getValue();
+        }
+        return sum / weightSum;
     }
 }
