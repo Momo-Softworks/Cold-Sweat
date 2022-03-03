442576aeeb8656e7c7c87bfc33b45182f7b1f11b
iMikul <gamer3mc@gmail.com>
2022-03-03 01:01:53 -0500

    Fix VaporParticle weird behavior

--- src/main/java/dev/momostudios/coldsweat/client/particle/VaporParticle.java
+++ src/main/java/dev/momostudios/coldsweat/client/particle/VaporParticle.java
@@ -5,43 +5,40 @@ import net.minecraft.client.ParticleStatus;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nonnull;
+import java.util.List;
+
 @OnlyIn(Dist.CLIENT)
 public class VaporParticle extends TextureSheetParticle
 {
-    private SpriteSet ageSprite;
-    private boolean gravity;
+    private final SpriteSet ageSprite;
+    private final boolean hasGravity;
+    private boolean collidedY;
+    private float maxAlpha;
 
-    protected VaporParticle(ClientLevel world, double x, double y, double z, double vx, double vy, double vz, SpriteSet spriteSet, boolean gravity)
+    protected VaporParticle(ClientLevel world, double x, double y, double z, double vx, double vy, double vz, SpriteSet spriteSet, boolean hasGravity)
     {
         super(world, x, y, z);
         this.ageSprite = spriteSet;
         this.alpha = 0.0f;
-        this.scale(0.4f + (float) (Math.random() / 2.5f));
+        this.maxAlpha = (float) (Math.random() / 3 + 0.2f);
+        this.scale(3f + (float) (Math.random() / 2.5f));
         this.lifetime = 40;
         this.hasPhysics = true;
         this.setParticleSpeed(vx, vy, vz);
         this.setSpriteFromAge(spriteSet);
-        this.gravity = gravity;
+        this.hasGravity = hasGravity;
+        this.gravity = hasGravity ? 0.04f : -0.04f;
     }
 
-    @Override
-    public void move(double x, double y, double z)
-    {
-        if (x != 0.0D || y != 0.0D || z != 0.0D)
-        {
-            this.setBoundingBox(this.getBoundingBox().move(x, onGround ? 0 : y, z));
-            AABB axisalignedbb = this.getBoundingBox();
-            this.x = (axisalignedbb.minX + axisalignedbb.maxX) / 2.0D;
-            this.y = (axisalignedbb.minY + axisalignedbb.maxY) / 2.0D;
-            this.z = (axisalignedbb.minZ + axisalignedbb.maxZ) / 2.0D;
-        }
-    }
-
+    @Nonnull
     @Override
     public ParticleRenderType getRenderType() {
         return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
@@ -53,24 +50,27 @@ public class VaporParticle extends TextureSheetParticle
         if (Minecraft.getInstance().options.particles == ParticleStatus.MINIMAL)
             this.remove();
 
-        if (this.age++ >= this.getLifetime())
+        this.xo = this.x;
+        this.yo = this.y;
+        this.zo = this.z;
+        if (this.age++ >= this.lifetime)
         {
             this.remove();
         }
         else
         {
-            yd += gravity ? -0.04d : 0.04d;
+            this.yd -= 0.04D * gravity;
             this.move(xd * (onGround ? 1 : 0.2), yd, zd * (onGround ? 1 : 0.2));
             this.xd *= 0.99;
             this.yd *= 0.99;
-            this.zd *= 0.99;
+            this.xd *= 0.99;
         }
 
         this.setSpriteFromAge(ageSprite);
 
-        if (gravity)
+        if (hasGravity)
         {
-            if (this.age < 10)
+            if (this.alpha < maxAlpha)
                 this.alpha += 0.02f;
             else if (this.age > 32)
                 this.alpha -= 0.02f;
@@ -82,7 +82,7 @@ public class VaporParticle extends TextureSheetParticle
         {
             if (this.age < 10)
                 this.alpha += 0.07f;
-            else if (this.age > 32)
+            else if (this.age > this.lifetime - this.alpha / 0.02f)
                 this.alpha -= 0.02f;
 
             if (this.alpha < 0.07  && this.age > 10)
@@ -90,30 +90,77 @@ public class VaporParticle extends TextureSheetParticle
         }
     }
 
-    @OnlyIn(Dist.CLIENT)
-    public record SteamFactory(SpriteSet sprite) implements ParticleProvider<SimpleParticleType>
+    @Override
+    public void move(double x, double y, double z)
     {
+        double d0 = x;
+        double d1 = y;
+        double d2 = z;
+        if (this.hasPhysics && (x != 0.0D || y != 0.0D || z != 0.0D)) {
+            Vec3 vec3 = Entity.collideBoundingBox((Entity)null, new Vec3(x, y, z), this.getBoundingBox(), this.level, List.of());
+            x = vec3.x;
+            y = vec3.y;
+            z = vec3.z;
+        }
+
+        if (x != 0.0D || y != 0.0D || z != 0.0D) {
+            this.setBoundingBox(this.getBoundingBox().move(x, collidedY ? 0 : y, z));
+            AABB axisalignedbb = this.getBoundingBox();
+            this.x = (axisalignedbb.minX + axisalignedbb.maxX) / 2.0D;
+            this.y = axisalignedbb.minY + (hasGravity ? 0.2 : 0);
+            this.z = (axisalignedbb.minZ + axisalignedbb.maxZ) / 2.0D;
+        }
+
+        if (Math.abs(d1) >= 1.0E-5d && Math.abs(y) < 1.0E-5d) {
+            this.collidedY = true;
+        }
+
+        this.onGround = d1 != y && d1 < 0.0D;
+        if (d0 != x) {
+            this.xd = 0.0D;
+        }
+
+        if (d2 != z) {
+            this.zd = 0.0D;
+        }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static class SteamFactory implements ParticleProvider<SimpleParticleType>
+    {
+        private final SpriteSet sprite;
+
+        public SteamFactory(SpriteSet spriteSet) {
+            this.sprite = spriteSet;
+        }
+
         @Nullable
         @Override
         public Particle createParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed)
         {
             if (Minecraft.getInstance().options.particles != ParticleStatus.MINIMAL)
-                return new VaporParticle(level, x, y, z, xSpeed, ySpeed, zSpeed, sprite, false);
+                return new VaporParticle(level, x, y, z, xSpeed, ySpeed, zSpeed, this.sprite, false);
             else
                 return null;
         }
     }
 
     @OnlyIn(Dist.CLIENT)
-    public record MistFactory(SpriteSet sprite) implements ParticleProvider<SimpleParticleType>
+    public static class MistFactory implements ParticleProvider<SimpleParticleType>
     {
+        private final SpriteSet sprite;
+
+        public MistFactory(SpriteSet spriteSet) {
+            this.sprite = spriteSet;
+        }
+
         @Nullable
         @Override
         public Particle createParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed)
         {
             ParticleStatus status = Minecraft.getInstance().options.particles;
             if (status != ParticleStatus.MINIMAL && status != ParticleStatus.DECREASED)
-                return new VaporParticle(level, x, y, z, xSpeed, ySpeed, zSpeed, this.sprite, true);
+                return new VaporParticle(level, x, y, z, xSpeed, ySpeed, zSpeed, sprite, true);
             else
                 return null;
         }
