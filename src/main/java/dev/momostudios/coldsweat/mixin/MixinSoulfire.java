8e8256a7a1a08003354babd58298250fe82bba0f
iMikul <gamer3mc@gmail.com>
2022-03-15 18:36:09 -0400

    Use mixin for soulfire instead of events

---
+++ src/main/java/dev/momostudios/coldsweat/mixin/MixinSoulfire.java
@@ -0,0 +1,28 @@
+package dev.momostudios.coldsweat.mixin;
+
+import dev.momostudios.coldsweat.ColdSweat;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseFireBlock;
+import net.minecraft.world.level.block.SoulFireBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BaseFireBlock.class)
+public class MixinSoulfire
+{
+    @Inject(method = "entityInside(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/Entity;)V",
+    at = @At("HEAD"), cancellable = true, remap = ColdSweat.remapMixins)
+    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity, CallbackInfo ci)
+    {
+        if (state.getBlock() instanceof SoulFireBlock)
+        {
+            entity.clearFire();
+            ci.cancel();
+        }
+    }
+}
