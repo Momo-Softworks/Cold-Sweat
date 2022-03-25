adc703367777fa444b93cd07a10d67d7e8238aed
iMikul <gamer3mc@gmail.com>
2022-03-25 14:30:31 -0400

    Fix LegacyMappings not retrieving method for getBiome() in 1.18.1

--- src/main/java/dev/momostudios/coldsweat/util/LegacyMappings.java
+++ src/main/java/dev/momostudios/coldsweat/util/LegacyMappings.java
@@ -1,20 +1,25 @@
 package dev.momostudios.coldsweat.util;
 
+import dev.momostudios.coldsweat.ColdSweat;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.biome.BiomeManager;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 
 import java.lang.reflect.Method;
 
 public class LegacyMappings
 {
     public static Method getBiomeOld;
+
     static
     {
         try
         {
-            getBiomeOld = ObfuscationReflectionHelper.findMethod(BiomeManager.class, "m_47881", BlockPos.class);
+            getBiomeOld = BiomeManager.class.getDeclaredMethod("m_47881_", BlockPos.class);
         }
-        catch (Exception e) {}
+        catch (Exception e)
+        {
+            ColdSweat.LOGGER.warn("Failed to find method getBiome (m_47881) in BiomeManager");
+            ColdSweat.LOGGER.warn(e.getMessage(), e);
+        }
     }
 }
