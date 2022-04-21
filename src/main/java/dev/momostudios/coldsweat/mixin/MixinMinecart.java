5f2c9494500f1bf8f073276a8d3bc4abf4480c71
Mikul <gamer3mc@gmail.com>
2022-04-21 02:37:12 -0400

    Fix Minecart mixin duplicating items

--- src/main/java/dev/momostudios/coldsweat/mixin/MixinMinecart.java
+++ src/main/java/dev/momostudios/coldsweat/mixin/MixinMinecart.java
@@ -2,9 +2,13 @@ package dev.momostudios.coldsweat.mixin;
 
 import dev.momostudios.coldsweat.ColdSweat;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.vehicle.AbstractMinecart;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.vehicle.*;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,9 +22,15 @@ public class MixinMinecart
     @Inject(method = "destroy(Lnet/minecraft/world/damagesource/DamageSource;)V", at = @At("HEAD"), remap = ColdSweat.remapMixins)
     public void destroy(DamageSource source, CallbackInfo ci)
     {
-        if (minecart.level.getGameRules().getBoolean(GameRules.RULE_DOENTITYDROPS))
+        Block block = minecart.getDisplayBlockState().getBlock();
+
+        if (minecart.level.getGameRules().getBoolean(GameRules.RULE_DOENTITYDROPS)
+        && block != Blocks.HOPPER
+        && block != Blocks.CHEST
+        && block != Blocks.TNT
+        && block != Blocks.FURNACE)
         {
-            ItemStack itemStack = new ItemStack(minecart.getDisplayBlockState().getBlock().asItem());
+            ItemStack itemStack = new ItemStack(block.asItem());
             minecart.spawnAtLocation(itemStack);
         }
     }
