b94fb52eb072cc5f27e41a16804b6e6405019f7a
Mikul <gamer3mc@gmail.com>
2022-05-05 19:03:26 -0400

    BlockDataUpdateMessage use CompoundTag instead of list of tags

--- src/main/java/dev/momostudios/coldsweat/core/network/message/BlockDataUpdateMessage.java
+++ src/main/java/dev/momostudios/coldsweat/core/network/message/BlockDataUpdateMessage.java
@@ -3,58 +3,31 @@ package dev.momostudios.coldsweat.core.network.message;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import dev.momostudios.coldsweat.common.blockentity.HearthBlockEntity;
 import net.minecraftforge.network.NetworkEvent;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Supplier;
 
 public class BlockDataUpdateMessage
 {
     BlockPos blockPos;
-    List<Tag> tagValues;
-    List<String> blockTags;
+    CompoundTag tag;
 
-    public BlockDataUpdateMessage() {
-    }
-
-    public BlockDataUpdateMessage(BlockPos blockPos, List<String> blockTags, List<Tag> tagValues) {
+    public BlockDataUpdateMessage(BlockPos blockPos, CompoundTag tag) {
         this.blockPos = blockPos;
-        this.blockTags = blockTags;
-        this.tagValues = tagValues;
+        this.tag = tag;
     }
 
     public static void encode(BlockDataUpdateMessage message, FriendlyByteBuf buffer)
     {
         buffer.writeBlockPos(message.blockPos);
-        if (message.blockTags.size() == message.tagValues.size())
-        {
-            CompoundTag tags = new CompoundTag();
-            for (int i = 0; i < message.blockTags.size(); i++)
-            {
-                tags.put(message.blockTags.get(i), message.tagValues.get(i));
-            }
-            buffer.writeNbt(tags);
-        }
+        buffer.writeNbt(message.tag);
     }
 
     public static BlockDataUpdateMessage decode(FriendlyByteBuf buffer)
     {
-        BlockPos blockPos = buffer.readBlockPos();
-
-        CompoundTag tags = buffer.readNbt();
-        List<String> blockTags = tags.getAllKeys().stream().toList();
-        List<Tag> tagValues = new ArrayList<>(blockTags.size());
-        for (String blockTag : blockTags)
-        {
-            tagValues.add(tags.get(blockTag));
-        }
-
-        return new BlockDataUpdateMessage(blockPos, blockTags, tagValues);
+        return new BlockDataUpdateMessage(buffer.readBlockPos(), buffer.readNbt());
     }
 
     public static void handle(BlockDataUpdateMessage message, Supplier<NetworkEvent.Context> contextSupplier)
@@ -64,13 +37,12 @@ public class BlockDataUpdateMessage
         {
             context.enqueueWork(() ->
             {
-                BlockEntity te = Minecraft.getInstance().level.getBlockEntity(message.blockPos);
-                if (te != null)
+                Minecraft minecraft = Minecraft.getInstance();
+                BlockPos pos = message.blockPos;
+                BlockEntity blockEntity = minecraft.level.getBlockEntity(pos);
+                if (blockEntity != null)
                 {
-                    for (int i = 0; i < message.blockTags.size(); i++)
-                    {
-                        te.getTileData().put(message.blockTags.get(i), message.tagValues.get(i));
-                    }
+                    blockEntity.load(message.tag);
                 }
             });
         }
