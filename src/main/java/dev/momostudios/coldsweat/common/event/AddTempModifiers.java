1e67ca9faa0772ccffb467763d9bf2076c3ccb16
mikulgamour <gamer3mc@gmail.com>
2022-06-20 12:18:03 -0400

    Optimized how TempModifiers are added

--- src/main/java/dev/momostudios/coldsweat/common/event/AddTempModifiers.java
+++ src/main/java/dev/momostudios/coldsweat/common/event/AddTempModifiers.java
@@ -1,6 +1,5 @@
 package dev.momostudios.coldsweat.common.event;
 
-import dev.momostudios.coldsweat.ColdSweat;
 import dev.momostudios.coldsweat.api.registry.TempModifierRegistry;
 import dev.momostudios.coldsweat.api.temperature.Temperature;
 import dev.momostudios.coldsweat.api.temperature.modifier.*;
@@ -9,45 +8,59 @@ import dev.momostudios.coldsweat.util.registries.ModEffects;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.PotionEvent;
 import net.minecraftforge.event.world.SleepFinishedTimeEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = ColdSweat.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+@Mod.EventBusSubscriber
 public class AddTempModifiers
 {
     @SubscribeEvent
-    public static void onPlayerTick(TickEvent.PlayerTickEvent event)
+    public static void onPlayerCreated(EntityJoinWorldEvent event)
     {
-        if (event.phase == TickEvent.Phase.START)
+        if (event.getEntity() instanceof Player player)
         {
-            Player player = event.player;
-
             /*
              * Add TempModifiers if not present
              */
-            if (player.tickCount % 20 == 0)
-            {
-                TempHelper.addModifier(player, new BiomeTempModifier().tickRate(5), Temperature.Types.WORLD, false);
-                TempHelper.addModifier(player, new TimeTempModifier().tickRate(10), Temperature.Types.WORLD, false);
-                TempHelper.addModifier(player, new DepthTempModifier().tickRate(5), Temperature.Types.WORLD, false);
-                TempHelper.addModifier(player, new BlockTempModifier().tickRate(5), Temperature.Types.WORLD, false);
-                if (ModList.get().isLoaded("sereneseasons"))
-                    TempHelper.addModifier(player, TempModifierRegistry.getEntryFor("sereneseasons:season"), Temperature.Types.WORLD, false);
+            TempHelper.addModifier(player, new BiomeTempModifier().tickRate(5), Temperature.Types.WORLD, false);
+            TempHelper.addModifier(player, new TimeTempModifier().tickRate(20), Temperature.Types.WORLD, false);
+            TempHelper.addModifier(player, new DepthTempModifier().tickRate(5), Temperature.Types.WORLD, false);
+            TempHelper.addModifier(player, new BlockTempModifier().tickRate(5), Temperature.Types.WORLD, false);
+            if (ModList.get().isLoaded("sereneseasons"))
+                TempHelper.addModifier(player, TempModifierRegistry.getEntryFor("sereneseasons:season").tickRate(20), Temperature.Types.WORLD, false);
+        }
+    }
 
-                // Hearth
-                MobEffectInstance effect = player.getEffect(ModEffects.INSULATION);
-                if (effect != null)
-                {
-                    TempHelper.insertModifier(player, new HearthTempModifier(effect.getAmplifier() + 1).expires(20), Temperature.Types.WORLD);
-                }
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event)
+    {
+        Player player = event.player;
+
+        // Water / Rain
+        if (player.tickCount % 5 == 0 && player.isInWaterRainOrBubble())
+        {
+            TempHelper.addModifier(player, new WaterTempModifier(0.01), Temperature.Types.WORLD, false);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onInsulationUpdate(PotionEvent event)
+    {
+        if (event.getEntity() instanceof Player player && event.getPotionEffect() != null
+                && event.getPotionEffect().getEffect() == ModEffects.INSULATION)
+        {
+            if (event instanceof PotionEvent.PotionAddedEvent)
+            {
+                MobEffectInstance effect = event.getPotionEffect();
+                TempHelper.insertModifier(player, new HearthTempModifier(effect.getAmplifier() + 1).expires(effect.getDuration()), Temperature.Types.WORLD);
             }
-
-            // Water / Rain
-            if (player.tickCount % 5 == 0 && player.isInWaterRainOrBubble())
+            else if (event instanceof PotionEvent.PotionRemoveEvent || event instanceof PotionEvent.PotionExpiryEvent)
             {
-                TempHelper.addModifier(player, new WaterTempModifier(0.01), Temperature.Types.WORLD, false);
+                TempHelper.removeModifiers(player, Temperature.Types.WORLD, 1, mod -> mod instanceof HearthTempModifier);
             }
         }
     }
