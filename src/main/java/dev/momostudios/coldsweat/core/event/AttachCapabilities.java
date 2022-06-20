ac6e6c6f3e2d10a337a5033d8f11e396df008081
mikulgamour <gamer3mc@gmail.com>
2022-06-20 12:38:53 -0400

    Optimize AttachCapabilities

--- src/main/java/dev/momostudios/coldsweat/core/event/AttachCapabilities.java
+++ src/main/java/dev/momostudios/coldsweat/core/event/AttachCapabilities.java
@@ -1,6 +1,9 @@
 package dev.momostudios.coldsweat.core.event;
 
-import dev.momostudios.coldsweat.common.capability.*;
+import dev.momostudios.coldsweat.ColdSweat;
+import dev.momostudios.coldsweat.common.capability.ITemperatureCap;
+import dev.momostudios.coldsweat.common.capability.ModCapabilities;
+import dev.momostudios.coldsweat.common.capability.PlayerTempCapability;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
@@ -8,13 +11,12 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import dev.momostudios.coldsweat.ColdSweat;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -22,69 +24,68 @@ import javax.annotation.Nullable;
 @Mod.EventBusSubscriber
 public class AttachCapabilities
 {
-    private static class TempCapProvider implements ICapabilityProvider, INBTSerializable<CompoundTag>
-    {
-        private ITemperatureCap playerTempCap = new PlayerTempCapability();
-        private final LazyOptional<ITemperatureCap> capOptional = LazyOptional.of(this::getCapability);
-
-        @Nonnull
-        private ITemperatureCap getCapability()
-        {
-            if (playerTempCap == null)
-            {
-                playerTempCap = new PlayerTempCapability();
-            }
-            return playerTempCap;
-        }
-
-        @Nonnull
-        @Override
-        public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction direction)
-        {
-            if (cap == ModCapabilities.PLAYER_TEMPERATURE)
-            {
-                return capOptional.cast();
-            }
-            return LazyOptional.empty();
-        }
-
-        @Override
-        public CompoundTag serializeNBT() {
-            return this.playerTempCap.serializeNBT();
-        }
-
-        @Override
-        public void deserializeNBT(CompoundTag nbt) {
-            this.playerTempCap.deserializeNBT(nbt);
-        }
-    }
-
     @SubscribeEvent
     public static void attachCapabilityToEntityHandler(final AttachCapabilitiesEvent<Entity> event)
     {
-        if (event.getObject() instanceof Player)
+        if (!(event.getObject() instanceof Player)) return;
+
+        // Make a new capability instance to attach to the entity
+        ITemperatureCap playerTempCap = new PlayerTempCapability();
+        // Optional that holds the capability instance
+        LazyOptional<ITemperatureCap> capOptional = LazyOptional.of(() -> playerTempCap);
+        Capability<ITemperatureCap> capability = ModCapabilities.PLAYER_TEMPERATURE;
+
+        ICapabilityProvider provider = new ICapabilitySerializable<CompoundTag>()
         {
-            if (!event.getObject().getCapability(ModCapabilities.PLAYER_TEMPERATURE, null).isPresent())
+            @Nonnull
+            @Override
+            public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction direction)
             {
-                event.addCapability(new ResourceLocation(ColdSweat.MOD_ID, "temperature"), new TempCapProvider());
+                // If the requested cap is the temperature cap, return the temperature cap
+                if (cap == capability)
+                {
+                    return capOptional.cast();
+                }
+                return LazyOptional.empty();
             }
-        }
+
+            @Override
+            public CompoundTag serializeNBT()
+            {
+                return playerTempCap.serializeNBT();
+            }
+
+            @Override
+            public void deserializeNBT(CompoundTag nbt)
+            {
+                playerTempCap.deserializeNBT(nbt);
+            }
+        };
+
+        event.addCapability(new ResourceLocation(ColdSweat.MOD_ID, "temperature"), provider);
     }
 
+    /**
+     * Transfer the player's capability when traveling from the End
+     */
     @SubscribeEvent
     public static void copyCaps(PlayerEvent.Clone event)
     {
         if (!event.isWasDeath())
         {
+            // Get the old player's capability
             Player oldPlayer = event.getOriginal();
-            oldPlayer.revive();
+            oldPlayer.reviveCaps();
+
             oldPlayer.getCapability(ModCapabilities.PLAYER_TEMPERATURE).ifPresent(oldTempCap ->
             {
                 event.getPlayer().getCapability(ModCapabilities.PLAYER_TEMPERATURE).ifPresent(newTempCap ->
                 {
+                    // Copy the capability to the new player
                     newTempCap.copy(oldTempCap);
                 });
             });
+            // Remove the old player's capability (we don't need it anymore)
             oldPlayer.invalidateCaps();
         }
     }
