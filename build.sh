62f5bb3d68f4fb501c0bed0ce3570639e808e144
mikulgamour <gamer3mc@gmail.com>
2022-07-21 14:40:53 -0400

    Fix build script for Windows

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-sed -i "s/REMAP_MIXINS = false/REMAP_MIXINS = true/g" ~/Programs/IdeaProjects/Cold-Sweat/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
+sed -i "s/REMAP_MIXINS = false/REMAP_MIXINS = true/g" C:/Projects/IdeaProjects/Cold-Sweat/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
 ./gradlew build
-sed -i "s/REMAP_MIXINS = true/REMAP_MIXINS = false/g" ~/Programs/IdeaProjects/Cold-Sweat/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
+sed -i "s/REMAP_MIXINS = true/REMAP_MIXINS = false/g" C:/Projects/IdeaProjects/Cold-Sweat/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
