56eef14f61cc0f2c41886c683ee1b9b49a3d823d
mikulgamour <gamer3mc@gmail.com>
2022-07-21 23:08:03 -0400

    Fix buggy behavior when player is fully frozen

--- src/main/java/dev/momostudios/coldsweat/common/event/StopSnowDamage.java
+++ src/main/java/dev/momostudios/coldsweat/common/event/StopSnowDamage.java
@@ -7,6 +7,7 @@ import dev.momostudios.coldsweat.util.config.ConfigCache;
 import dev.momostudios.coldsweat.util.entity.TempHelper;
 import dev.momostudios.coldsweat.util.registries.ModEffects;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -27,15 +28,20 @@ public class StopSnowDamage
     @SubscribeEvent
     public static void playerFreezingTick(TickEvent.PlayerTickEvent event)
     {
-        if (!event.player.level.isClientSide() && event.player.getTicksFrozen() > 0 && event.player.getTicksFrozen() < event.player.getTicksRequiredToFreeze())
+        Player player = event.player;
+        if (!player.level.isClientSide() && player.getTicksFrozen() > 0)
         {
             TempModifier insulationModifier;
-            int insulation;
-            if ((event.player.hasEffect(ModEffects.ICE_RESISTANCE) && ConfigCache.getInstance().iceRes)
-            || ((insulation = ((insulationModifier = TempHelper.getModifier(event.player, Temperature.Types.RATE, InsulationTempModifier.class)) == null ? 0
-            : insulationModifier.<Integer>getArgument("warmth"))) > 0 && (event.player.tickCount % Math.max(1, 37 - insulation)) == 0))
+            int insulation = 0;
+            boolean hasIcePotion = player.hasEffect(ModEffects.ICE_RESISTANCE) && ConfigCache.getInstance().iceRes;
+            if (hasIcePotion
+            || ((insulation = ((insulationModifier = TempHelper.getModifier(player, Temperature.Types.RATE, InsulationTempModifier.class)) == null ? 0
+            : insulationModifier.<Integer>getArgument("warmth"))) > 0 && (player.tickCount % Math.max(1, 37 - insulation)) == 0))
             {
-                event.player.setTicksFrozen(event.player.getTicksFrozen() - 1);
+                if (player.getTicksFrozen() < event.player.getTicksRequiredToFreeze() || insulation >= 37 || hasIcePotion)
+                {
+                    player.setTicksFrozen(player.getTicksFrozen() - 1);
+                }
             }
         }
     }
