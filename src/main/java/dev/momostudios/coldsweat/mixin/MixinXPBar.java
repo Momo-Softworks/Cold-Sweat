86a3171e2f461856149bfcb050cb0c4c2939cd9f
mikulgamour <gamer3mc@gmail.com>
2022-09-10 19:10:22 -0400

    Fix UI mixins to be less invasive

--- src/main/java/dev/momostudios/coldsweat/mixin/MixinXPBar.java
+++ src/main/java/dev/momostudios/coldsweat/mixin/MixinXPBar.java
@@ -1,13 +1,10 @@
 package dev.momostudios.coldsweat.mixin;
 
-import com.mojang.blaze3d.vertex.*;
+import com.mojang.blaze3d.vertex.PoseStack;
 import dev.momostudios.coldsweat.ColdSweat;
-import net.minecraft.client.Minecraft;
-import dev.momostudios.coldsweat.client.event.RearrangeHotbar;
-import net.minecraft.client.gui.Font;
+import dev.momostudios.coldsweat.config.ClientSettingsConfig;
 import net.minecraft.client.gui.Gui;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Slice;
@@ -16,16 +13,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = Gui.class)
 public class MixinXPBar
 {
-    Gui gui = (Gui) (Object) this;
-
-    @Shadow
-    protected int screenWidth;
-    @Shadow
-    protected int screenHeight;
-
     /**
      * @author iMikul
-     * @reason Move XP bar elements to make room for body temperature readout
+     * @reason Move XP bar number to make room for body temperature readout (2 methods needed)
      */
     @Inject(method = "renderExperienceBar(Lcom/mojang/blaze3d/vertex/PoseStack;I)V",
             at = @At
@@ -39,26 +29,68 @@ public class MixinXPBar
                 from = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiling/ProfilerFiller;pop()V"),
                 to   = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Font;width(Ljava/lang/String;)I")
             ),
-            cancellable = true,
             remap = ColdSweat.REMAP_MIXINS)
-    public void renderExperienceBar(PoseStack poseStack, int xPos, CallbackInfo ci)
+    public void renderExperienceBar1(PoseStack poseStack, int xPos, CallbackInfo ci)
     {
-        Minecraft mc = Minecraft.getInstance();
-        Font font = gui.getFont();
-
         // Render XP bar
-        if (RearrangeHotbar.CUSTOM_HOTBAR)
+        if (ClientSettingsConfig.getInstance().customHotbar())
         {
-            String s = "" + mc.player.experienceLevel;
-            int i1 = (this.screenWidth - font.width(s)) / 2;
-            int j1 = this.screenHeight - 31;
-            font.draw(poseStack, s, (float)(i1 + 1), (float)j1, 0);
-            font.draw(poseStack, s, (float)(i1 - 1), (float)j1, 0);
-            font.draw(poseStack, s, (float)i1, (float)(j1 + 1), 0);
-            font.draw(poseStack, s, (float)i1, (float)(j1 - 1), 0);
-            font.draw(poseStack, s, (float)i1, (float)j1, 8453920);
-            mc.getProfiler().pop();
-            ci.cancel();
+            poseStack.translate(0.0D, 4.0D, 0.0D);
+        }
+    }
+
+    @Inject(method = "renderExperienceBar(Lcom/mojang/blaze3d/vertex/PoseStack;I)V",
+            at = @At
+            (
+                value = "INVOKE",
+                target = "Lnet/minecraft/util/profiling/ProfilerFiller;pop()V"
+            ),
+            slice = @Slice
+            (
+                from = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Font;width(Ljava/lang/String;)I"),
+                to   = @At(value = "RETURN")
+            ),
+            remap = ColdSweat.REMAP_MIXINS)
+    public void renderExperienceBar2(PoseStack poseStack, int xPos, CallbackInfo ci)
+    {
+        // Render XP bar
+        if (ClientSettingsConfig.getInstance().customHotbar())
+        {
+            poseStack.translate(0.0D, -4.0D, 0.0D);
+        }
+    }
+
+    @Mixin(Gui.class)
+    public static class MixinItemLabel
+    {
+        @Inject(method = "renderSelectedItemName(Lcom/mojang/blaze3d/vertex/PoseStack;)V",
+                at = @At
+                (
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/profiling/ProfilerFiller;push(Ljava/lang/String;)V",
+                    shift = At.Shift.AFTER
+                ), remap = ColdSweat.REMAP_MIXINS)
+        public void renderItemNamePre(PoseStack matrixStack, CallbackInfo ci)
+        {
+            if (ClientSettingsConfig.getInstance().customHotbar())
+            {
+                matrixStack.translate(0, -4, 0);
+            }
+        }
+
+        @Inject(method = "renderSelectedItemName(Lcom/mojang/blaze3d/vertex/PoseStack;)V",
+                at = @At
+                (
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/profiling/ProfilerFiller;pop()V",
+                    shift = At.Shift.BEFORE
+                ), remap = ColdSweat.REMAP_MIXINS)
+        public void renderItemNamePost(PoseStack matrixStack, CallbackInfo ci)
+        {
+            if (ClientSettingsConfig.getInstance().customHotbar())
+            {
+                matrixStack.translate(0, 4, 0);
+            }
         }
     }
 }
