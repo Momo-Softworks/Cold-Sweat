bc89095a15fbb0918b5b8db891782a29ba8da516
mikulgamour <gamer3mc@gmail.com>
2022-11-04 14:34:39 -0400

    Break from RemapMissingIDs if job is done (saves looping through all loaded items)

--- src/main/java/dev/momostudios/coldsweat/core/event/RemapMissingIDs.java
+++ src/main/java/dev/momostudios/coldsweat/core/event/RemapMissingIDs.java
@@ -1,9 +1,7 @@
 package dev.momostudios.coldsweat.core.event;
 
-import dev.momostudios.coldsweat.core.init.BlockInit;
-import dev.momostudios.coldsweat.core.init.ItemInit;
+import dev.momostudios.coldsweat.util.registries.ModItems;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,22 +12,13 @@ public class RemapMissingIDs
     @SubscribeEvent
     public static void remapMissingItems(RegistryEvent.MissingMappings<Item> event)
     {
-        event.getAllMappings().forEach(mapping -> {
+        for (RegistryEvent.MissingMappings.Mapping<Item> mapping : event.getAllMappings())
+        {
             if (mapping.key.toString().equals("cold_sweat:hellspring_lamp"))
             {
-                mapping.remap(ItemInit.SOULSPRING_LAMP.get());
+                mapping.remap(ModItems.SOULSPRING_LAMP);
+                break;
             }
-        });
-    }
-
-    @SubscribeEvent
-    public static void remapMissingBlocks(RegistryEvent.MissingMappings<Block> event)
-    {
-        event.getAllMappings().forEach(mapping -> {
-            if (mapping.key.toString().equals("cold_sweat:hearth"))
-            {
-                mapping.remap(BlockInit.HEARTH_BOTTOM.get());
-            }
-        });
+        }
     }
 }
