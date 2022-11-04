7d3fd892d013550d30aca1695f930dcc2fbf8320
mikulgamour <gamer3mc@gmail.com>
2022-11-04 14:32:16 -0400

    Fix MixinLevelRender clash with performance mods

--- src/main/java/dev/momostudios/coldsweat/mixin/MixinLevelRender.java
+++ src/main/java/dev/momostudios/coldsweat/mixin/MixinLevelRender.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LevelRenderer.class)
+@Mixin(value = LevelRenderer.class, priority = 1001)
 public class MixinLevelRender
 {
     @Shadow
