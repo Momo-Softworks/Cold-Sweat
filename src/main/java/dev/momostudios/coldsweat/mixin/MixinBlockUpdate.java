eba8813c3f7bdf8666fb8ef70ef962c7b1d98abf
mikulgamour <gamer3mc@gmail.com>
2022-11-04 14:31:35 -0400

    Fix MixinBlockUpdate softlock if chunk is unloaded

--- src/main/java/dev/momostudios/coldsweat/mixin/MixinBlockUpdate.java
+++ src/main/java/dev/momostudios/coldsweat/mixin/MixinBlockUpdate.java
@@ -4,6 +4,7 @@ import dev.momostudios.coldsweat.ColdSweat;
 import dev.momostudios.coldsweat.api.event.common.BlockChangedEvent;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,10 +22,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerLevel.class)
 public class MixinBlockUpdate
 {
+    Level level = (Level) (Object) this;
+
     @Inject(method = "onBlockStateChange", at = @At("HEAD"), remap = ColdSweat.REMAP_MIXINS)
     private void onBlockUpdate(BlockPos pos, BlockState oldState, BlockState newState, CallbackInfo ci)
     {
-        if (oldState != newState)
+        if (level.getChunkSource().hasChunk(pos.getX() >> 4, pos.getZ() >> 4) && oldState != newState)
         {
             MinecraftForge.EVENT_BUS.post(new BlockChangedEvent(pos, oldState, newState, (ServerLevel) (Object) this));
         }
