1bafe0dc024162aa3ec77ab04791539c56d91eac
mikulgamour <gamer3mc@gmail.com>
2022-12-09 13:19:29 -0500

    Optimize MixinBlockUpdate & fix chunkloading deadlocks

--- src/main/java/dev/momostudios/coldsweat/mixin/MixinBlockUpdate.java
+++ src/main/java/dev/momostudios/coldsweat/mixin/MixinBlockUpdate.java
@@ -3,9 +3,10 @@ package dev.momostudios.coldsweat.mixin;
 import dev.momostudios.coldsweat.ColdSweat;
 import dev.momostudios.coldsweat.api.event.common.BlockChangedEvent;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.ChunkStatus;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,13 +14,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
- * Called on the server side when a block state is changed.<br>
+ * Called when a block state is changed.<br>
  * This event is not {@link net.minecraftforge.eventbus.api.Cancelable}.<br>
  * <br>
- * {@link net.minecraftforge.event.world.BlockEvent.NeighborNotifyEvent} is only called on break/place. <br>
- * It doesn't trigger when a state is updated, nor does it know what the previous state was.<br>
+ * The event isn't called if the chunk does not have the {@link ChunkStatus#FULL} status to prevent chunkloading deadlocks.
  */
-@Mixin(ServerLevel.class)
+@Mixin(Level.class)
 public class MixinBlockUpdate
 {
     Level level = (Level) (Object) this;
@@ -27,9 +27,13 @@ public class MixinBlockUpdate
     @Inject(method = "onBlockStateChange", at = @At("HEAD"), remap = ColdSweat.REMAP_MIXINS)
     private void onBlockUpdate(BlockPos pos, BlockState oldState, BlockState newState, CallbackInfo ci)
     {
-        if (level.getChunkSource().hasChunk(pos.getX() >> 4, pos.getZ() >> 4) && oldState != newState)
+        if (oldState != newState)
         {
-            MinecraftForge.EVENT_BUS.post(new BlockChangedEvent(pos, oldState, newState, (ServerLevel) (Object) this));
+            ChunkAccess chunk = level.getChunkSource().getChunk(pos.getX() >> 4, pos.getZ() >> 4, ChunkStatus.FULL, false);
+            if (chunk != null && chunk.getStatus() == ChunkStatus.FULL)
+            {
+                MinecraftForge.EVENT_BUS.post(new BlockChangedEvent(pos, oldState, newState, level));
+            }
         }
     }
 }
