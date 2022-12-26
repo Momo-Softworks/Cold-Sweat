8d91be4a09ecf71d7ca2f15232c29c6b0c3fec42
mikulgamour <gamer3mc@gmail.com>
2022-12-26 01:08:42 -0500

    Insulated minecart item

--- src/main/java/dev/momostudios/coldsweat/core/init/ItemInit.java
+++ src/main/java/dev/momostudios/coldsweat/core/init/ItemInit.java
@@ -1,6 +1,7 @@
 package dev.momostudios.coldsweat.core.init;
 
-import dev.momostudios.coldsweat.common.item.MinecartInsulationItem;
+import dev.momostudios.coldsweat.common.item.*;
+import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
@@ -11,9 +12,6 @@ import dev.momostudios.coldsweat.common.block.BoilerBlock;
 import dev.momostudios.coldsweat.common.block.HearthBottomBlock;
 import dev.momostudios.coldsweat.common.block.IceboxBlock;
 import dev.momostudios.coldsweat.common.block.SewingTableBlock;
-import dev.momostudios.coldsweat.common.item.FilledWaterskinItem;
-import dev.momostudios.coldsweat.common.item.SoulspringLampItem;
-import dev.momostudios.coldsweat.common.item.WaterskinItem;
 import dev.momostudios.coldsweat.core.itemgroup.ColdSweatGroup;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -26,12 +24,14 @@ public class ItemInit
     public static final RegistryObject<Item> FILLED_WATERSKIN = ITEMS.register("filled_waterskin", FilledWaterskinItem::new);
     public static final RegistryObject<Item> MINECART_INSULATION = ITEMS.register("minecart_insulation", MinecartInsulationItem::new);
     public static final RegistryObject<Item> THERMOMETER = ITEMS.register("thermometer", () ->
-            new Item((new Item.Properties()).tab(ColdSweatGroup.COLD_SWEAT).rarity(Rarity.UNCOMMON)));
+            new Item(new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT).rarity(Rarity.UNCOMMON)));
     public static final RegistryObject<Item> SOULSPRING_LAMP = ITEMS.register("soulspring_lamp", SoulspringLampItem::new);
     public static final RegistryObject<Item> GOAT_FUR = ITEMS.register("goat_fur", () ->
-            new Item((new Item.Properties()).tab(ColdSweatGroup.COLD_SWEAT)));
+            new Item(new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
     public static final RegistryObject<Item> HOGLIN_HIDE = ITEMS.register("hoglin_hide", () ->
-            new Item((new Item.Properties()).tab(ColdSweatGroup.COLD_SWEAT)));
+            new Item(new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
+    public static final RegistryObject<Item> INSULATED_MINECART = ITEMS.register("insulated_minecart", () ->
+            new InsulatedMinecartItem(new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
 
     //BlockItems
     public static final RegistryObject<BlockItem> BOILER = ITEMS.register("boiler", () -> new BlockItem(BlockInit.BOILER.get(), BoilerBlock.getItemProperties()));
