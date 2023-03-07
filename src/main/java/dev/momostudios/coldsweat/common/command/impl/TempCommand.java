c0c80e3bdce61948797e0425bfa297125463e0d7
mikulgamour <gamer3mc@gmail.com>
2023-03-07 17:41:34 -0500

    Make temp command compatible with entities

--- src/main/java/dev/momostudios/coldsweat/common/command/impl/TempCommand.java
+++ src/main/java/dev/momostudios/coldsweat/common/command/impl/TempCommand.java
@@ -4,14 +4,17 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import dev.momostudios.coldsweat.api.util.Temperature;
+import dev.momostudios.coldsweat.common.capability.ITemperatureCap;
 import dev.momostudios.coldsweat.common.capability.ModCapabilities;
 import dev.momostudios.coldsweat.common.command.BaseCommand;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.capabilities.Capability;
 
 import java.util.Collection;
 import java.util.Comparator;
@@ -27,52 +30,64 @@ public class TempCommand extends BaseCommand
     {
         return builder
                 .then(Commands.literal("set")
-                        .then(Commands.argument("players", EntityArgument.players())
+                        .then(Commands.argument("entities", EntityArgument.entities())
                                 .then(Commands.argument("amount", IntegerArgumentType.integer(-150, 150))
                                         .executes(source -> executeSetPlayerTemp(
-                                        source.getSource(), EntityArgument.getPlayers(source, "players"), IntegerArgumentType.getInteger(source, "amount")))
+                                        source.getSource(), EntityArgument.getEntities(source, "entities"), IntegerArgumentType.getInteger(source, "amount")))
                                 )
                         )
                 )
                 .then(Commands.literal("get")
-                        .then(Commands.argument("players", EntityArgument.players())
+                        .then(Commands.argument("entities", EntityArgument.entities())
                                 .executes(source -> executeGetPlayerTemp(
-                                source.getSource(), EntityArgument.getPlayers(source, "players")))
+                                source.getSource(), EntityArgument.getEntities(source, "entities")))
                         )
                 );
     }
 
-    private int executeSetPlayerTemp(CommandSourceStack source, Collection<ServerPlayer> players, int temp)
+    private int executeSetPlayerTemp(CommandSourceStack source, Collection<? extends Entity> entities, int temp)
     {
-        // Set the temperature for all affected targets
-        for (ServerPlayer player : players)
+        if (entities.stream().anyMatch(entity -> !(entity instanceof LivingEntity)))
         {
-            player.getCapability(ModCapabilities.PLAYER_TEMPERATURE).ifPresent(cap ->
+            source.sendFailure(new TranslatableComponent("commands.cold_sweat.temperature.invalid"));
+            return Command.SINGLE_SUCCESS;
+        }
+        // Set the temperature for all affected targets
+        for (Entity entity : entities)
+        {
+            Capability<ITemperatureCap> iCap = entity instanceof Player ? ModCapabilities.PLAYER_TEMPERATURE : ModCapabilities.ENTITY_TEMPERATURE;
+            if (iCap == null) continue;
+            entity.getCapability(iCap).ifPresent(cap ->
             {
                 cap.setTemp(Temperature.Type.CORE, temp);
-                Temperature.updateTemperature(player, cap, true);
+                Temperature.updateTemperature((LivingEntity) entity, cap, true);
             });
         }
 
         //Compose & send message
-        if (players.size() == 1)
+        if (entities.size() == 1)
         {
-            Player target = players.iterator().next();
+            Entity target = entities.iterator().next();
             source.sendSuccess(new TranslatableComponent("commands.cold_sweat.temperature.set.single.result", target.getName().getString(), temp), true);
         }
         else
         {
-            source.sendSuccess(new TranslatableComponent("commands.cold_sweat.temperature.set.many.result", players.size(), temp), true);
+            source.sendSuccess(new TranslatableComponent("commands.cold_sweat.temperature.set.many.result", entities.size(), temp), true);
         }
         return Command.SINGLE_SUCCESS;
     }
 
-    private int executeGetPlayerTemp(CommandSourceStack source, Collection<ServerPlayer> players)
+    private int executeGetPlayerTemp(CommandSourceStack source, Collection<? extends Entity> entities)
     {
-        for (ServerPlayer target : players.stream().sorted(Comparator.comparing(player -> player.getName().getString())).toList())
+        if (entities.stream().anyMatch(entity -> !(entity instanceof LivingEntity)))
+        {
+            source.sendFailure(new TranslatableComponent("commands.cold_sweat.temperature.invalid"));
+            return Command.SINGLE_SUCCESS;
+        }
+        for (Entity target : entities.stream().sorted(Comparator.comparing(player -> player.getName().getString())).toList())
         {
             //Compose & send message
-            int bodyTemp = (int) Temperature.get(target, Temperature.Type.BODY);
+            int bodyTemp = (int) Temperature.get((LivingEntity) target, Temperature.Type.BODY);
             source.sendSuccess(new TranslatableComponent("commands.cold_sweat.temperature.get.result", target.getName().getString(), bodyTemp), false);
         }
         return Command.SINGLE_SUCCESS;
