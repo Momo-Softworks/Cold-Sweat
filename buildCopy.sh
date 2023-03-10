03ee940da4651e61b8e7015c7b1a7bc72a1e0018
mikulgamour <gamer3mc@gmail.com>
2023-03-10 16:09:12 -0500

    Update build 2.2-b03b

--- buildCopy.sh
+++ buildCopy.sh
@@ -2,4 +2,4 @@ sed -i "s/REMAP_MIXINS = false/REMAP_MIXINS = true/g" C:/Projects/IdeaProjects/C
 ./gradlew build
 sed -i "s/REMAP_MIXINS = true/REMAP_MIXINS = false/g" C:/Projects/IdeaProjects/Cold-Sweat/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
 cd C:/Projects/IdeaProjects/Cold-Sweat/build/libs
-mv ColdSweat-2.2-b01a.jar "C:/Games/CurseForge/Instances/CS Test/mods"
+mv ColdSweat-2.2-b03b.jar "C:/Games/CurseForge/Instances/CS Test/mods"
