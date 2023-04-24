27be95e6e13f6cab9240923f3ef59822543750da
Mikul <gamer3mc@gmail.com>
2023-04-24 16:25:06 -0400

    Remove build scripts

--- build.sh
+++
@@ -1,3 +0,0 @@
-sed -i "s/REMAP_MIXINS = false/REMAP_MIXINS = true/g" C:/Projects/IdeaProjects/Cold-Sweat/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
-./gradlew build
-sed -i "s/REMAP_MIXINS = true/REMAP_MIXINS = false/g" C:/Projects/IdeaProjects/Cold-Sweat/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
--- buildCopy.sh
+++
@@ -1,5 +0,0 @@
-sed -i "s/REMAP_MIXINS = false/REMAP_MIXINS = true/g" C:/Projects/IdeaProjects/Cold-Sweat/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
-./gradlew build
-sed -i "s/REMAP_MIXINS = true/REMAP_MIXINS = false/g" C:/Projects/IdeaProjects/Cold-Sweat/src/main/java/dev/momostudios/coldsweat/ColdSweat.java
-cd C:/Projects/IdeaProjects/Cold-Sweat/build/libs
-mv ColdSweat-2.2-b04a.jar "C:/Games/CurseForge/Instances/CS Test/mods"
