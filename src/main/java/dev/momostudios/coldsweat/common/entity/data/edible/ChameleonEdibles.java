b86627f4ef446b38180eaeea833170c301eef52a
Mikul <gamer3mc@gmail.com>
2023-06-12 01:44:47 -0400

    Misc

--- src/main/java/dev/momostudios/coldsweat/common/entity/data/edible/ChameleonEdibles.java
+++ src/main/java/dev/momostudios/coldsweat/common/entity/data/edible/ChameleonEdibles.java
@@ -1,8 +1,6 @@
 package dev.momostudios.coldsweat.common.entity.data.edible;
 
-import dev.momostudios.coldsweat.ColdSweat;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 
 import java.util.*;
 
@@ -11,8 +9,7 @@ public class ChameleonEdibles
     public static final List<Edible> EDIBLES = new ArrayList<>();
 
     public static Optional<Edible> getEdible(Item item)
-    {
-        return EDIBLES.stream().filter(edible -> item.builtInRegistryHolder().is(edible.associatedItems())).findFirst();
+    {   return EDIBLES.stream().filter(edible -> item.builtInRegistryHolder().is(edible.associatedItems())).findFirst();
     }
 
     public static void addEdible(Edible edible)
