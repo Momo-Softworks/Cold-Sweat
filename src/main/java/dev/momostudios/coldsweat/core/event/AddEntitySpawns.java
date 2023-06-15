416ab4f3f6249cba72b09366e09f33ac635f2f15
Mikul <gamer3mc@gmail.com>
2023-06-14 23:26:04 -0400

    Add check for null biomes
    Took 2 hours 29 minutes

--- src/main/java/dev/momostudios/coldsweat/core/event/AddEntitySpawns.java
+++ src/main/java/dev/momostudios/coldsweat/core/event/AddEntitySpawns.java
@@ -1,6 +1,5 @@
 package dev.momostudios.coldsweat.core.event;
 
-import dev.momostudios.coldsweat.config.EntitySettingsConfig;
 import dev.momostudios.coldsweat.util.config.ConfigSettings;
 import dev.momostudios.coldsweat.util.registries.ModEntities;
 import net.minecraft.world.entity.EntityType;
@@ -16,19 +15,22 @@ public class AddEntitySpawns
     @SubscribeEvent
     public static void onBiomeLoading(BiomeLoadingEvent event)
     {
-        // Add chameleon spawns
-        Integer chameleonWeight = ConfigSettings.CHAMELEON_BIOMES.get().get(event.getName().toString());
-        if (chameleonWeight != null)
-        {   event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(ModEntities.CHAMELEON, chameleonWeight, 1, 1));
-        }
+        if (event.getName() != null)
+        {
+            // Add chameleon spawns
+            Integer chameleonWeight = ConfigSettings.CHAMELEON_BIOMES.get().get(event.getName());
+            if (chameleonWeight != null)
+            {   event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(ModEntities.CHAMELEON, chameleonWeight, 1, 1));
+            }
 
-        // Add goat spawns
-        Integer goatWeight = ConfigSettings.GOAT_BIOMES.get().get(event.getName().toString());
-        if (goatWeight != null)
-        {   // Remove vanilla goat spawn settings
-            event.getSpawns().getSpawner(MobCategory.CREATURE).removeIf(spawnerData -> spawnerData.type == EntityType.GOAT);
-            // Add new goat spawn settings
-            event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(EntityType.GOAT, goatWeight, 2, 3));
+            // Add goat spawns
+            Integer goatWeight = ConfigSettings.GOAT_BIOMES.get().get(event.getName());
+            if (goatWeight != null)
+            {   // Remove vanilla goat spawn settings
+                event.getSpawns().getSpawner(MobCategory.CREATURE).removeIf(spawnerData -> spawnerData.type == EntityType.GOAT);
+                // Add new goat spawn settings
+                event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(EntityType.GOAT, goatWeight, 2, 3));
+            }
         }
     }
 }
