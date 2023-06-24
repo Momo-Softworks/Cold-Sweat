23c60dc9bfdcf53f4b35e7516e3a196a5fb493ab
Mikul <gamer3mc@gmail.com>
2023-06-23 21:44:45 -0400

    Add goat drops & refine loot table system

--- src/main/java/dev/momostudios/coldsweat/core/event/LootTableModifiers.java
+++ src/main/java/dev/momostudios/coldsweat/core/event/LootTableModifiers.java
@@ -1,8 +1,9 @@
 package dev.momostudios.coldsweat.core.event;
 
 import dev.momostudios.coldsweat.ColdSweat;
-import dev.momostudios.coldsweat.common.loot.HoglinDrops;
+import dev.momostudios.coldsweat.common.loot.AddDropsModifier;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.loot.LootModifier;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,7 +16,7 @@ public class LootTableModifiers
     public static void registerModifierSerializers(RegisterEvent event)
     {
         event.register(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, helper -> {
-            helper.register(new ResourceLocation(ColdSweat.MOD_ID, "hide_from_hoglin"), HoglinDrops.CODEC);
+            helper.register(new ResourceLocation(ColdSweat.MOD_ID, "mob_drops"), AddDropsModifier.CODEC);
         });
     }
 }
