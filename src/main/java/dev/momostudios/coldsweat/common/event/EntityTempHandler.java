35464b1232e49f98aeb30acdeddca7d40a9bf823
mikuldev <gamer3mc@gmail.com>
2023-07-05 10:39:03 -0400

    Optimize insulation effect
    Took 1 minute

--- src/main/java/dev/momostudios/coldsweat/common/event/EntityTempHandler.java
+++ src/main/java/dev/momostudios/coldsweat/common/event/EntityTempHandler.java
@@ -339,14 +339,7 @@ public class EntityTempHandler
             {   MobEffectInstance effect = event.getPotionEffect();
                 // New HearthTempModifier
                 TempModifier newMod = new HearthTempModifier(effect.getAmplifier() + 1).expires(effect.getDuration());
-                // Current HearthTempModifier
-                Optional<HearthTempModifier> currentMod = Temperature.getModifier(player, Temperature.Type.WORLD, HearthTempModifier.class);
-                // If the player already has the modifier, extend its duration
-                if (currentMod.isPresent() && currentMod.get().getNBT().getInt("Strength") == newMod.getNBT().getInt("Strength"))
-                {   currentMod.get().setTicksExisted(0);
-                    currentMod.get().expires(effect.getDuration());
-                }
-                else Temperature.addOrReplaceModifier(player, newMod, Temperature.Type.WORLD);
+                Temperature.addOrReplaceModifier(player, newMod, Temperature.Type.WORLD);
             }
             // Remove TempModifier on potion effect removed
             else if (event instanceof PotionEvent.PotionRemoveEvent)
