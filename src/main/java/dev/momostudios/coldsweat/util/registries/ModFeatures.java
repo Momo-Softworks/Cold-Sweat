832ca8a1136ae95700ed58773057a0fc380a8499
mikuldev <gamer3mc@gmail.com>
2023-07-23 21:10:50 -0400

    Fix soul stalk rarity (port from 1.19)
    Took 47 seconds

--- src/main/java/dev/momostudios/coldsweat/util/registries/ModFeatures.java
+++ src/main/java/dev/momostudios/coldsweat/util/registries/ModFeatures.java
@@ -13,8 +13,8 @@ import net.minecraft.world.level.levelgen.placement.*;
 public class ModFeatures
 {
     public static final Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> SOUL_STALK = FeatureUtils.register("soul_stalk", Feature.RANDOM_PATCH,
-            new RandomPatchConfiguration(32, 10, 5, PlacementUtils.onlyWhenEmpty(FeatureInit.SOUL_STALK_FEATURE.get(), NoneFeatureConfiguration.INSTANCE)));
+            new RandomPatchConfiguration(8, 10, 5, PlacementUtils.onlyWhenEmpty(FeatureInit.SOUL_STALK_FEATURE.get(), NoneFeatureConfiguration.INSTANCE)));
 
     public static final Holder<PlacedFeature> SOUL_STALK_PLACEMENT = PlacementUtils.register("soul_stalk_placement", SOUL_STALK,
-            RarityFilter.onAverageOnceEvery(3), InSquarePlacement.spread(), PlacementUtils.RANGE_10_10, BiomeFilter.biome(), CountPlacement.of(2));
+            RarityFilter.onAverageOnceEvery(5), InSquarePlacement.spread(), PlacementUtils.RANGE_10_10, BiomeFilter.biome(), CountPlacement.of(2));
 }
