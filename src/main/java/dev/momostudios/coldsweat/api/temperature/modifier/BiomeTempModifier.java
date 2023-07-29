334ec685fc793218371dbe80f9a9ab169094bb60
mikuldev <gamer3mc@gmail.com>
2023-07-29 12:47:04 -0400

    Optimize BiomeTempModifier & UndergroundTempModifier & expand vertical check range

--- src/main/java/dev/momostudios/coldsweat/api/temperature/modifier/BiomeTempModifier.java
+++ src/main/java/dev/momostudios/coldsweat/api/temperature/modifier/BiomeTempModifier.java
@@ -68,9 +68,12 @@ public class BiomeTempModifier extends TempModifier
 
                 for (BlockPos blockPos : WorldHelper.getPositionGrid(entity.blockPosition(), samples, 16))
                 {
-                    if (!level.isLoaded(blockPos)) continue;
-                    List<Holder<Biome>> biomeList = Stream.of(level.getBiomeManager().getBiome(blockPos),
+                    List<Holder<Biome>> biomeList = WorldHelper.getHeight(blockPos, level) < entity.getY()
+                                                    ? List.of(level.getBiomeManager().getBiome(blockPos))
+                                                    : Stream.of(level.getBiomeManager().getBiome(blockPos),
+                                                              level.getBiomeManager().getBiome(blockPos.above(8)),
                                                               level.getBiomeManager().getBiome(blockPos.above(16)),
+                                                              level.getBiomeManager().getBiome(blockPos.below(8)),
                                                               level.getBiomeManager().getBiome(blockPos.below(16))).distinct().toList();
                     for (Holder<Biome> holder : biomeList)
                     {
