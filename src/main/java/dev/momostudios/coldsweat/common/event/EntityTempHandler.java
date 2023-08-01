0bf296e8d6392d1e84e0b7c80ace63c5d5b9a3ab
mikuldev <gamer3mc@gmail.com>
2023-08-01 12:52:21 -0400

    Better way of getting if player frozen
    Took 4 seconds

--- src/main/java/dev/momostudios/coldsweat/common/event/EntityTempHandler.java
+++ src/main/java/dev/momostudios/coldsweat/common/event/EntityTempHandler.java
@@ -292,14 +292,14 @@ public class EntityTempHandler
                 if (WorldHelper.isInWater(player) || (player.tickCount % 40 == 0 && WorldHelper.isRainingAt(player.level, player.blockPosition())))
                     Temperature.addModifier(player, new WaterTempModifier(0.01f).tickRate(5), Temperature.Type.WORLD, false);
 
-                if (player.getTicksFrozen() > 0)
+                if (player.isFreezing())
                     Temperature.addOrReplaceModifier(player, new FreezingTempModifier(player.getTicksFrozen() / 13.5f).expires(5), Temperature.Type.BASE);
 
                 if (player.isOnFire())
                     Temperature.addOrReplaceModifier(player, new FireTempModifier().expires(5), Temperature.Type.BASE);
             }
 
-            if (player.getTicksFrozen() > 0)
+            if (player.isFreezing() && player.getTicksFrozen() > 0)
             {
                 AtomicReference<Double> insulation = new AtomicReference<>((double) 0);
                 boolean hasIcePotion = player.hasEffect(ModEffects.ICE_RESISTANCE) && ConfigSettings.ICE_RESISTANCE_ENABLED.get();
