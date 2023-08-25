2e822c98af81a4c3c47589343e1233c766cc4d9a
MikulDev <gamer3mc@gmail.com>
2023-08-25 15:06:57 -0400

    Make Microsoft authentication hack always enabled.
    Took 40 minutes

--- src/main/java/dev/momostudios/coldsweat/mixin/MixinMultiplayerHack.java
+++ src/main/java/dev/momostudios/coldsweat/mixin/MixinMultiplayerHack.java
@@ -1,6 +1,5 @@
 package dev.momostudios.coldsweat.mixin;
 
-import dev.momostudios.coldsweat.ColdSweat;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,11 +11,11 @@ public class MixinMultiplayerHack
 {
     @Inject(method = "allowsChat()Z", at = @At("HEAD"), cancellable = true)
     private void allowsChat(CallbackInfoReturnable<Boolean> cir)
-    {   cir.setReturnValue(cir.getReturnValueZ() || !ColdSweat.REMAP_MIXINS);
+    {   cir.setReturnValue(true);
     }
 
     @Inject(method = "allowsMultiplayer()Z", at = @At("HEAD"), cancellable = true)
     private void allowsMultiplayer(CallbackInfoReturnable<Boolean> cir)
-    {   cir.setReturnValue(cir.getReturnValueZ() || !ColdSweat.REMAP_MIXINS);
+    {   cir.setReturnValue(true);
     }
 }
