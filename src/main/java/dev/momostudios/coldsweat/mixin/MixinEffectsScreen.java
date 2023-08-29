c09ce7c2c1343e055291e23c7f8cbd0f1809d81f
MikulDev <gamer3mc@gmail.com>
2023-08-29 15:50:58 -0400

    Fix invasive MixinEffectsScreen

--- src/main/java/dev/momostudios/coldsweat/mixin/MixinEffectsScreen.java
+++ src/main/java/dev/momostudios/coldsweat/mixin/MixinEffectsScreen.java
@@ -1,28 +1,28 @@
 package dev.momostudios.coldsweat.mixin;
 
 import dev.momostudios.coldsweat.common.container.HearthContainer;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.Collection;
 
 @Mixin(EffectRenderingInventoryScreen.class)
 public class MixinEffectsScreen
 {
-    AbstractContainerScreen screen = (AbstractContainerScreen) (Object) this;
+    AbstractContainerScreen<?> screen = (AbstractContainerScreen<?>) (Object) this;
 
-    @Redirect(method = "renderEffects(Lcom/mojang/blaze3d/vertex/PoseStack;II)V",
-              at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;getActiveEffects()Ljava/util/Collection;"))
-    private Collection<MobEffectInstance> getEffects(LocalPlayer instance)
+    @ModifyVariable(method = "renderEffects",
+              at = @At(value = "STORE"), ordinal = 0)
+    private Collection<MobEffectInstance> getEffects(Collection<MobEffectInstance> collection)
     {
         if (screen.getMenu() instanceof HearthContainer container)
         {   return container.te.getEffects();
         }
-        else return instance.getActiveEffects();
+        else return Minecraft.getInstance().player.getActiveEffects();
     }
 }
