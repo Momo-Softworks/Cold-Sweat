f554e42fbbf44331a767ebc8bbc641e6d7bd0b66
MikulDev <gamer3mc@gmail.com>
2023-09-06 21:06:01 -0400

    Rename "goat fur" to "fur" for parity

--- src/main/java/dev/momostudios/coldsweat/core/init/ItemInit.java
+++ src/main/java/dev/momostudios/coldsweat/core/init/ItemInit.java
@@ -5,14 +5,9 @@ import dev.momostudios.coldsweat.common.block.*;
 import dev.momostudios.coldsweat.common.item.*;
 import dev.momostudios.coldsweat.core.itemgroup.ColdSweatGroup;
 import dev.momostudios.coldsweat.util.registries.ModArmorMaterials;
-import net.minecraft.advancements.Advancement;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.*;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -29,7 +24,7 @@ public class ItemInit
     public static final RegistryObject<Item> THERMOMETER = ITEMS.register("thermometer", () ->
             new Item(new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT).rarity(Rarity.UNCOMMON).stacksTo(1)));
     public static final RegistryObject<Item> SOULSPRING_LAMP = ITEMS.register("soulspring_lamp", SoulspringLampItem::new);
-    public static final RegistryObject<Item> GOAT_FUR = ITEMS.register("goat_fur", () ->
+    public static final RegistryObject<Item> FUR = ITEMS.register("fur", () ->
             new Item(new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
     public static final RegistryObject<Item> HOGLIN_HIDE = ITEMS.register("hoglin_hide", () ->
             new Item(new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
@@ -51,14 +46,14 @@ public class ItemInit
     public static final RegistryObject<Item> HOGLIN_HOOVES = ITEMS.register("hoglin_hooves", () ->
             new HoglinArmorItem(ModArmorMaterials.HOGLIN, EquipmentSlot.FEET, new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
 
-    public static final RegistryObject<Item> GOAT_FUR_CAP = ITEMS.register("goat_fur_cap", () ->
-            new GoatArmorItem(ModArmorMaterials.GOAT, EquipmentSlot.HEAD, new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
-    public static final RegistryObject<Item> GOAT_FUR_PARKA = ITEMS.register("goat_fur_parka", () ->
-            new GoatArmorItem(ModArmorMaterials.GOAT, EquipmentSlot.CHEST, new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
-    public static final RegistryObject<Item> GOAT_FUR_PANTS = ITEMS.register("goat_fur_pants", () ->
-            new GoatArmorItem(ModArmorMaterials.GOAT, EquipmentSlot.LEGS, new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
-    public static final RegistryObject<Item> GOAT_FUR_BOOTS = ITEMS.register("goat_fur_boots", () ->
-            new GoatArmorItem(ModArmorMaterials.GOAT, EquipmentSlot.FEET, new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
+    public static final RegistryObject<Item> FUR_CAP = ITEMS.register("fur_cap", () ->
+            new GoatArmorItem(ModArmorMaterials.FUR, EquipmentSlot.HEAD, new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
+    public static final RegistryObject<Item> FUR_PARKA = ITEMS.register("fur_parka", () ->
+            new GoatArmorItem(ModArmorMaterials.FUR, EquipmentSlot.CHEST, new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
+    public static final RegistryObject<Item> FUR_PANTS = ITEMS.register("fur_pants", () ->
+            new GoatArmorItem(ModArmorMaterials.FUR, EquipmentSlot.LEGS, new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
+    public static final RegistryObject<Item> FUR_BOOTS = ITEMS.register("fur_boots", () ->
+            new GoatArmorItem(ModArmorMaterials.FUR, EquipmentSlot.FEET, new Item.Properties().tab(ColdSweatGroup.COLD_SWEAT)));
 
     // Block Items
     public static final RegistryObject<BlockItem> BOILER = ITEMS.register("boiler", () -> new BlockItem(BlockInit.BOILER.get(), BoilerBlock.getItemProperties()));
