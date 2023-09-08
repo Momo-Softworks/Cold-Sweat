776405f646e8f6f4c5cb86f40a6a3fd5efd876f7
MikulDev <gamer3mc@gmail.com>
2023-09-08 13:26:00 -0400

    Merge AttachGoatCaps with GoatFurHandler

--- src/main/java/dev/momostudios/coldsweat/common/event/GoatFurHandler.java
+++ src/main/java/dev/momostudios/coldsweat/common/event/GoatFurHandler.java
@@ -1,5 +1,8 @@
 package dev.momostudios.coldsweat.common.event;
 
+import dev.momostudios.coldsweat.ColdSweat;
+import dev.momostudios.coldsweat.common.capability.GoatFurCap;
+import dev.momostudios.coldsweat.common.capability.IShearableCap;
 import dev.momostudios.coldsweat.common.capability.ModCapabilities;
 import dev.momostudios.coldsweat.core.event.TaskScheduler;
 import dev.momostudios.coldsweat.core.network.ColdSweatPacketHandler;
@@ -7,8 +10,11 @@ import dev.momostudios.coldsweat.core.network.message.SyncShearableDataMessage;
 import dev.momostudios.coldsweat.config.ConfigSettings;
 import dev.momostudios.coldsweat.util.registries.ModItems;
 import dev.momostudios.coldsweat.util.world.WorldHelper;
+import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.game.ClientboundEntityEventPacket;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
@@ -23,7 +29,11 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.schedule.Activity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -32,9 +42,54 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.PacketDistributor;
 import oshi.util.tuples.Triplet;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 @Mod.EventBusSubscriber
 public class GoatFurHandler
 {
+    @SubscribeEvent
+    public static void attachCapabilityToEntityHandler(AttachCapabilitiesEvent<Entity> event)
+    {
+        if (event.getObject() instanceof Goat)
+        {
+            // Make a new capability instance to attach to the entity
+            IShearableCap cap = new GoatFurCap();
+            // Optional that holds the capability instance
+            LazyOptional<IShearableCap> capOptional = LazyOptional.of(() -> cap);
+            Capability<IShearableCap> capability = ModCapabilities.SHEARABLE_FUR;
+
+            ICapabilityProvider provider = new ICapabilitySerializable<CompoundTag>()
+            {
+                @Nonnull
+                @Override
+                public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction direction)
+                {
+                    // If the requested cap is the temperature cap, return the temperature cap
+                    if (cap == capability)
+                    {
+                        return capOptional.cast();
+                    }
+                    return LazyOptional.empty();
+                }
+
+                @Override
+                public CompoundTag serializeNBT()
+                {
+                    return cap.serializeNBT();
+                }
+
+                @Override
+                public void deserializeNBT(CompoundTag nbt)
+                {
+                    cap.deserializeNBT(nbt);
+                }
+            };
+
+            event.addCapability(new ResourceLocation(ColdSweat.MOD_ID, "fur"), provider);
+        }
+    }
+
     @SubscribeEvent
     public static void onShearGoat(PlayerInteractEvent.EntityInteract event)
     {
@@ -112,7 +167,7 @@ public class GoatFurHandler
         Entity entity = event.getEntity();
         if (!(entity instanceof Goat goat)) return;
 
-        Triplet<Integer, Integer, Double> furConfig = ConfigSettings.GOAT_FUR_TIMINGS.get();
+        Triplet<Integer, Integer, Double> furConfig = ConfigSettings.FUR_TIMINGS.get();
         // Entity is goat, current tick is a multiple of the regrow time, and random chance succeeds
         if (!goat.level.isClientSide && goat.tickCount % furConfig.getA() == 0 && Math.random() < furConfig.getC())
         {
