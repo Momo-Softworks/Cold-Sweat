16bd8113f71ffefa5f53cf7a8bc862724adcff8c
MikulDev <gamer3mc@gmail.com>
2023-10-06 15:30:51 -0400

    Better raycasting method for picking up water with waterskin

--- src/main/java/com/momosoftworks/coldsweat/common/item/WaterskinItem.java
+++ src/main/java/com/momosoftworks/coldsweat/common/item/WaterskinItem.java
@@ -5,16 +5,16 @@ import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.util.math.CSMath;
 import com.momosoftworks.coldsweat.util.registries.ModItems;
 import com.momosoftworks.coldsweat.util.world.BlockPos;
-import com.momosoftworks.coldsweat.util.world.BlockState;
 import com.momosoftworks.coldsweat.util.world.ItemHelper;
-import com.momosoftworks.coldsweat.util.world.WorldHelper;
-import net.minecraft.block.material.Material;
+import com.momosoftworks.coldsweat.util.world.TaskScheduler;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 public class WaterskinItem extends Item
@@ -25,43 +25,38 @@ public class WaterskinItem extends Item
     @Override
     public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
     {
+        if (player.worldObj.isRemote) return stack;
         ItemStack result = stack;
-        MovingObjectPosition ar = this.getMovingObjectPositionFromPlayer(world, player, false);
-        if (ar == null || ar.typeOfHit != MovingObjectPosition.MovingObjectType.BLOCK)
-        {   return stack;
-        }
+        double reach = ((EntityPlayerMP) player).theItemInWorldManager.getBlockReachDistance();
+        MovingObjectPosition ar = world.rayTraceBlocks(Vec3.createVectorHelper(player.posX, player.posY + player.eyeHeight, player.posZ),
+                                                       Vec3.createVectorHelper(player.posX + player.getLookVec().xCoord * reach,
+                                                                               player.posY + player.getLookVec().yCoord * reach,
+                                                                               player.posZ + player.getLookVec().zCoord * reach));
+        if (ar == null || ar.typeOfHit != MovingObjectPosition.MovingObjectType.BLOCK) return stack;
+        BlockPos clickedPos = new BlockPos(ar.blockX, ar.blockY, ar.blockZ);
+        ItemStack filledWaterskin = new ItemStack(ModItems.FILLED_WATERSKIN);
+        filledWaterskin.setTagCompound(ItemHelper.getOrCrateTag(stack));
+        filledWaterskin.getTagCompound().setDouble("temperature", CSMath.clamp((Temperature.getTemperatureAt(clickedPos, world)
+                                                                - (CSMath.average(ConfigSettings.MAX_TEMP.get(), ConfigSettings.MIN_TEMP.get()))) * 15, -50, 50));
 
-        BlockPos hitPos = new BlockPos(ar.blockX, ar.blockY, ar.blockZ).offset(ar.sideHit);
-        BlockState lookingAt = WorldHelper.getBlockState(world, hitPos);
-        if (lookingAt.getBlock().getMaterial() == Material.water)
+        //Replace 1 of the stack with a FilledWaterskinItem
+        if (stack.stackSize > 1)
         {
-            ItemStack filledWaterskin = new ItemStack(ModItems.FILLED_WATERSKIN);
-            filledWaterskin.setTagCompound(ItemHelper.getOrCrateTag(stack));
-            filledWaterskin.getTagCompound().setDouble("temperature",
-                                                                  CSMath.clamp((Temperature.getTemperatureAt(hitPos, world)
-                                                                    - (CSMath.average(ConfigSettings.MAX_TEMP.get(), ConfigSettings.MIN_TEMP.get()))) * 15, -50, 50));
-
-            //Replace 1 of the stack with a FilledWaterskinItem
-            if (stack.stackSize > 1)
+            if (!player.inventory.addItemStackToInventory(filledWaterskin))
             {
-                if (!player.inventory.addItemStackToInventory(filledWaterskin))
-                {
-                    EntityItem itementity = player.entityDropItem(filledWaterskin, player.getEyeHeight());
-                    if (itementity != null)
-                    {   itementity.delayBeforeCanPickup = 0;
-                    }
+                EntityItem itementity = player.entityDropItem(filledWaterskin, player.getEyeHeight());
+                if (itementity != null)
+                {   itementity.delayBeforeCanPickup = 0;
                 }
-                stack.stackSize--;
             }
-            else
-            {
-                player.setCurrentItemOrArmor(0, result = filledWaterskin);
-            }
-            //Play filling sound
-            world.playSoundEffect(hitPos.getX(), hitPos.getY(), hitPos.getZ(), Blocks.water.stepSound.getStepResourcePath(), 1, (float) Math.random() / 5 + 0.9f);
-            player.swingItem();
-            return result;
+            stack.stackSize--;
         }
-        return stack;
+        else
+        {   player.setCurrentItemOrArmor(0, result = filledWaterskin);
+        }
+        //Play filling sound
+        world.playSoundEffect(clickedPos.getX(), clickedPos.getY(), clickedPos.getZ(), Blocks.water.stepSound.getStepResourcePath(), 1, (float) Math.random() / 5 + 0.9f);
+        TaskScheduler.scheduleClient(() -> player.swingItem(), 0);
+        return result;
     }
 }
