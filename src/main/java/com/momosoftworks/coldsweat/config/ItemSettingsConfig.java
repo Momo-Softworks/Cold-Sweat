507634e278d11d3ec53729d413bc58f45b11dc89
MikulDev <gamer3mc@gmail.com>
2023-10-13 12:02:49 -0400

    Pretty-ify configs & update setting descriptions
    Took 2 minutes

--- src/main/java/com/momosoftworks/coldsweat/config/ItemSettingsConfig.java
+++ src/main/java/com/momosoftworks/coldsweat/config/ItemSettingsConfig.java
@@ -130,7 +130,7 @@ public final class ItemSettingsConfig
         soulLampItems = BUILDER
                 .comment("Defines items that the Soulspring Lamp can use as fuel",
                         "Format: [[\"item-id-1\", amount-1], [\"item-id-2\", amount-2], ...etc]")
-                .defineListAllowEmpty(Collections.singletonList("Fuel Items"), () -> ListBuilder.begin(
+                .defineListAllowEmpty(Arrays.asList("Fuel Items"), () -> ListBuilder.begin(
                                     Arrays.asList("cold_sweat:soul_sprout", 4)
                         ).build(),
                         it ->
