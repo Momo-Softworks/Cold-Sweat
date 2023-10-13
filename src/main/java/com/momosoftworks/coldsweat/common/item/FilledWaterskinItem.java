c6eb5cf1f7b48d56bd4ad043ec368e4070b10c9d
MikulDev <gamer3mc@gmail.com>
2023-10-13 12:03:39 -0400

    Better balanced waterskin hotbar effect
    Took 26 seconds

--- src/main/java/com/momosoftworks/coldsweat/common/item/FilledWaterskinItem.java
+++ src/main/java/com/momosoftworks/coldsweat/common/item/FilledWaterskinItem.java
@@ -46,6 +46,8 @@ import java.util.Random;
 
 public class FilledWaterskinItem extends Item
 {
+    private static final double EFFECT_RATE = 0.4;
+
     public FilledWaterskinItem()
     {
         super(new Properties().tab(ColdSweatGroup.COLD_SWEAT).stacksTo(1).craftRemainder(ItemInit.WATERSKIN.get()));
@@ -81,19 +83,17 @@ public class FilledWaterskinItem extends Item
                 }, i);
             }
 
-            final AABB[] aabb = { new AABB(pos).inflate(0.5) };
-
             // Spawn a hitbox that falls at the same rate as the particles and gives players below the waterskin effect
             new Object()
             {
                 double acceleration = 0;
                 int tick = 0;
+                AABB aabb = new AABB(pos).inflate(0.5);
                 // Track affected players to prevent duplicate effects
                 List<Player> affectedPlayers = new ArrayList<>();
 
                 void start()
-                {
-                    MinecraftForge.EVENT_BUS.register(this);
+                {   MinecraftForge.EVENT_BUS.register(this);
                 }
 
                 @SubscribeEvent
@@ -105,23 +105,20 @@ public class FilledWaterskinItem extends Item
                         double waterTemp = CSMath.blend(itemTemp, itemTemp / 5, tick, 20, 100);
 
                         // Move the box down at the speed of gravity
-                        AABB movedBox;
-                        aabb[0] = movedBox = aabb[0].move(0, -acceleration, 0);
+                        aabb = aabb.move(0, -acceleration, 0);
 
                         // If there's ground, stop
-                        BlockPos pos = new BlockPos(movedBox.minX, movedBox.minY, movedBox.minZ);
+                        BlockPos pos = new BlockPos(aabb.minX, aabb.minY, aabb.minZ);
                         if (WorldHelper.isSpreadBlocked(level, chunk.getBlockState(pos), pos, Direction.DOWN, Direction.DOWN))
-                        {
-                            MinecraftForge.EVENT_BUS.unregister(this);
+                        {   MinecraftForge.EVENT_BUS.unregister(this);
                             return;
                         }
 
                         // Apply the waterskin modifier to all entities in the box
-                        level.getEntitiesOfClass(Player.class, movedBox).forEach(player ->
+                        level.getEntitiesOfClass(Player.class, aabb).forEach(player ->
                         {
                             if (!affectedPlayers.contains(player))
-                            {
-                                // Apply the effect and store the player
+                            {   // Apply the effect and store the player
                                 Temperature.addModifier(player, new WaterskinTempModifier(waterTemp).expires(0), Temperature.Type.CORE, true);
                                 affectedPlayers.add(player);
                             }
@@ -153,13 +150,12 @@ public class FilledWaterskinItem extends Item
             double itemTemp = itemstack.getOrCreateTag().getDouble("temperature");
             if (itemTemp != 0 && slot <= 8 || player.getOffhandItem().equals(itemstack))
             {
-                double temp = 0.04 * ConfigSettings.TEMP_RATE.get() * CSMath.getSign(itemTemp);
-                double newTemp = itemTemp - temp * 2;
-                if (CSMath.withinRange(newTemp, -1, 1)) newTemp = 0;
+                double temp = (EFFECT_RATE / 20) * ConfigSettings.TEMP_RATE.get();
+                double newTemp = CSMath.shrink(itemTemp, temp * 5);
 
                 itemstack.getOrCreateTag().putDouble("temperature", newTemp);
 
-                Temperature.addModifier(player, new WaterskinTempModifier(temp).expires(5), Temperature.Type.CORE, true);
+                Temperature.addModifier(player, new WaterskinTempModifier(temp * CSMath.getSign(itemTemp)).expires(5), Temperature.Type.CORE, true);
             }
         }
     }
@@ -186,8 +182,7 @@ public class FilledWaterskinItem extends Item
             player.setItemInHand(hand, ItemStack.EMPTY);
         }
         else
-        {
-            player.setItemInHand(hand, emptyStack);
+        {   player.setItemInHand(hand, emptyStack);
         }
 
         player.swing(hand);
@@ -236,7 +231,9 @@ public class FilledWaterskinItem extends Item
         // Info tooltip for hotbar functionality
         tooltip.add(new TextComponent(""));
         tooltip.add(new TranslatableComponent("tooltip.cold_sweat.hotbar").withStyle(ChatFormatting.GRAY));
-        tooltip.add(new TranslatableComponent("tooltip.cold_sweat.temperature_effect", (CSMath.getSign(temp) >= 0 ? "+" : "-") + (temp != 0 ? 0.8 * ConfigSettings.TEMP_RATE.get() : 0))
+        tooltip.add(new TranslatableComponent("tooltip.cold_sweat.temperature_effect",
+                                                 (CSMath.getSign(temp) >= 0 ? "+" : "-")
+                                               + (temp != 0 ? EFFECT_RATE * ConfigSettings.TEMP_RATE.get() : 0))
                             .withStyle(temp > 0 ? TooltipHandler.HOT : temp < 0 ? TooltipHandler.COLD : ChatFormatting.WHITE));
 
         // Tooltip to display temperature
@@ -259,7 +256,6 @@ public class FilledWaterskinItem extends Item
     }
 
     public String getDescriptionId()
-    {
-        return new TranslatableComponent("item.cold_sweat.waterskin").getString();
+    {   return new TranslatableComponent("item.cold_sweat.waterskin").getString();
     }
 }
