ff27bdc3d00188643a3611459078da7bb627aa08
MikulDev <gamer3mc@gmail.com>
2023-11-20 21:53:43 -0500

    Merge SoulLampTooltip into TooltipHandler

--- src/main/java/com/momosoftworks/coldsweat/client/event/TooltipHandler.java
+++ src/main/java/com/momosoftworks/coldsweat/client/event/TooltipHandler.java
@@ -1,7 +1,10 @@
 package com.momosoftworks.coldsweat.client.event;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.datafixers.util.Either;
 import com.mojang.datafixers.util.Pair;
+import com.momosoftworks.coldsweat.client.gui.tooltip.ClientSoulspringTooltip;
 import com.momosoftworks.coldsweat.client.gui.tooltip.InsulationTooltip;
 import com.momosoftworks.coldsweat.client.gui.tooltip.InsulatorTooltip;
 import com.momosoftworks.coldsweat.client.gui.tooltip.SoulspringTooltip;
@@ -12,15 +15,20 @@ import com.momosoftworks.coldsweat.common.capability.ItemInsulationManager;
 import com.momosoftworks.coldsweat.common.item.SoulspringLampItem;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.util.math.CSMath;
+import com.momosoftworks.coldsweat.util.registries.ModItems;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Equipable;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderTooltipEvent;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -123,6 +131,59 @@ public class TooltipHandler
             if (insulation.size() > 0)
             {   event.getTooltipElements().add(1, Either.right(new InsulationTooltip(insulation, stack)));
             }
+        }
+    }
+
+    static int FUEL_FADE_TIMER = 0;
+
+    @SubscribeEvent
+    public static void renderSoulLampInsertTooltip(ScreenEvent.Render.Post event)
+    {
+        if (event.getScreen() instanceof AbstractContainerScreen<?> screen)
+        {
+            if (screen.getSlotUnderMouse() != null && screen.getSlotUnderMouse().getItem().getItem() == ModItems.SOULSPRING_LAMP)
+            {
+                float fuel = screen.getSlotUnderMouse().getItem().getOrCreateTag().getFloat("fuel");
+                ItemStack carriedStack = screen.getMenu().getCarried();
+
+                if (!carriedStack.isEmpty() && ConfigSettings.LAMP_FUEL_ITEMS.get().containsKey(carriedStack.getItem()))
+                {
+                    int fuelValue = screen.getMenu().getCarried().getCount() * ConfigSettings.LAMP_FUEL_ITEMS.get().get(carriedStack.getItem());
+                    int slotX = screen.getSlotUnderMouse().x + screen.getGuiLeft();
+                    int slotY = screen.getSlotUnderMouse().y + screen.getGuiTop();
+
+                    PoseStack ps = event.getPoseStack();
+                    if (event.getMouseY() < slotY + 8)
+                    {   ps.translate(0, 32, 0);
+                    }
+
+                    event.getScreen().renderComponentTooltip(event.getPoseStack(), List.of(Component.literal("       ")), slotX - 18, slotY + 1);
+
+                    RenderSystem.defaultBlendFunc();
+
+                    // Render background
+                    RenderSystem.setShaderTexture(0, ClientSoulspringTooltip.TOOLTIP_LOCATION.get());
+                    GuiComponent.blit(ps, slotX - 7, slotY - 11, 401, 0, 0, 30, 8, 30, 34);
+
+                    // Render ghost overlay
+                    RenderSystem.enableBlend();
+                    RenderSystem.setShaderColor(1f, 1f, 1f, 0.15f + (float) ((Math.sin(FUEL_FADE_TIMER / 5f) + 1f) / 2f) * 0.4f);
+                    GuiComponent.blit(ps, slotX - 7, slotY - 11, 401, 0, 8, Math.min(30, (int) ((fuel + fuelValue) / 2.1333f)), 8, 30, 34);
+                    RenderSystem.disableBlend();
+
+                    // Render fuel
+                    RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1f);
+                    GuiComponent.blit(ps, slotX - 7, slotY - 11, 401, 0, 16, (int) (fuel / 2.1333f), 8, 30, 34);
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void tickSoulLampInsertTooltip(TickEvent.ClientTickEvent event)
+    {
+        if (event.phase == TickEvent.Phase.END)
+        {   FUEL_FADE_TIMER++;
         }
     }
 }
