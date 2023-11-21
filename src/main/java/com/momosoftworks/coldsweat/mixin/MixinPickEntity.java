e4a74507d87e1ab44dc715594bba5862ae8ad360
MikulDev <gamer3mc@gmail.com>
2023-11-20 20:36:13 -0500

    Fix unmapped mixin causing crash

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinPickEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinPickEntity.java
@@ -1,11 +1,11 @@
 package com.momosoftworks.coldsweat.mixin;
 
+import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.api.event.common.EntityPickEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.HitResult;
-import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,12 +15,11 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class MixinPickEntity
 {
     @Redirect(method = "pickBlock",
-              at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getPickedResult(Lnet/minecraft/world/phys/HitResult;)Lnet/minecraft/world/item/ItemStack;"), remap = false)
+              at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getPickedResult(Lnet/minecraft/world/phys/HitResult;)Lnet/minecraft/world/item/ItemStack;"), remap = ColdSweat.REMAP_MIXINS)
     private ItemStack getPickResult(Entity entity, HitResult hitResult)
     {
         if (hitResult.getType() == HitResult.Type.ENTITY)
-        {
-            EntityPickEvent event = new EntityPickEvent(entity, entity.getPickedResult(hitResult));
+        {   EntityPickEvent event = new EntityPickEvent(entity, entity.getPickedResult(hitResult));
             MinecraftForge.EVENT_BUS.post(event);
             return event.getStack();
         }
