a24675632eecc6014330ba0dcbf34adcabf392bf
MikulDev <gamer3mc@gmail.com>
2024-01-07 10:32:11 -0500

    Fix ChatComponentClickedEvent causing server crash when subscribed to
    Took 39 minutes

--- src/main/java/com/momosoftworks/coldsweat/api/event/common/ChatComponentClickedEvent.java
+++ src/main/java/com/momosoftworks/coldsweat/api/event/common/ChatComponentClickedEvent.java
@@ -1,8 +1,8 @@
 package com.momosoftworks.coldsweat.api.event.common;
 
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Style;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.eventbus.api.Event;
 
 import javax.annotation.Nullable;
@@ -10,24 +10,18 @@ import javax.annotation.Nullable;
 public class ChatComponentClickedEvent extends Event
 {
     private Style style;
-    private LocalPlayer player;
-    private Screen screen;
+    private Player player;
 
-    public ChatComponentClickedEvent(@Nullable Style style, LocalPlayer player, Screen screen)
+    public ChatComponentClickedEvent(@Nullable Style style, Player player)
     {   this.style = style;
         this.player = player;
-        this.screen = screen;
     }
 
     public Style getStyle()
     {   return style;
     }
 
-    public LocalPlayer getPlayer()
+    public Player getPlayer()
     {   return player;
-    }
-
-    public Screen getScreen()
-    {   return screen;
     }
 }
