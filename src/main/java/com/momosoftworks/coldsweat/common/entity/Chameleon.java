5f40cd684b1b594e378190fd322a213a92b17469
MikulDev <gamer3mc@gmail.com>
2024-01-09 13:38:00 -0500

    Fix using deprecated methods in chameleon edibles

--- src/main/java/com/momosoftworks/coldsweat/common/entity/Chameleon.java
+++ src/main/java/com/momosoftworks/coldsweat/common/entity/Chameleon.java
@@ -428,7 +428,7 @@ public class Chameleon extends Animal
                         }
                     }
                 }
-                ChameleonEdibles.getEdible(itemEntity.getItem().getItem()).ifPresent(edible ->
+                ChameleonEdibles.getEdible(itemEntity.getItem()).ifPresent(edible ->
                 {
                     if (edible.onEaten(this, itemEntity) == Edible.Result.SUCCESS)
                     {   this.setCooldown(edible, edible.getCooldown());
