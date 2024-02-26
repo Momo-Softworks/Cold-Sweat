c387d0aeb4f21782378d0cee87336f8829b0db3c
MikulDev <gamer3mc@gmail.com>
2024-02-26 11:56:53 -0500

    Allow lists of entries in data configs

--- src/main/java/com/momosoftworks/coldsweat/data/configuration/BlockTempData.java
+++ src/main/java/com/momosoftworks/coldsweat/data/configuration/BlockTempData.java
@@ -1,8 +1,12 @@
 package com.momosoftworks.coldsweat.data.configuration;
 
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -10,10 +14,30 @@ import net.minecraftforge.registries.ForgeRegistries;
 import java.util.List;
 import java.util.Optional;
 
-public record BlockTempData(List<Block> blocks, double temperature, double maxEffect, boolean fade, List<BlockPredicate> conditions, Optional<CompoundTag> tag)
+public record BlockTempData(List<Either<TagKey<Block>, Block>> blocks, double temperature, double maxEffect, boolean fade, List<BlockPredicate> conditions, Optional<CompoundTag> tag)
 {
     public static final Codec<BlockTempData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            ForgeRegistries.BLOCKS.getCodec().listOf().fieldOf("blocks").forGetter(BlockTempData::blocks),
+            Codec.STRING.xmap(
+            // Convert from a string to a TagKey
+            string ->
+            {
+                ResourceLocation tagLocation = ResourceLocation.tryParse(string.replace("#", ""));
+                if (tagLocation == null) throw new IllegalArgumentException("Biome tag is null");
+                if (!string.contains("#")) return Either.<TagKey<Block>, Block>right(ForgeRegistries.BLOCKS.getValue(tagLocation));
+
+                return Either.<TagKey<Block>, Block>left(TagKey.create(Registry.BLOCK_REGISTRY, tagLocation));
+            },
+            // Convert from a TagKey to a string
+            tag ->
+            {   if (tag == null) throw new IllegalArgumentException("Biome tag is null");
+                String result = tag.left().isPresent()
+                                ? "#" + tag.left().get().location()
+                                : tag.right().map(block -> ForgeRegistries.BLOCKS.getKey(block).toString()).orElse("");
+                if (result.isEmpty()) throw new IllegalArgumentException("Biome field is not a tag or valid ID");
+                return result;
+            })
+            .listOf()
+            .fieldOf("blocks").forGetter(BlockTempData::blocks),
             Codec.DOUBLE.fieldOf("temperature").forGetter(BlockTempData::temperature),
             Codec.DOUBLE.optionalFieldOf("max_effect", Double.MAX_VALUE).forGetter(BlockTempData::maxEffect),
             Codec.BOOL.optionalFieldOf("fade", true).forGetter(BlockTempData::fade),
