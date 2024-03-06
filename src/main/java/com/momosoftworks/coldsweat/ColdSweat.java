a10f9de7e2f518767a12fdb916c96faed232478e
MikulDev <gamer3mc@gmail.com>
2024-03-06 16:48:46 -0500

    Add insulators tab
    Took 1 minute

--- src/main/java/com/momosoftworks/coldsweat/ColdSweat.java
+++ src/main/java/com/momosoftworks/coldsweat/ColdSweat.java
@@ -9,6 +9,7 @@ import com.momosoftworks.coldsweat.common.entity.Chameleon;
 import com.momosoftworks.coldsweat.config.*;
 import com.momosoftworks.coldsweat.core.advancement.trigger.ModAdvancementTriggers;
 import com.momosoftworks.coldsweat.core.init.*;
+import com.momosoftworks.coldsweat.core.itemgroup.InsulationItemsGroup;
 import com.momosoftworks.coldsweat.core.network.ColdSweatPacketHandler;
 import com.momosoftworks.coldsweat.util.compat.CompatManager;
 import net.minecraft.advancements.CriteriaTriggers;
@@ -83,9 +84,13 @@ public class ColdSweat
             CriteriaTriggers.register(ModAdvancementTriggers.SOUL_LAMP_FUELLED);
             CriteriaTriggers.register(ModAdvancementTriggers.BLOCK_AFFECTS_TEMP);
             CriteriaTriggers.register(ModAdvancementTriggers.ARMOR_INSULATED);
+
+            // Load configs to memory
+            ConfigSettings.load();
+
+            // Register insulation items tab
+            InsulationItemsGroup.INSULATION_ITEMS.register();
         });
-        // Load configs to memory
-        ConfigSettings.load();
     }
 
     public void clientSetup(final FMLClientSetupEvent event)
