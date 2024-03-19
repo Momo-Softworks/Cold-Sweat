b3ea98e85731d4989e64c6ba722f4fc902e15161
MikulDev <gamer3mc@gmail.com>
2024-03-19 17:30:26 -0400

    No netherite backtank in Create 1.16 (sad)
    Took 5 minutes

--- src/main/java/com/momosoftworks/coldsweat/util/compat/CompatManager.java
+++ src/main/java/com/momosoftworks/coldsweat/util/compat/CompatManager.java
@@ -7,12 +7,15 @@ import com.momosoftworks.coldsweat.api.temperature.modifier.compat.CuriosTempMod
 import com.momosoftworks.coldsweat.api.util.Temperature;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.config.util.ItemData;
+import com.simibubi.create.content.curiosities.armor.BackTankUtil;
+import com.simibubi.create.content.curiosities.armor.DivingHelmetItem;
 import de.teamlapen.werewolves.entities.player.werewolf.WerewolfPlayer;
 import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.util.math.CSMath;
 import com.momosoftworks.coldsweat.util.registries.ModDamageSources;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
