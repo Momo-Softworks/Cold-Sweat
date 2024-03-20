64b6695ef4b43d463b9447879d9a50f6fb297e2d
MikulDev <gamer3mc@gmail.com>
2024-03-20 00:08:45 -0400

    Make world temp frame larger
    Took 1 second
    
    Took 56 seconds

--- src/main/java/com/momosoftworks/coldsweat/client/gui/Overlays.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/Overlays.java
@@ -251,13 +251,11 @@ public class Overlays
                 // Set gauge texture
                 mc.textureManager.bind(VAGUE_TEMP_GAUGE_LOCATION.get());
 
-                // Render frame
-                AbstractGui.blit(poseStack,
-                                  //(width / 2) + 96 + CLIENT_CONFIG.getWorldGaugeX(),
-                                  //height - 19 + CLIENT_CONFIG.getWorldGaugeY() - renderOffset,
-                                  (width / 2) - 8 + ConfigSettings.BODY_ICON_POS.get().x(),
-                                  height - 56 + ConfigSettings.BODY_ICON_POS.get().y() - renderOffset - threatOffset,
-                                  0, 64 - severity * 16, 16, 16, 16, 144);
+            // Render frame
+            AbstractGui.blit(poseStack,
+                              (width / 2) - 9 + ConfigSettings.BODY_ICON_POS.get().x(),
+                              height - 57 + ConfigSettings.BODY_ICON_POS.get().y() - renderOffset - threatOffset,
+                              0, 72 - severity * 18, 18, 18, 18, 162);
 
                 poseStack.popPose();
             }
@@ -324,7 +322,7 @@ public class Overlays
         int absTemp = Math.abs(temp);
 
         return (absTemp < 100 ? CSMath.blend(0d, 3d, absTemp, 0, 100)
-                             : CSMath.blend(3d, 7d, absTemp, 100, 150))
+                              : CSMath.blend(3d, 7d, absTemp, 100, 150))
                 * sign;
     }
 
