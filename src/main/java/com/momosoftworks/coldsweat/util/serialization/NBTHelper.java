c665110de4bd816e88c8ed0ddf796a5f22c1db74
MikulDev <gamer3mc@gmail.com>
2024-04-07 09:41:59 -0400

    Add NBT un-breaker

--- src/main/java/com/momosoftworks/coldsweat/util/serialization/NBTHelper.java
+++ src/main/java/com/momosoftworks/coldsweat/util/serialization/NBTHelper.java
@@ -153,8 +153,14 @@ public class NBTHelper
         for (ItemStack stack : items)
         {
             Item item = stack.getItem();
-            CompoundTag tag = stack.getTag();
-            if (tag == null || tag.isEmpty()) continue;
+            CompoundTag tag = NBTHelper.getTagOrEmpty(stack);
+
+            // Remove empty tags from stackable items
+            if (stack.getMaxStackSize() > 1
+            && stack.getTag() != null && stack.getTag().isEmpty())
+            {   stack.setTag(null);
+            }
+            // Convert old tags on existing items
             if (item == ModItems.SOULSPRING_LAMP)
             {
                 if (tag.contains("fuel"))
