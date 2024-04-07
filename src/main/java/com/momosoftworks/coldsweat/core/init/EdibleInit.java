a040ee06743be2a9191e0c521278f794593e60bb
MikulDev <gamer3mc@gmail.com>
2024-04-06 22:31:32 -0400

    Better edible registration & spin new thread for biome edibles

--- src/main/java/com/momosoftworks/coldsweat/core/init/EdibleInit.java
+++ src/main/java/com/momosoftworks/coldsweat/core/init/EdibleInit.java
@@ -1,21 +1,15 @@
-package com.momosoftworks.coldsweat.common.event;
+package com.momosoftworks.coldsweat.core.init;
 
 import com.momosoftworks.coldsweat.api.event.core.EdiblesRegisterEvent;
 import com.momosoftworks.coldsweat.common.entity.data.edible.*;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartedEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
-public class RegisterChameleonEdibles
+public class EdibleInit
 {
-    @SubscribeEvent
-    public static void onWorldLoaded(ServerStartedEvent event)
-    {   EdiblesRegisterEvent edibleEvent = new EdiblesRegisterEvent();
-        MinecraftForge.EVENT_BUS.post(edibleEvent);
-    }
-
     @SubscribeEvent
     public static void onEdiblesRegister(EdiblesRegisterEvent event)
     {
@@ -23,6 +17,12 @@ public class RegisterChameleonEdibles
         event.registerEdible(new ColdBiomeEdible());
         event.registerEdible(new HumidBiomeEdible());
         event.registerEdible(new HealingEdible());
-        event.registerEdible(new HealingEdible());
+    }
+
+    @SubscribeEvent
+    public static void onServerStart(ServerStartingEvent event)
+    {
+        EdiblesRegisterEvent edibleEvent = new EdiblesRegisterEvent();
+        MinecraftForge.EVENT_BUS.post(edibleEvent);
     }
 }
