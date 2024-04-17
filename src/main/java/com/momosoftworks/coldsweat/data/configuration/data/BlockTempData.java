54ea2e0e1ad46eb0a7810291ed7295218b519bd7
MikulDev <gamer3mc@gmail.com>
2024-04-17 18:31:26 -0400

    Add range to BlockTempData

--- src/main/java/com/momosoftworks/coldsweat/data/configuration/data/BlockTempData.java
+++ src/main/java/com/momosoftworks/coldsweat/data/configuration/data/BlockTempData.java
@@ -4,6 +4,7 @@ import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Registry;
+import com.momosoftworks.coldsweat.config.ConfigSettings;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
@@ -14,8 +15,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 import java.util.List;
 import java.util.Optional;
 
-public record BlockTempData(List<Either<TagKey<Block>, Block>> blocks, double temperature, double maxEffect, boolean fade,
-                            List<BlockPredicate> conditions, Optional<CompoundTag> tag, Optional<List<String>> requiredMods)
+public record BlockTempData(List<Either<TagKey<Block>, Block>> blocks, double temperature, double range, double maxEffect, boolean fade,
+                            BlockPredicate condition, Optional<CompoundTag> nbt, Optional<List<String>> requiredMods)
 {
     public static final Codec<BlockTempData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.STRING.xmap(
@@ -40,9 +41,10 @@ public record BlockTempData(List<Either<TagKey<Block>, Block>> blocks, double te
             .fieldOf("blocks").forGetter(BlockTempData::blocks),
             Codec.DOUBLE.fieldOf("temperature").forGetter(BlockTempData::temperature),
             Codec.DOUBLE.optionalFieldOf("max_effect", Double.MAX_VALUE).forGetter(BlockTempData::maxEffect),
+            Codec.DOUBLE.optionalFieldOf("range", ConfigSettings.BLOCK_RANGE.get().doubleValue()).forGetter(BlockTempData::range),
             Codec.BOOL.optionalFieldOf("fade", true).forGetter(BlockTempData::fade),
-            BlockPredicate.CODEC.listOf().optionalFieldOf("conditions", List.of()).forGetter(BlockTempData::conditions),
-            CompoundTag.CODEC.optionalFieldOf("tag").forGetter(BlockTempData::tag),
+            BlockPredicate.CODEC.optionalFieldOf("condition", BlockPredicate.alwaysTrue()).forGetter(BlockTempData::condition),
+            CompoundTag.CODEC.optionalFieldOf("nbt").forGetter(BlockTempData::nbt),
             Codec.STRING.listOf().optionalFieldOf("required_mods").forGetter(BlockTempData::requiredMods)
     ).apply(instance, BlockTempData::new));
 }
