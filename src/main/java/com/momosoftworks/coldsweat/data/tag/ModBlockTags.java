bd12ad3b6bbc71467025565421dc8afb8c8ad5a9
MikulDev <gamer3mc@gmail.com>
2024-04-19 22:45:22 -0400

    Remove useless campfire tags

--- src/main/java/com/momosoftworks/coldsweat/data/tag/ModBlockTags.java
+++ src/main/java/com/momosoftworks/coldsweat/data/tag/ModBlockTags.java
@@ -8,10 +8,8 @@ import net.minecraft.world.level.block.Block;
 
 public class ModBlockTags
 {
-    public static final TagKey<Block> CAMPFIRES = createTag("campfires");
-    public static final TagKey<Block> SOUL_CAMPFIRES = createTag("soul_campfires");
-
     public static final TagKey<Block> SOUL_STALK_PLACEABLE_ON = createTag("may_place_on/soul_stalk");
+    public static final TagKey<Block> SOUL_SAND_REPLACEABLE = createTag("soul_sand_replaceable");
 
     public static final TagKey<Block> HEARTH_SPREAD_WHITELIST = createTag("hearth/spread_whitelist");
     public static final TagKey<Block> HEARTH_SPREAD_BLACKLIST = createTag("hearth/spread_blacklist");
