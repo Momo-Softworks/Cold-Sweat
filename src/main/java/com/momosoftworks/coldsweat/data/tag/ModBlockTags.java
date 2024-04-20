23abd74692594c323fa01b83f6fa2df36fe2b7c0
MikulDev <gamer3mc@gmail.com>
2024-04-20 00:47:13 -0400

    Fix duplicate block tag declaration
    Took 31 seconds

--- src/main/java/com/momosoftworks/coldsweat/data/tag/ModBlockTags.java
+++ src/main/java/com/momosoftworks/coldsweat/data/tag/ModBlockTags.java
@@ -15,7 +15,6 @@ public class ModBlockTags
     public static final TagKey<Block> HEARTH_SPREAD_BLACKLIST = createTag("hearth/spread_blacklist");
 
     public static final TagKey<Block> IGNORE_SLEEP_CHECK = createTag("ignore_sleep_check");
-    public static final TagKey<Block> SOUL_SAND_REPLACEABLE = createTag("soul_sand_replaceable");
 
     private static TagKey<Block> createTag(String name)
     {
