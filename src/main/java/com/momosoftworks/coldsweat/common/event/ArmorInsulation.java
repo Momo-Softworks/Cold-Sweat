0cd3f892c184edea49a27a5e66b4da84a5d84a1f
MikulDev <gamer3mc@gmail.com>
2024-05-03 20:18:08 -0400

    Fix possible null in ArmorInsulation
    Took 4 minutes

--- src/main/java/com/momosoftworks/coldsweat/common/event/ArmorInsulation.java
+++ src/main/java/com/momosoftworks/coldsweat/common/event/ArmorInsulation.java
@@ -71,7 +71,9 @@ public class ArmorInsulation
                         List<Insulation> insulation = ItemInsulationManager.getInsulationCap(armorStack)
                                                       .map(IInsulatableCap::getInsulation).orElse(new ArrayList<>())
                                                       .stream()
-                                                      .filter(pair -> ConfigSettings.INSULATION_ITEMS.get().get(pair.getFirst().getItem()).test(serverPlayer, pair.getFirst()))
+                                                      .filter(pair -> CSMath.getIfNotNull(ConfigSettings.INSULATION_ITEMS.get().get(pair.getFirst().getItem()),
+                                                                                          insulator -> insulator.test(serverPlayer, pair.getFirst()),
+                                                                                          false))
                                                       .map(pair -> pair.getSecond())
                                                       .flatMap(List::stream).toList();
 
