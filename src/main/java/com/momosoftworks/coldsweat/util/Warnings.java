7d22bbf85a20a512f4edf8426ff0baaedf458414
MikulDev <gamer3mc@gmail.com>
2024-05-12 22:01:36 -0400

    Remove Warnings helper class
    Took 3 seconds
    
    Took 3 seconds

--- src/main/java/com/momosoftworks/coldsweat/util/Warnings.java
+++
@@ -1,9 +0,0 @@
-package com.momosoftworks.coldsweat.util;
-
-public class Warnings
-{
-    public static String dupConfigKeys(Class<?> keyClass, String configName, String keyName)
-    {
-        return String.format("Duplicate %s found in %s: %s", keyClass, configName, keyName);
-    }
-}
