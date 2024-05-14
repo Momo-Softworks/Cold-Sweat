7d618cd026daf97483ecaa268f657f9a5dbdd33d
MikulDev <gamer3mc@gmail.com>
2024-05-13 21:56:16 -0400

    Better baby model handling for chameleon

--- src/main/java/com/momosoftworks/coldsweat/client/renderer/ChameleonAnimations.java
+++ src/main/java/com/momosoftworks/coldsweat/client/renderer/ChameleonAnimations.java
@@ -8,6 +8,6 @@ public class ChameleonAnimations
 {
     public static AnimationBatch WALK = AnimationBatch.loadFromFile(new ResourceLocation(ColdSweat.MOD_ID, "animation/chameleon.animation.walk.json"));
     public static AnimationBatch IDLE = AnimationBatch.loadFromFile(new ResourceLocation(ColdSweat.MOD_ID, "animation/chameleon.animation.idle.json"));
-    public static AnimationBatch EAT  = AnimationBatch.loadFromFile(new ResourceLocation(ColdSweat.MOD_ID,  "animation/chameleon.animation.eat.json"));
-    public static AnimationBatch RIDE = AnimationBatch.loadFromFile(new ResourceLocation(ColdSweat.MOD_ID,  "animation/chameleon.animation.ride.json"));
+    public static AnimationBatch EAT  = AnimationBatch.loadFromFile(new ResourceLocation(ColdSweat.MOD_ID, "animation/chameleon.animation.eat.json"));
+    public static AnimationBatch RIDE = AnimationBatch.loadFromFile(new ResourceLocation(ColdSweat.MOD_ID, "animation/chameleon.animation.ride.json"));
 }
