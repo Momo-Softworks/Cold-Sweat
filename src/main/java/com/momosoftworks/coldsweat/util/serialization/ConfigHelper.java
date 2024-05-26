ed0ca4566b7d8cef20f327ece451dd988d2b8923
MikulDev <gamer3mc@gmail.com>
2024-05-26 13:18:10 -0400

    Misc

--- src/main/java/com/momosoftworks/coldsweat/util/serialization/ConfigHelper.java
+++ src/main/java/com/momosoftworks/coldsweat/util/serialization/ConfigHelper.java
@@ -164,7 +164,7 @@ public class ConfigHelper
             }
             catch (Exception e)
             {
-                ColdSweat.LOGGER.error("Error parsing biome config for \"{}\"", entry.toString());
+                ColdSweat.LOGGER.error("Error parsing biome config \"{}\"", entry.toString());
                 e.printStackTrace();
             }
         }
@@ -193,7 +193,7 @@ public class ConfigHelper
             }
             catch (Exception e)
             {
-                ColdSweat.LOGGER.error("Error parsing dimension config for \"{}\"", entry.toString());
+                ColdSweat.LOGGER.error("Error parsing dimension config \"{}\"", entry.toString());
                 e.printStackTrace();
             }
         }
@@ -220,7 +220,7 @@ public class ConfigHelper
                 }
             }
             catch (Exception e)
-            {   ColdSweat.LOGGER.error("Error parsing structure config for \"{}\"", entry.toString());
+            {   ColdSweat.LOGGER.error("Error parsing structure config \"{}\"", entry.toString());
                 e.printStackTrace();
             }
         }
