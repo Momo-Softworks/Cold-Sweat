b105bae2323f53d1e96dab928a94c64bafc0b89d
MikulDev <gamer3mc@gmail.com>
2024-05-30 22:16:11 -0400

    Fix not testing item predicates for item attributes when owner entity is null

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ItemInsulationManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ItemInsulationManager.java
@@ -175,7 +175,7 @@ public class ItemInsulationManager
             {
                 CSMath.doIfNotNull(ConfigSettings.INSULATION_ITEMS.get().get(pair.getFirst().getItem()), insulator ->
                 {
-                    if (owner == null || insulator.test(owner, pair.getFirst()))
+                    if (insulator.test(owner, pair.getFirst()))
                     {
                         modifiers.addAll(insulator.attributes().get(attribute)
                                                   .stream()
@@ -187,7 +187,7 @@ public class ItemInsulationManager
         });
         CSMath.doIfNotNull(ConfigSettings.INSULATING_ARMORS.get().get(stack.getItem()), insulator ->
         {
-            if (owner == null || insulator.test(owner, stack))
+            if (insulator.test(owner, stack))
             {
                 modifiers.addAll(insulator.attributes().get(attribute)
                                           .stream()
