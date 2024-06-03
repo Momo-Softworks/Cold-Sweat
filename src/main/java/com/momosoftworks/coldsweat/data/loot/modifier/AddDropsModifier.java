f4da4ab88f1b70ec510285cb2012b98a3c92b751
MikulDev <gamer3mc@gmail.com>
2024-06-03 16:00:43 -0400

    Remove leather drops from hoglin

--- src/main/java/com/momosoftworks/coldsweat/data/loot/modifier/AddDropsModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/data/loot/modifier/AddDropsModifier.java
@@ -4,11 +4,13 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.momosoftworks.coldsweat.data.codec.LootEntry;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -17,15 +19,18 @@ public class AddDropsModifier extends LootModifier
 {
     public static Codec<AddDropsModifier> CODEC = RecordCodecBuilder.create(inst -> inst.group(
             IGlobalLootModifier.LOOT_CONDITIONS_CODEC.fieldOf("conditions").forGetter(modifier -> modifier.conditions),
-            LootEntry.CODEC.listOf().fieldOf("additions").forGetter(modifier -> modifier.additions)
+            LootEntry.CODEC.listOf().fieldOf("additions").forGetter(modifier -> modifier.additions),
+            ForgeRegistries.ITEMS.getCodec().listOf().optionalFieldOf("removals", List.of()).forGetter(modifier -> modifier.removals)
     ).apply(inst, AddDropsModifier::new));
 
     private final List<LootEntry> additions;
+    private final List<Item> removals;
 
-    protected AddDropsModifier(LootItemCondition[] conditionsIn, List<LootEntry> additions)
+    protected AddDropsModifier(LootItemCondition[] conditionsIn, List<LootEntry> additions, List<Item> removals)
     {
         super(conditionsIn);
         this.additions = additions;
+        this.removals = removals;
     }
 
     @NotNull
@@ -38,6 +43,9 @@ public class AddDropsModifier extends LootModifier
             generatedLoot.add(new ItemStack(entry.item(),
                                             context.getRandom().nextIntBetweenInclusive(entry.count().min(), entry.count().max())
                                           + context.getRandom().nextIntBetweenInclusive(0, countRange * context.getLootingModifier())));
+        }
+        if (!removals.isEmpty())
+        {   generatedLoot.removeIf(stack -> removals.contains(stack.getItem()));
         }
         return generatedLoot;
     }
