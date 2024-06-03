edd202850b269735074a90e63a571dd794e55842
MikulDev <gamer3mc@gmail.com>
2024-06-03 16:00:43 -0400

    Remove leather drops from hoglin
    Took 1 minute

--- src/main/java/com/momosoftworks/coldsweat/data/loot/modifier/AddDropsModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/data/loot/modifier/AddDropsModifier.java
@@ -3,20 +3,15 @@ package com.momosoftworks.coldsweat.data.loot.modifier;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.mojang.serialization.Codec;
 import com.mojang.serialization.JsonOps;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.momosoftworks.coldsweat.data.codec.LootEntry;
-import com.momosoftworks.coldsweat.data.codec.util.IntegerBounds;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
-import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
@@ -24,16 +19,17 @@ import org.jetbrains.annotations.NotNull;
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class AddDropsModifier extends LootModifier
 {
     private final List<LootEntry> additions;
+    private final List<Item> removals;
 
-    protected AddDropsModifier(LootItemCondition[] conditionsIn, List<LootEntry> additions)
+    protected AddDropsModifier(LootItemCondition[] conditionsIn, List<LootEntry> additions, List<Item> removals)
     {
         super(conditionsIn);
         this.additions = additions;
+        this.removals = removals;
     }
 
     @NotNull
@@ -47,6 +43,9 @@ public class AddDropsModifier extends LootModifier
                                             context.getRandom().nextInt(entry.count().min(), entry.count().max() + 1)
                                           + context.getRandom().nextInt(0, countRange * context.getLootingModifier() + 1)));
         }
+        if (!removals.isEmpty())
+        {   generatedLoot.removeIf(stack -> removals.contains(stack.getItem()));
+        }
         return generatedLoot;
     }
 
@@ -59,7 +58,13 @@ public class AddDropsModifier extends LootModifier
             for (JsonElement element : GsonHelper.getAsJsonArray(object, "additions"))
             {   additions.add(LootEntry.CODEC.parse(JsonOps.INSTANCE, element).result().orElseThrow(RuntimeException::new));
             }
-            return new AddDropsModifier(conditionsIn, additions);
+            List<Item> removals = new ArrayList<>();
+            if (object.has("removals"))
+            {   for (JsonElement element : GsonHelper.getAsJsonArray(object, "removals"))
+                {   removals.add(ForgeRegistries.ITEMS.getValue(new ResourceLocation(element.getAsString())));
+                }
+            }
+            return new AddDropsModifier(conditionsIn, additions, removals);
         }
 
         @Override
@@ -67,11 +72,15 @@ public class AddDropsModifier extends LootModifier
         {
             JsonObject object = new JsonObject();
             JsonArray additions = new JsonArray();
+            JsonArray removals = new JsonArray();
             for (LootEntry entry : instance.additions)
-            {
-                additions.add(LootEntry.CODEC.encodeStart(JsonOps.INSTANCE, entry).result().orElseThrow(RuntimeException::new));
+            {    additions.add(LootEntry.CODEC.encodeStart(JsonOps.INSTANCE, entry).result().orElseThrow(RuntimeException::new));
+            }
+            for (Item item : instance.removals)
+            {    removals.add(item.getRegistryName().toString());
             }
             object.add("additions", additions);
+            object.add("removals", removals);
             return object;
         }
     }
