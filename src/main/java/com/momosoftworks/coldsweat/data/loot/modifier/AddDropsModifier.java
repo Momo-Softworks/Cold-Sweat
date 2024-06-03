8726eb3ed326e74ed26374dbfb53a7820cfb2837
MikulDev <gamer3mc@gmail.com>
2024-06-03 16:00:43 -0400

    Remove leather drops from hoglin
    Took 1 minute
    
    Took 40 seconds

--- src/main/java/com/momosoftworks/coldsweat/data/loot/modifier/AddDropsModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/data/loot/modifier/AddDropsModifier.java
@@ -6,12 +6,14 @@ import com.google.gson.JsonObject;
 import com.mojang.serialization.JsonOps;
 import com.momosoftworks.coldsweat.data.codec.LootEntry;
 import com.momosoftworks.coldsweat.util.serialization.JsonHelper;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.LootContext;
 import net.minecraft.loot.conditions.ILootCondition;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.LootModifier;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
@@ -20,11 +22,13 @@ import java.util.List;
 public class AddDropsModifier extends LootModifier
 {
     private final List<LootEntry> additions;
+    private final List<Item> removals;
 
-    protected AddDropsModifier(ILootCondition[] conditionsIn, List<LootEntry> additions)
+    protected AddDropsModifier(ILootCondition[] conditionsIn, List<LootEntry> additions, List<Item> removals)
     {
         super(conditionsIn);
         this.additions = additions;
+        this.removals = removals;
     }
 
     @Override
@@ -36,6 +40,9 @@ public class AddDropsModifier extends LootModifier
             generatedLoot.add(new ItemStack(entry.item,
                                             context.getRandom().nextInt(entry.count.max - entry.count.min + 1) + entry.count.min
                                           + context.getRandom().nextInt(countRange * context.getLootingModifier() + 1)));
+        }
+        if (!removals.isEmpty())
+        {   generatedLoot.removeIf(stack -> removals.contains(stack.getItem()));
         }
         return generatedLoot;
     }
@@ -49,7 +56,13 @@ public class AddDropsModifier extends LootModifier
             for (JsonElement element : JsonHelper.getAsJsonArray(object, "additions"))
             {   additions.add(LootEntry.CODEC.parse(JsonOps.INSTANCE, element).result().orElseThrow(RuntimeException::new));
             }
-            return new AddDropsModifier(conditionsIn, additions);
+            List<Item> removals = new ArrayList<>();
+            if (object.has("removals"))
+            {   for (JsonElement element : JsonHelper.getAsJsonArray(object, "removals"))
+                {   removals.add(ForgeRegistries.ITEMS.getValue(new ResourceLocation(element.getAsString())));
+                }
+            }
+            return new AddDropsModifier(conditionsIn, additions, removals);
         }
 
         @Override
@@ -57,11 +70,15 @@ public class AddDropsModifier extends LootModifier
         {
             JsonObject object = new JsonObject();
             JsonArray additions = new JsonArray();
+            JsonArray removals = new JsonArray();
             for (LootEntry entry : instance.additions)
-            {
-                additions.add(LootEntry.CODEC.encodeStart(JsonOps.INSTANCE, entry).result().orElseThrow(RuntimeException::new));
+            {    additions.add(LootEntry.CODEC.encodeStart(JsonOps.INSTANCE, entry).result().orElseThrow(RuntimeException::new));
+            }
+            for (Item item : instance.removals)
+            {    removals.add(item.getRegistryName().toString());
             }
             object.add("additions", additions);
+            object.add("removals", removals);
             return object;
         }
     }
