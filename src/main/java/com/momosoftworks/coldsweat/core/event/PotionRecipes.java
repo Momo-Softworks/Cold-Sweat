54660000788704d10b459eb0e0f9642576c6abc1
MikulDev <gamer3mc@gmail.com>
2024-06-04 12:08:19 -0400

    Fix brewing recipes working for any potion instead of just awkward
    Took 11 seconds

--- src/main/java/com/momosoftworks/coldsweat/core/event/PotionRecipes.java
+++ src/main/java/com/momosoftworks/coldsweat/core/event/PotionRecipes.java
@@ -8,9 +8,12 @@ import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import java.util.Arrays;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class PotionRecipes
@@ -21,11 +24,46 @@ public class PotionRecipes
         event.enqueueWork(() ->
         {
             ItemStack awkward = PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.AWKWARD);
-            ItemStack iceResPotion = PotionUtils.setPotion(Items.POTION.getDefaultInstance(), PotionInit.ICE_RESISTANCE.get());
-            BrewingRecipeRegistry.addRecipe(Ingredient.of(awkward), Ingredient.of(ModItems.SOUL_SPROUT),
-                                            PotionUtils.setPotion(Items.POTION.getDefaultInstance(), PotionInit.ICE_RESISTANCE.get()));
-            BrewingRecipeRegistry.addRecipe(Ingredient.of(iceResPotion), Ingredient.of(Items.REDSTONE),
-                                            PotionUtils.setPotion(Items.POTION.getDefaultInstance(), PotionInit.ICE_RESISTANCE_LONG.get()));
+            ItemStack icePotion = PotionUtils.setPotion(Items.POTION.getDefaultInstance(), PotionInit.ICE_RESISTANCE.get());
+            ItemStack longIcePotion = PotionUtils.setPotion(Items.POTION.getDefaultInstance(), PotionInit.ICE_RESISTANCE_LONG.get());
+
+            BrewingRecipeRegistry.addRecipe(new WorkingBrewingRecipe(Ingredient.of(awkward), Ingredient.of(ModItems.SOUL_SPROUT), icePotion));
+            BrewingRecipeRegistry.addRecipe(new WorkingBrewingRecipe(Ingredient.of(icePotion), Ingredient.of(Items.REDSTONE), longIcePotion));
         });
+    }
+
+    /**
+     * A brewing recipe that actually checks item stack data for ingredients instead of just the item type.
+     */
+    public static class WorkingBrewingRecipe implements IBrewingRecipe
+    {
+        Ingredient potionIn;
+        Ingredient reagent;
+        ItemStack output;
+
+        public WorkingBrewingRecipe(Ingredient potionIn, Ingredient reagent, ItemStack output)
+        {
+            this.potionIn = potionIn;
+            this.reagent = reagent;
+            this.output = output.copy();
+        }
+
+        @Override
+        public boolean isInput(ItemStack input)
+        {   return Arrays.stream(potionIn.getItems()).anyMatch(ingredient -> ItemStack.isSameItemSameTags(ingredient, input));
+        }
+
+        @Override
+        public boolean isIngredient(ItemStack ingredient)
+        {   return Arrays.stream(reagent.getItems()).anyMatch(ing -> ItemStack.isSameItemSameTags(ing, ingredient));
+        }
+
+        @Override
+        public ItemStack getOutput(ItemStack input, ItemStack ingredient)
+        {
+            return isInput(input) && isIngredient(ingredient)
+                   ? output
+                   : ItemStack.EMPTY;
+        }
     }
 }
