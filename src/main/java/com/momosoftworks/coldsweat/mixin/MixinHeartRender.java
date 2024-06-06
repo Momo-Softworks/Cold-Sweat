a9491a92dfca79e1b12d5ee028e7c93331f80f98
MikulDev <gamer3mc@gmail.com>
2024-06-05 23:38:05 -0400

    Fix frozen health rendering bug

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinHeartRender.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinHeartRender.java
@@ -16,7 +16,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 /**
  * A needlessly complex mixin to render frozen hearts when the player's temp falls below -50
@@ -26,12 +25,16 @@ public class MixinHeartRender
 {
     private static final ResourceLocation HEART_TEXTURE = new ResourceLocation(ColdSweat.MOD_ID, "textures/gui/overlay/hearts_frozen.png");
     private static final ResourceLocation ICONS_TEXTURE = new ResourceLocation("minecraft", "textures/gui/icons.png");
-    private static int HEART_COUNTER = 0;
+    private static int HEART_INDEX = 0;
 
     @Inject(method = "renderHeart(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/gui/Gui$HeartType;IIIZZ)V", at = @At("TAIL"), cancellable = true)
-    private void renderHeart(PoseStack ps, Gui.HeartType type, int x, int y, int texV, boolean blink, boolean half, CallbackInfo ci)
+    private void renderHeart(PoseStack ps, Gui.HeartType heartType, int x, int y, int texV, boolean blink, boolean half, CallbackInfo ci)
     {
         Player player = Minecraft.getInstance().player;
+
+        if (heartType == Gui.HeartType.CONTAINER)
+        {   HEART_INDEX += 1;
+        }
 
         if (player != null)
         {
@@ -44,18 +47,18 @@ public class MixinHeartRender
 
             int frozenHealth = (int) (player.getMaxHealth() - player.getMaxHealth() * CSMath.blend(coldLiningFactor, 1, temp, -100, -50));
             int frozenHearts = frozenHealth / 2;
-            int u = blink || type == Gui.HeartType.CONTAINER ? 14 : half ? 7 : 0;
+            int u = blink || heartType == Gui.HeartType.CONTAINER ? 14 : half ? 7 : 0;
 
             // Render frozen hearts
             RenderSystem.setShaderTexture(0, HEART_TEXTURE);
-            if (HEART_COUNTER > 0 && HEART_COUNTER < frozenHearts + 1)
+            if (HEART_INDEX > 0 && HEART_INDEX < frozenHearts + 1)
             {
                 GuiComponent.blit(ps, x, y, 21, 0, 9, 9, 30, 14);
                 GuiComponent.blit(ps, x + 1, y + 1, u, 0, 7, 7, 30, 14);
                 ci.cancel();
             }
             // Render half-frozen heart if needed
-            else if (HEART_COUNTER == frozenHearts + 1 && frozenHealth % 2 == 1)
+            else if (HEART_INDEX == frozenHearts + 1 && frozenHealth % 2 == 1)
             {
                 GuiComponent.blit(ps, x + 1, y + 1, u, 7, 7, 7, 30, 14);
             }
@@ -63,18 +66,8 @@ public class MixinHeartRender
         RenderSystem.setShaderTexture(0, ICONS_TEXTURE);
     }
 
-    @Inject(method = "renderHearts(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/player/Player;IIIIFIIIZ)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;renderHeart(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/gui/Gui$HeartType;IIIZZ)V", ordinal = 0),
-            locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void tickHeartCounter(PoseStack p_168689_, Player p_168690_, int p_168691_, int p_168692_, int p_168693_, int p_168694_, float p_168695_, int p_168696_, int p_168697_, int p_168698_, boolean p_168699_, CallbackInfo ci,
-                                  // local variables from the method
-                                  Gui.HeartType gui$hearttype, int i, int j, int k, int l, int i1, int j1, int k1, int l1, int i2)
-    {
-        Player player = Minecraft.getInstance().player;
-        if (player != null)
-        {
-            int playerHearts = j + k;
-            HEART_COUNTER = (HEART_COUNTER + 1) % playerHearts;
-        }
+    @Inject(method = "renderHearts", at = @At("HEAD"))
+    private void renderHearts(PoseStack pPoseStack, Player pPlayer, int pX, int pY, int pHeight, int p_168694_, float p_168695_, int p_168696_, int p_168697_, int p_168698_, boolean p_168699_, CallbackInfo ci)
+    {   HEART_INDEX = 0;
     }
 }
