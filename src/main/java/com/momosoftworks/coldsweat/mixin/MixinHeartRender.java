4e0ca00eed864eebba19ae976ada9b505f505ed3
MikulDev <gamer3mc@gmail.com>
2024-06-06 18:28:33 -0400

    Add frozen heart render
    Took 57 seconds

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinHeartRender.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinHeartRender.java
@@ -1,74 +1,94 @@
 package com.momosoftworks.coldsweat.mixin;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import com.momosoftworks.coldsweat.ColdSweat;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.IngameGui;
+import com.momosoftworks.coldsweat.client.gui.Overlays;
+import com.momosoftworks.coldsweat.common.event.TempEffectsCommon;
+import com.momosoftworks.coldsweat.config.ConfigSettings;
+import com.momosoftworks.coldsweat.config.spec.MainSettingsConfig;
+import com.momosoftworks.coldsweat.util.math.CSMath;
+import com.momosoftworks.coldsweat.util.registries.ModEffects;
+import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.gui.ForgeIngameGui;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import static net.minecraft.client.gui.AbstractGui.GUI_ICONS_LOCATION;
 
 /**
  * A needlessly complex mixin to render frozen hearts when the player's temp falls below -50
  */
 @Mixin(ForgeIngameGui.class)
-public class MixinHeartRender extends IngameGui
+public abstract class MixinHeartRender
 {
+    @Shadow protected abstract void bind(ResourceLocation res);
+
     private static final ResourceLocation HEART_TEXTURE = new ResourceLocation(ColdSweat.MOD_ID, "textures/gui/overlay/hearts_frozen.png");
-    private static final ResourceLocation ICONS_TEXTURE = new ResourceLocation("minecraft", "textures/gui/icons.png");
-    private static int HEART_COUNTER = 0;
+    private static int HEART_INDEX = 0;
+    private static boolean IS_CONTAINER = false;
 
-    public MixinHeartRender(Minecraft mc)
-    {   super(mc);
-    }
-
-    /*@Inject(method = "renderHealth", at = @At("TAIL"), cancellable = true)
-    private void renderHeart(int width, int height, MatrixStack mStack, CallbackInfo ci)
+    @Inject(method = "renderHealth",
+            at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/gui/ForgeIngameGui;blit(Lcom/mojang/blaze3d/matrix/MatrixStack;IIIIII)V", shift = At.Shift.AFTER),
+            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;ceil(F)I", ordinal = 3),
+                           to = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;disableBlend()V")),
+            locals = LocalCapture.CAPTURE_FAILHARD, remap = false)
+    private void renderHeart(int width, int height, MatrixStack ps, CallbackInfo ci,
+                             // Locals
+                             PlayerEntity player, int health, boolean blink, int healthLast,
+                             ModifiableAttributeInstance attrMaxHealth, float healthMax,
+                             float absorb, int healthRows, int rowHeight, int left,
+                             int top, int regen, int TOP, int BACKGROUND, int MARGIN,
+                             float absorbRemaining, int i, int row, int x, int y)
     {
-        PlayerEntity player = Minecraft.getInstance().player;
+        if (!MainSettingsConfig.getInstance().freezingHearts()) return;
 
-        if (player != null)
-        {
-            if (player.hasEffect(ModEffects.ICE_RESISTANCE)) return;
-            double temp = Overlays.BODY_TEMP;
-            boolean highlight = healthBlinkTime > (long)tickCount && (healthBlinkTime - (long)tickCount) / 3L %2L == 1L
+        int hearts = CSMath.ceil(health / 2d);
+        int lastHeartIndex = (int) (healthMax / 2 - hearts);
+        boolean half = HEART_INDEX == lastHeartIndex + 1 && health % 2 == 1;
 
-            // Get protection from armor underwear
-            float coldLiningFactor = CSMath.blend(0.5f, 1, TempEffectsCommon.getTempResistance(player, true), 0, 4);
-            if (coldLiningFactor == 1) return;
+        if (player.hasEffect(ModEffects.ICE_RESISTANCE)) return;
+        double temp = Overlays.BODY_TEMP;
 
-            int frozenHealth = (int) (player.getMaxHealth() - player.getMaxHealth() * CSMath.blend(coldLiningFactor, 1, temp, -100, -50));
-            int frozenHearts = frozenHealth / 2;
-            int u = highlight || type == Gui.HeartType.CONTAINER ? 14 : half ? 7 : 0;
+        // Get protection from armor underwear
+        float unfrozenHealthPercent = CSMath.blend(0.5f, 1, TempEffectsCommon.getTempResistance(player, true), 0, 4);
+        if (unfrozenHealthPercent == 1) return;
 
-            // Render frozen hearts
-            RenderSystem.setShaderTexture(0, HEART_TEXTURE);
-            if (HEART_COUNTER > 0 && HEART_COUNTER < frozenHearts + 1)
-            {
-                GuiComponent.blit(ps, x, y, 21, 0, 9, 9, 30, 14);
-                GuiComponent.blit(ps, x + 1, y + 1, u, 0, 7, 7, 30, 14);
-                ci.cancel();
-            }
-            // Render half-frozen heart if needed
-            else if (HEART_COUNTER == frozenHearts + 1 && frozenHealth % 2 == 1)
-            {
-                GuiComponent.blit(ps, x + 1, y + 1, u, 7, 7, 7, 30, 14);
-            }
+        int frozenHealth = CSMath.ceil(healthMax - healthMax * CSMath.blend(unfrozenHealthPercent, 1, temp, -100, -50));
+        int frozenHearts = frozenHealth / 2;
+        // "(HEART_INDEX < frozenHearts + 2 && blink)" ensures hearts don't render when they're blinking after the player takes damage
+        int u = IS_CONTAINER || blink ? 14 : half ? 7 : 0;
+
+        // Render frozen hearts
+        bind(HEART_TEXTURE);
+        if (HEART_INDEX < frozenHearts + 1)
+        {   AbstractGui.blit(ps, x + 1, y + 1, u, 0, 7, 7, 30, 14);
         }
-        RenderSystem.setShaderTexture(0, ICONS_TEXTURE);
+        // Render half-frozen heart if needed
+        else if (HEART_INDEX == frozenHearts + 1 && frozenHealth % 2 == 1)
+        {   AbstractGui.blit(ps, x + 1, y + 1, u, 7, 7, 7, 30, 14);
+        }
+        bind(GUI_ICONS_LOCATION);
+        IS_CONTAINER = false;
     }
 
     @Inject(method = "renderHealth",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;renderHeart(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/gui/Gui$HeartType;IIIZZ)V", ordinal = 0),
-            locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void tickHeartCounter(PoseStack p_168689_, PlayerEntity p_168690_, int p_168691_, int p_168692_, int p_168693_, int p_168694_, float p_168695_, int p_168696_, int p_168697_, int p_168698_, boolean p_168699_, CallbackInfo ci,
-                                  // local variables from the method
-                                  Gui.HeartType gui$hearttype, int i, int j, int k, int l, int i1, int j1, int k1, int l1, int i2)
-    {
-        PlayerEntity player = Minecraft.getInstance().player;
-        if (player != null)
-        {
-            int playerHearts = j + k;
-            HEART_COUNTER = (HEART_COUNTER + 1) % playerHearts;
-        }
-    }*/
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;ceil(F)I", ordinal = 4),
+            remap = false)
+    private void incrementHeartIndex(int width, int height, MatrixStack mStack, CallbackInfo ci)
+    {   HEART_INDEX++;
+        IS_CONTAINER = true;
+    }
+
+    @Inject(method = "renderHealth", at = @At(value = "HEAD"), remap = false)
+    private void resetHeartIndex(int width, int height, MatrixStack mStack, CallbackInfo ci)
+    {   HEART_INDEX = 0;
+    }
 }
