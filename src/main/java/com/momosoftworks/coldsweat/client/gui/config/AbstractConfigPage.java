10407f69367743a6aafcd91c6450f901f7fd89ec
MikulDev <gamer3mc@gmail.com>
2024-06-07 14:03:29 -0400

    Revert "Remove body temp readout"
    This reverts commit 1460ad75fc50f19a079b583636431545e00e0fc0.

--- src/main/java/com/momosoftworks/coldsweat/client/gui/config/AbstractConfigPage.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/config/AbstractConfigPage.java
@@ -298,8 +298,8 @@ public abstract class AbstractConfigPage extends Screen
         boolean shouldBeActive = !requireOP || MINECRAFT.player == null || MINECRAFT.player.hasPermissions(2);
 
         int labelWidth = font.width(label.getString());
-        int labelOffset = labelWidth > 75
-                        ? labelWidth - 75
+        int labelOffset = labelWidth > 84
+                        ? labelWidth - 84
                         : 0;
 
         List<GuiEventListener> widgetBatch = new ArrayList<>();
