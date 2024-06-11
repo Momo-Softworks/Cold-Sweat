d40e7911a35ec2083c3996dcde8e5526a3c53bba
MikulDev <gamer3mc@gmail.com>
2024-06-11 16:07:26 -0400

    Fix instances of null registries/registry values

--- src/main/java/com/momosoftworks/coldsweat/config/ConfigSettings.java
+++ src/main/java/com/momosoftworks/coldsweat/config/ConfigSettings.java
@@ -293,6 +293,7 @@ public class ConfigSettings
 
                                                          return Arrays.asList(dim.toString(), temp, units.toString());
                                                      })
+                                                     .filter(Objects::nonNull)
                                                      .collect(Collectors.toList())));
 
         DIMENSION_OFFSETS = addSyncedSetting("dimension_offsets", () -> ConfigHelper.getDimensionsWithValues(WorldSettingsConfig.getInstance().getDimensionTempOffsets(), false),
@@ -309,6 +310,7 @@ public class ConfigSettings
 
                                                          return Arrays.asList(dim.toString(), temp, units.toString());
                                                      })
+                                                     .filter(Objects::nonNull)
                                                      .collect(Collectors.toList())));
 
         STRUCTURE_TEMPS = addSyncedSetting("structure_temperatures", () -> ConfigHelper.getStructuresWithValues(WorldSettingsConfig.getInstance().getStructureTemperatures(), true),
@@ -325,6 +327,7 @@ public class ConfigSettings
 
                                                          return Arrays.asList(struct.toString(), temp, units.toString());
                                                      })
+                                                     .filter(Objects::nonNull)
                                                      .collect(Collectors.toList())));
 
         STRUCTURE_OFFSETS = addSyncedSetting("structure_offsets", () -> ConfigHelper.getStructuresWithValues(WorldSettingsConfig.getInstance().getStructureTempOffsets(), false),
@@ -341,6 +344,7 @@ public class ConfigSettings
 
                                                          return Arrays.asList(struct.toString(), temp, units.toString());
                                                      })
+                                                     .filter(Objects::nonNull)
                                                      .collect(Collectors.toList())));
 
         CAVE_INSULATION = addSyncedSetting("cave_insulation", () -> WorldSettingsConfig.getInstance().getCaveInsulation(),
