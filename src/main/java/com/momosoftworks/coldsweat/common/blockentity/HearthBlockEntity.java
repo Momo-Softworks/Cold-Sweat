f56b913f91f9ee7bb86d4192e638f2466c366536
MikulDev <gamer3mc@gmail.com>
2024-07-04 22:07:07 -0400

    Add subtitles for fuel depletion sound & unique subtitles for hearth/boiler/icebox

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
@@ -756,7 +756,7 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity
     }
 
     protected SoundEvent getFuelDepleteSound()
-    {   return ModSounds.HEARTH_FUEL;
+    {   return ModSounds.HEARTH_DEPLETE;
     }
 
     public void checkForSmokestack()
