6fa1abbd0a36c693cd1992618427759935c99ca4
MikulDev <gamer3mc@gmail.com>
2024-07-16 00:23:11 -0400

    Use mid temperature as base for BiomeTempModifier immunity level

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
@@ -282,11 +282,12 @@ public class EntityTempManager
     }
 
     @SubscribeEvent
-    public static void cancelImmuneModifiers(TempModifierEvent.Calculate.Modify event)
+    public static void calcModifierImmunity(TempModifierEvent.Calculate.Modify event)
     {
         if (!Arrays.stream(VALID_ATTRIBUTE_TYPES).toList().contains(event.getTrait())) return;
         TempModifier mod = event.getModifier();
         ResourceLocation modifierKey = TempModifierRegistry.getKey(mod);
+        LivingEntity entity = event.getEntity();
 
         for (Map.Entry<ItemStack, Insulator> entry : getInsulatorsOnEntity(event.getEntity()).entrySet())
         {
@@ -297,7 +298,10 @@ public class EntityTempManager
             if (immunity != null && insulator.test(event.getEntity(), stack))
             {
                 Function<Double, Double> func = event.getFunction();
-                event.setFunction(temp -> CSMath.blend(func.apply(temp), mod.getLastInput(), immunity, 0, 1));
+                double lastInput = mod instanceof BiomeTempModifier
+                                   ? (Temperature.get(entity, Temperature.Trait.FREEZING_POINT) + Temperature.get(entity, Temperature.Trait.BURNING_POINT)) / 2
+                                   : mod.getLastInput();
+                event.setFunction(temp -> CSMath.blend(func.apply(temp), lastInput, immunity, 0, 1));
             }
         }
     }
