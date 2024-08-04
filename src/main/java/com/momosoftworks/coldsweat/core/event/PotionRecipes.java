eea24410c19a517150879077ed30d22a5680d6d7
MikulDev <gamer3mc@gmail.com>
2024-08-04 19:27:46 -0400

    Register recipes in a way that JEI can detect

--- src/main/java/com/momosoftworks/coldsweat/core/event/PotionRecipes.java
+++ src/main/java/com/momosoftworks/coldsweat/core/event/PotionRecipes.java
@@ -4,15 +4,17 @@ import com.momosoftworks.coldsweat.core.init.PotionInit;
 import com.momosoftworks.coldsweat.util.registries.ModItems;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.brewing.BrewingRecipe;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
-import net.minecraftforge.common.brewing.IBrewingRecipe;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
+import javax.annotation.Nonnull;
 import java.util.Arrays;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -21,21 +23,22 @@ public class PotionRecipes
     @SubscribeEvent
     public static void register(FMLCommonSetupEvent event)
     {
-        event.enqueueWork(() ->
-        {
-            ItemStack awkward = PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.AWKWARD);
-            ItemStack icePotion = PotionUtils.setPotion(Items.POTION.getDefaultInstance(), PotionInit.ICE_RESISTANCE.get());
-            ItemStack longIcePotion = PotionUtils.setPotion(Items.POTION.getDefaultInstance(), PotionInit.LONG_ICE_RESISTANCE.get());
+        ItemStack awkward = createPotion(Potions.AWKWARD);
+        ItemStack icePotion = createPotion(PotionInit.ICE_RESISTANCE.get());
+        ItemStack longIcePotion = createPotion(PotionInit.LONG_ICE_RESISTANCE.get());
 
-            BrewingRecipeRegistry.addRecipe(new WorkingBrewingRecipe(Ingredient.of(awkward), Ingredient.of(ModItems.SOUL_SPROUT), icePotion));
-            BrewingRecipeRegistry.addRecipe(new WorkingBrewingRecipe(Ingredient.of(icePotion), Ingredient.of(Items.REDSTONE), longIcePotion));
-        });
+        BrewingRecipeRegistry.addRecipe(new WorkingBrewingRecipe(Ingredient.of(awkward), Ingredient.of(ModItems.SOUL_SPROUT), icePotion));
+        BrewingRecipeRegistry.addRecipe(new WorkingBrewingRecipe(Ingredient.of(icePotion), Ingredient.of(Items.REDSTONE), longIcePotion));
+    }
+
+    private static ItemStack createPotion(Potion potion)
+    {   return PotionUtils.setPotion(Items.POTION.getDefaultInstance(), potion);
     }
 
     /**
      * A brewing recipe that actually checks item stack data for ingredients instead of just the item type.
      */
-    public static class WorkingBrewingRecipe implements IBrewingRecipe
+    public static class WorkingBrewingRecipe extends BrewingRecipe
     {
         Ingredient potionIn;
         Ingredient reagent;
@@ -43,27 +46,42 @@ public class PotionRecipes
 
         public WorkingBrewingRecipe(Ingredient potionIn, Ingredient reagent, ItemStack output)
         {
+            super(potionIn, reagent, output);
             this.potionIn = potionIn;
             this.reagent = reagent;
             this.output = output.copy();
         }
 
         @Override
-        public boolean isInput(ItemStack input)
-        {   return Arrays.stream(potionIn.getItems()).anyMatch(ingredient -> ItemStack.isSameItemSameTags(ingredient, input));
+        public boolean isInput(@Nonnull ItemStack potionIn)
+        {
+            if (potionIn == null)
+            {   return false;
+            }
+
+            ItemStack[] matchingStacks = this.potionIn.getItems();
+
+            if (matchingStacks.length == 0)
+            {   return potionIn.isEmpty();
+            }
+
+            return Arrays.stream(matchingStacks).anyMatch(itemstack -> ItemStack.isSameItemSameTags(itemstack, potionIn));
         }
 
         @Override
         public boolean isIngredient(ItemStack ingredient)
-        {   return Arrays.stream(reagent.getItems()).anyMatch(ing -> ItemStack.isSameItemSameTags(ing, ingredient));
-        }
-
-        @Override
-        public ItemStack getOutput(ItemStack input, ItemStack ingredient)
         {
-            return isInput(input) && isIngredient(ingredient)
-                   ? output
-                   : ItemStack.EMPTY;
+            if (ingredient == null)
+            {   return false;
+            }
+
+            ItemStack[] matchingStacks = this.reagent.getItems();
+
+            if (matchingStacks.length == 0)
+            {   return ingredient.isEmpty();
+            }
+
+            return Arrays.stream(matchingStacks).anyMatch(itemstack -> ItemStack.isSameItemSameTags(itemstack, ingredient));
         }
     }
 }
