97b0c2a1c66c1b703794bbc11ec4da1c28ae5298
MikulDev <gamer3mc@gmail.com>
2024-08-04 19:24:02 -0400

    Enable seasonal temperatures for alternate dimensions if whitelisted in SS config

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/compat/SereneSeasonsTempModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/compat/SereneSeasonsTempModifier.java
@@ -7,6 +7,7 @@ import com.momosoftworks.coldsweat.util.math.CSMath;
 import net.minecraft.world.entity.LivingEntity;
 import sereneseasons.api.season.ISeasonState;
 import sereneseasons.api.season.SeasonHelper;
+import sereneseasons.config.ServerConfig;
 
 import java.util.function.Function;
 
@@ -20,7 +21,8 @@ public class SereneSeasonsTempModifier extends TempModifier
     @Override
     public Function<Double, Double> calculate(LivingEntity entity, Temperature.Trait trait)
     {
-        if (entity.level().dimensionType().natural())
+        if (entity.level().dimensionType().natural()
+        || ServerConfig.whitelistedDimensions.get().contains(entity.level().dimension().location()))
         {
             ISeasonState season = SeasonHelper.getSeasonState(entity.level());
             double startValue;
