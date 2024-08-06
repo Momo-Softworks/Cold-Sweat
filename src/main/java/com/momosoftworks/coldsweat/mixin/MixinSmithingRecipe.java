764c09c1a89fd7cc70f14006a766d634ea846816
MikulDev <gamer3mc@gmail.com>
2024-08-06 19:38:42 -0400

    Fix smithing mixin

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinSmithingRecipe.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinSmithingRecipe.java
@@ -1,20 +1,19 @@
 package com.momosoftworks.coldsweat.mixin;
 
 import com.momosoftworks.coldsweat.common.capability.ModCapabilities;
-import net.minecraft.core.RegistryAccess;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.SmithingTransformRecipe;
+import net.minecraft.world.item.crafting.UpgradeRecipe;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(SmithingTransformRecipe.class)
+@Mixin(UpgradeRecipe.class)
 public class MixinSmithingRecipe
 {
     @Inject(method = "assemble", at = @At("RETURN"), cancellable = true)
-    public void copyCapabilities(Container container, RegistryAccess registries, CallbackInfoReturnable<ItemStack> cir)
+    public void copyCapabilities(Container container, CallbackInfoReturnable<ItemStack> cir)
     {
         ItemStack result = cir.getReturnValue();
         ItemStack base = container.getItem(1);
