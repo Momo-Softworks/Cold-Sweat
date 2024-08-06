6a7c60a28866c4c5cac7f550a97bdac13dc4c760
MikulDev <gamer3mc@gmail.com>
2024-08-06 13:06:49 -0400

    Fix block temps multiplying every time a world is exited and re-joined
    Took 3 seconds

--- src/main/java/com/momosoftworks/coldsweat/api/registry/BlockTempRegistry.java
+++ src/main/java/com/momosoftworks/coldsweat/api/registry/BlockTempRegistry.java
@@ -58,6 +58,7 @@ public class BlockTempRegistry
     public static void flush()
     {
         MAPPED_BLOCKS.clear();
+        BLOCK_TEMPS.clear();
     }
 
     public static Collection<BlockTemp> getBlockTempsFor(BlockState blockstate)
