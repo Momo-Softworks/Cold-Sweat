756b07fbba927e5d8ae0dcdb2ca3522537ee0126
MikulDev <gamer3mc@gmail.com>
2024-08-13 20:01:53 -0400

    Fix Chill effect using WarmthEffect class
    Took 6 seconds

--- src/main/java/com/momosoftworks/coldsweat/core/init/EffectInit.java
+++ src/main/java/com/momosoftworks/coldsweat/core/init/EffectInit.java
@@ -1,5 +1,6 @@
 package com.momosoftworks.coldsweat.core.init;
 
+import com.momosoftworks.coldsweat.common.effect.ChillEffect;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,7 +14,7 @@ public class EffectInit
 {
     public static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, ColdSweat.MOD_ID);
 
-    public static final RegistryObject<MobEffect> CHILL = EFFECTS.register("chill", WarmthEffect::new);
+    public static final RegistryObject<MobEffect> CHILL = EFFECTS.register("chill", ChillEffect::new);
     public static final RegistryObject<MobEffect> WARMTH = EFFECTS.register("warmth", WarmthEffect::new);
     public static final RegistryObject<MobEffect> GRACE = EFFECTS.register("grace", GraceEffect::new);
     public static final RegistryObject<MobEffect> ICE_RESISTANCE = EFFECTS.register("ice_resistance", IceResistanceEffect::new);
