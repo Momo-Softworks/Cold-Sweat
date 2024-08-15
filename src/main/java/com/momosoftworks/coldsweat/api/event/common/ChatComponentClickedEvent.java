c4919908722ba3588fe4078a5e497beafc15e63e
MikulDev <gamer3mc@gmail.com>
2024-08-15 14:45:52 -0400

    Fix null style in ChatComponentClickedEvent

--- src/main/java/com/momosoftworks/coldsweat/api/event/common/ChatComponentClickedEvent.java
+++ src/main/java/com/momosoftworks/coldsweat/api/event/common/ChatComponentClickedEvent.java
@@ -1,6 +1,5 @@
 package com.momosoftworks.coldsweat.api.event.common;
 
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Style;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.eventbus.api.Event;
@@ -9,11 +8,11 @@ import javax.annotation.Nullable;
 
 public class ChatComponentClickedEvent extends Event
 {
-    private Style style;
-    private Player player;
+    private final Style style;
+    private final Player player;
 
     public ChatComponentClickedEvent(@Nullable Style style, Player player)
-    {   this.style = style;
+    {   this.style = style != null ? style : Style.EMPTY;
         this.player = player;
     }
 
