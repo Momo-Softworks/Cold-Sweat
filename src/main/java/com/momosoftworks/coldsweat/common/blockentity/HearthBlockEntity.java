b5a65d45316441b5d1d9ed8777a0190e239b0196
MikulDev <gamer3mc@gmail.com>
2024-08-18 12:08:27 -0400

    Add visible redstone inputs & fix smart hearth fuel consumption

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
@@ -271,8 +271,8 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity
         this.checkInputSignal();
 
         // Determine what types of fuel to use
-        this.shouldUseColdFuel = false;
-        this.shouldUseHotFuel = false;
+        boolean wasUsingColdFuel = this.shouldUseColdFuel;
+        boolean wasUsingHotFuel = this.shouldUseHotFuel;
         if (!ConfigSettings.SMART_HEARTH.get())
         {
             this.shouldUseColdFuel = this.isSidePowered;
@@ -334,8 +334,14 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity
                 // Give insulation to players
                 if (!isClient && this.ticksExisted % 20 == 0)
                 {
+                    // Reset the usage status for cold/hot fuel
+                    if (ConfigSettings.SMART_HEARTH.get())
+                    {   this.resetFuelStatus();
+                    }
+                    // Provide insulation to players & calculate fuel usage
                     for (int i = 0; i < players.size(); i++)
-                    {   Player player = players.get(i);
+                    {
+                        Player player = players.get(i);
                         if (player != null && pathLookup.contains(player.blockPosition()))
                         {   this.insulatePlayer(player);
                         }
@@ -354,7 +360,8 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity
         }
 
         // Update fuel
-        if (this.isFuelChanged())
+        if (this.isFuelChanged()
+        || (wasUsingColdFuel != this.shouldUseColdFuel || wasUsingHotFuel != this.shouldUseHotFuel))
         {   this.updateFuelState();
         }
 
@@ -486,6 +493,12 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity
             this.isBackPowered = this.hasSignalFromBack();
             this.isSidePowered = this.hasSignalFromSides();
             // Update signals for client
+            if (wasBackPowered != this.isBackPowered)
+            {   level.setBlock(this.getBlockPos(), this.getBlockState().setValue(HearthBottomBlock.BACK_POWERED, this.isBackPowered), 3);
+            }
+            if (wasSidePowered != this.isSidePowered)
+            {   level.setBlock(this.getBlockPos(), this.getBlockState().setValue(HearthBottomBlock.SIDE_POWERED, this.isSidePowered), 3);
+            }
             this.syncInputSignal(wasBackPowered, wasSidePowered);
         }
     }
@@ -599,6 +612,15 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity
         }
     }
 
+    protected void resetFuelStatus()
+    {
+        if (this.level == null || !this.level.isClientSide)
+        {
+            this.shouldUseColdFuel = false;
+            this.shouldUseHotFuel = false;
+        }
+    }
+
     void insulatePlayer(Player player)
     {
         for (int i = 0; i < effects.size(); i++)
@@ -630,31 +652,11 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity
         if (!shouldUseColdFuel || !shouldUseHotFuel)
         EntityTempManager.getTemperatureCap(player).ifPresent(cap ->
         {
-            double temp = cap.getTrait(Temperature.Trait.WORLD);
+            double temp = CSMath.getIfNotNull(Temperature.getModifier(cap, Temperature.Trait.WORLD, BlockInsulationTempModifier.class).orElse(null),
+                                              TempModifier::getLastInput,
+                                              cap.getTrait(Temperature.Trait.WORLD));
             double min = cap.getTrait(Temperature.Trait.FREEZING_POINT);
             double max = cap.getTrait(Temperature.Trait.BURNING_POINT);
-
-            // If the player is habitable, check the input temperature reported by their HearthTempModifier (if they have one)
-            if (CSMath.betweenInclusive(temp, min, max))
-            {
-                // Find the player's HearthTempModifier
-                TempModifier modifier = null;
-                for (TempModifier tempModifier : cap.getModifiers(Temperature.Trait.WORLD))
-                {   if (tempModifier instanceof BlockInsulationTempModifier)
-                    {   modifier = tempModifier;
-                        break;
-                    }
-                }
-                // If they have one, refresh it
-                if (modifier != null)
-                {   if (modifier.getExpireTime() - modifier.getTicksExisted() > 20)
-                    {   return;
-                    }
-                    temp = modifier.getLastInput();
-                }
-                // This means the player is not insulated, and they are habitable without it
-                else return;
-            }
 
             // Tell the hearth to use hot fuel
             shouldUseHotFuel |= this.getHotFuel() > 0 && temp < min;
@@ -983,6 +985,8 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity
         CompoundTag tag = super.getUpdateTag();
         tag.putInt("HotFuel",  this.getHotFuel());
         tag.putInt("ColdFuel", this.getColdFuel());
+        tag.putBoolean("ShouldUseColdFuel", this.shouldUseColdFuel);
+        tag.putBoolean("ShouldUseHotFuel", this.shouldUseHotFuel);
         tag.putInt("InsulationLevel", insulationLevel);
         this.saveEffects(tag);
 
@@ -993,6 +997,8 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity
     public void handleUpdateTag(CompoundTag tag)
     {   this.setHotFuel(tag.getInt("HotFuel"), false);
         this.setColdFuel(tag.getInt("ColdFuel"), false);
+        this.shouldUseColdFuel = tag.getBoolean("ShouldUseColdFuel");
+        this.shouldUseHotFuel = tag.getBoolean("ShouldUseHotFuel");
         this.updateFuelState();
         this.insulationLevel = tag.getInt("InsulationLevel");
         this.loadEffects(tag);
