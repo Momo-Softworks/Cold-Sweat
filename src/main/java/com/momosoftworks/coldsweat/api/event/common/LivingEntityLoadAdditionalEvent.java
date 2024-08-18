5e336232f147eac70380193640a469629ce5d0dc
MikulDev <gamer3mc@gmail.com>
2024-08-18 12:55:11 -0400

    Add attribute resetter for updating from 2.2
    Took 1 minute
    
    Took 2 minutes

---
+++ src/main/java/com/momosoftworks/coldsweat/api/event/common/LivingEntityLoadAdditionalEvent.java
@@ -0,0 +1,25 @@
+package com.momosoftworks.coldsweat.api.event.common;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.eventbus.api.Event;
+
+public class LivingEntityLoadAdditionalEvent extends Event
+{
+    private final LivingEntity entity;
+    private final CompoundTag nbt;
+
+    public LivingEntityLoadAdditionalEvent(LivingEntity entity, CompoundTag nbt)
+    {
+        this.entity = entity;
+        this.nbt = nbt;
+    }
+
+    public LivingEntity getEntity()
+    {   return entity;
+    }
+
+    public CompoundTag getNBT()
+    {   return nbt;
+    }
+}
