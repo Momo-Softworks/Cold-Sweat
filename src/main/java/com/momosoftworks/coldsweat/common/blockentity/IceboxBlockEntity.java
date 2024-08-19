8462e7cd2a267adec7172180be17861815c35aac
MikulDev <gamer3mc@gmail.com>
2024-08-18 23:16:16 -0400

    Add bottom power option for hearth-like blocks

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/IceboxBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/IceboxBlockEntity.java
@@ -232,7 +232,7 @@ public class IceboxBlockEntity extends HearthBlockEntity implements MenuProvider
 
     @Override
     protected boolean hasSignalFromSides()
-    {   return Direction.stream().anyMatch(dir -> dir.getAxis() != Direction.Axis.Y && this.level.hasSignal(this.getBlockPos().relative(dir), dir));
+    {   return Direction.stream().anyMatch(dir -> dir != Direction.UP && this.level.hasSignal(this.getBlockPos().relative(dir), dir));
     }
 
     @Override
