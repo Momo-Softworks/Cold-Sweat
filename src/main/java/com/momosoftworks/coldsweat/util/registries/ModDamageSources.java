c2715eb2186ca30f87b308fbb80a773b3b27fd1b
MikulDev <gamer3mc@gmail.com>
2024-08-18 21:28:44 -0400

    Remove scaled damage sources

--- src/main/java/com/momosoftworks/coldsweat/util/registries/ModDamageSources.java
+++ src/main/java/com/momosoftworks/coldsweat/util/registries/ModDamageSources.java
@@ -12,16 +12,14 @@ public class ModDamageSources
     private ModDamageSources() {}
 
     public static final ResourceKey<DamageType> COLD = register("cold");
-    public static final ResourceKey<DamageType> COLD_SCALING = register("cold_scaling");
     public static final ResourceKey<DamageType> HOT = register("hot");
-    public static final ResourceKey<DamageType> HOT_SCALING  = register("hot_scaling");
 
     public static boolean isFreezing(DamageSource damageSource)
-    {   return damageSource.is(COLD) || damageSource.is(COLD_SCALING);
+    {   return damageSource.is(COLD);
     }
     
     public static boolean isBurning(DamageSource damageSource)
-    {   return damageSource.is(HOT) || damageSource.is(HOT_SCALING);
+    {   return damageSource.is(HOT);
     }
 
     private static ResourceKey<DamageType> register(String name)
