4835bf5934223c372700adcd1cc22d5f2b00da61
MikulDev <gamer3mc@gmail.com>
2024-08-19 23:29:36 -0400

    Allow trident riptide when wet
    Took 2 minutes
    
    Took 4 minutes

---
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinTridentRiptide.java
@@ -0,0 +1,36 @@
+package com.momosoftworks.coldsweat.mixin;
+
+import com.momosoftworks.coldsweat.api.temperature.modifier.WaterTempModifier;
+import com.momosoftworks.coldsweat.api.util.Temperature;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.TridentItem;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TridentItem.class)
+public class MixinTridentRiptide
+{
+    @Redirect(method = "releaseUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isInWaterOrRain()Z"))
+    private boolean allowRiptideWhenWet(PlayerEntity player)
+    {   return player.isInWaterOrRain() || Temperature.getModifier(player, Temperature.Trait.WORLD, WaterTempModifier.class).isPresent();
+    }
+
+    @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isInWaterOrRain()Z"))
+    private boolean allowUseWhenWet(PlayerEntity player)
+    {   return player.isInWaterOrRain() || Temperature.getModifier(player, Temperature.Trait.WORLD, WaterTempModifier.class).isPresent();
+    }
+
+    @Inject(method = "releaseUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;startAutoSpinAttack(I)V"))
+    private void removeWetnessOnUse(ItemStack stack, World level, LivingEntity entity, int timeLeft, CallbackInfo ci)
+    {
+        if (!entity.isInWaterOrBubble())
+        {   Temperature.removeModifiers(entity, Temperature.Trait.WORLD, mod -> mod instanceof WaterTempModifier);
+        }
+    }
+}
