18277bf5fcb059982978d62cc2b245c73d2d7419
MikulDev <gamer3mc@gmail.com>
2024-08-19 23:29:36 -0400

    Allow trident riptide when wet

---
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinTridentRiptide.java
@@ -0,0 +1,36 @@
+package com.momosoftworks.coldsweat.mixin;
+
+import com.momosoftworks.coldsweat.api.temperature.modifier.WaterTempModifier;
+import com.momosoftworks.coldsweat.api.util.Temperature;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TridentItem;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TridentItem.class)
+public class MixinTridentRiptide
+{
+    @Redirect(method = "releaseUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;isInWaterOrRain()Z"))
+    private boolean allowRiptideWhenWet(Player player)
+    {   return player.isInWaterOrRain() || Temperature.getModifier(player, Temperature.Trait.WORLD, WaterTempModifier.class).isPresent();
+    }
+
+    @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;isInWaterOrRain()Z"))
+    private boolean allowUseWhenWet(Player player)
+    {   return player.isInWaterOrRain() || Temperature.getModifier(player, Temperature.Trait.WORLD, WaterTempModifier.class).isPresent();
+    }
+
+    @Inject(method = "releaseUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;startAutoSpinAttack(IFLnet/minecraft/world/item/ItemStack;)V"))
+    private void removeWetnessOnUse(ItemStack stack, Level level, LivingEntity entity, int timeLeft, CallbackInfo ci)
+    {
+        if (!entity.isInWaterOrBubble())
+        {   Temperature.removeModifiers(entity, Temperature.Trait.WORLD, mod -> mod instanceof WaterTempModifier);
+        }
+    }
+}
