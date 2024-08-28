2e6b216acf3c3c28f6fc18b5316c7472825e7587
MikulDev <gamer3mc@gmail.com>
2024-08-28 00:59:08 -0400

    Port issues

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/block_temp/IceBlockTemp.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/block_temp/IceBlockTemp.java
@@ -3,13 +3,15 @@ package com.momosoftworks.coldsweat.api.temperature.block_temp;
 import com.momosoftworks.coldsweat.util.math.CSMath;
 import com.momosoftworks.coldsweat.util.serialization.ObjectBuilder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +23,7 @@ public class IceBlockTemp extends BlockTemp
         super(ObjectBuilder.build(() ->
         {
             List<Block> blocks = new ArrayList<>(List.of(Blocks.ICE, Blocks.PACKED_ICE, Blocks.BLUE_ICE));
-            blocks.addAll(ForgeRegistries.BLOCKS.tags().getTag(BlockTags.ICE).stream().toList());
+            blocks.addAll(BuiltInRegistries.BLOCK.getTag(BlockTags.ICE).stream().flatMap(HolderSet::stream).map(Holder::value).toList());
             return blocks.stream().distinct().toArray(Block[]::new);
         }));
     }
