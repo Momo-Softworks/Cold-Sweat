a2ed3e6e544ab90041a1298f90bc4e63007088f9
MikulDev <gamer3mc@gmail.com>
2024-09-01 19:57:53 -0400

    Fix tag not working for BlockTempData

--- src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/BlockTempData.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/BlockTempData.java
@@ -3,6 +3,7 @@ package com.momosoftworks.coldsweat.data.codec.configuration;
 import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.tags.TagKey;
@@ -17,7 +18,7 @@ public record BlockTempData(List<Either<TagKey<Block>, Block>> blocks, double te
                             BlockPredicate condition, Optional<CompoundTag> nbt, Optional<List<String>> requiredMods)
 {
     public static final Codec<BlockTempData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            Codec.either(TagKey.codec(Registries.BLOCK), ForgeRegistries.BLOCKS.getCodec()).listOf().fieldOf("blocks").forGetter(BlockTempData::blocks),
+            ConfigHelper.tagOrRegistryObjectCodec(Registries.BLOCK, ForgeRegistries.BLOCKS).listOf().fieldOf("blocks").forGetter(BlockTempData::blocks),
             Codec.DOUBLE.fieldOf("temperature").forGetter(BlockTempData::temperature),
             Codec.DOUBLE.optionalFieldOf("max_effect", Double.MAX_VALUE).forGetter(BlockTempData::maxEffect),
             Codec.DOUBLE.optionalFieldOf("range", Double.MAX_VALUE).forGetter(BlockTempData::range),
