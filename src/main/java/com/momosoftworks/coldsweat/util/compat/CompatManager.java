87455b436f547afd72b6c4e2ba5d6d0c3f582925
MikulDev <gamer3mc@gmail.com>
2024-09-01 23:48:59 -0400

    Better version detection for compat managing

--- src/main/java/com/momosoftworks/coldsweat/util/compat/CompatManager.java
+++ src/main/java/com/momosoftworks/coldsweat/util/compat/CompatManager.java
@@ -25,7 +25,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
-import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.jwaresoftware.mcmods.lib.Armory;
 import sereneseasons.season.SeasonHooks;
 import top.theillusivec4.curios.api.CuriosCapability;
@@ -46,7 +46,7 @@ public class CompatManager
     private static final boolean SPIRIT_LOADED = modLoaded("spirit");
     private static final boolean ARMOR_UNDERWEAR_LOADED = modLoaded("armorunder");
     private static final boolean BYG_LOADED = modLoaded("byg");
-    private static final boolean CREATE_LOADED = modLoaded("create", 0, 5, 1);
+    private static final boolean CREATE_LOADED = modLoaded("create", "0.5.1");
     private static final boolean ATMOSPHERIC_LOADED = modLoaded("atmospheric");
     private static final boolean ENVIRONMENTAL_LOADED = modLoaded("environmental");
     private static final boolean TERRALITH_LOADED = modLoaded("terralith");
@@ -56,27 +56,24 @@ public class CompatManager
     private static final boolean CAVES_AND_CLIFFS_LOADED = modLoaded("cavesandcliffs");
     private static final boolean TOOLTIPS_LOADED = modLoaded("legendarytooltips");
     private static final boolean PRIMAL_WINTER_LOADED = modLoaded("primalwinter");
-    private static final boolean THIRST_LOADED = modLoaded("thirst");
+    private static final boolean THIRST_LOADED = modLoaded("thirst", "1.16.5-1.3.8");
     private static final boolean ICEBERG_LOADED = modLoaded("iceberg");
     private static final boolean SPOILED_LOADED = modLoaded("spoiled");
     private static final boolean SUPPLEMENTARIES_LOADED = modLoaded("supplementaries");
 
-    public static boolean modLoaded(String modID, int minMajorVer, int minMinorVer, int minPatchVer)
+    public static boolean modLoaded(String modID, String version)
     {
         ModContainer mod = ModList.get().getModContainerById(modID).orElse(null);
-        if (mod == null) return false;
-
-        if (minMajorVer > 0 || minMinorVer > 0 || minPatchVer > 0)
+        if (mod == null)
+        {   return false;
+        }
+        if (!version.isEmpty())
         {
-            ArtifactVersion version = mod.getModInfo().getVersion();
-            if (version.getMajorVersion() >= minMajorVer
-            &&  version.getMinorVersion() >= minMinorVer
-            &&  version.getIncrementalVersion() >= minPatchVer)
+            if (mod.getModInfo().getVersion().compareTo(new DefaultArtifactVersion(version)) >= 0)
             {   return true;
             }
             else
-            {   ColdSweat.LOGGER.error("Cold Sweat requires {} {} or higher for compat to be enabled!", modID.substring(0, 1).toUpperCase() + modID.substring(1),
-                                                                                                        minMajorVer + "." + minMinorVer + "." + minPatchVer);
+            {   ColdSweat.LOGGER.error("Cold Sweat requires {} {} or higher for compat to be enabled!", modID, version);
                 return false;
             }
         }
@@ -84,7 +81,7 @@ public class CompatManager
     }
 
     public static boolean modLoaded(String modID)
-    {   return modLoaded(modID, 0, 0, 0);
+    {   return modLoaded(modID, "");
     }
 
     public static boolean isBiomesOPlentyLoaded()
