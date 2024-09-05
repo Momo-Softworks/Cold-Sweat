4d9578a4c4d99fd5916f7e45cc56b9291652159b
MikulDev <gamer3mc@gmail.com>
2024-09-05 19:53:41 -0400

    Add tooltip to locked config settings for non-OP players
    Took 2 seconds

--- src/main/java/com/momosoftworks/coldsweat/client/gui/config/AbstractConfigPage.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/config/AbstractConfigPage.java
@@ -2,6 +2,7 @@ package com.momosoftworks.coldsweat.client.gui.config;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.datafixers.util.Pair;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.util.math.CSMath;
 import net.minecraft.ChatFormatting;
@@ -48,7 +49,7 @@ public abstract class AbstractConfigPage extends Screen
 
     private final Screen parentScreen;
 
-    public Map<String, List<GuiEventListener>> widgetBatches = new HashMap<>();
+    public Map<String, Pair<List<GuiEventListener>, Boolean>> widgetBatches = new HashMap<>();
     public Map<String, List<Component>> tooltips = new HashMap<>();
 
     protected int rightSideLength = 0;
@@ -97,7 +98,7 @@ public abstract class AbstractConfigPage extends Screen
         int labelY = this.height / 4 + (side == Side.LEFT ? leftSideLength : rightSideLength);
         ConfigLabel label = new ConfigLabel(id, text, labelX, labelY, color);
 
-        this.addWidgetBatch(id, List.of(label));
+        this.addWidgetBatch(id, List.of(label), true);
 
         if (side == Side.LEFT)
         {   this.leftSideLength += font.lineHeight + 4;
@@ -162,7 +163,7 @@ public abstract class AbstractConfigPage extends Screen
         // Assign the tooltip
         this.setTooltip(id, tooltipList);
 
-        this.addWidgetBatch(id, List.of(button));
+        this.addWidgetBatch(id, List.of(button), shouldBeActive);
 
         // Mark this space as used
         if (side == Side.LEFT)
@@ -255,7 +256,7 @@ public abstract class AbstractConfigPage extends Screen
         this.setTooltip(id, tooltipList);
 
         // Add the widget
-        this.addWidgetBatch(id, List.of(textBox, configLabel));
+        this.addWidgetBatch(id, List.of(textBox, configLabel), shouldBeActive);
 
         // Mark this space as used
         if (side == Side.LEFT)
@@ -378,7 +379,7 @@ public abstract class AbstractConfigPage extends Screen
         // Assign the tooltip
         this.setTooltip(id, tooltipList);
 
-        this.addWidgetBatch(id, widgetBatch);
+        this.addWidgetBatch(id, widgetBatch, shouldBeActive);
 
         // Add height to the list
         if (side == Side.LEFT)
@@ -432,7 +433,7 @@ public abstract class AbstractConfigPage extends Screen
         this.setTooltip(id, tooltipList);
 
         // Add the widget
-        this.addWidgetBatch(id, List.of(sliderButton));
+        this.addWidgetBatch(id, List.of(sliderButton), shouldBeActive);
 
         // Mark this space as used
         if (side == Side.LEFT)
@@ -510,10 +511,11 @@ public abstract class AbstractConfigPage extends Screen
         }
         if (MOUSE_STILL_TIMER < TOOLTIP_DELAY) return;
 
-        for (Map.Entry<String, List<GuiEventListener>> entry : widgetBatches.entrySet())
+        for (Map.Entry<String, Pair<List<GuiEventListener>, Boolean>> entry : widgetBatches.entrySet())
         {
             String id = entry.getKey();
-            List<GuiEventListener> widgets = entry.getValue();
+            List<GuiEventListener> widgets = entry.getValue().getFirst();
+            boolean enabled = entry.getValue().getSecond();
             int minX = 0, minY = 0, maxX = 0, maxY = 0;
             for (GuiEventListener listener : widgets)
             {
@@ -533,7 +535,9 @@ public abstract class AbstractConfigPage extends Screen
             // if the mouse is hovering over any of the widgets in the batch, show the corresponding tooltip
             if (CSMath.betweenInclusive(mouseX, minX, maxX) && CSMath.betweenInclusive(mouseY, minY, maxY))
             {
-                List<Component> tooltipList = this.tooltips.get(id);
+                List<Component> tooltipList = enabled
+                                              ? this.tooltips.get(id)
+                                              : List.of(Component.translatable("cold_sweat.config.require_op").withStyle(ChatFormatting.RED));
                 if (tooltipList != null && !tooltipList.isEmpty())
                 {   this.renderComponentTooltip(poseStack, tooltipList, mouseX, mouseY);
                 }
@@ -563,19 +567,19 @@ public abstract class AbstractConfigPage extends Screen
         RIGHT
     }
 
-    protected void addWidgetBatch(String id, List<GuiEventListener> elements)
+    protected void addWidgetBatch(String id, List<GuiEventListener> elements, boolean enabled)
     {
         for (GuiEventListener element : elements)
         {
             if (element instanceof Widget && element instanceof NarratableEntry widget)
                 this.addRenderableWidget((GuiEventListener & Widget & NarratableEntry) widget);
         }
-        this.widgetBatches.put(id, elements);
+        this.widgetBatches.put(id, Pair.of(elements, enabled));
     }
 
     public List<GuiEventListener> getWidgetBatch(String id)
     {
-        return this.widgetBatches.get(id);
+        return this.widgetBatches.get(id).getFirst();
     }
 
     protected void setTooltip(String id, List<Component> tooltip)
--- src/main/java/com/momosoftworks/coldsweat/client/gui/config/pages/ConfigPageOne.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/config/pages/ConfigPageOne.java
@@ -65,10 +65,10 @@ public class ConfigPageOne extends AbstractConfigPage
             properUnits[0] = ConfigSettings.CELSIUS.get() ? Temperature.Units.C : Temperature.Units.F;
 
             // Change the max & min temps to reflect the new setting
-            ((EditBox) this.widgetBatches.get("max_temp").get(0)).setValue(String.valueOf(ConfigScreen.TWO_PLACES.format(
+            ((EditBox) this.getWidgetBatch("max_temp").get(0)).setValue(String.valueOf(ConfigScreen.TWO_PLACES.format(
                     Temperature.convert(ConfigSettings.MAX_TEMP.get(), Temperature.Units.MC, properUnits[0], true))));
 
-            ((EditBox) this.widgetBatches.get("min_temp").get(0)).setValue(String.valueOf(ConfigScreen.TWO_PLACES.format(
+            ((EditBox) this.getWidgetBatch("min_temp").get(0)).setValue(String.valueOf(ConfigScreen.TWO_PLACES.format(
                     Temperature.convert(ConfigSettings.MIN_TEMP.get(), Temperature.Units.MC, properUnits[0], true))));
         }, false, false, true, new TranslatableComponent("cold_sweat.config.units.desc"));
 
