680d064096f32f1a9c5a7230ecb4ecd8cd005828
MikulDev <gamer3mc@gmail.com>
2024-09-05 12:04:40 -0400

    Use less samples for non-players in UndergroundTempModifier
    Took 2 seconds

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/UndergroundTempModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/UndergroundTempModifier.java
@@ -19,6 +19,14 @@ import java.util.function.Function;
 
 public class UndergroundTempModifier extends TempModifier
 {
+    public UndergroundTempModifier()
+    {   this(49);
+    }
+
+    public UndergroundTempModifier(int samples)
+    {   this.getNBT().putInt("Samples", samples);
+    }
+
     @Override
     public Function<Double, Double> calculate(LivingEntity entity, Temperature.Trait trait)
     {
@@ -32,7 +40,7 @@ public class UndergroundTempModifier extends TempModifier
         List<Pair<BlockPos, Double>> depthTable = new ArrayList<>();
 
         // Collect a list of depths taken at regular intervals around the entity, and their distances from the player
-        for (BlockPos pos : WorldHelper.getPositionGrid(entity.blockPosition(), 49, 10))
+        for (BlockPos pos : WorldHelper.getPositionGrid(entity.blockPosition(), this.getNBT().getInt("Samples"), 10))
         {
             if (!level.isInWorldBounds(pos))
             {   continue;
