3dda115be0c9c9052e32ae5cb5b49e246392938b
MikulDev <gamer3mc@gmail.com>
2024-09-23 20:44:43 -0400

    Add powered indicator to hearth-like block screens

--- src/main/java/com/momosoftworks/coldsweat/client/gui/IceboxScreen.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/IceboxScreen.java
@@ -2,6 +2,8 @@ package com.momosoftworks.coldsweat.client.gui;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.momosoftworks.coldsweat.common.blockentity.HearthBlockEntity;
+import com.momosoftworks.coldsweat.config.ConfigSettings;
+import com.momosoftworks.coldsweat.util.math.CSMath;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
@@ -51,6 +53,17 @@ public class IceboxScreen extends AbstractHearthScreen<IceboxContainer>
         graphics.blit(ICEBOX_GUI, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
 
         // Draw fuel gauge
-        graphics.blit(ICEBOX_GUI, leftPos + 109, topPos + 63, 176, 0, (int) (menu.te.getFuel() / 31.25), 16, 256, 256);
+        graphics.blit(ICEBOX_GUI, leftPos + 109, topPos + 63, 176, 0, (int) (menu.te.getFuel() / 31.25), 14, 256, 256);
+
+        if (!ConfigSettings.SMART_HEARTH.get() && this.menu.te.hasSmokeStack())
+        {
+            boolean powered = this.menu.te.isSidePowered();
+
+            graphics.blit(ICEBOX_GUI, leftPos + 117, topPos + 78, 176, powered ? 14 : 22, 14, 8, 256, 256);
+
+            if (CSMath.betweenInclusive(mouseX, leftPos + 117, leftPos + 131) && CSMath.betweenInclusive(mouseY, topPos + 78, topPos + 86))
+            {   this.setTooltipForNextRenderPass(Component.translatable(powered ? "gui.cold_sweat.hearth.powered" : "gui.cold_sweat.hearth.unpowered"));
+            }
+        }
     }
 }
