ac458910f5b76aed0e8c9efc8ed7e7b6c750c491
MikulDev <gamer3mc@gmail.com>
2024-09-23 20:44:43 -0400

    Add powered indicator to hearth-like block screens

--- src/main/java/com/momosoftworks/coldsweat/client/gui/HearthScreen.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/HearthScreen.java
@@ -5,9 +5,13 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.common.blockentity.HearthBlockEntity;
 import com.momosoftworks.coldsweat.common.container.HearthContainer;
+import com.momosoftworks.coldsweat.config.ConfigSettings;
+import com.momosoftworks.coldsweat.util.math.CSMath;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+
+import java.util.List;
 
 public class HearthScreen extends AbstractHearthScreen<HearthContainer>
 {
@@ -37,5 +41,22 @@ public class HearthScreen extends AbstractHearthScreen<HearthContainer>
         // Render hot/cold fuel gauges
         blit(poseStack, leftPos + 61,  topPos + 66 - hotFuel,  176, 36 - hotFuel,  12, hotFuel, 256, 256);
         blit(poseStack, leftPos + 103, topPos + 66 - coldFuel, 188, 36 - coldFuel, 12, coldFuel, 256, 256);
+
+        // Render redstone indicators
+        if (!ConfigSettings.SMART_HEARTH.get())
+        {
+            boolean sidePowered = this.menu.te.isSidePowered();
+            boolean backPowered = this.menu.te.isBackPowered();
+
+            blit(poseStack, leftPos + 60, topPos + 21, 176, backPowered ? 60 : 68, 14, 8, 256, 256);
+            blit(poseStack, leftPos + 102, topPos + 21, 190, sidePowered ? 60 : 68, 14, 8, 256, 256);
+
+            if (CSMath.betweenInclusive(mouseX, leftPos + 60, leftPos + 74) && CSMath.betweenInclusive(mouseY, topPos + 21, topPos + 29))
+            {   this.renderComponentTooltip(poseStack, List.of(Component.translatable(backPowered ? "gui.cold_sweat.hearth.powered" : "gui.cold_sweat.hearth.unpowered")), mouseX, mouseY);;
+            }
+            if (CSMath.betweenInclusive(mouseX, leftPos + 102, leftPos + 116) && CSMath.betweenInclusive(mouseY, topPos + 21, topPos + 29))
+            {   this.renderComponentTooltip(poseStack, List.of(Component.translatable(sidePowered ? "gui.cold_sweat.hearth.powered" : "gui.cold_sweat.hearth.unpowered")), mouseX, mouseY);
+            }
+        }
     }
 }
