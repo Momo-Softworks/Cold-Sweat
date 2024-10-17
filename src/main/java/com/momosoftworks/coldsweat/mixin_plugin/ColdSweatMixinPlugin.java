0e2c34069ff2e66a81f62f58d1147fcf66d2aa53
MikulDev <gamer3mc@gmail.com>
2024-10-17 17:55:48 -0400

    Make mixin load checks actually trigger

--- src/main/java/com/momosoftworks/coldsweat/mixin_plugin/ColdSweatMixinPlugin.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin_plugin/ColdSweatMixinPlugin.java
@@ -1,7 +1,7 @@
 package com.momosoftworks.coldsweat.mixin_plugin;
 
 import com.google.common.collect.ImmutableMap;
-import com.momosoftworks.coldsweat.util.compat.CompatManager;
+import net.neoforged.fml.loading.FMLLoader;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -13,12 +13,15 @@ import java.util.function.Supplier;
 
 public class ColdSweatMixinPlugin implements IMixinConfigPlugin
 {
-    private static final String COMPAT_MIXIN_PACKAGE = "com.momosoftworks.coldsweat.mixin.compat";
+    private static final String MIXIN_PACKAGE = "com.momosoftworks.coldsweat.mixin.";
+    private static final String COMPAT_MIXIN_PACKAGE = "com.momosoftworks.coldsweat.mixin.compat.";
+
+
     private static final Map<String, Supplier<Boolean>> CONDITIONS = ImmutableMap.of(
-            "MixinCreateOverlay", () -> CompatManager.isCreateLoaded(),
-            "MixinCreateConnect", () -> CompatManager.isCreateLoaded(),
-            "MixinSpoiledIcebox", () -> CompatManager.isSpoiledLoaded(),
-            "MixinSereneIceMelt", () -> CompatManager.isSereneSeasonsLoaded()
+            COMPAT_MIXIN_PACKAGE + "MixinCreateOverlay", () -> modLoaded("create"),
+            COMPAT_MIXIN_PACKAGE + "MixinCreateConnect", () -> modLoaded("create"),
+            COMPAT_MIXIN_PACKAGE + "MixinSpoiledIcebox", () -> modLoaded("spoiled"),
+            COMPAT_MIXIN_PACKAGE + "MixinSereneIceMelt", () -> modLoaded("sereneseasons")
     );
 
     @Override
@@ -36,7 +39,7 @@ public class ColdSweatMixinPlugin implements IMixinConfigPlugin
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName)
     {
-        return CONDITIONS.getOrDefault(COMPAT_MIXIN_PACKAGE + mixinClassName, () -> true).get();
+        return CONDITIONS.getOrDefault(mixinClassName, () -> true).get();
     }
 
     @Override
@@ -61,5 +64,9 @@ public class ColdSweatMixinPlugin implements IMixinConfigPlugin
     public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo)
     {
 
+    }
+
+    public static boolean modLoaded(String modId)
+    {   return FMLLoader.getLoadingModList().getModFileById(modId) != null;
     }
 }
