MikulGamour/Cold-Sweat-Rework-Forge

Add entity temp, spawn biome, & mount configs to KubeJS

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/EntityTempBuilderJS.java
@@ -1,0 +1,86 @@
+package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
+
+import com.momosoftworks.coldsweat.api.util.Temperature;
+import com.momosoftworks.coldsweat.data.codec.configuration.EntityTempData;
+import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
+import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class EntityTempBuilderJS
+{
+    public final Set<EntityType<?>> entities = new HashSet<>();
+    public double temperature = 0;
+    public double range = 0;
+    public Temperature.Units units = Temperature.Units.MC;
+    public double maxEffect = 0;
+    public Predicate<Entity> entityPredicate = null;
+    public Predicate<Entity> otherEntityPredicate = null;
+    public boolean affectsSelf = false;
+
+    public EntityTempBuilderJS()
+    {}
+
+    public EntityTempBuilderJS entities(String... entities)
+    {
+        this.entities.addAll(RegistryHelper.mapBuiltinRegistryTagList(BuiltInRegistries.ENTITY_TYPE, ConfigHelper.getEntityTypes(entities)));
+        return this;
+    }
+
+    public EntityTempBuilderJS temperature(double temperature)
+    {
+        this.temperature = temperature;
+        return this;
+    }
+
+    public EntityTempBuilderJS range(double range)
+    {
+        this.range = range;
+        return this;
+    }
+
+    public EntityTempBuilderJS units(String units)
+    {
+        this.units = Temperature.Units.fromID(units);
+        return this;
+    }
+
+    public EntityTempBuilderJS maxEffect(double maxEffect)
+    {
+        this.maxEffect = maxEffect;
+        return this;
+    }
+
+    public EntityTempBuilderJS entityPredicate(Predicate<Entity> entityPredicate)
+    {
+        this.entityPredicate = entityPredicate;
+        return this;
+    }
+
+    public EntityTempBuilderJS otherEntityPredicate(Predicate<Entity> otherEntityPredicate)
+    {
+        this.otherEntityPredicate = otherEntityPredicate;
+        return this;
+    }
+
+    public EntityTempBuilderJS affectsSelf(boolean affectsSelf)
+    {
+        this.affectsSelf = affectsSelf;
+        return this;
+    }
+
+    public EntityTempData build()
+    {
+        EntityTempData data = new EntityTempData(new EntityRequirement(this.entities, this.entityPredicate), this.temperature, this.range, this.units,
+                                                 new EntityRequirement(this.otherEntityPredicate), this.maxEffect, this.affectsSelf);
+        data.setType(ConfigData.Type.KUBEJS);
+        return data;
+    }
+}

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/InsulatingMountBuilderJS.java
@@ -1,0 +1,63 @@
+package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
+
+import com.momosoftworks.coldsweat.data.codec.configuration.MountData;
+import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
+import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class InsulatingMountBuilderJS
+{
+    public final Set<EntityType<?>> entities = new HashSet<>();
+    public Predicate<Entity> entityPredicate = null;
+    public Predicate<Entity> riderPredicate = null;
+    public double coldInsulation = 0;
+    public double heatInsulation = 0;
+
+    public InsulatingMountBuilderJS()
+    {}
+
+    public InsulatingMountBuilderJS entities(String... entities)
+    {
+        this.entities.addAll(RegistryHelper.mapBuiltinRegistryTagList(BuiltInRegistries.ENTITY_TYPE, ConfigHelper.getEntityTypes(entities)));
+        return this;
+    }
+
+    public InsulatingMountBuilderJS entityPredicate(Predicate<Entity> entityPredicate)
+    {
+        this.entityPredicate = entityPredicate;
+        return this;
+    }
+
+    public InsulatingMountBuilderJS riderPredicate(Predicate<Entity> riderPredicate)
+    {
+        this.riderPredicate = riderPredicate;
+        return this;
+    }
+
+    public InsulatingMountBuilderJS coldInsulation(double coldInsulation)
+    {
+        this.coldInsulation = coldInsulation;
+        return this;
+    }
+
+    public InsulatingMountBuilderJS heatInsulation(double heatInsulation)
+    {
+        this.heatInsulation = heatInsulation;
+        return this;
+    }
+
+    public MountData build()
+    {
+        MountData data = new MountData(new EntityRequirement(this.entities, this.entityPredicate), new EntityRequirement(this.riderPredicate), this.coldInsulation, this.heatInsulation);
+        data.setType(ConfigData.Type.KUBEJS);
+        return data;
+    }
+}

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/SpawnBiomeBuilderJS.java
@@ -1,0 +1,95 @@
+package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
+
+import com.mojang.datafixers.util.Either;
+import com.momosoftworks.coldsweat.ColdSweat;
+import com.momosoftworks.coldsweat.data.codec.configuration.SpawnBiomeData;
+import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
+import com.momosoftworks.coldsweat.data.codec.requirement.LocationRequirement;
+import com.momosoftworks.coldsweat.data.codec.util.IntegerBounds;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
+import net.minecraft.core.Holder;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.state.pattern.BlockInWorld;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class SpawnBiomeBuilderJS
+{
+    public final Set<Holder<Biome>> biomes = new HashSet<>();
+    public MobCategory category;
+    public int weight;
+    public final Set<EntityType<?>> entities = new HashSet<>();
+    public IntegerBounds count;
+    public Predicate<BlockInWorld> locationPredicate = null;
+
+    public SpawnBiomeBuilderJS()
+    {}
+
+    public SpawnBiomeBuilderJS biomes(String... biomes)
+    {
+        RegistryAccess registryAccess = RegistryHelper.getRegistryAccess();
+        if (registryAccess == null)
+        {   ColdSweat.LOGGER.error("Could not parse biomes for addSpawnBiomes(), as registries are not built yet.");
+            return this;
+        }
+        List<Either<TagKey<Biome>, Holder<Biome>>> biomesList = ConfigHelper.parseRegistryItems(Registries.BIOME, registryAccess, biomes);
+        this.biomes.addAll(RegistryHelper.mapRegistryTagList(Registries.BIOME, biomesList, registryAccess));
+        return this;
+    }
+
+    public SpawnBiomeBuilderJS category(String category)
+    {
+        for (MobCategory mobCategory : MobCategory.values())
+        {
+            if (mobCategory.getName().equals(category))
+            {
+                this.category = mobCategory;
+                return this;
+            }
+        }
+        return this;
+    }
+
+    public SpawnBiomeBuilderJS weight(int weight)
+    {
+        this.weight = weight;
+        return this;
+    }
+
+    public SpawnBiomeBuilderJS entities(String... entities)
+    {
+        this.entities.addAll(RegistryHelper.mapBuiltinRegistryTagList(BuiltInRegistries.ENTITY_TYPE, ConfigHelper.getEntityTypes(entities)));
+        return this;
+    }
+
+    public SpawnBiomeBuilderJS count(int min, int max)
+    {
+        this.count = new IntegerBounds(min, max);
+        return this;
+    }
+
+    public SpawnBiomeBuilderJS locationPredicate(Predicate<BlockInWorld> locationPredicate)
+    {
+        this.locationPredicate = locationPredicate;
+        return this;
+    }
+
+    public SpawnBiomeData build()
+    {
+        SpawnBiomeData data = new SpawnBiomeData(biomes, category, weight,
+                                                 entities,
+                                                 count, new LocationRequirement(locationPredicate), LocationRequirement.NONE);
+        data.setType(ConfigData.Type.KUBEJS);
+        return data;
+    }
+}

Momo-Softworks/Cold-Sweat

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/EntityTempBuilderJS.java
@@ -1,0 +1,86 @@
+package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
+
+import com.momosoftworks.coldsweat.api.util.Temperature;
+import com.momosoftworks.coldsweat.data.codec.configuration.EntityTempData;
+import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
+import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class EntityTempBuilderJS
+{
+    public final Set<EntityType<?>> entities = new HashSet<>();
+    public double temperature = 0;
+    public double range = 0;
+    public Temperature.Units units = Temperature.Units.MC;
+    public double maxEffect = 0;
+    public Predicate<Entity> entityPredicate = null;
+    public Predicate<Entity> otherEntityPredicate = null;
+    public boolean affectsSelf = false;
+
+    public EntityTempBuilderJS()
+    {}
+
+    public EntityTempBuilderJS entities(String... entities)
+    {
+        this.entities.addAll(RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.ENTITY_TYPES, ConfigHelper.getEntityTypes(entities)));
+        return this;
+    }
+
+    public EntityTempBuilderJS temperature(double temperature)
+    {
+        this.temperature = temperature;
+        return this;
+    }
+
+    public EntityTempBuilderJS range(double range)
+    {
+        this.range = range;
+        return this;
+    }
+
+    public EntityTempBuilderJS units(String units)
+    {
+        this.units = Temperature.Units.fromID(units);
+        return this;
+    }
+
+    public EntityTempBuilderJS maxEffect(double maxEffect)
+    {
+        this.maxEffect = maxEffect;
+        return this;
+    }
+
+    public EntityTempBuilderJS entityPredicate(Predicate<Entity> entityPredicate)
+    {
+        this.entityPredicate = entityPredicate;
+        return this;
+    }
+
+    public EntityTempBuilderJS otherEntityPredicate(Predicate<Entity> otherEntityPredicate)
+    {
+        this.otherEntityPredicate = otherEntityPredicate;
+        return this;
+    }
+
+    public EntityTempBuilderJS affectsSelf(boolean affectsSelf)
+    {
+        this.affectsSelf = affectsSelf;
+        return this;
+    }
+
+    public EntityTempData build()
+    {
+        EntityTempData data = new EntityTempData(new EntityRequirement(this.entities, this.entityPredicate), this.temperature, this.range, this.units,
+                                                 new EntityRequirement(this.otherEntityPredicate), this.maxEffect, this.affectsSelf);
+        data.setType(ConfigData.Type.KUBEJS);
+        return data;
+    }
+}

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/InsulatingMountBuilderJS.java
@@ -1,0 +1,63 @@
+package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
+
+import com.momosoftworks.coldsweat.data.codec.configuration.MountData;
+import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
+import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class InsulatingMountBuilderJS
+{
+    public final Set<EntityType<?>> entities = new HashSet<>();
+    public Predicate<Entity> entityPredicate = null;
+    public Predicate<Entity> riderPredicate = null;
+    public double coldInsulation = 0;
+    public double heatInsulation = 0;
+
+    public InsulatingMountBuilderJS()
+    {}
+
+    public InsulatingMountBuilderJS entities(String... entities)
+    {
+        this.entities.addAll(RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.ENTITY_TYPES, ConfigHelper.getEntityTypes(entities)));
+        return this;
+    }
+
+    public InsulatingMountBuilderJS entityPredicate(Predicate<Entity> entityPredicate)
+    {
+        this.entityPredicate = entityPredicate;
+        return this;
+    }
+
+    public InsulatingMountBuilderJS riderPredicate(Predicate<Entity> riderPredicate)
+    {
+        this.riderPredicate = riderPredicate;
+        return this;
+    }
+
+    public InsulatingMountBuilderJS coldInsulation(double coldInsulation)
+    {
+        this.coldInsulation = coldInsulation;
+        return this;
+    }
+
+    public InsulatingMountBuilderJS heatInsulation(double heatInsulation)
+    {
+        this.heatInsulation = heatInsulation;
+        return this;
+    }
+
+    public MountData build()
+    {
+        MountData data = new MountData(new EntityRequirement(this.entities, this.entityPredicate), new EntityRequirement(this.riderPredicate), this.coldInsulation, this.heatInsulation);
+        data.setType(ConfigData.Type.KUBEJS);
+        return data;
+    }
+}

src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/SpawnBiomeBuilderJS.java
@@ -1,0 +1,95 @@
+package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
+
+import com.mojang.datafixers.util.Either;
+import com.momosoftworks.coldsweat.ColdSweat;
+import com.momosoftworks.coldsweat.data.codec.configuration.SpawnBiomeData;
+import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
+import com.momosoftworks.coldsweat.data.codec.requirement.LocationRequirement;
+import com.momosoftworks.coldsweat.data.codec.util.IntegerBounds;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.state.pattern.BlockInWorld;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class SpawnBiomeBuilderJS
+{
+    public final Set<Holder<Biome>> biomes = new HashSet<>();
+    public MobCategory category;
+    public int weight;
+    public final Set<EntityType<?>> entities = new HashSet<>();
+    public IntegerBounds count;
+    public Predicate<BlockInWorld> locationPredicate = null;
+
+    public SpawnBiomeBuilderJS()
+    {}
+
+    public SpawnBiomeBuilderJS biomes(String... biomes)
+    {
+        RegistryAccess registryAccess = RegistryHelper.getRegistryAccess();
+        if (registryAccess == null)
+        {   ColdSweat.LOGGER.error("Could not parse biomes for addSpawnBiomes(), as registries are not built yet.");
+            return this;
+        }
+        List<Either<TagKey<Biome>, Holder<Biome>>> biomesList = ConfigHelper.parseRegistryItems(Registry.BIOME_REGISTRY, registryAccess, biomes);
+        this.biomes.addAll(RegistryHelper.mapVanillaRegistryTagList(Registry.BIOME_REGISTRY, biomesList, registryAccess));
+        return this;
+    }
+
+    public SpawnBiomeBuilderJS category(String category)
+    {
+        for (MobCategory mobCategory : MobCategory.values())
+        {
+            if (mobCategory.getName().equals(category))
+            {
+                this.category = mobCategory;
+                return this;
+            }
+        }
+        return this;
+    }
+
+    public SpawnBiomeBuilderJS weight(int weight)
+    {
+        this.weight = weight;
+        return this;
+    }
+
+    public SpawnBiomeBuilderJS entities(String... entities)
+    {
+        this.entities.addAll(RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.ENTITY_TYPES, ConfigHelper.getEntityTypes(entities)));
+        return this;
+    }
+
+    public SpawnBiomeBuilderJS count(int min, int max)
+    {
+        this.count = new IntegerBounds(min, max);
+        return this;
+    }
+
+    public SpawnBiomeBuilderJS locationPredicate(Predicate<BlockInWorld> locationPredicate)
+    {
+        this.locationPredicate = locationPredicate;
+        return this;
+    }
+
+    public SpawnBiomeData build()
+    {
+        SpawnBiomeData data = new SpawnBiomeData(biomes, category, weight,
+                                                 entities,
+                                                 count, new LocationRequirement(locationPredicate), LocationRequirement.NONE);
+        data.setType(ConfigData.Type.KUBEJS);
+        return data;
+    }
+}