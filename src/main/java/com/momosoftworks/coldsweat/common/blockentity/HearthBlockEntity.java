ab10aa4bd86c223d88482c7f840783e070995d57
MikulDev <gamer3mc@gmail.com>
2025-02-28 13:01:31 -0500

    Reset hearth pipe end particles immediately on block update

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
@@ -183,7 +183,10 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
         && this.pathLookup.contains(pos)
         && !event.getOldState().getCollisionShape(level, pos).equals(event.getNewState().getCollisionShape(level, pos)))
         {
-            this.sendBlockUpdate(pos);
+            if (!level.isClientSide())
+            {   this.sendBlockUpdate(pos);
+            }
+            this.pipeEnds.clear();
         }
     }
 
