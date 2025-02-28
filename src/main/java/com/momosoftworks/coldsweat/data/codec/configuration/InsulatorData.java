af9fabbfa13d737a639b36fa5c4d727a48a21508
MikulDev <gamer3mc@gmail.com>
2025-02-27 19:18:57 -0500

    Rename insulator parameter "multiSlot" to "fill"
    Took 28 seconds

--- src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/InsulatorData.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/InsulatorData.java
@@ -38,12 +38,12 @@ public class InsulatorData extends ConfigData implements RequirementHolder
     final EntityRequirement predicate;
     final AttributeModifierMap attributes;
     final Map<ResourceLocation, Double> immuneTempModifiers;
-    final boolean multiSlot;
+    final boolean fill;
 
     public InsulatorData(Insulation.Slot slot,
                          Insulation insulation, ItemRequirement data,
                          EntityRequirement predicate, AttributeModifierMap attributes,
-                         Map<ResourceLocation, Double> immuneTempModifiers, boolean multiSlot,
+                         Map<ResourceLocation, Double> immuneTempModifiers, boolean fill,
                          List<String> requiredMods)
     {
         super(requiredMods);
@@ -53,14 +53,14 @@ public class InsulatorData extends ConfigData implements RequirementHolder
         this.predicate = predicate;
         this.attributes = attributes;
         this.immuneTempModifiers = immuneTempModifiers;
-        this.multiSlot = multiSlot;
+        this.fill = fill;
     }
 
     public InsulatorData(Insulation.Slot slot, Insulation insulation, ItemRequirement data,
                          EntityRequirement predicate, AttributeModifierMap attributes,
-                         Map<ResourceLocation, Double> immuneTempModifiers, boolean multiSlot)
+                         Map<ResourceLocation, Double> immuneTempModifiers, boolean fill)
     {
-        this(slot, insulation, data, predicate, attributes, immuneTempModifiers, multiSlot, ConfigHelper.getModIDs(CSMath.listOrEmpty(data.items()), ForgeRegistries.ITEMS));
+        this(slot, insulation, data, predicate, attributes, immuneTempModifiers, fill, ConfigHelper.getModIDs(CSMath.listOrEmpty(data.items()), ForgeRegistries.ITEMS));
     }
 
     public static final Codec<InsulatorData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
@@ -70,7 +70,7 @@ public class InsulatorData extends ConfigData implements RequirementHolder
             EntityRequirement.getCodec().optionalFieldOf("entity", EntityRequirement.NONE).forGetter(data -> data.predicate),
             AttributeModifierMap.CODEC.optionalFieldOf("attributes", new AttributeModifierMap()).forGetter(data -> data.attributes),
             Codec.unboundedMap(ResourceLocation.CODEC, Codec.DOUBLE).optionalFieldOf("immune_temp_modifiers", new HashMap<>()).forGetter(data -> data.immuneTempModifiers),
-            Codec.BOOL.optionalFieldOf("multi_slot", false).forGetter(InsulatorData::multiSlot),
+            Codec.BOOL.optionalFieldOf("fill", false).forGetter(InsulatorData::multiSlot),
             Codec.STRING.listOf().optionalFieldOf("required_mods", Arrays.asList()).forGetter(InsulatorData::requiredMods)
     ).apply(instance, InsulatorData::new));
 
@@ -93,7 +93,7 @@ public class InsulatorData extends ConfigData implements RequirementHolder
     {   return immuneTempModifiers;
     }
     public boolean multiSlot()
-    {   return multiSlot;
+    {   return fill;
     }
 
     @Override
@@ -132,7 +132,7 @@ public class InsulatorData extends ConfigData implements RequirementHolder
     }
 
     public InsulatorData copy()
-    {   return new InsulatorData(this.slot, this.insulation.copy(), this.data, this.predicate, this.attributes, new HashMap<>(this.immuneTempModifiers), this.multiSlot);
+    {   return new InsulatorData(this.slot, this.insulation.copy(), this.data, this.predicate, this.attributes, new HashMap<>(this.immuneTempModifiers), this.fill);
     }
 
     @Override
