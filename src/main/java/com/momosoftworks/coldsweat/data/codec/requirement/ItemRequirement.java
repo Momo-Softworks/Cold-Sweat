MikulGamour/Cold-Sweat-Rework-Forge

Custom requirement changes

src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/ItemRequirement.java
@@ -19,50 +19,43 @@ import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Predicate;
 
-public record ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items, Optional<TagKey<Item>> tag,
+public record ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items,
                               Optional<IntegerBounds> count, Optional<IntegerBounds> durability,
-                              Optional<List<EnchantmentRequirement>> enchantments, Optional<List<EnchantmentRequirement>> storedEnchantments,
+                              Optional<List<EnchantmentRequirement>> enchantments,
                               Optional<Potion> potion, NbtRequirement nbt, Optional<Predicate<ItemStack>> predicate)
 {
     public static final Codec<ItemRequirement> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             ConfigHelper.tagOrBuiltinCodec(Registries.ITEM, ForgeRegistries.ITEMS).listOf().optionalFieldOf("items").forGetter(predicate -> predicate.items),
-            TagKey.codec(Registries.ITEM).optionalFieldOf("tag").forGetter(predicate -> predicate.tag),
             IntegerBounds.CODEC.optionalFieldOf("count").forGetter(predicate -> predicate.count),
             IntegerBounds.CODEC.optionalFieldOf("durability").forGetter(predicate -> predicate.durability),
             EnchantmentRequirement.CODEC.listOf().optionalFieldOf("enchantments").forGetter(predicate -> predicate.enchantments),
-            EnchantmentRequirement.CODEC.listOf().optionalFieldOf("stored_enchantments").forGetter(predicate -> predicate.storedEnchantments),
             ForgeRegistries.POTIONS.getCodec().optionalFieldOf("potion").forGetter(predicate -> predicate.potion),
-            NbtRequirement.CODEC.optionalFieldOf("nbt", new NbtRequirement(new CompoundTag())).forGetter(predicate -> predicate.nbt)
+            NbtRequirement.CODEC.optionalFieldOf("nbt", new NbtRequirement()).forGetter(predicate -> predicate.nbt)
     ).apply(instance, ItemRequirement::new));
 
     public static final ItemRequirement NONE = new ItemRequirement(Optional.empty(), Optional.empty(), Optional.empty(),
-                                                                   Optional.empty(), Optional.empty(), Optional.empty(),
-                                                                   Optional.empty(), new NbtRequirement());
+                                                                   Optional.empty(), Optional.empty(), new NbtRequirement());
 
-    public ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items, Optional<TagKey<Item>> tag,
+    public ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items,
                            Optional<IntegerBounds> count, Optional<IntegerBounds> durability,
                            Optional<List<EnchantmentRequirement>> enchantments,
-                           Optional<List<EnchantmentRequirement>> storedEnchantments,
                            Optional<Potion> potion, NbtRequirement nbt)
     {
-        this(items, tag, count, durability, enchantments, storedEnchantments, potion, nbt, Optional.empty());
+        this(items, count, durability, enchantments, potion, nbt, Optional.empty());
     }
 
     public ItemRequirement(List<Either<TagKey<Item>, Item>> items, NbtRequirement nbt)
     {
-        this(Optional.of(items), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), nbt);
+        this(Optional.of(items), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), nbt);
     }
 
     public ItemRequirement(Collection<Item> items, @Nullable Predicate<ItemStack> predicate)
     {
-        this(Optional.of(items.stream().map(Either::<TagKey<Item>, Item>right).toList()), Optional.empty(), Optional.empty(), Optional.empty(),
-             Optional.empty(), Optional.empty(), Optional.empty(), new NbtRequirement(), Optional.ofNullable(predicate));
+        this(Optional.of(items.stream().map(Either::<TagKey<Item>, Item>right).toList()), Optional.empty(), Optional.empty(),
+             Optional.empty(), Optional.empty(), new NbtRequirement(), Optional.ofNullable(predicate));
     }
 
     public boolean test(ItemStack stack, boolean ignoreCount)
@@ -91,9 +84,6 @@ public record ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items, 
         if (!this.nbt.test(stack.getTag()))
         {   return false;
         }
-        if (tag.isPresent() && !stack.is(tag.get()))
-        {   return false;
-        }
         if (!ignoreCount && count.isPresent() && !count.get().test(stack.getCount()))
         {   return false;
         }
@@ -109,18 +99,10 @@ public record ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items, 
         else if (enchantments.isPresent())
         {
             Map<Enchantment, Integer> stackEnchantments = EnchantmentHelper.deserializeEnchantments(stack.getEnchantmentTags());
+            stackEnchantments.putAll(EnchantmentHelper.deserializeEnchantments(EnchantedBookItem.getEnchantments(stack)));
             for (EnchantmentRequirement enchantment : enchantments.get())
             {
                 if (!enchantment.test(stackEnchantments))
-                {   return false;
-                }
-            }
-        }
-        else if (storedEnchantments.isPresent())
-        {
-            Map<Enchantment, Integer> stackEnchantments = EnchantmentHelper.deserializeEnchantments(EnchantedBookItem.getEnchantments(stack));
-            for (EnchantmentRequirement enchantment : storedEnchantments.get())
-            {   if (!enchantment.test(stackEnchantments))
                 {   return false;
                 }
             }
@@ -140,9 +122,12 @@ public record ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items, 
         if (obj == null || getClass() != obj.getClass()) return false;
 
         ItemRequirement that = (ItemRequirement) obj;
-        return items.equals(that.items) && tag.equals(that.tag) && count.equals(that.count)
-            && durability.equals(that.durability) && enchantments.equals(that.enchantments)
-            && storedEnchantments.equals(that.storedEnchantments) && potion.equals(that.potion)
-            && nbt.equals(that.nbt) && predicate.equals(that.predicate);
+        return items.equals(that.items)
+            && count.equals(that.count)
+            && durability.equals(that.durability)
+            && enchantments.equals(that.enchantments)
+            && potion.equals(that.potion)
+            && nbt.equals(that.nbt)
+            && predicate.equals(that.predicate);
     }
 }

Momo-Softworks/Cold-Sweat

src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/ItemRequirement.java
@@ -22,23 +22,19 @@ import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.enchantment.ItemEnchantments;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Predicate;
 
-public record ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items, Optional<TagKey<Item>> tag,
+public record ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items,
                               Optional<IntegerBounds> count, Optional<IntegerBounds> durability,
-                              Optional<List<EnchantmentRequirement>> enchantments, Optional<List<EnchantmentRequirement>> storedEnchantments,
+                              Optional<List<EnchantmentRequirement>> enchantments,
                               Optional<Potion> potion, ItemComponentsRequirement components, Optional<Predicate<ItemStack>> predicate)
 {
     public static final Codec<ItemRequirement> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             ConfigHelper.tagOrBuiltinCodec(Registries.ITEM, BuiltInRegistries.ITEM).listOf().optionalFieldOf("items").forGetter(predicate -> predicate.items),
-            TagKey.codec(Registries.ITEM).optionalFieldOf("tag").forGetter(predicate -> predicate.tag),
             IntegerBounds.CODEC.optionalFieldOf("count").forGetter(predicate -> predicate.count),
             IntegerBounds.CODEC.optionalFieldOf("durability").forGetter(predicate -> predicate.durability),
             EnchantmentRequirement.CODEC.listOf().optionalFieldOf("enchantments").forGetter(predicate -> predicate.enchantments),
-            EnchantmentRequirement.CODEC.listOf().optionalFieldOf("stored_enchantments").forGetter(predicate -> predicate.storedEnchantments),
             BuiltInRegistries.POTION.byNameCodec().optionalFieldOf("potion").forGetter(predicate -> predicate.potion),
             ItemComponentsRequirement.CODEC.optionalFieldOf("components", new ItemComponentsRequirement()).forGetter(predicate -> predicate.components)
     ).apply(instance, ItemRequirement::new));
@@ -47,49 +43,43 @@ public record ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items, 
     (buf, predicate) ->
     {
         CommonStreamCodecs.writeOptionalList(buf, predicate.items, CommonStreamCodecs.tagOrRegistryCodec(Registries.ITEM));
-        buf.writeOptional(predicate.tag, CommonStreamCodecs.tagKeyCodec(Registries.ITEM));
         buf.writeOptional(predicate.count, IntegerBounds.STREAM_CODEC);
         buf.writeOptional(predicate.durability, IntegerBounds.STREAM_CODEC);
         CommonStreamCodecs.writeOptionalList(buf, predicate.enchantments, EnchantmentRequirement.STREAM_CODEC);
-        CommonStreamCodecs.writeOptionalList(buf, predicate.storedEnchantments, EnchantmentRequirement.STREAM_CODEC);
         CommonStreamCodecs.writeOptional(buf, predicate.potion, ByteBufCodecs.registry(Registries.POTION));
         ItemComponentsRequirement.STREAM_CODEC.encode(buf, predicate.components);
     },
     (buf) ->
     {
         Optional<List<Either<TagKey<Item>, Item>>> items = CommonStreamCodecs.readOptionalList(buf, CommonStreamCodecs.tagOrRegistryCodec(Registries.ITEM));
-        Optional<TagKey<Item>> tag = buf.readOptional(CommonStreamCodecs.tagKeyCodec(Registries.ITEM));
         Optional<IntegerBounds> count = buf.readOptional(IntegerBounds.STREAM_CODEC);
         Optional<IntegerBounds> durability = buf.readOptional(IntegerBounds.STREAM_CODEC);
         Optional<List<EnchantmentRequirement>> enchantments = CommonStreamCodecs.readOptionalList(buf, EnchantmentRequirement.STREAM_CODEC);
-        Optional<List<EnchantmentRequirement>> storedEnchantments = CommonStreamCodecs.readOptionalList(buf, EnchantmentRequirement.STREAM_CODEC);
         Optional<Potion> potion = CommonStreamCodecs.readOptional(buf, ByteBufCodecs.registry(Registries.POTION));
         ItemComponentsRequirement components = ItemComponentsRequirement.STREAM_CODEC.decode(buf);
-        return new ItemRequirement(items, tag, count, durability, enchantments, storedEnchantments, potion, components);
+        return new ItemRequirement(items, count, durability, enchantments, potion, components);
     });
 
     public static final ItemRequirement NONE = new ItemRequirement(Optional.empty(), Optional.empty(), Optional.empty(),
-                                                                   Optional.empty(), Optional.empty(), Optional.empty(),
-                                                                   Optional.empty(), new ItemComponentsRequirement());
+                                                                   Optional.empty(), Optional.empty(), new ItemComponentsRequirement());
 
-    public ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items, Optional<TagKey<Item>> tag,
+    public ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items,
                            Optional<IntegerBounds> count, Optional<IntegerBounds> durability,
                            Optional<List<EnchantmentRequirement>> enchantments,
-                           Optional<List<EnchantmentRequirement>> storedEnchantments,
                            Optional<Potion> potion, ItemComponentsRequirement components)
     {
-        this(items, tag, count, durability, enchantments, storedEnchantments, potion, components, Optional.empty());
+        this(items, count, durability, enchantments, potion, components, Optional.empty());
     }
 
     public ItemRequirement(List<Either<TagKey<Item>, Item>> items, ItemComponentsRequirement components)
     {
-        this(Optional.of(items), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), components);
+        this(Optional.of(items), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), components);
     }
 
     public ItemRequirement(Collection<Item> items, @Nullable Predicate<ItemStack> predicate)
     {
-        this(Optional.of(items.stream().map(Either::<TagKey<Item>, Item>right).toList()), Optional.empty(), Optional.empty(), Optional.empty(),
-             Optional.empty(), Optional.empty(), Optional.empty(), new ItemComponentsRequirement(), Optional.ofNullable(predicate));
+        this(Optional.of(items.stream().map(Either::<TagKey<Item>, Item>right).toList()), Optional.empty(), Optional.empty(),
+             Optional.empty(), Optional.empty(), new ItemComponentsRequirement(), Optional.ofNullable(predicate));
     }
 
     public boolean test(ItemStack stack, boolean ignoreCount)
@@ -118,9 +108,6 @@ public record ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items, 
         if (!this.components.test(stack.getComponents()))
         {   return false;
         }
-        if (tag.isPresent() && !stack.is(tag.get()))
-        {   return false;
-        }
         if (!ignoreCount && count.isPresent() && !count.get().test(stack.getCount()))
         {   return false;
         }
@@ -135,25 +122,11 @@ public record ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items, 
         }
         else if (enchantments.isPresent())
         {
-            ItemEnchantments stackEnchantments = stack.get(DataComponents.ENCHANTMENTS);
-            if (stackEnchantments == null)
-            {   return false;
-            }
+            ItemEnchantments stackEnchantments = stack.getOrDefault(DataComponents.ENCHANTMENTS, ItemEnchantments.EMPTY);
+            stackEnchantments.entrySet().addAll(stack.getOrDefault(DataComponents.STORED_ENCHANTMENTS, ItemEnchantments.EMPTY).entrySet());
             for (EnchantmentRequirement enchantment : enchantments.get())
             {
                 if (!enchantment.test(stackEnchantments))
-                {   return false;
-                }
-            }
-        }
-        else if (storedEnchantments.isPresent())
-        {
-            ItemEnchantments stackEnchantments = stack.get(DataComponents.STORED_ENCHANTMENTS);
-            if (stackEnchantments == null)
-            {   return false;
-            }
-            for (EnchantmentRequirement enchantment : storedEnchantments.get())
-            {   if (!enchantment.test(stackEnchantments))
                 {   return false;
                 }
             }
@@ -173,9 +146,12 @@ public record ItemRequirement(Optional<List<Either<TagKey<Item>, Item>>> items, 
         if (obj == null || getClass() != obj.getClass()) return false;
 
         ItemRequirement that = (ItemRequirement) obj;
-        return items.equals(that.items) && tag.equals(that.tag) && count.equals(that.count)
-            && durability.equals(that.durability) && enchantments.equals(that.enchantments)
-            && storedEnchantments.equals(that.storedEnchantments) && potion.equals(that.potion)
-            && components.equals(that.components) && predicate.equals(that.predicate);
+        return items.equals(that.items)
+            && count.equals(that.count)
+            && durability.equals(that.durability)
+            && enchantments.equals(that.enchantments)
+            && potion.equals(that.potion)
+            && components.equals(that.components)
+            && predicate.equals(that.predicate);
     }
 }