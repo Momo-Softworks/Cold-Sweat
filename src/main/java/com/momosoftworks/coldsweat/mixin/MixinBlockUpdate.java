8963e8e98893f36c894afa29ea3ce1357167742c
MikulDev <gamer3mc@gmail.com>
2025-02-28 13:00:35 -0500

    Fire BlockStateChangedEvent on client

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinBlockUpdate.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinBlockUpdate.java
@@ -1,6 +1,7 @@
 package com.momosoftworks.coldsweat.mixin;
 
 import com.momosoftworks.coldsweat.api.event.vanilla.BlockStateChangedEvent;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.state.BlockState;
@@ -10,22 +11,29 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/**
- * Called when a block state is changed.<br>
- * This is not an {@link net.neoforged.bus.api.ICancellableEvent}.<br>
- * <br>
- * Updates must be delayed by 1 tick to prevent chunk deadlocking.
- */
 @Mixin(ServerLevel.class)
 public class MixinBlockUpdate
 {
-    ServerLevel level = (ServerLevel) (Object) this;
-
-    @Inject(method = "onBlockStateChange", at = @At("HEAD"))
-    private void onBlockUpdate(BlockPos pos, BlockState oldState, BlockState newState, CallbackInfo ci)
+    @Mixin(ServerLevel.class)
+    public static final class Server
     {
-        if (!oldState.equals(newState))
-        {   level.getServer().execute(() -> NeoForge.EVENT_BUS.post(new BlockStateChangedEvent(pos, level, oldState, newState)));
+        ServerLevel level = (ServerLevel) (Object) this;
+
+        @Inject(method = "onBlockStateChange", at = @At("HEAD"))
+        private void onBlockUpdate(BlockPos pos, BlockState oldState, BlockState newState, CallbackInfo ci)
+        {
+            if (!oldState.equals(newState))
+            {   level.getServer().execute(() -> NeoForge.EVENT_BUS.post(new BlockStateChangedEvent(pos, level, oldState, newState)));
+            }
+        }
+    }
+
+    @Mixin(ClientLevel.class)
+    public static final class Client
+    {
+        @Inject(method = "sendBlockUpdated", at = @At("HEAD"))
+        private void onBlockUpdate(BlockPos pos, BlockState oldState, BlockState newState, int flags, CallbackInfo ci)
+        {   NeoForge.EVENT_BUS.post(new BlockStateChangedEvent(pos, (ClientLevel) (Object) this, oldState, newState));
         }
     }
 }
