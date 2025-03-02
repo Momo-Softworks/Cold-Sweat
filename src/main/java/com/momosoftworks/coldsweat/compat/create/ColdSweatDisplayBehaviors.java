e751e405e8bc1e0c0473d9ac8e46261d24b5f0a8
MikulDev <gamer3mc@gmail.com>
2025-03-01 20:11:09 -0500

    Add decimal places to MC units for thermolith display source

--- src/main/java/com/momosoftworks/coldsweat/compat/create/ColdSweatDisplayBehaviors.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/create/ColdSweatDisplayBehaviors.java
@@ -31,7 +31,8 @@ public class ColdSweatDisplayBehaviors
 
                 double convertedTemp = Temperature.convert(temperature, Temperature.Units.MC, units, true);
 
-                String text = String.format("%.0f%s", convertedTemp, units.getFormattedName());
+                String places = units == Temperature.Units.MC ? "%.2f" : "%.0f";
+                String text = String.format(places+"%s", convertedTemp, units.getFormattedName());
                 return Component.literal(text);
             }
             return Component.empty();
