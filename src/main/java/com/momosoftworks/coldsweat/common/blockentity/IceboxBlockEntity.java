b57a816508ac57fe759a3d2afad3202d93e2a696
MikulDev <gamer3mc@gmail.com>
2025-03-02 17:02:33 -0500

    More robust check for rechecking icebox openers

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/IceboxBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/IceboxBlockEntity.java
@@ -147,8 +147,8 @@ public class IceboxBlockEntity extends HearthBlockEntity implements LidBlockEnti
         super.tick(level, pos);
 
         // Recheck openers
-        if (!this.remove)
-        {   this.openersCounter.recheckOpeners(this.getLevel(), this.getBlockPos(), this.getBlockState());
+        if (!this.remove && this.level != null && this.level.getServer() != null)
+        {   this.openersCounter.recheckOpeners(this.level, this.getBlockPos(), this.getBlockState());
         }
 
         if (getFuel() > 0)
