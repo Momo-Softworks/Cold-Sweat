bd5c095fc6aaf55d731769637ac5941e795308ff
MikulDev <gamer3mc@gmail.com>
2025-03-02 01:49:08 -0500

    Update attributes for item that was changed upon LivingEquipmentChangeEvent

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
@@ -619,6 +619,7 @@ public class EntityTempManager
     @SubscribeEvent
     public static void updateInsulationAttributesOnEquipmentChange(LivingEquipmentChangeEvent event)
     {
+        updateInsulationAttributeModifiers(event.getEntity(), event.getFrom(), event.getTo());
         for (ItemStack armor : event.getEntity().getArmorSlots())
         {
             if (!armor.isEmpty())
