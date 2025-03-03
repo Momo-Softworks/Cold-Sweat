dc475760952e071c2d6a5d2ad6bced98416f3300
MikulDev <gamer3mc@gmail.com>
2025-03-03 16:17:11 -0500

    Only invalidate items for sewing if they have non-empty insulation

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ItemInsulationManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ItemInsulationManager.java
@@ -25,6 +25,7 @@ import net.neoforged.neoforge.event.entity.player.PlayerContainerEvent;
 
 import javax.annotation.Nullable;
 import java.util.*;
+import java.util.stream.Stream;
 
 @EventBusSubscriber
 public class ItemInsulationManager
@@ -56,13 +57,33 @@ public class ItemInsulationManager
                : 0;
     }
 
+    /**
+     * Checks if the item is allowed to receive insulation from sewing.<br>
+     * The item must be equipable and not have any built-in insulation.
+     */
     public static boolean isInsulatable(ItemStack stack)
     {
         return stack.getItem() instanceof Equipable
-            && !ConfigSettings.INSULATION_ITEMS.get().containsKey(stack.getItem())
-            && !ConfigSettings.INSULATING_ARMORS.get().containsKey(stack.getItem());
+            && getBuiltinInsulation(stack).isEmpty();
     }
 
+    /**
+     * Gives a collection of all insulation that is built-in to the item (not applied via sewing)
+     * @return an IMMUTABLE list of insulation the item has.
+     */
+    public static List<Insulation> getBuiltinInsulation(ItemStack stack)
+    {
+        return Stream.of(ConfigSettings.INSULATION_ITEMS.get().get(stack.getItem()),
+                         ConfigSettings.INSULATING_ARMORS.get().get(stack.getItem()),
+                         ConfigSettings.INSULATING_CURIOS.get().get(stack.getItem()))
+               .flatMap(Collection::stream).map(InsulatorData::insulation)
+               .filter(ins -> !ins.isEmpty()).toList();
+    }
+
+    /**
+     * Returns a list of {@link InsulatorData} attached to the item, including both built-in and applied insulation.
+     * @return an IMMUTABLE list of insulation the item has.
+     */
     public static List<InsulatorData> getAllInsulatorsForStack(ItemStack stack)
     {
         if (stack.isEmpty()) return new ArrayList<>();
