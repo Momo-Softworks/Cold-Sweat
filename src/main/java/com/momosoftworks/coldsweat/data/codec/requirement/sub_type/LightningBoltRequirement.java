adfa6de36624e99b1c9316bf8af2470abd062980
MikulDev <gamer3mc@gmail.com>
2025-03-04 19:45:19 -0500

    Remove redundant location fields from type-specific data requirements

--- src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/sub_type/LightningBoltRequirement.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/sub_type/LightningBoltRequirement.java
@@ -3,7 +3,6 @@ package com.momosoftworks.coldsweat.data.codec.requirement.sub_type;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
-import com.momosoftworks.coldsweat.data.codec.requirement.LocationRequirement;
 import com.momosoftworks.coldsweat.data.codec.util.IntegerBounds;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LightningBolt;
@@ -11,12 +10,11 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
-public record LightningBoltRequirement(IntegerBounds blocksSetOnFire, EntityRequirement entityStruck, LocationRequirement location) implements EntitySubRequirement
+public record LightningBoltRequirement(IntegerBounds blocksSetOnFire, EntityRequirement entityStruck) implements EntitySubRequirement
 {
     public static final MapCodec<LightningBoltRequirement> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
             IntegerBounds.CODEC.optionalFieldOf("blocks_set_on_fire", IntegerBounds.NONE).forGetter(LightningBoltRequirement::blocksSetOnFire),
-            EntityRequirement.getCodec().optionalFieldOf("entity_struck", EntityRequirement.NONE).forGetter(LightningBoltRequirement::entityStruck),
-            LocationRequirement.CODEC.optionalFieldOf("location", LocationRequirement.NONE).forGetter(LightningBoltRequirement::location)
+            EntityRequirement.getCodec().optionalFieldOf("entity_struck", EntityRequirement.NONE).forGetter(LightningBoltRequirement::entityStruck)
     ).apply(instance, LightningBoltRequirement::new));
 
     @Override
@@ -30,7 +28,6 @@ public record LightningBoltRequirement(IntegerBounds blocksSetOnFire, EntityRequ
         if (!(entity instanceof LightningBolt lightningbolt)) return false;
 
         return this.blocksSetOnFire.test(lightningbolt.getBlocksSetOnFire())
-             && lightningbolt.getHitEntities().anyMatch(this.entityStruck::test)
-             && this.location.test(level, position);
+             && lightningbolt.getHitEntities().anyMatch(this.entityStruck::test);
     }
 }
