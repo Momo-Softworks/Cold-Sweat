86eca8207cdc971a413fb065c506ffea9cd442ff
MikulDev <gamer3mc@gmail.com>
2025-03-04 19:45:46 -0500

    Optimize check for hearth AOE containing player

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
@@ -393,7 +393,7 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
                         {   playerBB = playerBB.inflate(0, 0.5, 0);
                         }
                         playerBB = CompatManager.Valkyrien.transformIfShipPos(level, playerBB).inflate(-0.1);
-                        if (BlockPos.betweenClosedStream(playerBB).anyMatch(ps -> paths.contains(new SpreadPath(ps)))
+                        if (this.isAffectingPos(BlockPos.betweenClosedStream(playerBB).toArray(BlockPos[]::new))
                         && !WorldHelper.canSeeSky(level, player.blockPosition(), 64))
                         {   this.insulatePlayer(player);
                         }
@@ -874,6 +874,21 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
                 return false;
             });
         }
+    }
+
+    public boolean isAffectingPos(BlockPos... positions)
+    {
+        for (int i = 0; i < this.paths.size(); i++)
+        {
+            SpreadPath path = this.paths.get(i);
+            for (int j = 0; j < positions.length; j++)
+            {
+                if (path.pos.equals(positions[j]))
+                {   return true;
+                }
+            }
+        }
+        return false;
     }
 
     void resetPaths()
