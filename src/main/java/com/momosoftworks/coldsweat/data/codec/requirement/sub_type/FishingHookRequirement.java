96c4c7ba9e1a6007cbf418117d90c2450d45b4fc
MikulDev <gamer3mc@gmail.com>
2025-03-04 19:45:19 -0500

    Remove redundant location fields from type-specific data requirements

--- src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/sub_type/FishingHookRequirement.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/sub_type/FishingHookRequirement.java
@@ -3,7 +3,6 @@ package com.momosoftworks.coldsweat.data.codec.requirement.sub_type;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import com.momosoftworks.coldsweat.data.codec.requirement.LocationRequirement;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.FishingHook;
 import net.minecraft.world.level.Level;
@@ -12,13 +11,12 @@ import net.minecraft.world.phys.Vec3;
 import javax.annotation.Nullable;
 import java.util.Optional;
 
-public record FishingHookRequirement(Optional<Boolean> inOpenWater, LocationRequirement location) implements EntitySubRequirement
+public record FishingHookRequirement(Optional<Boolean> inOpenWater) implements EntitySubRequirement
 {
-    public static final FishingHookRequirement NONE = new FishingHookRequirement(Optional.empty(), LocationRequirement.NONE);
+    public static final FishingHookRequirement NONE = new FishingHookRequirement(Optional.empty());
 
     public static final MapCodec<FishingHookRequirement> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-            Codec.BOOL.optionalFieldOf("in_open_water").forGetter(FishingHookRequirement::inOpenWater),
-            LocationRequirement.CODEC.optionalFieldOf("location", LocationRequirement.NONE).forGetter(FishingHookRequirement::location)
+            Codec.BOOL.optionalFieldOf("in_open_water").forGetter(FishingHookRequirement::inOpenWater)
     ).apply(instance, FishingHookRequirement::new));
 
     @Override
@@ -29,7 +27,6 @@ public record FishingHookRequirement(Optional<Boolean> inOpenWater, LocationRequ
     @Override
     public boolean test(Entity entity, Level level, @Nullable Vec3 position)
     {
-        return this.inOpenWater.map(val -> entity instanceof FishingHook fishinghook && val == fishinghook.isOpenWaterFishing()).orElse(true)
-            && this.location.test(level, position);
+        return this.inOpenWater.map(val -> entity instanceof FishingHook fishinghook && val == fishinghook.isOpenWaterFishing()).orElse(true);
     }
 }
