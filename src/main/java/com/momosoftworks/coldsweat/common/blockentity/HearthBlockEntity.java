22323642618aebd63d0759d285b196e710ed2ca2
MikulDev <gamer3mc@gmail.com>
2025-03-05 20:59:06 -0500

    Add check before using Valkyrien skies code in HearthBlockEntity

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
@@ -394,7 +394,9 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
                         if (playerBB.maxY - playerBB.minY < 1.5)
                         {   playerBB = playerBB.inflate(0, 0.5, 0);
                         }
-                        playerBB = CompatManager.Valkyrien.transformIfShipPos(level, playerBB).inflate(-0.1);
+                        if (CompatManager.isValkyrienSkiesLoaded())
+                        {   playerBB = CompatManager.Valkyrien.transformIfShipPos(level, playerBB).inflate(-0.1);
+                        }
                         if (BlockPos.betweenClosedStream(playerBB).anyMatch(ps -> paths.contains(new SpreadPath(ps)))
                         && !WorldHelper.canSeeSky(level, player.blockPosition(), 64))
                         {   this.insulatePlayer(player);
@@ -1085,7 +1087,7 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
     @OnlyIn(Dist.CLIENT)
     public void spawnAirParticle(int x, int y, int z, Random rand)
     {
-        if (rand.nextFloat() > (spreading ? 0.016f : 0.032f)) return;
+        if (rand.nextFloat() > (spreading ? 0.002f : 0.032f)) return;
 
         float xr = rand.nextFloat();
         float yr = rand.nextFloat();
