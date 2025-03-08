d33e4b6fd133c6201e592c005394a6b5aaa62302
MikulDev <gamer3mc@gmail.com>
2025-03-07 20:17:48 -0500

    Decrease default lava temp

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/block_temp/LavaBlockTemp.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/block_temp/LavaBlockTemp.java
@@ -14,13 +14,13 @@ public class LavaBlockTemp extends BlockTemp
 {
     public LavaBlockTemp()
     {
-        super(0, 6.66, -Double.MAX_VALUE, 21.5, 7, true, Blocks.LAVA);
+        super(0, 4, -Double.MAX_VALUE, 21.5, 7, true, Blocks.LAVA);
     }
 
     @Override
     public double getTemperature(Level level, LivingEntity entity, BlockState state, BlockPos pos, double distance)
     {
         int height = state.getFluidState().getAmount();
-        return (height / 7f) / (entity.getVehicle() instanceof Strider ? 50d : 3d);
+        return (height / 7f) / (entity.getVehicle() instanceof Strider ? 50d : 5d);
     }
 }
