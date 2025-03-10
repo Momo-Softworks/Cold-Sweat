9a938ebe89bbf488eb4848ce1c97a631c636eb1e
MikulDev <gamer3mc@gmail.com>
2025-03-10 14:42:24 -0400

    Set chameleon temp immediately after spawning
    Took 3 seconds

--- src/main/java/com/momosoftworks/coldsweat/common/entity/ChameleonEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/entity/ChameleonEntity.java
@@ -403,9 +403,8 @@ public class ChameleonEntity extends AnimalEntity
         }
 
         // Control temperature
-        if (this.tickCount % 20 == 0)
-        {   // Use the player's temperature if mounted
-            this.desiredTemp = (float) CSMath.clamp(Temperature.get(this, Temperature.Trait.WORLD), ConfigSettings.MIN_TEMP.get(), ConfigSettings.MAX_TEMP.get());
+        if (this.tickCount % 20 == 0 || this.tickCount  == 1)
+        {   this.desiredTemp = (float) CSMath.clamp(Temperature.get(this, Temperature.Trait.WORLD), ConfigSettings.MIN_TEMP.get(), ConfigSettings.MAX_TEMP.get());
         }
         this.setTemperature(this.getTemperature() + (this.desiredTemp - this.getTemperature()) * 0.03f);
 
@@ -807,5 +806,6 @@ public class ChameleonEntity extends AnimalEntity
             });
         }
         this.setTemperature(nbt.getFloat("Temperature"));
+        this.desiredTemp = this.getTemperature();
     }
 }
