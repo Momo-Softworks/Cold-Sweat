31d8147838ba56306142ae52b737926766c3b757
MikulDev <gamer3mc@gmail.com>
2025-03-10 01:56:22 -0400

    Fix clientside temp effects changing after death
    Took 7 seconds

--- src/main/java/com/momosoftworks/coldsweat/client/event/TempEffectsClient.java
+++ src/main/java/com/momosoftworks/coldsweat/client/event/TempEffectsClient.java
@@ -47,7 +47,7 @@ public class TempEffectsClient
     public static void setCamera(EntityViewRenderEvent.CameraSetup event)
     {
         PlayerEntity player = Minecraft.getInstance().player;
-        if (player == null || !player.isAlive() && EntityTempManager.isPeacefulMode(player)) return;
+        if (player == null || !player.isAlive() || EntityTempManager.isPeacefulMode(player)) return;
 
         if (!Minecraft.getInstance().isPaused())
         {
