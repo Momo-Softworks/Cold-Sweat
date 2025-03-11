fbb4704375de8bdf880c2496f70d3647cc49ec90
MikulDev <gamer3mc@gmail.com>
2025-03-10 22:58:38 -0400

    Fix potential chunk deadlock when calling ITemperatureCap.tick() immediately
    Took 10 minutes

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
@@ -113,8 +113,10 @@ public class EntityTempManager
                     cap.getModifiers(trait).clear();
                     cap.getModifiers(trait).addAll(gatherEvent.getModifiers());
                 }
-                cap.tick(living);
-                TaskScheduler.scheduleServer(() -> Temperature.updateTemperature(living, cap, true), 1);
+                TaskScheduler.scheduleServer(() ->
+                {   cap.tick(living);
+                    Temperature.updateTemperature(living, cap, true);
+                }, 1);
             });
         }
     }
