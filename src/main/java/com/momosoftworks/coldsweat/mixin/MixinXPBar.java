e840e83edc165f6e254938659ea6e4224187c8d4
Mikul <gamer3mc@gmail.com>
2025-03-17 10:03:55 -0500

    Fix mixin conflict w/ ModifyVariable in MixinXPBar

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinXPBar.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinXPBar.java
@@ -1,34 +1,55 @@
 package com.momosoftworks.coldsweat.mixin;
 
 import com.momosoftworks.coldsweat.config.ConfigSettings;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Gui.class)
 public class MixinXPBar
 {
-    @ModifyVariable(method = "renderExperienceLevel", at = @At(value = "LOAD", ordinal = 0), ordinal = 2)
-    public int renderExperienceBar2(int y)
+    @Inject(method = "renderExperienceLevel", at = @At("HEAD"))
+    public void renderExperienceBar1(GuiGraphics graphics, DeltaTracker delta, CallbackInfo ci)
     {
         // Render XP bar
         if (ConfigSettings.CUSTOM_HOTBAR_LAYOUT.get())
-        {   return y + 4;
+        {   graphics.pose().pushPose();
+            graphics.pose().translate(0, 4, 0);
         }
-        return y;
+    }
+
+    @Inject(method = "renderExperienceLevel", at = @At("TAIL"))
+    public void renderExperienceBar2(GuiGraphics graphics, DeltaTracker delta, CallbackInfo ci)
+    {
+        // Render XP bar
+        if (ConfigSettings.CUSTOM_HOTBAR_LAYOUT.get())
+        {   graphics.pose().popPose();
+        }
     }
 
     @Mixin(Gui.class)
     public static class MixinItemLabel
     {
-        @ModifyVariable(method = "renderSelectedItemName(Lnet/minecraft/client/gui/GuiGraphics;I)V", at = @At(value = "LOAD"), ordinal = 3)
-        public int renderItemNamePre(int y)
+        @Inject(method = "renderSelectedItemName(Lnet/minecraft/client/gui/GuiGraphics;I)V", at = @At("HEAD"))
+        public void renderItemNamePre1(GuiGraphics graphics, int yShift, CallbackInfo ci)
         {
+            // Render XP bar
             if (ConfigSettings.CUSTOM_HOTBAR_LAYOUT.get())
-            {   return y - 4;
+            {   graphics.pose().pushPose();
+                graphics.pose().translate(0, -4, 0);
             }
-            return y;
+        }
+        @Inject(method = "renderSelectedItemName(Lnet/minecraft/client/gui/GuiGraphics;I)V", at = @At("TAIL"))
+        public void renderItemNamePre2(GuiGraphics graphics, int yShift, CallbackInfo ci)
+        {
+            // Render XP bar
+            if (ConfigSettings.CUSTOM_HOTBAR_LAYOUT.get())
+            {   graphics.pose().popPose();
+            }
         }
     }
 }
