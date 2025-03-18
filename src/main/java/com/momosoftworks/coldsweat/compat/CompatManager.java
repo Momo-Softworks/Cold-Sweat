af0ff7d37039a4c8726c7dee64ac2806138c3489
Mikul <gamer3mc@gmail.com>
2025-03-18 09:25:08 -0400

    Add config for dryoff speed

--- src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
@@ -68,7 +68,7 @@ public class CompatManager
     private static final boolean WYTHERS_LOADED = modLoaded("wwoo");
     private static final boolean TOOLTIPS_LOADED = modLoaded("legendarytooltips");
     private static final boolean PRIMAL_WINTER_LOADED = modLoaded("primalwinter");
-    private static final boolean THIRST_LOADED = modLoaded("thirst", "1.21.0-2.0.0");
+    private static final boolean THIRST_LOADED = modLoaded("thirst", "1.21.0-2.1.0");
     private static final boolean ICEBERG_LOADED = modLoaded("iceberg");
     private static final boolean SPOILED_LOADED = modLoaded("spoiled");
     private static final boolean SUPPLEMENTARIES_LOADED = modLoaded("supplementaries");
