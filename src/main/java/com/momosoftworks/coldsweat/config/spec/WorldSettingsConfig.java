c9f42751917dc5d3ad79f20d023f6ca4d4b60041
MikulDev <gamer3mc@gmail.com>
2025-03-31 13:14:47 -0400

    Touch up config documentation

--- src/main/java/com/momosoftworks/coldsweat/config/spec/WorldSettingsConfig.java
+++ src/main/java/com/momosoftworks/coldsweat/config/spec/WorldSettingsConfig.java
@@ -506,7 +506,7 @@ public class WorldSettingsConfig
 
         BLOCK_TEMPERATURES = BUILDER
                 .comment(" Format:",
-                         " [[\"block-ids\", <temperature>, <range>, <*units>, <*max effect>, <*predicates>, <*nbt>, <*temperature-limit>], [etc...], [etc...]]",
+                         " [[\"block-ids\", <temperature>, <range>, <*units>, <*max effect>, <*predicates>, <*\"nbt\">, <*temperature-limit>], [etc...], [etc...]]",
                          " * = Optional",
                          " ",
                          " Arguments:",
