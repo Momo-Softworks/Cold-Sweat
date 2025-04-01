099706deccf94c365b767b1e3eea3b6beab848bd
MikulDev <gamer3mc@gmail.com>
2025-04-01 15:16:11 -0400

    Periodically clear item cap cache to prevent memory leak
    Took 18 seconds

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ItemInsulationManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ItemInsulationManager.java
@@ -35,6 +35,7 @@ import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerContainerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -120,6 +121,14 @@ public class ItemInsulationManager
     public static void handleInventoryOpen(PlayerContainerEvent event)
     {
         event.getPlayer().getPersistentData().putBoolean("InventoryOpen", event instanceof PlayerContainerEvent.Open);
+    }
+
+    @SubscribeEvent
+    public static void clearCachePeriodically(TickEvent.WorldTickEvent event)
+    {
+        if (event.phase == TickEvent.Phase.END && event.world.getGameTime() % 200 == 0)
+        {   CAP_CACHE.clear();
+        }
     }
 
     static IContainerListener INSULATION_LISTENER = new IContainerListener()
