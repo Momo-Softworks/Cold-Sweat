320e4a071968272cfe0766fc6138228c0cd60086
MikulDev <gamer3mc@gmail.com>
2025-04-03 14:47:48 -0400

    Set min Iceberg version

--- src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
@@ -71,7 +71,7 @@ public class CompatManager
     private static final boolean TOOLTIPS_LOADED = modLoaded("legendarytooltips");
     private static final boolean PRIMAL_WINTER_LOADED = modLoaded("primalwinter");
     private static final boolean THIRST_LOADED = modLoaded("thirst", "1.21.1-2.1.0");
-    private static final boolean ICEBERG_LOADED = modLoaded("iceberg");
+    private static final boolean ICEBERG_LOADED = modLoaded("iceberg", "1.3.0");
     private static final boolean SPOILED_LOADED = modLoaded("spoiled");
     private static final boolean SUPPLEMENTARIES_LOADED = modLoaded("supplementaries");
     private static final boolean TOUGH_AS_NAILS_LOADED = modLoaded("toughasnails");
