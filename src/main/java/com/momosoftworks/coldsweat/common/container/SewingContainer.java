bb6d0b48998488dfb445016da480d8637c7056d0
MikulDev <gamer3mc@gmail.com>
2025-04-03 16:15:40 -0400

    Misc updates & improvements for SewingContainer

--- src/main/java/com/momosoftworks/coldsweat/common/container/SewingContainer.java
+++ src/main/java/com/momosoftworks/coldsweat/common/container/SewingContainer.java
@@ -316,7 +316,6 @@ public class SewingContainer extends ItemCombinerMenu
                     this.growItem(1, -1);
                 }
                 while (this.insulateArmorItem(result, this.getItem(1)));
-                this.onTake(player, result);
             }
         }
         finally
