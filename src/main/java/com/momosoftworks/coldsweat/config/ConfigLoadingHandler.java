f1cb5d8ac0a11bf69dd6df3a8cd77ede5ca5a9f7
MikulDev <gamer3mc@gmail.com>
2025-04-03 21:13:18 -0400

    More ConfigLoadingHandler fixes

--- src/main/java/com/momosoftworks/coldsweat/config/ConfigLoadingHandler.java
+++ src/main/java/com/momosoftworks/coldsweat/config/ConfigLoadingHandler.java
@@ -213,7 +213,7 @@ public class ConfigLoadingHandler
         NeoForge.EVENT_BUS.post(event);
 
         // Remove registries that don't have required loaded mods
-        registries.values().removeIf(holder -> holder.value().areRequiredModsLoaded());
+        registries.values().removeIf(holder -> !holder.value().areRequiredModsLoaded());
 
         // Remove registry entries that match removal criteria
         removeRegistries(event.getRegistries());
@@ -623,7 +623,8 @@ public class ConfigLoadingHandler
         List<Holder<T>> output = new ArrayList<>();
         DynamicOps<JsonElement> registryOps = RegistryOps.create(JsonOps.INSTANCE, registryAccess);
 
-        Path coldSweatDataPath = FMLPaths.CONFIGDIR.get().resolve("coldsweat/data").resolve(registry.location().getPath());
+        String configFolder = registry.location().getNamespace().replace("_", "");
+        Path coldSweatDataPath = FMLPaths.CONFIGDIR.get().resolve(configFolder + "/data").resolve(registry.location().getPath());
         File jsonDirectory = coldSweatDataPath.toFile();
 
         if (!jsonDirectory.exists())
