69e049342274872054a65d5181d400e93a5538ef
MikulDev <gamer3mc@gmail.com>
2025-04-03 21:50:17 -0400

    Make registry removals use NegatableList

--- src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/RemoveRegistryData.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/RemoveRegistryData.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.momosoftworks.coldsweat.data.ModRegistries;
 import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
 import com.momosoftworks.coldsweat.data.codec.requirement.NbtRequirement;
+import com.momosoftworks.coldsweat.data.codec.util.NegatableList;
 import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
@@ -17,9 +18,9 @@ import java.util.Optional;
 public class RemoveRegistryData<T extends ConfigData> extends ConfigData
 {
     ResourceKey<Registry<T>> registry;
-    List<CompoundTag> entries;
+    NegatableList<CompoundTag> entries;
 
-    public RemoveRegistryData(ResourceKey<Registry<T>> registry, List<CompoundTag> entries)
+    public RemoveRegistryData(ResourceKey<Registry<T>> registry, NegatableList<CompoundTag> entries)
     {
         super(List.of());
         this.registry = registry;
@@ -28,29 +29,22 @@ public class RemoveRegistryData<T extends ConfigData> extends ConfigData
 
     public static final Codec<RemoveRegistryData<?>> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.STRING.xmap(s -> (ResourceKey)ModRegistries.getRegistry(s), key -> ModRegistries.getRegistryName(key)).fieldOf("registry").forGetter(data -> data.registry()),
-            CompoundTag.CODEC.listOf().fieldOf("matches").forGetter(data -> data.entries)
+            NegatableList.codec(CompoundTag.CODEC).fieldOf("matches").forGetter(data -> data.entries)
     ).apply(instance, RemoveRegistryData::new));
 
     public ResourceKey<Registry<T>> registry()
     {   return registry;
     }
-    public List<CompoundTag> entries()
+    public NegatableList<CompoundTag> entries()
     {   return entries;
     }
 
     public boolean matches(T object)
     {
-        Optional<Tag> serialized = ModRegistries.getCodec((ResourceKey) registry).encodeStart(NbtOps.INSTANCE, object).result();
-        if (serialized.isPresent())
-        {
-            for (CompoundTag data : entries)
-            {
-                if (NbtRequirement.compareNbt(data, serialized.get(), true))
-                {   return true;
-                }
-            }
-        }
-        return false;
+        Optional<Tag> serializedOpt = ModRegistries.getCodec((ResourceKey) registry).encodeStart(NbtOps.INSTANCE, object).result();
+        return serializedOpt.map(serialized ->
+        {   return entries.test(nbt -> NbtRequirement.compareNbt(nbt, serialized, true));
+        }).orElse(false);
     }
 
     @Override
