f5d670f5ccdace52c8c27d355a32fe54dfe749a6
MikulDev <gamer3mc@gmail.com>
2025-04-07 21:32:18 -0400

    Use constant DEFAULT_NAMESPACE to detect "minecraft" namespace

--- src/main/java/com/momosoftworks/coldsweat/common/command/argument/TempModifierArgument.java
+++ src/main/java/com/momosoftworks/coldsweat/common/command/argument/TempModifierArgument.java
@@ -33,7 +33,7 @@ public class TempModifierArgument implements ArgumentType<ResourceLocation>
     @Override
     public ResourceLocation parse(StringReader stringReader) throws CommandSyntaxException
     {   ResourceLocation location = ResourceLocation.read(stringReader);
-        if (location.getNamespace().equals("minecraft"))
+        if (location.getNamespace().equals(ResourceLocation.DEFAULT_NAMESPACE))
         {   location = new ResourceLocation(ColdSweat.MOD_ID, location.getPath());
         }
         return location;
