f0123874ad67d52982c311acbcd1b3d3355f66c2
MikulDev <gamer3mc@gmail.com>
2025-04-07 20:45:20 -0400

    Default to cold_sweat namespace for TempModifierArgument

--- src/main/java/com/momosoftworks/coldsweat/common/command/argument/TempModifierArgument.java
+++ src/main/java/com/momosoftworks/coldsweat/common/command/argument/TempModifierArgument.java
@@ -7,6 +7,7 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.api.registry.TempModifierRegistry;
 import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
@@ -31,7 +32,11 @@ public class TempModifierArgument implements ArgumentType<ResourceLocation>
 
     @Override
     public ResourceLocation parse(StringReader stringReader) throws CommandSyntaxException
-    {   return ResourceLocation.read(stringReader);
+    {   ResourceLocation location = ResourceLocation.read(stringReader);
+        if (location.getNamespace().equals("minecraft"))
+        {   location = new ResourceLocation(ColdSweat.MOD_ID, location.getPath());
+        }
+        return location;
     }
 
     @Override
