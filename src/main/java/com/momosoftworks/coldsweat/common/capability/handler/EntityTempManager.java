dcc52c4d902199f5e586d8887e27941be3a7cd01
MikulDev <gamer3mc@gmail.com>
2025-04-13 20:47:02 -0400

    Make attribute carry-over more null resistant
    Took 3 seconds

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
@@ -330,8 +330,14 @@ public class EntityTempManager
             // Get the old player's capability
             getTemperatureCap(oldPlayer).map(ITemperatureCap::getPersistentAttributes).orElse(new HashSet<>())
             .forEach(attr ->
-            {   newPlayer.getAttribute(attr).setBaseValue(oldPlayer.getAttribute(attr).getBaseValue());
-                getTemperatureCap(newPlayer).ifPresent(cap -> cap.markPersistentAttribute(attr));
+            {
+                AttributeInstance newAttr = newPlayer.getAttribute(attr);
+                AttributeInstance oldAttr = oldPlayer.getAttribute(attr);
+                if (newAttr != null && oldAttr != null)
+                {
+                    newAttr.setBaseValue(oldAttr.getBaseValue());
+                    getTemperatureCap(newPlayer).ifPresent(cap -> cap.markPersistentAttribute(attr));
+                }
             });
         }
     }
