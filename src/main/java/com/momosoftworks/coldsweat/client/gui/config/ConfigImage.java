942cbe435e24d624002de25175d2efab9920afbf
MikulDev <gamer3mc@gmail.com>
2025-04-15 20:59:07 -0400

    Make ConfigImage extent AbstractWidget

--- src/main/java/com/momosoftworks/coldsweat/client/gui/config/ConfigImage.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/config/ConfigImage.java
@@ -2,21 +2,23 @@ package com.momosoftworks.coldsweat.client.gui.config;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Widget;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.narration.NarratedElementType;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
 
-public class ConfigImage extends GuiComponent implements Widget, GuiEventListener, NarratableEntry
+public class ConfigImage extends AbstractWidget implements Widget, GuiEventListener, NarratableEntry
 {
     ResourceLocation texture;
     int x, y, width, height, u, v;
 
     public ConfigImage(ResourceLocation texture, int x, int y, int width, int height, int u, int v)
     {
+        super(x, y, width, height, new TextComponent(""));
         this.texture = texture;
         this.x = x;
         this.y = y;
