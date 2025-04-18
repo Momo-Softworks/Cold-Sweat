eb2c59c089112a9281252c820953ee26e80755f6
MikulDev <gamer3mc@gmail.com>
2025-04-17 21:41:58 -0400

    Use optional mapping instead of creating new temp caps in Temperature util methods

--- src/main/java/com/momosoftworks/coldsweat/api/util/Temperature.java
+++ src/main/java/com/momosoftworks/coldsweat/api/util/Temperature.java
@@ -171,7 +171,7 @@ public class Temperature
      * @return The first modifier of the given class that is applied to the player.
      */
     public static <T extends TempModifier> Optional<T> getModifier(LivingEntity entity, Trait trait, Class<T> modClass)
-    {   return getModifier(EntityTempManager.getTemperatureCap(entity).orElse(new PlayerTempCap()), trait, modClass);
+    {   return EntityTempManager.getTemperatureCap(entity).map(cap -> getModifier(cap, trait, modClass)).orElse(Optional.empty());
     }
 
     public static <T extends TempModifier> Optional<T> getModifier(ITemperatureCap cap, Trait trait, Class<T> modClass)
@@ -184,7 +184,7 @@ public class Temperature
     @Nullable
     public static TempModifier getModifier(LivingEntity entity, Trait trait, Predicate<TempModifier> condition)
     {
-        for (TempModifier modifier : EntityTempManager.getTemperatureCap(entity).orElse(new PlayerTempCap()).getModifiers(trait))
+        for (TempModifier modifier : EntityTempManager.getTemperatureCap(entity).map(cap -> cap.getModifiers(trait)).orElse(List.of()))
         {
             if (condition.test(modifier))
             {   return modifier;
