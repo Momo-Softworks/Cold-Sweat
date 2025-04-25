ec454aa0466133fd4745f82837e17acb40283a22
MikulDev <gamer3mc@gmail.com>
2025-04-24 21:53:47 -0400

    Make soul fire deal contact damage
    Took 1 minute

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinSoulFire.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinSoulFire.java
@@ -1,13 +1,11 @@
 package com.momosoftworks.coldsweat.mixin;
 
 import com.momosoftworks.coldsweat.config.ConfigSettings;
-import com.momosoftworks.coldsweat.compat.CompatManager;
-import com.momosoftworks.coldsweat.util.registries.ModDamageSources;
 import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoulFireBlock;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
@@ -20,19 +18,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(AbstractFireBlock.class)
 public class MixinSoulFire
 {
-    @Final
-    @Shadow
-    private float fireDamage;
+    @Shadow @Final private float fireDamage;
 
     @Inject(method = "entityInside(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;)V",
     at = @At("HEAD"), cancellable = true)
     public void entityInside(BlockState state, World level, BlockPos pos, Entity entity, CallbackInfo ci)
     {
-        if (state.getBlock() instanceof SoulFireBlock && ConfigSettings.COLD_SOUL_FIRE.get()
-        && !(entity instanceof ItemEntity && CompatManager.isSpiritLoaded()))
+        if (state.getBlock() instanceof SoulFireBlock && ConfigSettings.COLD_SOUL_FIRE.get())
         {
-            entity.hurt(ModDamageSources.COLD, this.fireDamage);
             entity.clearFire();
+            entity.hurt(DamageSource.IN_FIRE, this.fireDamage);
             ci.cancel();
         }
     }
