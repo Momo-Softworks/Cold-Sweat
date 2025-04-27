5b5038e7923ea1757811cfc5e92bc101795d18e6
MikulDev <gamer3mc@gmail.com>
2025-04-26 21:31:51 -0400

    Add entity climate builder for KubeJS

---
+++ src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/EntityClimateBuilderJS.java
@@ -0,0 +1,58 @@
+package com.momosoftworks.coldsweat.compat.kubejs.event.builder;
+
+import com.momosoftworks.coldsweat.api.util.Temperature;
+import com.momosoftworks.coldsweat.data.codec.configuration.EntityClimateData;
+import com.momosoftworks.coldsweat.data.codec.impl.ConfigData;
+import com.momosoftworks.coldsweat.data.codec.requirement.EntityRequirement;
+import com.momosoftworks.coldsweat.data.codec.util.NegatableList;
+import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
+import com.momosoftworks.coldsweat.util.serialization.RegistryHelper;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Collection;
+
+public class EntityClimateBuilderJS
+{
+    public NegatableList<EntityRequirement> entityPredicate = new NegatableList<>();
+    public double minTemp = -Double.MAX_VALUE;
+    public double maxTemp = Double.MAX_VALUE;
+    public double rate = 1;
+    public Temperature.Units units = Temperature.Units.MC;
+
+    public EntityClimateBuilderJS()
+    {}
+
+    public EntityClimateBuilderJS entities(String... entities)
+    {
+        Collection<EntityType<?>> entList = RegistryHelper.mapForgeRegistryTagList(ForgeRegistries.ENTITY_TYPES, ConfigHelper.getEntityTypes(entities));
+        this.entityPredicate.add(new EntityRequirement(entList, null), false);
+        return this;
+    }
+
+    public EntityClimateBuilderJS temperature(double minTemp, double maxTemp)
+    {
+        this.minTemp = minTemp;
+        this.maxTemp = maxTemp;
+        return this;
+    }
+
+    public EntityClimateBuilderJS rate(double rate)
+    {
+        this.rate = rate;
+        return this;
+    }
+
+    public EntityClimateBuilderJS units(String units)
+    {
+        this.units = Temperature.Units.fromID(units);
+        return this;
+    }
+
+    public EntityClimateData build()
+    {
+        EntityClimateData data = new EntityClimateData(entityPredicate, minTemp, maxTemp, rate, units);
+        data.setRegistryType(ConfigData.Type.KUBEJS);
+        return data;
+    }
+}
