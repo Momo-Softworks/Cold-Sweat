71cae7fa5559ac5877c4e03b648350009181b1bb
MikulDev <gamer3mc@gmail.com>
2025-04-28 23:32:17 -0400

    Use entity configured min/max climate temp

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/EntityClimateTempModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/EntityClimateTempModifier.java
@@ -1,6 +1,7 @@
 package com.momosoftworks.coldsweat.api.temperature.modifier;
 
 import com.momosoftworks.coldsweat.api.util.Temperature;
+import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.util.world.WorldHelper;
 import net.minecraft.world.entity.LivingEntity;
 
@@ -11,7 +12,19 @@ public class EntityClimateTempModifier extends TempModifier
     @Override
     protected Function<Double, Double> calculate(LivingEntity entity, Temperature.Trait trait)
     {
-        double worldTemp = WorldHelper.getRoughTemperatureAt(entity.level, entity.blockPosition(), true);
-        return temp -> temp + worldTemp;
+        switch (trait)
+        {
+            case WORLD ->
+            {   double worldTemp = WorldHelper.getRoughTemperatureAt(entity.level, entity.blockPosition(), true);
+                return temp -> temp + worldTemp;
+            }
+            case BURNING_POINT ->
+            {   return temp -> temp + ConfigSettings.ENTITY_CLIMATES.get().get(entity.getType()).getMaxTemp();
+            }
+            case FREEZING_POINT ->
+            {   return temp -> temp + ConfigSettings.ENTITY_CLIMATES.get().get(entity.getType()).getMinTemp();
+            }
+        }
+        return temp -> temp;
     }
 }
