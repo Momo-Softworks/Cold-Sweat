8ca6901af337d76f04be8b65376192564c379c43
MikulDev <gamer3mc@gmail.com>
2025-04-29 19:25:50 -0400

    Tweak soul sprout bud hitbox

--- src/main/java/com/momosoftworks/coldsweat/common/block/SoulStalkBlock.java
+++ src/main/java/com/momosoftworks/coldsweat/common/block/SoulStalkBlock.java
@@ -33,7 +33,7 @@ public class SoulStalkBlock extends Block implements IPlantable
     public static final IntegerProperty AGE = BlockStateProperties.AGE_15;
     public static final EnumProperty<Section> SECTION = EnumProperty.create("section", Section.class);
     protected static final VoxelShape SHAPE = Block.box(3.0D, 0.0D, 3.0D, 13.0D, 16.0D, 13.0D);
-    protected static final VoxelShape SHAPE_BUD = Block.box(5.0D, 0.0D, 5.0D, 11.0D, 13.0D, 11.0D);
+    protected static final VoxelShape SHAPE_BUD = Block.box(4.5D, 0.0D, 4.5D, 11.5D, 14.0D, 11.5D);
 
     public SoulStalkBlock(Properties p_49795_)
     {
@@ -48,7 +48,7 @@ public class SoulStalkBlock extends Block implements IPlantable
                 .sound(SoundType.BIG_DRIPLEAF)
                 .strength(0f, 0.5f)
                 .randomTicks()
-                .lightLevel(state -> state.getValue(SECTION).hasFruit() ? 6 : 0)
+                .lightLevel(state -> state.getValue(SECTION).hasFruit() ? 4 : 0)
                 .noOcclusion()
                 .noCollission();
     }
