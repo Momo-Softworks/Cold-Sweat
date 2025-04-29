8a21dda9d933054858a4f925f160c1e51e54597a
MikulDev <gamer3mc@gmail.com>
2025-04-29 19:27:20 -0400

    Synchronize cache reset
    Took 3 seconds

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
@@ -181,7 +181,7 @@ public class EntityTempManager
     }
 
     @SubscribeEvent
-    public static void cleanRemovedEntities(EntityLeaveWorldEvent event)
+    public static synchronized void cleanRemovedEntities(EntityLeaveWorldEvent event)
     {
         if (isTemperatureEnabled(event.getEntity()))
         {   CAP_CACHE.removeIf(ent -> ent.removed);
