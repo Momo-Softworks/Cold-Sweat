4c279d9ca57c2294685b84a79a7064b4317a7ac8
MikulDev <gamer3mc@gmail.com>
2025-05-01 18:54:53 -0400

    Fix dimension temp offset being unused

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/ElevationTempModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/ElevationTempModifier.java
@@ -111,10 +111,10 @@ public class ElevationTempModifier extends TempModifier
                 }
             }
             if (depthTemps.isEmpty())
-            {   return temp;
+            {   return temp + dimOffset;
             }
             // Calculate the weighted average of the depth temperatures
-            return CSMath.weightedAverage(depthTemps);
+            return CSMath.weightedAverage(depthTemps) + dimOffset;
         };
     }
 
