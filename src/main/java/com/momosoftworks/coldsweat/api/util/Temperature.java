ed6b69ee4aa95697a3b6a88b5a2ce858d094bdd5
MikulDev <gamer3mc@gmail.com>
2025-05-01 18:01:23 -0400

    Remove "ALL" trait, allow single modifier instance to apply to multiple traits

--- src/main/java/com/momosoftworks/coldsweat/api/util/Temperature.java
+++ src/main/java/com/momosoftworks/coldsweat/api/util/Temperature.java
@@ -165,7 +165,7 @@ public class Temperature
     }
 
     public static <T extends TempModifier> Optional<T> getModifier(ITemperatureCap cap, Trait trait, Class<T> modClass)
-    {   return (Optional<T>) cap.getAllModifiers(trait).stream().filter(modClass::isInstance).findFirst();
+    {   return (Optional<T>) cap.getModifiers(trait).stream().filter(modClass::isInstance).findFirst();
     }
 
     /**
@@ -174,7 +174,7 @@ public class Temperature
     @Nullable
     public static TempModifier getModifier(LivingEntity entity, Trait trait, Predicate<TempModifier> condition)
     {
-        for (TempModifier modifier : EntityTempManager.getTemperatureCap(entity).map(cap -> cap.getAllModifiers(trait)).orElse(List.of()))
+        for (TempModifier modifier : EntityTempManager.getTemperatureCap(entity).map(cap -> cap.getModifiers(trait)).orElse(List.of()))
         {
             if (condition.test(modifier))
             {   return modifier;
@@ -358,25 +358,12 @@ public class Temperature
 
     /**
      * Gets all TempModifiers of the specified type on the entity.<br>
-     * This includes modifiers applied to {@link Trait#ALL}
      * @param entity is the entity being sampled
      * @param trait determines which TempModifier list to pull from
      * @return an <b>IMMUTABLE</b> list of all TempModifiers for the specified trait
      */
     public static List<TempModifier> getModifiers(LivingEntity entity, Trait trait)
-    {   return EntityTempManager.getTemperatureCap(entity).map(cap -> ImmutableList.copyOf(cap.getAllModifiers(trait))).orElse(ImmutableList.of());
-    }
-
-    /**
-     * Directly gets the list of TempModifiers of the specified type on the entity.<br>
-     * Changes to this list will be reflected on the entity.<br>
-     * <b>For most operations, consider using the other helper methods instead.</b>
-     * @param entity is the entity being sampled
-     * @param trait determines which TempModifier list to pull from
-     * @return The TempModifiers for the specified trait.
-     */
-    public static List<TempModifier> accessModifiers(LivingEntity entity, Trait trait)
-    {   return EntityTempManager.getTemperatureCap(entity).map(cap -> cap.getModifiers(trait)).orElse(ImmutableList.of());
+    {   return EntityTempManager.getTemperatureCap(entity).map(cap -> ImmutableList.copyOf(cap.getModifiers(trait))).orElse(ImmutableList.of());
     }
 
     /**
@@ -388,7 +375,7 @@ public class Temperature
     {
         EntityTempManager.getTemperatureCap(entity).ifPresent(cap ->
         {
-            cap.getAllModifiers(trait).forEach(action);
+            cap.getModifiers(trait).forEach(action);
         });
     }
 
@@ -396,7 +383,7 @@ public class Temperature
     {
         EntityTempManager.getTemperatureCap(entity).ifPresent(cap ->
         {
-            CSMath.breakableForEach(cap.getAllModifiers(trait), action);
+            CSMath.breakableForEach(cap.getModifiers(trait), action);
         });
     }
 
@@ -459,8 +446,6 @@ public class Temperature
      * {@link #HEAT_RESISTANCE}: Resistance to heat temperature-related damage. <br>
      * {@link #COLD_DAMPENING}: Changes the rate of body temperature increase. <br>
      * {@link #HEAT_DAMPENING}: Changes the rate of body temperature decrease. <br>
-     * <br>
-     * {@link #ALL}: Modifiers added to this trait are applied to all temperature stats. <br>
      */
     public enum Trait implements StringRepresentable
     {
@@ -475,9 +460,7 @@ public class Temperature
         COLD_RESISTANCE("cold_resistance", true, true, true),
         HEAT_RESISTANCE("heat_resistance", true, true, true),
         COLD_DAMPENING("cold_dampening", true, true, true),
-        HEAT_DAMPENING("heat_dampening", true, true, true),
-
-        ALL("all", false, true, false);
+        HEAT_DAMPENING("heat_dampening", true, true, true);
 
         public static final Codec<Trait> CODEC = StringRepresentable.fromEnum(Trait::values);
 
@@ -566,5 +549,4 @@ public class Temperature
         {   return id;
         }
     }
-
 }
