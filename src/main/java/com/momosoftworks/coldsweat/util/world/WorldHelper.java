0da3de6857152bd3ca5b5127b2c72a16445ef0d4
MikulDev <gamer3mc@gmail.com>
2025-05-03 18:23:08 -0400

    Remove useless flag from WorldHelper#shouldFreeze()

--- src/main/java/com/momosoftworks/coldsweat/util/world/WorldHelper.java
+++ src/main/java/com/momosoftworks/coldsweat/util/world/WorldHelper.java
@@ -755,7 +755,7 @@ public abstract class WorldHelper
             if (surroundedByBlock(levelReader, pos, Blocks.ICE))
             {   return true;
             }
-            DynamicHolder<Boolean> freezingTemp = DynamicHolder.create(() -> getRoughTemperatureAt(serverLevel, pos, 4) < 0f);
+            DynamicHolder<Boolean> freezingTemp = DynamicHolder.create(() -> getRoughTemperatureAt(serverLevel, pos) < 0f);
 
             if (!mustBeAtEdge)
             {   return freezingTemp.get();
