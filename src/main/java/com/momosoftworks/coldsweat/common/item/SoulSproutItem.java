3b39f5cfeab795b509a67a986d568b9b98d8e254
MikulDev <gamer3mc@gmail.com>
2025-05-02 23:52:29 -0400

    Add "bonemeal" function to soul stalk w/ glowstone dust

--- src/main/java/com/momosoftworks/coldsweat/common/item/SoulSproutItem.java
+++ src/main/java/com/momosoftworks/coldsweat/common/item/SoulSproutItem.java
@@ -1,12 +1,6 @@
 package com.momosoftworks.coldsweat.common.item;
 
-import com.momosoftworks.coldsweat.util.registries.ModBlocks;
 import net.minecraft.advancements.Advancement;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.BlockSource;
-import net.minecraft.core.Position;
-import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
-import net.minecraft.core.dispenser.DispenseItemBehavior;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
@@ -16,14 +10,11 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DispenserBlock;
-import net.minecraft.world.level.block.state.BlockState;
 
 public class SoulSproutItem extends ItemNameBlockItem
 {
     public SoulSproutItem(Block block, Properties properties)
     {   super(block, properties);
-        DispenserBlock.registerBehavior(this, DISPENSE_BEHAVIOR);
     }
 
     @Override
@@ -48,27 +39,4 @@ public class SoulSproutItem extends ItemNameBlockItem
     {   entity.clearFire();
         return super.finishUsingItem(stack, level, entity);
     }
-
-    public static final DispenseItemBehavior DISPENSE_BEHAVIOR = new DefaultDispenseItemBehavior()
-    {
-        @Override
-        protected ItemStack execute(BlockSource source, ItemStack stack)
-        {
-            Level level = source.getLevel();
-            Position position = DispenserBlock.getDispensePosition(source);
-            BlockPos pos = new BlockPos(position);
-
-            if (level.getBlockState(pos).getMaterial().isReplaceable())
-            {
-                BlockState state = ModBlocks.SOUL_STALK.defaultBlockState();
-                if (state.canSurvive(level, pos))
-                {
-                    level.setBlock(pos, state, 3);
-                    stack.shrink(1);
-                    return stack;
-                }
-            }
-            return super.execute(source, stack);
-        }
-    };
 }
