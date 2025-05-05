8d0a18429bf512b71d7b8412c811cc2c4a8e5f13
MikulDev <gamer3mc@gmail.com>
2025-05-05 00:56:34 -0400

    Give ice resistance when eating golden apple

--- src/main/java/com/momosoftworks/coldsweat/common/event/ApplyPotionEffects.java
+++ src/main/java/com/momosoftworks/coldsweat/common/event/ApplyPotionEffects.java
@@ -3,13 +3,16 @@ package com.momosoftworks.coldsweat.common.event;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.util.registries.ModEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.potion.EffectInstance;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
-public class GracePeriod
+public class ApplyPotionEffects
 {
     @SubscribeEvent
     public static void onSpawn(EntityJoinWorldEvent event)
@@ -19,6 +22,15 @@ public class GracePeriod
         {
             event.getEntity().getPersistentData().putBoolean("GivenGracePeriod", true);
             ((PlayerEntity) event.getEntity()).addEffect(new EffectInstance(ModEffects.GRACE, ConfigSettings.GRACE_LENGTH.get(), 0, false, false, true));
+        }
+    }
+
+    @SubscribeEvent
+    public static void onGoldenAppleEaten(LivingEntityUseItemEvent.Finish event)
+    {
+        ItemStack item = event.getItem();
+        if (item.getItem() == Items.ENCHANTED_GOLDEN_APPLE)
+        {   event.getEntityLiving().addEffect(new EffectInstance(ModEffects.ICE_RESISTANCE, 6000, 0));
         }
     }
 }
