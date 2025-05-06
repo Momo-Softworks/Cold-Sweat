aa737429af136793ffcefb08413120f1beca8d5d
MikulDev <gamer3mc@gmail.com>
2025-05-06 12:29:49 -0400

    Check for items in boiler on init

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/BoilerBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/BoilerBlockEntity.java
@@ -105,9 +105,12 @@ public class BoilerBlockEntity extends HearthBlockEntity
             }
         }
         // Update lit state
-        boolean shouldBeLit = this.getFuel() > 0 && (this.hasWaterskins || this.hasDrinkables || this.shouldUseHotFuel);
-        if (state.getValue(BoilerBlock.LIT) != shouldBeLit)
-        {   level.setBlock(pos, state.setValue(BoilerBlock.LIT, shouldBeLit), 3);
+        if (!this.level.isClientSide())
+        {
+            boolean shouldBeLit = this.getFuel() > 0 && (this.hasWaterskins || this.hasDrinkables || this.shouldUseHotFuel);
+            if (state.getValue(BoilerBlock.LIT) != shouldBeLit)
+            {   level.setBlock(pos, state.setValue(BoilerBlock.LIT, shouldBeLit), 3);
+            }
         }
     }
 
@@ -131,6 +134,13 @@ public class BoilerBlockEntity extends HearthBlockEntity
             {   this.hasDrinkables = true;
             }
         }
+    }
+
+    @Override
+    protected void init()
+    {
+        super.init();
+        this.checkForItems();
     }
 
     @Override
