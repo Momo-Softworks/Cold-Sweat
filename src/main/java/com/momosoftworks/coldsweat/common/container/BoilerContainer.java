30754eaa023098d5b38487b19f5398118e8b463a
MikulDev <gamer3mc@gmail.com>
2025-05-06 01:25:16 -0400

    Use slot mayPlace() methods for shift-clicking items in boiler

--- src/main/java/com/momosoftworks/coldsweat/common/container/BoilerContainer.java
+++ src/main/java/com/momosoftworks/coldsweat/common/container/BoilerContainer.java
@@ -111,14 +111,14 @@ public class BoilerContainer extends AbstractContainerMenu
             }
             else
             {
-                if (this.slots.get(1).mayPlace(itemstack))
+                if (this.getSlot(1).mayPlace(itemstack))
                 {
                     if (!this.moveItemStackTo(itemstack1, 1, 10, false))
                     {   slot.onQuickCraft(itemstack1, itemstack);
                         return ItemStack.EMPTY;
                     }
                 }
-                else if (this.slots.get(0).mayPlace(itemstack))
+                else if (this.getSlot(0).mayPlace(itemstack))
                 {
                     if (!this.moveItemStackTo(itemstack1, 0, 1, false))
                     {   slot.onQuickCraft(itemstack1, itemstack);
