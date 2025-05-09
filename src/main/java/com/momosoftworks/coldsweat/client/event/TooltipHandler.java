d340a8c0b714af00388f53b95ba3b021600a80bd
MikulDev <gamer3mc@gmail.com>
2025-05-09 01:56:52 -0400

    Add option to hide insulators with unmet requirements

--- src/main/java/com/momosoftworks/coldsweat/client/event/TooltipHandler.java
+++ src/main/java/com/momosoftworks/coldsweat/client/event/TooltipHandler.java
@@ -359,7 +359,7 @@ public class TooltipHandler
             List<Insulation> unmetArmorInsulation = new ArrayList<>();
             {
                 for (InsulatorData insulator : ConfigSettings.INSULATING_ARMORS.get().get(item))
-                {   validateInsulator(insulator, armorInsulation, unmetArmorInsulation);
+                {   validateInsulator(insulator, armorInsulation, insulator.hideIfUnmet() ? new ArrayList<>() : unmetArmorInsulation);
                 }
 
                 ItemInsulationManager.getInsulationCap(stack).ifPresent(cap ->
@@ -374,7 +374,7 @@ public class TooltipHandler
                     {
                         Pair<ItemStack, Collection<InsulatorData>> pair = insulatorPairs.get(i);
                         for (InsulatorData insulator : pair.getSecond())
-                        {   validateInsulator(insulator, armorInsulation, unmetArmorInsulation);
+                        {   validateInsulator(insulator, armorInsulation, insulator.hideIfUnmet() ? new ArrayList<>() : unmetArmorInsulation);
                         }
                     }
                 });
@@ -392,7 +392,7 @@ public class TooltipHandler
                 List<Insulation> insulation = new ArrayList<>();
                 List<Insulation> unmetInsulation = new ArrayList<>();
                 for (InsulatorData insulator : ConfigSettings.INSULATION_ITEMS.get().get(item))
-                {   validateInsulator(insulator, insulation, unmetInsulation);
+                {   validateInsulator(insulator, insulation, insulator.hideIfUnmet() ? new ArrayList<>() : unmetArmorInsulation);
                 }
                 if (!insulation.isEmpty() && !insulation.equals(Insulation.splitList(armorInsulation)))
                 {   elements.add(tooltipStartIndex, Either.right(new InsulationTooltip(insulation, Insulation.Slot.ITEM, stack, false)));
