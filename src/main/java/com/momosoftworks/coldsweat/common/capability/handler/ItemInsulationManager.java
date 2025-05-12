c3c49081d1b19efe243eb9078077a7ce5b30fe8c
MikulDev <gamer3mc@gmail.com>
2025-05-12 01:16:18 -0400

    Only prevent armor insulation if armor has insulation value

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ItemInsulationManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ItemInsulationManager.java
@@ -160,7 +160,9 @@ public class ItemInsulationManager
      */
     public static boolean isInsulatable(ItemStack stack)
     {
-        return stack.getItem() instanceof Wearable && !hasBuiltinInsulation(stack);
+        return stack.getItem() instanceof Wearable
+            && !ConfigSettings.INSULATION_BLACKLIST.get().contains(stack.getItem())
+            && getBuiltinInsulation(stack).isEmpty();
     }
 
     /**
