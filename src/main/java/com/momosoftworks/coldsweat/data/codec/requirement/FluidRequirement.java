044d47f97f2cc026a959d6eec402115adf3e23ca
MikulDev <gamer3mc@gmail.com>
2025-05-14 00:03:55 -0400

    Update FluidRequirement
    Took 2 minutes

--- src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/FluidRequirement.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/requirement/FluidRequirement.java
@@ -7,48 +7,42 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
-import net.minecraft.tags.FluidTags;
 import net.minecraft.tags.ITag;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 public class FluidRequirement
 {
-    private final Optional<List<Either<ITag<Fluid>, Fluid>>> fluids;
-    private final Optional<ITag<Fluid>> tag;
-    private final Optional<BlockRequirement.StateRequirement> state;
-    private final Optional<NbtRequirement> nbt;
+    private final List<Either<ITag<Fluid>, Fluid>> fluids;
+    private final BlockRequirement.StateRequirement state;
+    private final Optional<Boolean> isSource;
 
-    public FluidRequirement(Optional<List<Either<ITag<Fluid>, Fluid>>> fluids, Optional<ITag<Fluid>> tag, Optional<BlockRequirement.StateRequirement> state, Optional<NbtRequirement> nbt)
+    public FluidRequirement(List<Either<ITag<Fluid>, Fluid>> fluids, BlockRequirement.StateRequirement state, Optional<Boolean> isSource)
     {
         this.fluids = fluids;
-        this.tag = tag;
         this.state = state;
-        this.nbt = nbt;
+        this.isSource = isSource;
     }
 
     public static final Codec<FluidRequirement> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            ConfigHelper.tagOrBuiltinCodec(Registry.FLUID_REGISTRY, Registry.FLUID).listOf().optionalFieldOf("fluids").forGetter(predicate -> predicate.fluids),
-            ITag.codec(FluidTags::getAllTags).optionalFieldOf("tag").forGetter(predicate -> predicate.tag),
-            BlockRequirement.StateRequirement.CODEC.optionalFieldOf("state").forGetter(predicate -> predicate.state),
-            NbtRequirement.CODEC.optionalFieldOf("nbt").forGetter(predicate -> predicate.nbt)
+            ConfigHelper.tagOrBuiltinCodec(Registry.FLUID_REGISTRY, Registry.FLUID).listOf().optionalFieldOf("fluids", Arrays.asList()).forGetter(FluidRequirement::fluids),
+            BlockRequirement.StateRequirement.CODEC.optionalFieldOf("state", BlockRequirement.StateRequirement.NONE).forGetter(FluidRequirement::state),
+            Codec.BOOL.optionalFieldOf("is_source").forGetter(FluidRequirement::isSource)
     ).apply(instance, FluidRequirement::new));
 
-    public Optional<List<Either<ITag<Fluid>, Fluid>>> fluids()
+    public List<Either<ITag<Fluid>, Fluid>> fluids()
     {   return fluids;
     }
-    public Optional<ITag<Fluid>> tag()
-    {   return tag;
-    }
-    public Optional<BlockRequirement.StateRequirement> state()
+    public BlockRequirement.StateRequirement state()
     {   return state;
     }
-    public Optional<NbtRequirement> nbt()
-    {   return nbt;
+    public Optional<Boolean> isSource()
+    {   return isSource;
     }
 
     public boolean test(World pLevel, BlockPos pPos)
@@ -62,16 +56,16 @@ public class FluidRequirement
         }
     }
 
-    public boolean test(FluidState fluidState)
+    public boolean test(FluidState state)
     {
-        if (this.tag.isPresent() && !fluidState.is(this.tag.get()))
+        if (this.fluids.stream().anyMatch(either -> either.map(tag -> state.is(tag), fluid -> state.getType() == fluid)))
         {   return false;
         }
-        else if (this.fluids.isPresent() && fluids.get().stream().noneMatch(either -> either.map(tag -> tag.contains(fluidState.getType()), fluid -> fluid == fluidState.getType())))
+        if (this.isSource.isPresent() && this.isSource.get() != state.isSource())
         {   return false;
         }
         else
-        {   return !this.state.isPresent() || state.get().test(fluidState);
+        {   return this.state.test(state);
         }
     }
 
@@ -88,8 +82,6 @@ public class FluidRequirement
 
         FluidRequirement that = (FluidRequirement) obj;
         return fluids.equals(that.fluids)
-            && tag.equals(that.tag)
-            && state.equals(that.state)
-            && nbt.equals(that.nbt);
+            && state.equals(that.state);
     }
 }
