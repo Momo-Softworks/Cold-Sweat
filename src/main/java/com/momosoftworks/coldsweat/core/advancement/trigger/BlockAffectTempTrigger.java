a0eeedd37f32fd723270467941baba076129b494
MikulDev <gamer3mc@gmail.com>
2025-05-21 20:48:46 -0400

    Use null-safe map check in BlockAffectTempTrigger

--- src/main/java/com/momosoftworks/coldsweat/core/advancement/trigger/BlockAffectTempTrigger.java
+++ src/main/java/com/momosoftworks/coldsweat/core/advancement/trigger/BlockAffectTempTrigger.java
@@ -75,7 +75,7 @@ public class BlockAffectTempTrigger extends SimpleCriterionTrigger<BlockAffectTe
             return this.distance.matches(distance)
                 && this.totalEffect.matches(totalEffect)
                 && this.block.matches(player.getLevel(), pos)
-                && conditions.stream().allMatch(condition -> condition.matches(temps.get(condition.trait())));
+                && conditions.stream().allMatch(condition -> condition.matches(temps.getOrDefault(condition.trait(), 0d)));
         }
 
         @Override
