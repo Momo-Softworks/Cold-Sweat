f8cb8228ed0d6ed01234e235e72645ffcb95884b
MikulDev <gamer3mc@gmail.com>
2025-06-05 14:02:40 -0400

    Ensure preferred units sync upon respawn

--- src/main/java/com/momosoftworks/coldsweat/client/event/ClientJoinSetup.java
+++ src/main/java/com/momosoftworks/coldsweat/client/event/ClientJoinSetup.java
@@ -6,11 +6,13 @@ import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.core.init.TempModifierInit;
 import com.momosoftworks.coldsweat.core.network.message.ClientConfigAskMessage;
 import com.momosoftworks.coldsweat.core.network.message.SyncPreferredUnitsMessage;
+import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 
 @EventBusSubscriber(Dist.CLIENT)
@@ -21,9 +23,16 @@ public class ClientJoinSetup
     {
         // Get configs
         PacketDistributor.sendToServer(new ClientConfigAskMessage());
-        PacketDistributor.sendToServer(new SyncPreferredUnitsMessage(ConfigSettings.CELSIUS.get() ? Temperature.Units.C : Temperature.Units.F));
         // Rebuild TempModifier registries
         TempModifierInit.buildModifierRegistries();
         NeoForge.EVENT_BUS.post(new EdiblesRegisterEvent());
+    }
+
+    @SubscribeEvent
+    public static void onEverySpawn(EntityJoinLevelEvent event)
+    {
+        if (event.getEntity() == Minecraft.getInstance().player)
+        {   PacketDistributor.sendToServer(new SyncPreferredUnitsMessage(ConfigSettings.CELSIUS.get() ? Temperature.Units.C : Temperature.Units.F));
+        }
     }
 }
