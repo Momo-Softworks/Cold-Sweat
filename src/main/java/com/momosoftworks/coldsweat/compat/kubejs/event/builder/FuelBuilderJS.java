affa2344bf1f9ea1fb85483615a826efd88d4cb0
MikulDev <gamer3mc@gmail.com>
2025-06-06 22:05:39 -0400

    Fix wrong method name in FuelBuilderJS

--- src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/FuelBuilderJS.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/FuelBuilderJS.java
@@ -28,7 +28,7 @@ public class FuelBuilderJS
         return this;
     }
 
-    public FuelBuilderJS temperature(double temperature)
+    public FuelBuilderJS fuel(double temperature)
     {
         this.fuel = temperature;
         return this;
