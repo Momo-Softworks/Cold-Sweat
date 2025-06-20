86c7ae4f778b44e75af80ecdc45756708f00403f
MikulDev <gamer3mc@gmail.com>
2025-06-19 23:54:06 -0400

    Fix client RegistryAccess used instead of server RegistryAccess in singleplayer

--- src/main/java/com/momosoftworks/coldsweat/util/serialization/RegistryHelper.java
+++ src/main/java/com/momosoftworks/coldsweat/util/serialization/RegistryHelper.java
@@ -2,6 +2,7 @@ package com.momosoftworks.coldsweat.util.serialization;
 
 import com.mojang.datafixers.util.Either;
 import com.momosoftworks.coldsweat.util.math.CSMath;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
@@ -45,7 +46,10 @@ public class RegistryHelper
     {
         @SubscribeEvent
         public static void onClientLoading(ClientPlayerNetworkEvent.LoggedInEvent event)
-        {   REGISTRY_ACCESS = event.getPlayer().connection.registryAccess();
+        {
+            if (!Minecraft.getInstance().hasSingleplayerServer())
+            {   REGISTRY_ACCESS = event.getPlayer().connection.registryAccess();
+            }
         }
     }
 
