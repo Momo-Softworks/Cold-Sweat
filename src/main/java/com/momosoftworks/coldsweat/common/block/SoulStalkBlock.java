d2e0f239066b3e3fbbebd4ffbf945c3246412791
MikulDev <gamer3mc@gmail.com>
2025-06-19 21:35:13 -0400

    Don't try to fix block state on random tick

--- src/main/java/com/momosoftworks/coldsweat/common/block/SoulStalkBlock.java
+++ src/main/java/com/momosoftworks/coldsweat/common/block/SoulStalkBlock.java
@@ -135,7 +135,6 @@ public class SoulStalkBlock extends Block implements IPlantable
     @Override
     public void randomTick(BlockState state, ServerLevel level, BlockPos pos, Random rand)
     {
-        this.ensureProperState(level, pos);
         if (level.isEmptyBlock(pos.above()))
         {
             // Get the height of the plant
