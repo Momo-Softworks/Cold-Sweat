e37020e0852bb543ecff4a820cd21569e979cb5f
MikulDev <gamer3mc@gmail.com>
2025-06-19 21:35:00 -0400

    Fix dimension temp override not working properly

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/ElevationTempModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/ElevationTempModifier.java
@@ -36,7 +36,7 @@ public class ElevationTempModifier extends TempModifier
         // If a dimension temperature override is defined, return
         DimensionTempData dimTempOverride = ConfigSettings.DIMENSION_TEMPS.get(entity.level.registryAccess()).get(level.dimensionTypeRegistration());
         if (dimTempOverride != null)
-        {   return temp -> temp + dimTempOverride.getTemperature();
+        {   return temp -> temp;
         }
         // Don't calculate elevation for roofed dimensions
         if (level.dimensionType().hasCeiling()) return temp -> temp;
@@ -82,9 +82,6 @@ public class ElevationTempModifier extends TempModifier
         }
         double midTemp = Temperature.getNeutralWorldTemp(entity);
 
-        DimensionTempData dimTempOffsetConf = ConfigSettings.DIMENSION_OFFSETS.get(entity.level.registryAccess()).get(level.dimensionTypeRegistration());
-        double dimOffset = dimTempOffsetConf != null ? dimTempOffsetConf.getTemperature() : 0;
-
         return temp ->
         {
             List<Pair<Double, Double>> depthTemps = new ArrayList<>();
@@ -112,10 +109,10 @@ public class ElevationTempModifier extends TempModifier
                 }
             }
             if (depthTemps.isEmpty())
-            {   return temp + dimOffset;
+            {   return temp;
             }
             // Calculate the weighted average of the depth temperatures
-            return CSMath.weightedAverage(depthTemps) + dimOffset;
+            return CSMath.weightedAverage(depthTemps);
         };
     }
 
