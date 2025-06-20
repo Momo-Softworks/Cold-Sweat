5f3d4c5d74de0ce76b9e86ab0a4259ce78d9ee3c
MikulDev <gamer3mc@gmail.com>
2025-06-19 21:50:40 -0400

    Make dripstone caves have no temperature

--- src/main/java/com/momosoftworks/coldsweat/config/spec/WorldSettingsConfig.java
+++ src/main/java/com/momosoftworks/coldsweat/config/spec/WorldSettingsConfig.java
@@ -157,7 +157,8 @@ public class WorldSettingsConfig
                             List.of("minecraft:badlands", 84, 120, "F"),
                             List.of("minecraft:wooded_badlands", 80, 108, "F"),
                             List.of("minecraft:eroded_badlands", 88, 120, "F"),
-                            List.of("minecraft:deep_dark", 63, 63, "F"))
+                            List.of("minecraft:deep_dark", 63, 63, "F"),
+                            List.of("minecraft:dripstone_caves", "disable"))
                      .addIf(CompatManager.isBiomesOPlentyLoaded(),
                             () -> List.of("biomesoplenty:bayou", 67, 78, "F"),
                             () -> List.of("biomesoplenty:bog", 62, 73, "F"),
