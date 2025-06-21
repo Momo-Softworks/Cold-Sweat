3aa9e2f61ea64b8c212113ebe927c35c51a95c3f
MikulDev <gamer3mc@gmail.com>
2025-06-21 16:53:55 -0400

    Add dispense behavior to soul sprout

--- src/main/java/com/momosoftworks/coldsweat/common/item/SoulSproutItem.java
+++ src/main/java/com/momosoftworks/coldsweat/common/item/SoulSproutItem.java
@@ -1,6 +1,15 @@
 package com.momosoftworks.coldsweat.common.item;
 
+import com.momosoftworks.coldsweat.common.block.SoulStalkBlock;
+import com.momosoftworks.coldsweat.data.tag.ModBlockTags;
+import com.momosoftworks.coldsweat.util.registries.ModBlocks;
+import com.momosoftworks.coldsweat.util.registries.ModItems;
 import net.minecraft.advancements.Advancement;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.BlockSource;
+import net.minecraft.core.Direction;
+import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.core.dispenser.DispenseItemBehavior;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
@@ -10,11 +19,14 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class SoulSproutItem extends ItemNameBlockItem
 {
     public SoulSproutItem(Block block, Properties properties)
     {   super(block, properties);
+        DispenserBlock.registerBehavior(this, DISPENSE_BEHAVIOR);
     }
 
     @Override
@@ -39,4 +51,31 @@ public class SoulSproutItem extends ItemNameBlockItem
     {   entity.clearFire();
         return super.finishUsingItem(stack, level, entity);
     }
+
+    public static final DispenseItemBehavior DISPENSE_BEHAVIOR = new DefaultDispenseItemBehavior()
+    {
+        @Override
+        protected ItemStack execute(BlockSource source, ItemStack stack)
+        {
+            if (stack.is(ModItems.SOUL_SPROUT))
+            {
+                Level level = source.getLevel();
+                Direction direction = source.getBlockState().getValue(DispenserBlock.FACING);
+                BlockPos frontPos = new BlockPos(DispenserBlock.getDispensePosition(source));
+                BlockState frontState = level.getBlockState(frontPos);
+                BlockState groundState = level.getBlockState(frontPos.below());
+
+                if (frontState.getMaterial().isReplaceable() && frontState.getFluidState().isEmpty()
+                && groundState.is(ModBlockTags.SOUL_STALK_PLACEABLE_ON))
+                {
+                    level.setBlock(frontPos, ModBlocks.SOUL_STALK.defaultBlockState().setValue(SoulStalkBlock.SECTION, SoulStalkBlock.Section.BUD), 3);
+                    this.playAnimation(source, direction);
+                    this.playSound(source);
+                    stack.shrink(1);
+                    return stack;
+                }
+            }
+            return super.execute(source, stack);
+        }
+    };
 }
