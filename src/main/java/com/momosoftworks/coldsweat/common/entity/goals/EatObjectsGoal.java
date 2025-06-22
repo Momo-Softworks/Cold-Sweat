1b0af0f560714d192651a7c5d3739456102c3138
MikulDev <gamer3mc@gmail.com>
2025-06-22 00:38:11 -0400

    Use #chameleon_eats entity tag for entities that chameleon can eat

--- src/main/java/com/momosoftworks/coldsweat/common/entity/goals/EatObjectsGoal.java
+++ src/main/java/com/momosoftworks/coldsweat/common/entity/goals/EatObjectsGoal.java
@@ -5,8 +5,8 @@ import com.momosoftworks.coldsweat.common.entity.data.edible.ChameleonEdibles;
 import com.momosoftworks.coldsweat.common.entity.data.edible.Edible;
 import com.momosoftworks.coldsweat.core.event.TaskScheduler;
 import com.momosoftworks.coldsweat.core.init.ModSounds;
-import com.momosoftworks.coldsweat.util.serialization.NBTHelper;
 import com.momosoftworks.coldsweat.util.world.WorldHelper;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.goal.*;
@@ -22,13 +22,13 @@ import java.util.Optional;
 
 public class EatObjectsGoal extends Goal
 {
-    List<EntityType<?>> wantedEntities;
+    TagKey<EntityType<?>> wantedEntities;
     Chameleon entity;
     Entity target;
     boolean stoppedTasks;
     Vec3 lookPos = null;
 
-    public EatObjectsGoal(Chameleon chameleon, List<EntityType<?>> wantedEntities)
+    public EatObjectsGoal(Chameleon chameleon, TagKey<EntityType<?>> wantedEntities)
     {
         this.wantedEntities = wantedEntities;
         this.entity = chameleon;
@@ -67,7 +67,7 @@ public class EatObjectsGoal extends Goal
                     }
                 }
             }
-            else if (this.wantedEntities.contains(ent.getType()))
+            else if (ent.getType().is(this.wantedEntities))
             {
                 this.target = ent;
                 this.lookPos = ent.position();
