e716cb6811e2198d3f1dc3c7dfe5a84d051c3204
MikulDev <gamer3mc@gmail.com>
2025-06-22 14:48:29 -0400

    Allow insulating armor items to get insulation

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ItemInsulationManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/ItemInsulationManager.java
@@ -35,7 +35,6 @@ import net.minecraftforge.fml.common.Mod;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.*;
-import java.util.stream.Stream;
 
 @Mod.EventBusSubscriber
 public class ItemInsulationManager
@@ -158,31 +157,36 @@ public class ItemInsulationManager
     {
         return stack.getItem() instanceof Equipable
             && !ConfigSettings.INSULATION_BLACKLIST.get().contains(stack.getItem())
-            && getBuiltinInsulation(stack).isEmpty();
+            && getInsulatorInsulation(stack).isEmpty();
+    }
+
+    public static int getSlotsFilled(Collection<InsulatorData> insulators)
+    {
+        int slots = 0;
+        for (InsulatorData data : insulators)
+        {
+            if (data.fillSlots())
+            {   slots += Insulation.splitList(data.insulation()).size();
+            }
+            else slots++;
+        }
+        return slots;
     }
 
     /**
      * Gives a collection of all insulation that is built-in to the item (not applied via sewing)
      * @return an IMMUTABLE list of insulation the item has.
      */
-    public static List<Insulation> getBuiltinInsulation(ItemStack stack)
+    public static List<Insulation> getInsulatorInsulation(ItemStack stack)
     {
-        if (!hasBuiltinInsulation(stack)) return ImmutableList.of();
+        if (!ConfigSettings.INSULATION_ITEMS.get().containsKey(stack.getItem()))
+        {   return ImmutableList.of();
+        }
 
-        return Stream.of(ConfigSettings.INSULATION_ITEMS.get().get(stack.getItem()),
-                         ConfigSettings.INSULATING_ARMORS.get().get(stack.getItem()),
-                         ConfigSettings.INSULATING_CURIOS.get().get(stack.getItem()))
-               .flatMap(Collection::stream).map(InsulatorData::insulation)
+        return ConfigSettings.INSULATION_ITEMS.get().get(stack.getItem()).stream()
+               .map(InsulatorData::insulation)
                .flatMap(List::stream)
                .filter(ins -> !ins.isEmpty()).toList();
-    }
-
-    public static boolean hasBuiltinInsulation(ItemStack stack)
-    {
-        Item item = stack.getItem();
-        return ConfigSettings.INSULATION_ITEMS.get().containsKey(item)
-            || ConfigSettings.INSULATING_ARMORS.get().containsKey(item)
-            || ConfigSettings.INSULATING_CURIOS.get().containsKey(item);
     }
 
     /**
