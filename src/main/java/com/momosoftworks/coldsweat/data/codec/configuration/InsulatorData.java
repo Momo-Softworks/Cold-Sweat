a725f8655f7ad376f79fa2948fd5cc3ec8c71cf2
MikulDev <gamer3mc@gmail.com>
2025-06-24 15:40:51 -0400

    Fix tooltips for compound & non-fill armor insulation

--- src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/InsulatorData.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/InsulatorData.java
@@ -159,7 +159,7 @@ public class InsulatorData extends ConfigData implements RequirementHolder, IFor
         }
 
         CompoundTag tag = entry.size() > 4 ? NBTHelper.parseCompoundNbt((String) entry.get(4)) : new CompoundTag();
-        boolean fillSlots = entry.size() > 5 && (Boolean) entry.get(5);
+        boolean fillSlots = entry.size() <= 5 || (Boolean) entry.get(5);
 
         ItemRequirement itemRequirement = new ItemRequirement(items, new NbtRequirement(tag));
 
