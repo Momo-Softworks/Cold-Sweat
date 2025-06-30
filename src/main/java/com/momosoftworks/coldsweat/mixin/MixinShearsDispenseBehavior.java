048372ca6afacc8d9cea3c939459d28385e9ff2e
MikulDev <gamer3mc@gmail.com>
2025-06-29 22:05:31 -0400

    Fix inconsistent goat shear behavior

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinShearsDispenseBehavior.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinShearsDispenseBehavior.java
@@ -2,12 +2,10 @@ package com.momosoftworks.coldsweat.mixin;
 
 import com.momosoftworks.coldsweat.common.capability.handler.ShearableFurManager;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.BlockSource;
 import net.minecraft.core.dispenser.ShearsDispenseItemBehavior;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,19 +15,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ShearsDispenseItemBehavior.class)
 public class MixinShearsDispenseBehavior
 {
-    private static ItemStack SHEARS = ItemStack.EMPTY;
-
-    @Inject(method = "execute", at = @At("HEAD"))
-    private void storeShearsItem(BlockSource blockSource, ItemStack stack, CallbackInfoReturnable<ItemStack> cir)
-    {   SHEARS = stack;
-    }
-
-    @Inject(method = "tryShearLivingEntity", at = @At("TAIL"))
+    @Inject(method = "tryShearLivingEntity", at = @At("TAIL"), cancellable = true)
     private static void tryShearFurCapability(ServerLevel level, BlockPos pos, CallbackInfoReturnable<Boolean> cir)
     {
+        boolean success = false;
         for (LivingEntity living : level.getEntitiesOfClass(LivingEntity.class, new AABB(pos), EntitySelector.NO_SPECTATORS))
-        {   ShearableFurManager.shear(living, SHEARS, null);
+        {   success |= ShearableFurManager.shear(living, null);
         }
-        SHEARS = ItemStack.EMPTY;
+        if (success) cir.setReturnValue(true);
     }
 }
