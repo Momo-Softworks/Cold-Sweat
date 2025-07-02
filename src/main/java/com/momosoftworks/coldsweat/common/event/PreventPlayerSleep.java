af6e7a8a41d1112d691250103e368440685e3c99
MikulDev <gamer3mc@gmail.com>
2025-07-01 20:27:13 -0400

    Fix spawnpoint not set via bed if world temp is uninhabitable

--- src/main/java/com/momosoftworks/coldsweat/common/event/PreventPlayerSleep.java
+++ src/main/java/com/momosoftworks/coldsweat/common/event/PreventPlayerSleep.java
@@ -4,6 +4,7 @@ import com.momosoftworks.coldsweat.api.util.Temperature;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.util.math.CSMath;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -49,6 +50,9 @@ public class PreventPlayerSleep
             // Prevent sleep with message
             player.displayClientMessage(new TranslatableComponent("cold_sweat.message.sleep.world." + (worldTemp > maxTemp ? "hot" : "cold")), true);
             event.setResult(Player.BedSleepingProblem.OTHER_PROBLEM);
+        }
+        if (player instanceof ServerPlayer serverPlayer && !player.isSleeping() && player.isAlive())
+        {   serverPlayer.setRespawnPosition(serverPlayer.level.dimension(), event.getPos(), serverPlayer.getYRot(), false, true);
         }
     }
 }
