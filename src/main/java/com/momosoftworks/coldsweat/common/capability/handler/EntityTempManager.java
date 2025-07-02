c237f832dc39eb761dc6cdafa42264a8156e9392
MikulDev <gamer3mc@gmail.com>
2025-07-01 22:24:54 -0400

    Better method of cleaning capability caches

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
@@ -92,7 +92,7 @@ public class EntityTempManager
 
     public static final Set<EntityType<? extends LivingEntity>> TEMPERATURE_ENABLED_ENTITIES = new HashSet<>(List.of(EntityType.PLAYER));
 
-    public static SidedCapabilityCache<ITemperatureCap, Entity> CAP_CACHE = new SidedCapabilityCache<>(ModCapabilities.ENTITY_TEMPERATURE);
+    public static SidedCapabilityCache<ITemperatureCap, Entity> CAP_CACHE = new SidedCapabilityCache<>(ModCapabilities.ENTITY_TEMPERATURE, Entity::isRemoved);
     public static Map<Entity, Map<ResourceLocation, Double>> TEMP_MODIFIER_IMMUNITIES = new WeakHashMap<>();
 
     public static LazyOptional<ITemperatureCap> getTemperatureCap(Entity entity)
@@ -272,8 +272,7 @@ public class EntityTempManager
     public static synchronized void cleanRemovedEntities(EntityLeaveLevelEvent event)
     {
         if (isTemperatureEnabled(event.getEntity()))
-        {   CAP_CACHE.removeIf(Entity::isRemoved);
-            TEMP_MODIFIER_IMMUNITIES.keySet().removeIf(Entity::isRemoved);
+        {   TEMP_MODIFIER_IMMUNITIES.keySet().removeIf(Entity::isRemoved);
         }
     }
 
