0d5f0f4495e31ffc761a21a6f873045ff69b026e
MikulDev <gamer3mc@gmail.com>
2025-07-05 17:11:13 -0400

    Clear OptionalHolders on logging out

--- src/main/java/com/momosoftworks/coldsweat/client/event/ClientJoinSetup.java
+++ src/main/java/com/momosoftworks/coldsweat/client/event/ClientJoinSetup.java
@@ -40,7 +40,6 @@ public class ClientJoinSetup
 
     @SubscribeEvent
     public static void onLogout(ClientPlayerNetworkEvent.LoggingOut event)
-    {
-
+    {   ConfigLoadingHandler.destroyOptionalHolders();
     }
 }
