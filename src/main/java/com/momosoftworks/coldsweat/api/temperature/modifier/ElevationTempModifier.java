28bf03965e6d8a93825e85d09a22c9cc2394fee7
MikulDev <gamer3mc@gmail.com>
2025-07-07 00:11:15 -0400

    Use both VS translated and real positions to calculate light level in ElevationTempModifier
    Took 4 minutes

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/ElevationTempModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/ElevationTempModifier.java
@@ -2,9 +2,7 @@ package com.momosoftworks.coldsweat.api.temperature.modifier;
 
 import com.mojang.datafixers.util.Pair;
 import com.momosoftworks.coldsweat.api.util.Temperature;
-import com.momosoftworks.coldsweat.compat.CompatManager;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
-import com.momosoftworks.coldsweat.data.codec.configuration.BiomeTempData;
 import com.momosoftworks.coldsweat.data.codec.configuration.DepthTempData;
 import com.momosoftworks.coldsweat.data.codec.configuration.DimensionTempData;
 import com.momosoftworks.coldsweat.util.math.CSMath;
@@ -13,8 +11,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LightLayer;
-import net.minecraft.world.level.biome.Biome;
-import net.neoforged.neoforge.common.Tags;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,7 +46,9 @@ public class ElevationTempModifier extends TempModifier
             depthTable.add(Pair.of(pos, CSMath.getDistance(entity.blockPosition(), pos)));
         }
 
-        int skylight = entity.level().getBrightness(LightLayer.SKY, entity.blockPosition());
+        int normalSkylight = entity.level().getBrightness(LightLayer.SKY, entity.blockPosition());
+        int translatedSkylight = entity.level().getBrightness(LightLayer.SKY, entity.blockPosition());
+        int skylight = Math.min(normalSkylight, translatedSkylight);
 
         List<Pair<Elevation, RegionEntry>> depthRegions = new ArrayList<>(depthTable.size());
 
