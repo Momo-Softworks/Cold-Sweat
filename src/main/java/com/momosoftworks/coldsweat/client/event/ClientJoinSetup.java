adc2d5a7b5b815f1d536510eb36221af46d242c1
MikulDev <gamer3mc@gmail.com>
2025-07-06 23:00:11 -0400

    Prevent clearing OptionalHolders as the player joins

--- src/main/java/com/momosoftworks/coldsweat/client/event/ClientJoinSetup.java
+++ src/main/java/com/momosoftworks/coldsweat/client/event/ClientJoinSetup.java
@@ -40,6 +40,8 @@ public class ClientJoinSetup
 
     @SubscribeEvent
     public static void onLogout(ClientPlayerNetworkEvent.LoggedOutEvent event)
-    {   ConfigLoadingHandler.destroyOptionalHolders();
+    {
+        if (event.getPlayer() == null) return;
+        ConfigLoadingHandler.destroyOptionalHolders();
     }
 }
