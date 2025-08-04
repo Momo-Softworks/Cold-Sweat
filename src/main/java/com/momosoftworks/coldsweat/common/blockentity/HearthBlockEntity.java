c256ac14008fd87ce2e4cd885ec59bf8803b2c1b
MikulDev <gamer3mc@gmail.com>
2025-08-04 11:03:38 -0400

    Fix unloaded hearths not being deregistered from Forge event bus on clientside

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
@@ -87,6 +87,8 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.event.level.ChunkEvent;
+import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.IFluidHandler;
@@ -181,16 +183,42 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
     {
         BlockPos pos = event.getPosition();
         Level level = event.getLevel();
+        BlockState oldState = event.getOldState();
+        BlockState newState = event.getNewState();
 
         if (level == this.level
         && this.pathLookup.containsKey(pos)
-        && !event.getOldState().getCollisionShape(level, pos).equals(event.getNewState().getCollisionShape(level, pos)))
+        && (oldState == null || !oldState.getCollisionShape(level, pos).equals(newState.getCollisionShape(level, pos))))
         {
             if (!level.isClientSide())
             {   this.sendBlockUpdate(pos);
             }
             if (isTransferPipe(event.getOldState()) || isTransferPipe(event.getNewState()))
             {   this.searchForPipeEnds(this.getBlockPos().above(), Direction.UP);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onLevelUnloaded(LevelEvent.Unload event)
+    {
+        if (event.getLevel().equals(this.level))
+        {   this.cleanup();
+        }
+    }
+
+    @SubscribeEvent
+    public static void onChunkUnloaded(ChunkEvent.Unload event)
+    {
+        ChunkAccess chunk = event.getChunk();
+        // Remove all paths in this chunk
+        if (chunk instanceof LevelChunk levelChunk)
+        {
+            for (BlockEntity te : levelChunk.getBlockEntities().values())
+            {
+                if (te instanceof HearthBlockEntity hearth)
+                {   hearth.cleanup();
+                }
             }
         }
     }
@@ -1292,15 +1320,13 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
         this.sendBlockUpdate(this.getBlockPos());
     }
 
-    @Override
-    public void setRemoved()
+    protected void cleanup()
     {
-        super.setRemoved();
         HearthSaveDataHandler.HEARTH_POSITIONS.remove(Pair.of(this.getBlockPos(), this.getLevel().dimension().location()));
+        MinecraftForge.EVENT_BUS.unregister(this);
         if (this.level.isClientSide)
         {   ClientOnlyHelper.removeHearthPosition(this.getBlockPos());
         }
-        MinecraftForge.EVENT_BUS.unregister(this);
     }
 
     public Multimap<BlockPos, Direction> getPathLookup()
