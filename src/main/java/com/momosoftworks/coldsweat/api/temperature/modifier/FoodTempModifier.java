428acb616e5d479eee03ee64534841242083880c
MikulDev <gamer3mc@gmail.com>
2025-08-08 15:47:21 -0400

    Make over-time food effects stack like potions

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/FoodTempModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/FoodTempModifier.java
@@ -1,25 +1,54 @@
 package com.momosoftworks.coldsweat.api.temperature.modifier;
 
 import com.momosoftworks.coldsweat.api.util.Temperature;
+import com.momosoftworks.coldsweat.util.math.CSMath;
 import net.minecraft.world.entity.LivingEntity;
 
 import java.util.function.Function;
 
 public class FoodTempModifier extends TempModifier
 {
+    int[] overridden = new int[Temperature.Trait.values().length];
+
     public FoodTempModifier()
     {
         this(0);
     }
 
-    public FoodTempModifier(double effect)
+    public FoodTempModifier(double temperature)
     {
-        this.getNBT().putDouble("effect", effect);
+        this.getNBT().putDouble("temperature", temperature);
     }
 
     @Override
     public Function<Double, Double> calculate(LivingEntity entity, Temperature.Trait trait)
     {
-        return temp -> temp + this.getNBT().getDouble("effect");
+        if (this.overridden[trait.ordinal()] > 0)
+        {   return temp -> temp;
+        }
+        return temp -> temp + this.getNBT().getDouble("temperature");
+    }
+
+    private void updateOverridden(LivingEntity entity, Temperature.Trait trait, TempModifier sibling, boolean add)
+    {
+        double temperature = this.getNBT().getDouble("temperature");
+        if (sibling instanceof FoodTempModifier)
+        {
+            double siblingTemp = sibling.getNBT().getDouble("temperature");
+            if (CSMath.sign(temperature) == CSMath.sign(siblingTemp) && Math.abs(siblingTemp) > Math.abs(temperature))
+            {   this.overridden[trait.ordinal()] += add ? 1 : -1;
+            }
+            this.update(this.getLastInput(trait), entity, trait);
+        }
+    }
+
+    @Override
+    public void onSiblingAdded(LivingEntity entity, Temperature.Trait trait, TempModifier sibling)
+    {   this.updateOverridden(entity, trait, sibling, true);
+    }
+
+    @Override
+    public void onSiblingRemoved(LivingEntity entity, Temperature.Trait trait, TempModifier sibling)
+    {   this.updateOverridden(entity, trait, sibling, false);
     }
 }
