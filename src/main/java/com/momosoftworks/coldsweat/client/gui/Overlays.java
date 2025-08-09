b98f13d0e7928e57a8f94b971287f95e31ada24a
MikulDev <gamer3mc@gmail.com>
2025-08-09 18:12:49 -0400

    Increase spacing between food effects

--- src/main/java/com/momosoftworks/coldsweat/client/gui/Overlays.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/Overlays.java
@@ -325,7 +325,7 @@ public class Overlays
 
                     mc.font.drawShadow(poseStack, tempString, x, y - mc.font.lineHeight - 11, color, true);
 
-                    offset += 24;
+                    offset += 25;
                     poseStack.popPose();
                 }
             }
