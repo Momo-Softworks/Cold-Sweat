b42ee4a1348c9f9ecc98f2e7caa0c3b48a24cd50
MikulDev <gamer3mc@gmail.com>
2025-08-12 14:51:16 -0400

    Decrease default acclimation speed

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
@@ -224,8 +224,7 @@ public class EntityTempManager
 
         if (isPlayer && !(entity instanceof DummyPlayer))
         {
-            event.addModifier(List.of(Trait.FREEZING_POINT, Trait.BURNING_POINT),
-                              new AcclimationTempModifier().tickRate(20), Placement.Duplicates.BY_CLASS, Placement.AFTER_LAST);
+            event.addModifier(List.of(Trait.FREEZING_POINT, Trait.BURNING_POINT), new AcclimationTempModifier().tickRate(20), Placement.Duplicates.BY_CLASS, Placement.AFTER_LAST);
             event.addModifier(Arrays.asList(VALID_MODIFIER_TRAITS), new InventoryItemsTempModifier().tickRate(5), Placement.Duplicates.BY_CLASS, Placement.AFTER_LAST);
         }
     }
