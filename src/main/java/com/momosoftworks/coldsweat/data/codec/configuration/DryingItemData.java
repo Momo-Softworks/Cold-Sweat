88f8385615483dd2676875939e72ddc23580613d
MikulDev <gamer3mc@gmail.com>
2025-08-14 00:48:17 -0400

    Fix drying item registry result stack being cleared upon use

--- src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/DryingItemData.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/configuration/DryingItemData.java
@@ -56,7 +56,7 @@ public class DryingItemData extends ConfigData implements RequirementHolder
     {   return item;
     }
     public ItemStack result()
-    {   return result;
+    {   return result.copy();
     }
     public NegatableList<EntityRequirement> entity()
     {   return entity;
