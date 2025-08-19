e44efc6ece031ad01bdcf02aef37a91fb9e66e8f
MikulDev <gamer3mc@gmail.com>
2025-08-19 15:14:06 -0400

    Fix NegatableList exclusion logic

--- src/main/java/com/momosoftworks/coldsweat/data/codec/util/NegatableList.java
+++ src/main/java/com/momosoftworks/coldsweat/data/codec/util/NegatableList.java
@@ -221,7 +221,7 @@ public class NegatableList<T>
             {
                 boolean result = test.test(this.exclusions.get(i));
                 if (this.excludeAll && !result)
-                {   break;
+                {   return true;
                 }
                 if (!this.excludeAll && result)
                 {   return false;
