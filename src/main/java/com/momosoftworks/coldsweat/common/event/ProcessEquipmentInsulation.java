9f0c66576058d27a9bc53a6eb7a353de0ce091c4
MikulDev <gamer3mc@gmail.com>
2025-08-20 23:18:43 -0400

    Drop insulation on armor break
    Took 3 minutes

--- src/main/java/com/momosoftworks/coldsweat/common/event/ProcessEquipmentInsulation.java
+++ src/main/java/com/momosoftworks/coldsweat/common/event/ProcessEquipmentInsulation.java
@@ -2,6 +2,7 @@ package com.momosoftworks.coldsweat.common.event;
 
 import com.mojang.datafixers.util.Pair;
 import com.momosoftworks.coldsweat.api.event.common.insulation.InsulationTickEvent;
+import com.momosoftworks.coldsweat.api.event.vanilla.ItemBreakEvent;
 import com.momosoftworks.coldsweat.api.insulation.AdaptiveInsulation;
 import com.momosoftworks.coldsweat.api.insulation.Insulation;
 import com.momosoftworks.coldsweat.api.insulation.StaticInsulation;
@@ -19,6 +20,7 @@ import com.momosoftworks.coldsweat.util.math.FastMap;
 import com.momosoftworks.coldsweat.util.registries.ModItems;
 import com.momosoftworks.coldsweat.util.world.WorldHelper;
 import net.minecraft.advancements.Advancement;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.item.ItemEntity;
@@ -191,14 +193,27 @@ public class ProcessEquipmentInsulation
         while (filledInsulationSlots > ItemInsulationManager.getInsulationSlots(armorStack))
         {
             ItemStack removedItem = cap.removeInsulationItem(totalInsulation.get(totalInsulation.size() - 1).getFirst());
-            ItemEntity droppedInsulation = new ItemEntity(player.level, player.getX(), player.getY() + player.getBbHeight() / 2, player.getZ(), removedItem);
-            droppedInsulation.setPickUpDelay(8);
-            droppedInsulation.setDeltaMovement(new Vector3d(player.getRandom().nextGaussian() * 0.05,
-                                                            player.getRandom().nextGaussian() * 0.05 + 0.2,
-                                                            player.getRandom().nextGaussian() * 0.05));
-            player.level.addFreshEntity(droppedInsulation);
+            WorldHelper.entityDropItem(player, removedItem);
 
             filledInsulationSlots--;
+        }
+    }
+
+    @SubscribeEvent
+    public static void onArmorBroken(ItemBreakEvent event)
+    {
+        ItemStack stack = event.getItemStack();
+        LivingEntity entity = event.getEntityLiving();
+        if (ItemInsulationManager.isInsulatable(stack))
+        {
+            ItemInsulationManager.getInsulationCap(stack).ifPresent(cap ->
+            {
+                for (Pair<ItemStack, List<InsulatorData>> insulation : cap.getInsulation())
+                {
+                    ItemEntity itemEntity = WorldHelper.entityDropItem(entity, insulation.getFirst());
+                    itemEntity.setPickUpDelay(8);
+                }
+            });
         }
     }
 
---
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinArmorBreak.java
@@ -0,0 +1,21 @@
+package com.momosoftworks.coldsweat.mixin;
+
+import com.momosoftworks.coldsweat.api.event.vanilla.ItemBreakEvent;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Consumer;
+
+@Mixin(ItemStack.class)
+public class MixinArmorBreak
+{
+    @Inject(method = "hurtAndBreak", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;shrink(I)V"))
+    private <T extends LivingEntity> void onHurtAndBreak(int damage, T entity, Consumer<T> onBroken, CallbackInfo ci)
+    {   MinecraftForge.EVENT_BUS.post(new ItemBreakEvent((ItemStack) (Object) this, entity, (Consumer<LivingEntity>) onBroken));
+    }
+}
