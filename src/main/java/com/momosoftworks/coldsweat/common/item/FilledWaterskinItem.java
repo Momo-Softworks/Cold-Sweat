9d503eb6935150b7685c31050748743c8f6a89e3
MikulDev <gamer3mc@gmail.com>
2025-08-21 22:55:18 -0400

    Account for waterskin consume strength in tooltip

--- src/main/java/com/momosoftworks/coldsweat/common/item/FilledWaterskinItem.java
+++ src/main/java/com/momosoftworks/coldsweat/common/item/FilledWaterskinItem.java
@@ -293,6 +293,7 @@ public class FilledWaterskinItem extends Item
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag advanced)
     {
         double temp = CSMath.round(stack.getOrCreateTag().getDouble(FilledWaterskinItem.NBT_TEMPERATURE), 2);
+        double multiplier = ConfigSettings.WATERSKIN_CONSUME_STRENGTH.get() / 50d;
 
         // Display filled state
         MutableComponent filledLabel = new TranslatableComponent("item.cold_sweat.waterskin.filled").withStyle(ChatFormatting.GRAY);
@@ -301,9 +302,10 @@ public class FilledWaterskinItem extends Item
         tooltip.add(filledLabel);
 
         // Info tooltip for drinking/pouring functionality
-        Component tempText = temp > 0  ? new TranslatableComponent("tooltip.cold_sweat.temperature_effect", "+" + Math.round(temp)).withStyle(TooltipHandler.HOT) :
-                             temp == 0 ? new TranslatableComponent("tooltip.cold_sweat.temperature_effect", "+" + Math.round(temp)).withStyle(ChatFormatting.WHITE)
-                                       : new TranslatableComponent("tooltip.cold_sweat.temperature_effect", Math.round(temp)).withStyle(TooltipHandler.COLD);
+        double useEffect = Math.round(temp * multiplier);
+        Component tempText = temp > 0  ? new TranslatableComponent("tooltip.cold_sweat.temperature_effect", "+" + useEffect).withStyle(TooltipHandler.HOT) :
+                             temp == 0 ? new TranslatableComponent("tooltip.cold_sweat.temperature_effect", "+" + useEffect).withStyle(ChatFormatting.WHITE)
+                                       : new TranslatableComponent("tooltip.cold_sweat.temperature_effect", useEffect).withStyle(TooltipHandler.COLD);
         tooltip.add(new TextComponent(""));
         tooltip.add(new TranslatableComponent("tooltip.cold_sweat.consumed").withStyle(ChatFormatting.GRAY));
         tooltip.add(tempText);
