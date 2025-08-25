74ac047967dcc1b3b32e4dec8c03ad6024083726
MikulDev <gamer3mc@gmail.com>
2025-08-24 23:31:38 -0400

    Remove equipable curios tag

--- src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
@@ -4,16 +4,15 @@ import com.anthonyhilyard.iceberg.util.Tooltips;
 import com.mojang.datafixers.util.Either;
 import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.api.event.core.init.FetchSeasonsModsEvent;
-import com.momosoftworks.coldsweat.api.event.core.registry.LoadRegistriesEvent;
 import com.momosoftworks.coldsweat.api.util.Temperature;
 import com.momosoftworks.coldsweat.common.capability.handler.EntityTempManager;
+import com.momosoftworks.coldsweat.common.item.ThermometerItem;
+import com.momosoftworks.coldsweat.compat.create.ColdSweatDisplayBehaviors;
 import com.momosoftworks.coldsweat.compat.curios.EquipableCurio;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.core.init.BlockInit;
-import com.momosoftworks.coldsweat.compat.create.ColdSweatDisplayBehaviors;
 import com.momosoftworks.coldsweat.data.codec.configuration.InsulatorData;
 import com.momosoftworks.coldsweat.data.tag.ModInsulatorTags;
-import com.momosoftworks.coldsweat.data.tag.ModItemTags;
 import com.momosoftworks.coldsweat.util.math.CSMath;
 import com.momosoftworks.coldsweat.util.registries.ModBlocks;
 import com.momosoftworks.coldsweat.util.registries.ModItems;
@@ -51,9 +50,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.loading.moddiscovery.ModFileInfo;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.IForgeRegistry;
-import net.minecraftforge.registries.RegisterEvent;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.joml.Vector3d;
@@ -61,9 +57,7 @@ import org.valkyrienskies.core.api.ships.Ship;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
 import sereneseasons.season.SeasonHooks;
-import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.CuriosCapability;
-import top.theillusivec4.curios.api.event.CurioAttributeModifierEvent;
 import top.theillusivec4.curios.api.event.CurioChangeEvent;
 import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 import top.theillusivec4.curios.common.capability.CurioItemCapability;
@@ -373,13 +367,11 @@ public class CompatManager
                 public void registerEquipableCurios(AttachCapabilitiesEvent<ItemStack> event)
                 {
                     ItemStack item = event.getObject();
-                    if (item.is(ModItemTags.EQUIPABLE_CURIOS))
-                    {
-                        if (item.getCapability(CuriosCapability.ITEM) != null) return;
-                        EquipableCurio itemCurio = new EquipableCurio();
-                        ItemizedCurioCapability itemizedCapability = new ItemizedCurioCapability(itemCurio, item);
-                        event.addCapability(CuriosCapability.ID_ITEM, CurioItemCapability.createProvider(itemizedCapability));
-                    }
+                    if (!(item.getItem() instanceof ThermometerItem)) return;
+
+                    EquipableCurio itemCurio = new EquipableCurio();
+                    ItemizedCurioCapability itemizedCapability = new ItemizedCurioCapability(itemCurio, item);
+                    event.addCapability(CuriosCapability.ID_ITEM, CurioItemCapability.createProvider(itemizedCapability));
                 }
             });
         }
--- src/main/java/com/momosoftworks/coldsweat/data/tag/ModItemTags.java
+++ src/main/java/com/momosoftworks/coldsweat/data/tag/ModItemTags.java
@@ -2,7 +2,6 @@ package com.momosoftworks.coldsweat.data.tag;
 
 import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.api.event.core.init.InitDynamicTagsEvent;
-import com.momosoftworks.coldsweat.compat.CompatManager;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
@@ -28,8 +27,6 @@ public class ModItemTags
 
     public static final TagKey<Item> FOOD = createForgeTag("food");
 
-    public static final TagKey<Item> EQUIPABLE_CURIOS = createNamespaceTag("curios", "equipable");
-
     @SubscribeEvent
     public static void initDynamicTags(InitDynamicTagsEvent event)
     {
@@ -42,13 +39,5 @@ public class ModItemTags
 
     private static TagKey<Item> createForgeTag(String name)
     {   return ItemTags.create(new ResourceLocation("forge", name));
-    }
-
-    private static TagKey<Item> createNamespaceTag(String namespace, String name)
-    {
-        if (!CompatManager.modLoaded(namespace))
-        {   return null;
-        }
-        return ItemTags.create(new ResourceLocation(namespace, name));
     }
 }
