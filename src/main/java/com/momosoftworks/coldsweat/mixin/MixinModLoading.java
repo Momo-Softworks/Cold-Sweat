59d5f3495bb3da6c2208524d4a256acf0d8207cb
MikulDev <gamer3mc@gmail.com>
2025-08-27 23:32:32 -0400

    Fix potential duplicate registrations

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinModLoading.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinModLoading.java
@@ -22,7 +22,7 @@ public class MixinModLoading
     @Mixin(Minecraft.class)
     public static class onClient
     {
-        @Inject(method = "lambda$new$2(Ljava/lang/String;I)V", at = @At("TAIL"), remap = false)
+        @Inject(method = "<init>", at = @At("RETURN"))
         private void beforeClientStart(CallbackInfo ci)
         {   ConfigLoadingHandler.initRegistries();
         }
