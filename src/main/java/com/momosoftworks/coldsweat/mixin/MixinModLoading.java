a84516b069a1078c28fcf8771fda07d436402bab
MikulDev <gamer3mc@gmail.com>
2025-08-30 20:32:54 -0400

    Better trigger for clientside registry initialization

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinModLoading.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinModLoading.java
@@ -2,28 +2,31 @@ package com.momosoftworks.coldsweat.mixin;
 
 import com.momosoftworks.coldsweat.config.ConfigLoadingHandler;
 import net.minecraft.client.Minecraft;
-import net.minecraft.server.Main;
+import net.minecraft.server.WorldLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+@Mixin(WorldLoader.class)
 public class MixinModLoading
 {
-    @Mixin(Main.class)
-    public static class OnServer
-    {
-        @Inject(method = "main", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/repository/ServerPacksSource;createPackRepository(Lnet/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess;)Lnet/minecraft/server/packs/repository/PackRepository;"))
-        private static void beforeServerStart(String[] args, CallbackInfo ci)
-        {   ConfigLoadingHandler.initRegistries();
-        }
+    @Inject(method = "load", at = @At("HEAD"))
+    private static <D, R> void onWorldLoad(WorldLoader.InitConfig initConfig, WorldLoader.WorldDataSupplier<D> worldDate,
+                                           WorldLoader.ResultFactory<D, R> something, Executor somethingElse, Executor anotherThing,
+                                           CallbackInfoReturnable<CompletableFuture<R>> cir)
+    {   ConfigLoadingHandler.initRegistries();
     }
 
     @Mixin(Minecraft.class)
-    public static class onClient
+    public static class Client
     {
-        @Inject(method = "<init>", at = @At("RETURN"))
-        private void beforeClientStart(CallbackInfo ci)
+        @Inject(method = "disconnect(Lnet/minecraft/client/gui/screens/Screen;Z)V", at = @At("HEAD"))
+        private void onConnectServer(CallbackInfo ci)
         {   ConfigLoadingHandler.initRegistries();
         }
     }
