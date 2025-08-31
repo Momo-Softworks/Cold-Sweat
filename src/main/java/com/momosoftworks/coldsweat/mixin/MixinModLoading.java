748e11fe5b4b3b7aded1e34009fdaa0774b68e46
MikulDev <gamer3mc@gmail.com>
2025-08-30 20:32:54 -0400

    Tweak registry loading mixin
    Tweak registry loading mixin
    
    
    Took 11 seconds

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinModLoading.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinModLoading.java
@@ -2,28 +2,34 @@ package com.momosoftworks.coldsweat.mixin;
 
 import com.momosoftworks.coldsweat.config.ConfigLoadingHandler;
 import net.minecraft.client.Minecraft;
-import net.minecraft.server.Main;
+import net.minecraft.command.Commands;
+import net.minecraft.resources.DataPackRegistries;
+import net.minecraft.resources.IResourcePack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+@Mixin(DataPackRegistries.class)
 public class MixinModLoading
 {
-    @Mixin(Main.class)
-    public static class OnServer
-    {
-        @Inject(method = "main", at = @At(value = "INVOKE", target = "Lnet/minecraft/resources/DataPackRegistries;updateGlobals()V"))
-        private static void beforeServerStart(String[] args, CallbackInfo ci)
-        {   ConfigLoadingHandler.initRegistries();
-        }
+    @Inject(method = "loadResources", at = @At("HEAD"))
+    private static <D, R> void onWorldLoad(List<IResourcePack> resourcePacks, Commands.EnvironmentType environmentType,
+                                           int something, Executor somethingElse, Executor anotherThing,
+                                           CallbackInfoReturnable<CompletableFuture<DataPackRegistries>> cir)
+    {   ConfigLoadingHandler.initRegistries();
     }
 
     @Mixin(Minecraft.class)
-    public static class onClient
+    public static class Client
     {
-        @Inject(method = "clearLevel(Lnet/minecraft/client/gui/screens/Screen;)V", at = @At("HEAD"))
-        private void beforeClientStart(CallbackInfo ci)
+        @Inject(method = "clearLevel(Lnet/minecraft/client/gui/screen/Screen;)V", at = @At("HEAD"))
+        private void onConnectServer(CallbackInfo ci)
         {   ConfigLoadingHandler.initRegistries();
         }
     }
