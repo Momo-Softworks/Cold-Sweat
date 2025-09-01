4a1d1e757edb7e60cbf26d5e5cb0daa5b388ba96
MikulDev <gamer3mc@gmail.com>
2025-09-01 13:41:56 -0400

    Return String for TempChangedEventJS.getTrait()

--- src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/TempChangedEventJS.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/TempChangedEventJS.java
@@ -1,7 +1,6 @@
 package com.momosoftworks.coldsweat.compat.kubejs.event;
 
 import com.momosoftworks.coldsweat.api.event.common.temperautre.TemperatureChangedEvent;
-import com.momosoftworks.coldsweat.api.util.Temperature;
 import dev.latvian.mods.kubejs.entity.LivingEntityEventJS;
 import net.minecraft.world.entity.LivingEntity;
 
@@ -18,8 +17,8 @@ public class TempChangedEventJS extends LivingEntityEventJS
     {   return event.getEntity();
     }
 
-    public Temperature.Trait getTrait()
-    {   return event.getTrait();
+    public String getTrait()
+    {   return event.getTrait().getSerializedName();
     }
 
     public double getOldTemperature()
