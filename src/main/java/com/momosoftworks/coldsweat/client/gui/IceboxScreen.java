57082062c432035fe52f5cb09f7bba330205b703
MikulDev <gamer3mc@gmail.com>
2025-09-04 16:12:47 -0400

    Hearth/boiler/icebox GUI redesign

--- src/main/java/com/momosoftworks/coldsweat/client/gui/IceboxScreen.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/IceboxScreen.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class IceboxScreen extends AbstractHearthScreen<IceboxContainer>
 {
     private static final ResourceLocation ICEBOX_GUI = new ResourceLocation(ColdSweat.MOD_ID, "textures/gui/screen/icebox_gui.png");
+    private static final ResourceLocation HEARTH_GUI = new ResourceLocation(ColdSweat.MOD_ID, "textures/gui/screen/hearth_gui.png");
 
     @Override
     HearthBlockEntity getBlockEntity()
@@ -34,38 +35,26 @@ public class IceboxScreen extends AbstractHearthScreen<IceboxContainer>
     }
 
     @Override
-    public void render(PoseStack ps, int mouseX, int mouseY, float partialTicks)
-    {
-        super.render(ps, mouseX, mouseY, partialTicks);
-    }
-
-    @Override
-    public void init()
-    {
-        super.init();
-        if (particleButton != null)
-        {   particleButton.x = leftPos + 151;
-            particleButton.y = topPos + 63;
-        }
-    }
-
-    @Override
     protected void renderBg(PoseStack matrixStack, float partialTicks, int mouseX, int mouseY)
     {
         RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
         RenderSystem.setShaderTexture(0, ICEBOX_GUI);
         this.blit(matrixStack, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
 
+        int maxGaugeHeight = 14;
+        int gaugeHeight  = this.menu.getFuel() <= 0 ? 0 : Math.round(CSMath.blend(2, 14, this.menu.getFuel(), 0, this.menu.te.getMaxFuel()));
+
         // Draw fuel gauge
-        blit(matrixStack, leftPos + 109, topPos + 63, 176, 0, (int) (menu.te.getFuel() / 31.25), 14, 256, 256);
+        blit(matrixStack, leftPos + 100,  topPos + 63 + (maxGaugeHeight-gaugeHeight),  176, maxGaugeHeight - gaugeHeight,  14, gaugeHeight, 256, 256);
 
         if (!ConfigSettings.SMART_HEARTH.get() && this.menu.te.hasSmokeStack())
         {
             boolean powered = this.menu.te.isCoolingOn();
 
-            blit(matrixStack, leftPos + 117, topPos + 78, 176, powered ? 14 : 22, 14, 8, 256, 256);
+            RenderSystem.setShaderTexture(0, HEARTH_GUI);
+            blit(matrixStack, leftPos + 101, topPos + 78, 176, powered ? 28 : 32, 13, 4, 256, 256);
 
-            if (CSMath.betweenInclusive(mouseX, leftPos + 114, leftPos + 132) && CSMath.betweenInclusive(mouseY, topPos + 78, topPos + 86))
+            if (CSMath.betweenInclusive(mouseX, leftPos + 98, leftPos + 117) && CSMath.betweenInclusive(mouseY, topPos + 75, topPos + 82))
             {   this.renderComponentTooltip(matrixStack, List.of(new TranslatableComponent(powered ? "gui.cold_sweat.hearth.powered" : "gui.cold_sweat.hearth.unpowered")), mouseX, mouseY);;
             }
         }
