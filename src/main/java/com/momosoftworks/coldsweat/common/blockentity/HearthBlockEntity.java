213c2b329fe645ce8752a4493270e6dbd9dd90e6
MikulDev <gamer3mc@gmail.com>
2025-09-04 10:29:53 -0400

    Fix hearth fuel slot not exposed

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
@@ -1364,7 +1364,7 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
 
     @Override
     public int[] getSlotsForFace(Direction side)
-    {   return new int[0];
+    {   return new int[]{0};
     }
 
     @Override
