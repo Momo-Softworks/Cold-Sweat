3f6db9b4cbf990b42ff9abc292ea18c49348273b
MikulDev <gamer3mc@gmail.com>
2025-09-05 23:14:30 -0400

    Fix WaterTempModifier from waterskins ticking too fast

--- src/main/java/com/momosoftworks/coldsweat/common/item/FilledWaterskinItem.java
+++ src/main/java/com/momosoftworks/coldsweat/common/item/FilledWaterskinItem.java
@@ -117,7 +117,7 @@ public class FilledWaterskinItem extends Item
             double effectAmount = temperature * (ConfigSettings.WATERSKIN_CONSUME_STRENGTH.get() / 50d);
             Temperature.addModifier(player, new WaterskinTempModifier(effectAmount).expires(0), Temperature.Trait.CORE, Placement.Duplicates.ALLOW);
             double wetnessTemp = temperature > 0 ? Math.max(0.5, temperature / 200) : Math.min(-0.5, temperature / 200);
-            Temperature.addModifier(player, new WaterTempModifier(wetnessTemp), Temperature.Trait.WORLD, Placement.Duplicates.ALLOW);
+            Temperature.addModifier(player, new WaterTempModifier(wetnessTemp).tickRate(5), Temperature.Trait.WORLD, Placement.Duplicates.ALLOW);
 
             WorldHelper.playEntitySound(ModSounds.WATERSKIN_POUR, player, player.getSoundSource(), 2f, (float) ((Math.random() / 5) + 0.9));
         }
@@ -419,6 +419,7 @@ public class FilledWaterskinItem extends Item
                     // Temperature of waterskin weakens over time
                     double waterTemp = CSMath.blend(itemTemp, itemTemp / 5, tick, 20, 100);
                     double effectAmount = waterTemp * (ConfigSettings.WATERSKIN_CONSUME_STRENGTH.get() / 50d);
+                    double wetnessTemp = waterTemp > 0 ? Math.max(0.5, waterTemp / 200) : Math.min(-0.5, waterTemp / 200);
 
                     // Move the box down at the speed of gravity
                     aabb = aabb.move(0, -acceleration, 0);
@@ -436,7 +437,7 @@ public class FilledWaterskinItem extends Item
                         if (!affectedPlayers.contains(player))
                         {   // Apply the effect and store the player
                             Temperature.addModifier(player, new WaterskinTempModifier(effectAmount).expires(0), Temperature.Trait.CORE, Placement.Duplicates.ALLOW);
-                            Temperature.addModifier(player, new WaterTempModifier(waterTemp / 200), Temperature.Trait.WORLD, Placement.Duplicates.ALLOW);
+                            Temperature.addModifier(player, new WaterTempModifier(wetnessTemp).tickRate(5), Temperature.Trait.WORLD, Placement.Duplicates.ALLOW);
                             affectedPlayers.add(player);
                         }
                     });
