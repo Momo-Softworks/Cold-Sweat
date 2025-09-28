70d1a0890204298db0f2efb60c62c238a728f5d2
MikulDev <gamer3mc@gmail.com>
2025-09-28 11:52:32 -0400

    Set player temp to 0 when in spectator mode

--- src/main/java/com/momosoftworks/coldsweat/common/capability/temperature/PlayerTempCap.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/temperature/PlayerTempCap.java
@@ -33,7 +33,7 @@ public class PlayerTempCap extends AbstractTempCap
             if (player.tickCount % 20 == 0)
             {   calculateHudVisibility(player);
             }
-            if (player.isCreative())
+            if (player.isCreative() || player.isSpectator())
             {   this.setTrait(Temperature.Trait.CORE, 0);
             }
         }
