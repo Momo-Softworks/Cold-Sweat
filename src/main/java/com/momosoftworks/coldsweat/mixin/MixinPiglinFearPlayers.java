f63ddd87799664a6fb62692b789b39cba64d7674
MikulDev <gamer3mc@gmail.com>
2025-10-04 17:31:27 -0400

    Make piglin scare items a tag

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinPiglinFearPlayers.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinPiglinFearPlayers.java
@@ -1,6 +1,7 @@
 package com.momosoftworks.coldsweat.mixin;
 
-import com.momosoftworks.coldsweat.util.entity.EntityHelper;
+import com.momosoftworks.coldsweat.data.tag.ModItemTags;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.monster.piglin.Piglin;
@@ -19,22 +20,36 @@ public class MixinPiglinFearPlayers
     @Inject(method = "isNearZombified", at = @At("RETURN"), cancellable = true)
     private static void fearSoulspringLamp(Piglin entity, CallbackInfoReturnable<Boolean> cir)
     {
-        if (!cir.getReturnValue())
+        Brain<Piglin> brain = entity.getBrain();
+        // Check if piglin should still be fleeing from current target
+        if (entity.getPersistentData().getBoolean("FleeingItem"))
         {
-            Brain<Piglin> brain = entity.getBrain();
-            List<Player> nearbyPlayers = entity.level.getEntitiesOfClass(Player.class, entity.getBoundingBox().inflate(6.0D, 3.0D, 6.0D));
-            for (Player player : nearbyPlayers)
+            brain.getMemory(MemoryModuleType.NEAREST_VISIBLE_ZOMBIFIED).ifPresent(ent ->
             {
-                if (EntityHelper.holdingLitLamp(player))
+                if (!(ent instanceof Player player) || !isPlayerHoldingScaryItem(player) || !player.closerThan(entity, 6))
                 {
-                    brain.setMemory(MemoryModuleType.NEAREST_VISIBLE_ZOMBIFIED, player);
-                    cir.setReturnValue(true);
-                    return;
+                    entity.getPersistentData().putBoolean("FleeingItem", false);
+                    brain.eraseMemory(MemoryModuleType.NEAREST_VISIBLE_ZOMBIFIED);
                 }
-                else if (brain.getMemory(MemoryModuleType.NEAREST_VISIBLE_ZOMBIFIED).map(mem -> mem.equals(player)).orElse(false))
-                {   brain.eraseMemory(MemoryModuleType.NEAREST_VISIBLE_ZOMBIFIED);
-                }
+            });
+        }
+        // Check for targets to flee from
+        List<Player> nearbyPlayers = entity.level.getEntitiesOfClass(Player.class, entity.getBoundingBox().inflate(6.0D, 6.0D, 6.0D));
+        for (Player player : nearbyPlayers)
+        {
+            if (isPlayerHoldingScaryItem(player))
+            {
+                brain.setMemory(MemoryModuleType.NEAREST_VISIBLE_ZOMBIFIED, player);
+                entity.getPersistentData().putBoolean("FleeingItem", true);
+                cir.setReturnValue(true);
+                return;
             }
         }
+    }
+
+    private static boolean isPlayerHoldingScaryItem(Player entity)
+    {
+        return entity.getItemInHand(InteractionHand.MAIN_HAND).is(ModItemTags.SCARES_PIGLINS)
+            || entity.getItemInHand(InteractionHand.OFF_HAND).is(ModItemTags.SCARES_PIGLINS);
     }
 }
