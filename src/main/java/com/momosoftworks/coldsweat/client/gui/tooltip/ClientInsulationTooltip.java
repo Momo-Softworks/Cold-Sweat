29cd0266b2d417f2b45ae9cd6a324e9ae5c25682
MikulDev <gamer3mc@gmail.com>
2025-10-09 16:51:56 -0400

    Show overflow insulation tooltip when shift is held
    Took 3 seconds

--- src/main/java/com/momosoftworks/coldsweat/client/gui/tooltip/ClientInsulationTooltip.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/tooltip/ClientInsulationTooltip.java
@@ -246,7 +246,7 @@ public class ClientInsulationTooltip extends Tooltip
         setAdaptations(sortedInsulation, stack);
 
         Mode mode;
-        if (sortedInsulation.stream().map(Insulation::split).mapToInt(List::size).sum() > 10)
+        if (Screen.hasShiftDown() || sortedInsulation.stream().map(Insulation::split).mapToInt(List::size).sum() > 10)
         {   mode = Mode.OVERFLOW;
         }
         else if (insulations.stream().anyMatch(insul -> insul.split().size() > 1))
