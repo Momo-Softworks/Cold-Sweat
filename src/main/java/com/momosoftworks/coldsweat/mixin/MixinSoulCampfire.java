80de06990453f514b5be8dffce8f47472ec697b5
MikulDev <gamer3mc@gmail.com>
2025-10-10 01:03:10 -0400

    Make fire/campfire blocks check #forge:soul_campfire tag for damage
    Took 28 seconds

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinSoulCampfire.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinSoulCampfire.java
@@ -1,10 +1,10 @@
 package com.momosoftworks.coldsweat.mixin;
 
-import com.momosoftworks.coldsweat.config.ConfigSettings;
 import com.momosoftworks.coldsweat.compat.CompatManager;
+import com.momosoftworks.coldsweat.config.ConfigSettings;
+import com.momosoftworks.coldsweat.data.tag.ModBlockTags;
 import com.momosoftworks.coldsweat.util.registries.ModDamageSources;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.CampfireBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.ItemEntity;
@@ -28,7 +28,7 @@ public class MixinSoulCampfire
     at = @At("HEAD"), cancellable = true)
     public void entityInside(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo ci)
     {
-        if (state.getBlock() == Blocks.SOUL_CAMPFIRE && state.getValue(CampfireBlock.LIT)
+        if (state.is(ModBlockTags.SOUL_FIRE) && state.getValue(CampfireBlock.LIT)
         && ConfigSettings.COLD_SOUL_FIRE.get() && !(entity instanceof ItemEntity && CompatManager.isSpiritLoaded()))
         {
             entity.hurt(ModDamageSources.COLD, this.fireDamage);
--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinSoulFire.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinSoulFire.java
@@ -1,16 +1,15 @@
 package com.momosoftworks.coldsweat.mixin;
 
+import com.momosoftworks.coldsweat.compat.CompatManager;
 import com.momosoftworks.coldsweat.config.ConfigSettings;
+import com.momosoftworks.coldsweat.data.tag.ModBlockTags;
 import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.SoulFireBlock;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.DamageSource;
+import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,16 +17,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(AbstractFireBlock.class)
 public class MixinSoulFire
 {
-    @Shadow @Final private float fireDamage;
-
     @Inject(method = "entityInside(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;)V",
     at = @At("HEAD"), cancellable = true)
     public void entityInside(BlockState state, World level, BlockPos pos, Entity entity, CallbackInfo ci)
     {
-        if (state.getBlock() instanceof SoulFireBlock && ConfigSettings.COLD_SOUL_FIRE.get())
+        if (state.is(ModBlockTags.SOUL_FIRE) && ConfigSettings.COLD_SOUL_FIRE.get()
+        && !(entity instanceof ItemEntity && CompatManager.isSpiritLoaded()))
         {
             entity.clearFire();
-            entity.hurt(DamageSource.IN_FIRE, this.fireDamage);
             ci.cancel();
         }
     }
