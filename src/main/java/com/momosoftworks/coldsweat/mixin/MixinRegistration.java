a565afe4559cf603ed781f80a370deae069b782d
MikulDev <gamer3mc@gmail.com>
2025-10-16 10:44:24 -0400

    Simplify required mods registration mixin

--- src/main/java/com/momosoftworks/coldsweat/mixin/MixinRegistration.java
+++ src/main/java/com/momosoftworks/coldsweat/mixin/MixinRegistration.java
@@ -3,9 +3,12 @@ package com.momosoftworks.coldsweat.mixin;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.Decoder;
+import com.mojang.serialization.JsonOps;
 import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.compat.CompatManager;
+import com.momosoftworks.coldsweat.data.codec.util.NegatableList;
 import net.minecraft.core.Registry;
 import net.minecraft.core.WritableRegistry;
 import net.minecraft.resources.*;
@@ -40,58 +43,34 @@ public class MixinRegistration
             JsonObject json = jsonelement.getAsJsonObject();
             if (json.has("required_mods"))
             {
-                JsonArray requiredMods = new JsonArray();
-                JsonArray excludedMods = new JsonArray();
-                JsonElement requiredModField = json.get("required_mods");
-                if (requiredModField.isJsonArray())
-                {
-                    requiredMods = requiredModField.getAsJsonArray();
-                }
-                else
-                {
-                    JsonObject requiredModCompound = requiredModField.getAsJsonObject();
-                    if (requiredModCompound.has("require"))
-                    {   requiredMods = requiredModCompound.getAsJsonArray("require");
-                    }
-                    if (requiredModCompound.has("exclude"))
-                    {   excludedMods = requiredModCompound.getAsJsonArray("exclude");
-                    }
+                JsonElement requiredModsField = json.get("required_mods");
+                NegatableList<String> requiredMods = NegatableList.listCodec(Codec.STRING).parse(JsonOps.INSTANCE, requiredModsField).result().orElse(new NegatableList<>());
+                if (!json.has("forge:conditions"))
+                {   json.add("forge:conditions", new JsonArray());
                 }
                 JsonArray conditions = json.getAsJsonArray("forge:conditions");
-                // Create conditions block if it doesn't exist
-                if (conditions == null)
-                {   conditions = new JsonArray();
-                    json.add("forge:conditions", conditions);
-                }
-                // Add required mods as forge conditions
-                for (JsonElement requiredMod : requiredMods)
-                {
-                    JsonObject condition = new JsonObject();
-                    condition.addProperty("type", "forge:mod_loaded");
-                    // If required mod isn't loaded by CS standards, add an impossible condition
-                    if (!CompatManager.modLoaded(requiredMod.getAsString()))
-                    {   condition.addProperty("modid", "cs_impossible");
-                        conditions.add(condition);
-                        return;
-                    }
-                    // Require mod as normal
-                    else
-                    {   condition.addProperty("modid", requiredMod.getAsString());
-                        conditions.add(condition);
-                    }
-                }
-                // Add excluded mods as an impossible forge condition
-                for (JsonElement excludedMod : excludedMods)
-                {
-                    if (CompatManager.modLoaded(excludedMod.getAsString()))
+                requiredMods.forEach(
+                    req ->
                     {
                         JsonObject condition = new JsonObject();
                         condition.addProperty("type", "forge:mod_loaded");
-                        condition.addProperty("modid", "cs_impossible");
-                        conditions.add(condition);
-                        return;
-                    }
-                }
+                        // If required mod isn't loaded by CS standards, add an impossible condition
+                        if (!CompatManager.modLoaded(req))
+                        {   condition.addProperty("modid", "cs_impossible");
+                            conditions.add(condition);
+                            ColdSweat.LOGGER.info("Skipping registration of {} {}: missing mod \"{}\"", pRegistryKey.location(), resourcelocation, req);
+                        }
+                    },
+                    exc ->
+                    {
+                        if (CompatManager.modLoaded(exc))
+                        {   JsonObject condition = new JsonObject();
+                            condition.addProperty("type", "forge:mod_loaded");
+                            condition.addProperty("modid", "cs_impossible");
+                            conditions.add(condition);
+                            ColdSweat.LOGGER.info("Skipping registration of {} {}: disallowed mod \"{}\" is loaded", pRegistryKey.location(), resourcelocation, exc);
+                        }
+                    });
             }
         }
     }
