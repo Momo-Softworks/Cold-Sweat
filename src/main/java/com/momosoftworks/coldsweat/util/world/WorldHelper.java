2bd308bc4537202bd567f920b968b75c373c13b3
MikulDev <gamer3mc@gmail.com>
2025-10-21 14:51:17 -0500

    Fix hearth adding duplicate path entries

--- src/main/java/com/momosoftworks/coldsweat/util/world/WorldHelper.java
+++ src/main/java/com/momosoftworks/coldsweat/util/world/WorldHelper.java
@@ -855,7 +855,7 @@ public abstract class WorldHelper
             for (BlockPos bePos : chunk.getBlockEntitiesPos())
             {
                 BlockEntity be = chunk.getBlockEntity(bePos);
-                if (be instanceof HearthBlockEntity hearth && hearth.getPathLookup().containsKey(pos))
+                if (be instanceof HearthBlockEntity hearth && hearth.getPathLookup().contains(pos))
                 {
                     maxCoolingLevel = Math.max(maxCoolingLevel, hearth.getCoolingLevel());
                     maxHeatingLevel = Math.max(maxHeatingLevel, hearth.getHeatingLevel());
