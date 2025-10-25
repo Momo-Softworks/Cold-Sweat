0f14228b463b510bce69dab7229e1415ff001aec
MikulDev <gamer3mc@gmail.com>
2025-10-25 18:23:41 -0400

    Fix insulators sometimes parsing extra empty insulation

--- src/main/java/com/momosoftworks/coldsweat/api/insulation/Insulation.java
+++ src/main/java/com/momosoftworks/coldsweat/api/insulation/Insulation.java
@@ -92,7 +92,7 @@ public abstract class Insulation
             }
         }
         combined.addAll(remaining);
-
+        combined.removeIf(Insulation::isEmpty);
         return combined;
     }
 
