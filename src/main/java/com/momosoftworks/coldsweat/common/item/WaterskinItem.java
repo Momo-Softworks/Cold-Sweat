caf2a140326b2f3ab1f522158947029f1bf29998
MikulDev <gamer3mc@gmail.com>
2025-11-02 13:05:13 -0500

    Remove cooldown when filling waterskins

--- src/main/java/com/momosoftworks/coldsweat/common/item/WaterskinItem.java
+++ src/main/java/com/momosoftworks/coldsweat/common/item/WaterskinItem.java
@@ -157,7 +157,6 @@ public class WaterskinItem extends Item
         }
         player.swing(usedHand);
         player.getCooldowns().addCooldown(ModItems.FILLED_WATERSKIN, 10);
-        player.getCooldowns().addCooldown(ModItems.WATERSKIN, 10);
         player.awardStat(Stats.ITEM_USED.get(thisStack.getItem()));
         level.playSound(null, filledAtPos, ModSounds.WATERSKIN_FILL, SoundSource.PLAYERS, 2f, (float) Math.random() / 5 + 0.9f);
     }
