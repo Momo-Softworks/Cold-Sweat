c83a682b324125fd0719497fbf4b270fc5632c4b
MikulDev <gamer3mc@gmail.com>
2025-11-08 10:17:45 -0500

    Fix hunger draining due to trying to regenerate frozen health

--- src/main/java/com/momosoftworks/coldsweat/core/init/TempEffectInit.java
+++ src/main/java/com/momosoftworks/coldsweat/core/init/TempEffectInit.java
@@ -15,22 +15,19 @@ public class TempEffectInit
     public static final DeferredRegister<TempEffectType<?>> TEMP_EFFECTS = DeferredRegister.create(new ResourceLocation(ColdSweat.MOD_ID, "temp_effect"), ColdSweat.MOD_ID);
     public static final Supplier<IForgeRegistry<TempEffectType<?>>> REGISTRY = TEMP_EFFECTS.makeRegistry(RegistryBuilder::new);
 
-    static
-    {
-        TEMP_EFFECTS.register("freeze_healing", () -> new TempEffectType<>(FreezeHealingEffect::new));
-        TEMP_EFFECTS.register("freeze_hearts", () -> new TempEffectType<>(FreezeHeartsEffect::new));
-        TEMP_EFFECTS.register("freeze_knockback", () -> new TempEffectType<>(FreezeKnockbackEffect::new));
-        TEMP_EFFECTS.register("freeze_mining_speed", () -> new TempEffectType<>(FreezeMineSpeedEffect::new));
-        TEMP_EFFECTS.register("freeze_movement_speed", () -> new TempEffectType<>(FreezeMoveSpeedEffect::new));
-        TEMP_EFFECTS.register("freeze_shiver", () -> new TempEffectType<>(FreezeShiverEffect::new));
-        TEMP_EFFECTS.register("freeze_overlay", () -> new TempEffectType<>(FreezeVignetteEffect::new));
+    public static final RegistryObject<TempEffectType<?>> FREEZE_HEALING = TEMP_EFFECTS.register("freeze_healing", () -> new TempEffectType<>(FreezeHealingEffect::new));
+    public static final RegistryObject<TempEffectType<?>> FREEZE_HEARTS = TEMP_EFFECTS.register("freeze_hearts", () -> new TempEffectType<>(FreezeHeartsEffect::new));
+    public static final RegistryObject<TempEffectType<?>> FREEZE_KNOCKBACK = TEMP_EFFECTS.register("freeze_knockback", () -> new TempEffectType<>(FreezeKnockbackEffect::new));
+    public static final RegistryObject<TempEffectType<?>> FREEZE_MINING_SPEED = TEMP_EFFECTS.register("freeze_mining_speed", () -> new TempEffectType<>(FreezeMineSpeedEffect::new));
+    public static final RegistryObject<TempEffectType<?>> FREEZE_MOVEMENT_SPEED = TEMP_EFFECTS.register("freeze_movement_speed", () -> new TempEffectType<>(FreezeMoveSpeedEffect::new));
+    public static final RegistryObject<TempEffectType<?>> FREEZE_SHIVER = TEMP_EFFECTS.register("freeze_shiver", () -> new TempEffectType<>(FreezeShiverEffect::new));
+    public static final RegistryObject<TempEffectType<?>> FREEZE_OVERLAY = TEMP_EFFECTS.register("freeze_overlay", () -> new TempEffectType<>(FreezeVignetteEffect::new));
 
-        TEMP_EFFECTS.register("heat_blur", () -> new TempEffectType<>(HeatBlurEffect::new));
-        TEMP_EFFECTS.register("heat_fog", () -> new TempEffectType<>(HeatFogEffect::new));
-        TEMP_EFFECTS.register("heat_sway", () -> new TempEffectType<>(HeatSwayEffect::new));
-        TEMP_EFFECTS.register("heat_vignette", () -> new TempEffectType<>(HeatVignetteEffect::new));
+    public static final RegistryObject<TempEffectType<?>> HEAT_BLUR = TEMP_EFFECTS.register("heat_blur", () -> new TempEffectType<>(HeatBlurEffect::new));
+    public static final RegistryObject<TempEffectType<?>> HEAT_FOG = TEMP_EFFECTS.register("heat_fog", () -> new TempEffectType<>(HeatFogEffect::new));
+    public static final RegistryObject<TempEffectType<?>> HEAT_SWAY = TEMP_EFFECTS.register("heat_sway", () -> new TempEffectType<>(HeatSwayEffect::new));
+    public static final RegistryObject<TempEffectType<?>> HEAT_OVERLAY = TEMP_EFFECTS.register("heat_vignette", () -> new TempEffectType<>(HeatVignetteEffect::new));
 
-        TEMP_EFFECTS.register("prevent_breeding", () -> new TempEffectType<>(PreventBreedingEffect::new));
-        TEMP_EFFECTS.register("decrease_drops", () -> new TempEffectType<>(DecreaseDropsEffect::new));
-    }
+    public static final RegistryObject<TempEffectType<?>> PREVENT_BREEDING = TEMP_EFFECTS.register("prevent_breeding", () -> new TempEffectType<>(PreventBreedingEffect::new));
+    public static final RegistryObject<TempEffectType<?>> DECREASE_DROPS = TEMP_EFFECTS.register("decrease_drops", () -> new TempEffectType<>(DecreaseDropsEffect::new));
 }
