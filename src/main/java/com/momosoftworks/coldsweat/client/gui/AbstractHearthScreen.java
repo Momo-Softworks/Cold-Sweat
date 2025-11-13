ad66186da05ed4f640cc558929fa15f1a36c6798
MikulDev <gamer3mc@gmail.com>
2025-11-13 17:39:01 -0500

    Use separate sprites for elements in GUIs

--- src/main/java/com/momosoftworks/coldsweat/client/gui/AbstractHearthScreen.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/AbstractHearthScreen.java
@@ -1,14 +1,19 @@
 package com.momosoftworks.coldsweat.client.gui;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.datafixers.util.Pair;
 import com.momosoftworks.coldsweat.ColdSweat;
+import com.momosoftworks.coldsweat.client.gui.config.ConfigImageButton;
+import com.momosoftworks.coldsweat.client.gui.util.WidgetSprites;
 import com.momosoftworks.coldsweat.common.blockentity.HearthBlockEntity;
 import com.momosoftworks.coldsweat.common.event.HearthSaveDataHandler;
 import com.momosoftworks.coldsweat.core.network.ColdSweatPacketHandler;
 import com.momosoftworks.coldsweat.core.network.message.DisableHearthParticlesMessage;
+import com.momosoftworks.coldsweat.util.math.CSMath;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.ImageButton;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.core.BlockPos;
@@ -24,7 +29,16 @@ import java.util.List;
 
 public abstract class AbstractHearthScreen<T extends AbstractContainerMenu> extends EffectRenderingInventoryScreen<T>
 {
-    private static final ResourceLocation HEARTH_GUI = new ResourceLocation(ColdSweat.MOD_ID, "textures/gui/screen/hearth_gui.png");
+    public static final WidgetSprites PARTICLES_BUTTON_SPRITES = new WidgetSprites(ResourceLocation.fromNamespaceAndPath(ColdSweat.MOD_ID, "textures/gui/sprites/hearth/particle_button_on.png"),
+                                                                                      ResourceLocation.fromNamespaceAndPath(ColdSweat.MOD_ID, "textures/gui/sprites/hearth/particle_button_off.png"),
+                                                                                      ResourceLocation.fromNamespaceAndPath(ColdSweat.MOD_ID, "textures/gui/sprites/hearth/particle_button_on_focus.png"),
+                                                                                      ResourceLocation.fromNamespaceAndPath(ColdSweat.MOD_ID, "textures/gui/sprites/hearth/particle_button_off_focus.png"));
+    public static final WidgetSprites POWER_INDICATOR_SPRITES = new WidgetSprites(ResourceLocation.fromNamespaceAndPath(ColdSweat.MOD_ID, "textures/gui/sprites/hearth/power_indicator_off.png"),
+                                                                                     ResourceLocation.fromNamespaceAndPath(ColdSweat.MOD_ID, "textures/gui/sprites/hearth/power_indicator_on.png"));
+    public static final ResourceLocation HOT_FUEL_GAUGE  = ResourceLocation.fromNamespaceAndPath(ColdSweat.MOD_ID, "textures/gui/sprites/hearth/fuel_gauge_hot.png");
+    public static final ResourceLocation HOT_FUEL_GAUGE_EMPTY = ResourceLocation.fromNamespaceAndPath(ColdSweat.MOD_ID, "textures/gui/sprites/hearth/fuel_gauge_hot_empty.png");
+    public static final ResourceLocation COLD_FUEL_GAUGE = ResourceLocation.fromNamespaceAndPath(ColdSweat.MOD_ID, "textures/gui/sprites/hearth/fuel_gauge_cold.png");
+    public static final ResourceLocation COLD_FUEL_GAUGE_EMPTY = ResourceLocation.fromNamespaceAndPath(ColdSweat.MOD_ID, "textures/gui/sprites/hearth/fuel_gauge_cold_empty.png");
 
     ImageButton particleButton = null;
     Pair<BlockPos, ResourceLocation> levelPos = Pair.of(this.getBlockEntity().getBlockPos(), this.getBlockEntity().getLevel().dimension().location());
@@ -42,7 +56,7 @@ public abstract class AbstractHearthScreen<T extends AbstractContainerMenu> exte
     {   super.init();
         if (this.getBlockEntity().hasSmokestack())
         {
-            particleButton = this.addRenderableWidget(new ImageButton(leftPos + 160, topPos + 8, 8, 7, 176 + (!hideParticles ? 0 : 8), 14, 7, HEARTH_GUI, (button) ->
+            particleButton = this.addRenderableWidget(new ConfigImageButton(leftPos + 160, topPos + 8, 8, 7, PARTICLES_BUTTON_SPRITES, 8, 7, (button) ->
             {
                 hideParticles = !hideParticles;
                 // If particles are disabled, add the hearth to the list of disabled hearths
@@ -83,6 +97,11 @@ public abstract class AbstractHearthScreen<T extends AbstractContainerMenu> exte
                 public void renderToolTip(PoseStack poseStack, int mouseX, int mouseY)
                 {   AbstractHearthScreen.this.renderComponentTooltip(poseStack, List.of(Component.translatable("cold_sweat.screen.hearth.show_particles")), mouseX, mouseY, font);
                 }
+
+                @Override
+                protected boolean isEnabled()
+                {   return !hideParticles;
+                }
             });
         }
     }
@@ -100,5 +119,26 @@ public abstract class AbstractHearthScreen<T extends AbstractContainerMenu> exte
         if (this.minecraft.player != null && hideParticlesOld != hideParticles)
         {   ColdSweatPacketHandler.INSTANCE.sendToServer(new DisableHearthParticlesMessage(HearthSaveDataHandler.serializeDisabledHearths()));
         }
+    }
+
+    protected void renderFuelGauge(HearthBlockEntity.FuelType fuelType, PoseStack poseStack, int x, int y, int fuel, int maxFuel)
+    {
+        ResourceLocation emptyTexture = fuelType == HearthBlockEntity.FuelType.HOT ? HOT_FUEL_GAUGE_EMPTY : COLD_FUEL_GAUGE_EMPTY;
+        ResourceLocation fullTexture = fuelType == HearthBlockEntity.FuelType.HOT ? HOT_FUEL_GAUGE : COLD_FUEL_GAUGE;
+
+        int maxGaugeHeight = 14;
+        int gaugeHeight  = fuel <= 0 ? 0 : Math.round(CSMath.blend(2, 14, fuel, 0, maxFuel));
+
+        RenderSystem.setShaderTexture(0, emptyTexture);
+        Screen.blit(poseStack, x, y, 0, 0, 14, 14, 14, 14);
+        RenderSystem.setShaderTexture(0, fullTexture);
+        Screen.blit(poseStack, x, y + (maxGaugeHeight-gaugeHeight),  0, maxGaugeHeight - gaugeHeight,  14, gaugeHeight, 14, 14);
+    }
+
+    protected void renderPowerIndicator(PoseStack poseStack, int x, int y, boolean powered)
+    {
+        ResourceLocation sprite = POWER_INDICATOR_SPRITES.get(true, powered);
+        RenderSystem.setShaderTexture(0, sprite);
+        Screen.blit(poseStack, x, y, 0, 0, 13, 4, 13, 4);
     }
 }
--- src/main/java/com/momosoftworks/coldsweat/client/gui/IceboxScreen.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/IceboxScreen.java
@@ -16,8 +16,7 @@ import java.util.List;
 
 public class IceboxScreen extends AbstractHearthScreen<IceboxContainer>
 {
-    private static final ResourceLocation ICEBOX_GUI = new ResourceLocation(ColdSweat.MOD_ID, "textures/gui/screen/icebox_gui.png");
-    private static final ResourceLocation HEARTH_GUI = new ResourceLocation(ColdSweat.MOD_ID, "textures/gui/screen/hearth_gui.png");
+    public static final ResourceLocation ICEBOX_GUI = new ResourceLocation(ColdSweat.MOD_ID, "textures/gui/screen/icebox_gui.png");
 
     @Override
     HearthBlockEntity getBlockEntity()
@@ -40,18 +39,14 @@ public class IceboxScreen extends AbstractHearthScreen<IceboxContainer>
         RenderSystem.setShaderTexture(0, ICEBOX_GUI);
         this.blit(matrixStack, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
 
-        int maxGaugeHeight = 14;
-        int gaugeHeight  = this.menu.getFuel() <= 0 ? 0 : Math.round(CSMath.blend(2, 14, this.menu.getFuel(), 0, this.menu.te.getMaxFuel()));
-
         // Draw fuel gauge
-        blit(matrixStack, leftPos + 100,  topPos + 63 + (maxGaugeHeight-gaugeHeight),  176, maxGaugeHeight - gaugeHeight,  14, gaugeHeight, 256, 256);
+        this.renderFuelGauge(HearthBlockEntity.FuelType.COLD, matrixStack, leftPos + 100,  topPos + 63, this.menu.getFuel(), this.menu.te.getMaxFuel());
 
         if (!ConfigSettings.SMART_HEARTH.get() && this.menu.te.hasSmokestack())
         {
             boolean powered = this.menu.te.isCoolingOn();
 
-            RenderSystem.setShaderTexture(0, HEARTH_GUI);
-            blit(matrixStack, leftPos + 101, topPos + 78, 176, powered ? 28 : 32, 13, 4, 256, 256);
+            this.renderPowerIndicator(matrixStack, leftPos + 101, topPos + 78, powered);
 
             if (CSMath.betweenInclusive(mouseX, leftPos + 98, leftPos + 117) && CSMath.betweenInclusive(mouseY, topPos + 75, topPos + 82))
             {   this.renderComponentTooltip(matrixStack, List.of(Component.translatable(powered ? "gui.cold_sweat.hearth.powered" : "gui.cold_sweat.hearth.unpowered")), mouseX, mouseY);;
