659bdb06b489e39ab423117a07853d28f6df2a92
MikulDev <gamer3mc@gmail.com>
2025-11-25 11:54:10 -0500

    Make temperature damage interval configurable

--- src/main/java/com/momosoftworks/coldsweat/config/spec/MainSettingsConfig.java
+++ src/main/java/com/momosoftworks/coldsweat/config/spec/MainSettingsConfig.java
@@ -27,6 +27,7 @@ public class MainSettingsConfig
     public static final ForgeConfigSpec.ConfigValue<Double> MIN_HABITABLE_TEMPERATURE;
     public static final ForgeConfigSpec.ConfigValue<Double> TEMP_RATE_MULTIPLIER;
     public static final ForgeConfigSpec.ConfigValue<Double> TEMP_DAMAGE;
+    public static final ForgeConfigSpec.ConfigValue<Integer> TEMPERATURE_HURT_INTERVAL;
 
     public static final ForgeConfigSpec.ConfigValue<Boolean> NULLIFY_IN_PEACEFUL;
 
@@ -94,6 +95,11 @@ public class MainSettingsConfig
                 .comment("─────────────────────────────────────────────────────────────────────────",
                          " Damage dealt to the player when they are too hot or too cold")
                 .defineInRange("Temperature Damage", defaultDiff.getOrDefault(ConfigSettings.TEMP_DAMAGE, 2d), 0d, Double.POSITIVE_INFINITY);
+
+        TEMPERATURE_HURT_INTERVAL = BUILDER
+                .comment("─────────────────────────────────────────────────────────────────────────",
+                         " The time between damage (in ticks) when the player is too hot or too cold")
+                .defineInRange("Temperature Hurt Interval", defaultDiff.getOrDefault(ConfigSettings.TEMPERATURE_HURT_INTERVAL, 40), 1, Integer.MAX_VALUE);
 
         NULLIFY_IN_PEACEFUL = BUILDER
                 .comment("─────────────────────────────────────────────────────────────────────────",
