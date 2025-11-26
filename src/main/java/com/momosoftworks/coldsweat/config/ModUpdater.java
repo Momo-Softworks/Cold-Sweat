b3874b9fcfdbd1cfd4655f278f474aad07730239
MikulDev <gamer3mc@gmail.com>
2025-11-26 11:07:54 -0500

    Remove entity updater; version older than 2.3-b05b

--- src/main/java/com/momosoftworks/coldsweat/config/ModUpdater.java
+++ src/main/java/com/momosoftworks/coldsweat/config/ModUpdater.java
@@ -24,13 +24,6 @@ public class ModUpdater
     public static void updateEntity(LivingEntity entity)
     {
         String entityVersion = entity.getPersistentData().getString("cs:version");
-        if (compareVersions(entityVersion, "2.3-b03a") < 0)
-        {
-            EntityTempManager.getTemperatureCap(entity).ifPresent(cap -> cap.getModifiers().forEach((trait, list) ->
-            {
-                list.removeIf(mod -> mod.getClass() == InventoryItemsTempModifier.class);
-            }));
-        }
         entity.getPersistentData().putString("cs:version", ColdSweat.getVersion());
     }
 
