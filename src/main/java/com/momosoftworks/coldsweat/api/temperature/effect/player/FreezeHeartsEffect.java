04f940e96d45d248c3372652b4013878d162582e
MikulDev <gamer3mc@gmail.com>
2025-11-26 12:00:18 -0500

    Fix half-frozen half-heart not rendering properly
    Took 20 seconds

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/effect/player/FreezeHeartsEffect.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/effect/player/FreezeHeartsEffect.java
@@ -71,7 +71,7 @@ public class FreezeHeartsEffect extends TempEffect
             }
             else
             {   u = isHardcore ? 7 : 0;
-                v = partialFrozen ? 14 : halfHeart ? 7 : 0;
+                v = partialFrozen ? halfHeart ? 21 : 14 : halfHeart ? 7 : 0;
             }
             AbstractGui.blit(event.getPoseStack(), x + 1, y + 1, u, v, 7, 7, 21, 28);
             Minecraft.getInstance().textureManager.bind(GUI_ICONS_LOCATION);
