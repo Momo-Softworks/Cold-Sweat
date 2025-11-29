4b80b71ccfedf0e6e6b297273a56e145f39eb902
MikulDev <gamer3mc@gmail.com>
2025-11-29 17:23:41 -0500

    Make food effects optionally stackable

--- src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/FoodBuilderJS.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/kubejs/event/builder/FoodBuilderJS.java
@@ -19,6 +19,7 @@ public class FoodBuilderJS
 {
     public double temperature = 0;
     public int duration = 0;
+    public boolean stackable = false;
     public NegatableList<ItemRequirement> itemPredicate = new NegatableList<>();
     public NegatableList<EntityRequirement> entityPredicate = new NegatableList<>();
 
@@ -49,6 +50,16 @@ public class FoodBuilderJS
         return this;
     }
 
+    public FoodBuilderJS stackable()
+    {   return this.stackable(true);
+    }
+
+    public FoodBuilderJS stackable(boolean stackable)
+    {
+        this.stackable = stackable;
+        return this;
+    }
+
     public FoodBuilderJS itemPredicate(Predicate<ItemStack> itemPredicate)
     {
         this.itemPredicate.add(new ItemRequirement(itemPredicate), false);
@@ -63,7 +74,7 @@ public class FoodBuilderJS
 
     public FoodData build()
     {
-        FoodData data = new FoodData(this.itemPredicate,this.temperature,  this.duration, this.entityPredicate);
+        FoodData data = new FoodData(this.itemPredicate,this.temperature,  this.duration, this.stackable, this.entityPredicate);
         data.setConfigType(ConfigData.Type.KUBEJS);
         return data;
     }
