0d89f57b9a4a2855ef8505ed628977d81fa90433
MikulDev <gamer3mc@gmail.com>
2025-11-30 19:46:02 -0500

    Add "stackable" param to TOML food temp documentation

--- src/main/java/com/momosoftworks/coldsweat/config/spec/ItemSettingsConfig.java
+++ src/main/java/com/momosoftworks/coldsweat/config/spec/ItemSettingsConfig.java
@@ -334,12 +334,13 @@ public class ItemSettingsConfig
             FOOD_TEMPERATURES = BUILDER
                 .comment("─────────────────────────────────────────────────────────────────────────//v",
                          " Defines items that affect the player's temperature when consumed",
-                         " ├── Format: [[\"item_id\", amount, *\"{nbt}\", *duration], [\"item_id\", amount, *\"{nbt}\", *duration], ...etc]",
+                         " ├── Format: [[\"item_id\", amount, *\"{nbt}\", *duration, *stackable], [...], etc]",
                          " └── [* = optional]",
                          " • item_id: The item's ID (i.e. \"minecraft:apple\").",
                          " • amount: The amount to change the player's temperature by. Negative values are cold, positive values are hot",
                          " • *nbt: If set, the item will only affect the player's temperature if it has the specified NBT tag.",
                          " • *duration: If set, the player's temperature will remain increased/decreased for this amount of time (in ticks).",
+                         " • *stackable: If true, consuming multiple of the same item will stack the temperature effect",
                          " ⌄ ")
                 .defineListAllowEmpty(List.of("Temperature-Affecting Foods"), () -> Arrays.asList(
                         List.of("cold_sweat:soul_sprout", -20, "{}", 1200)
