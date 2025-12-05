5857d9472ee5d5f81c4bf28020ce1a01772a17ae
MikulDev <gamer3mc@gmail.com>
2025-12-05 15:53:57 -0500

    Fix TOML item temps resetting when tempLimit is defined

--- src/main/java/com/momosoftworks/coldsweat/config/spec/ItemSettingsConfig.java
+++ src/main/java/com/momosoftworks/coldsweat/config/spec/ItemSettingsConfig.java
@@ -416,13 +416,14 @@ public class ItemSettingsConfig
                          " ⌄ ")
                 .defineListAllowEmpty(List.of("Item Temperatures"), () -> List.of(
                 ),
-                it -> it instanceof List<?> list && CSMath.betweenInclusive(list.size(), 4, 6)
+                it -> it instanceof List<?> list
                         && list.get(0) instanceof String
                         && list.get(1) instanceof Number
                         && list.get(2) instanceof String
                         && list.get(3) instanceof String
                         && (list.size() < 5 || list.get(4) instanceof String)
-                        && (list.size() < 6 || list.get(5) instanceof Number));
+                        && (list.size() < 6 || list.get(5) instanceof Number)
+                        && (list.size() < 7 || list.get(6) instanceof Number));
 
         BUILDER.pop();
 
