6e990fe1d8c9b23158ea88ef9df68822adc325b3
MikulDev <gamer3mc@gmail.com>
2025-12-09 11:58:41 -0500

    Fix default water temp saving incorrectly

--- src/main/java/com/momosoftworks/coldsweat/config/ConfigSettings.java
+++ src/main/java/com/momosoftworks/coldsweat/config/ConfigSettings.java
@@ -892,7 +892,7 @@ public class ConfigSettings
             holder.set(Temperature.convert(temp, units, Temperature.Units.MC, false));
         },
         Codec.DOUBLE,
-        (saver) -> WorldSettingsConfig.DEFAULT_WATER_TEMP.set(List.of(saver, DEFAULT_WATER_TEMP_UNITS.getSerializedName())),
+        (saver) -> WorldSettingsConfig.DEFAULT_WATER_TEMP.set(List.of(Temperature.convert(saver, Temperature.Units.MC, DEFAULT_WATER_TEMP_UNITS, false), DEFAULT_WATER_TEMP_UNITS.getSerializedName())),
         SyncType.BOTH_WAYS);
 
         MAX_RAIN_SOAK = addSyncedSetting(ColdSweat.createKey("max_rain_soak"), () -> 1.0, holder -> holder.set(WorldSettingsConfig.MAX_RAIN_SOAK.get()),
