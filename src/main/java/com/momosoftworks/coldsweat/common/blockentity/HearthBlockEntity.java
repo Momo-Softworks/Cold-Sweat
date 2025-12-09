11b64e12fcc79c210fab3dccc715404f734d353a
MikulDev <gamer3mc@gmail.com>
2025-12-09 11:42:10 -0500

    Better hearth loading/unloading behavior

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
@@ -194,30 +194,6 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
         }
     }
 
-    @SubscribeEvent
-    public void onLevelUnloaded(LevelEvent.Unload event)
-    {
-        if (event.getLevel().equals(this.level))
-        {   this.cleanup();
-        }
-    }
-
-    @SubscribeEvent
-    public static void onChunkUnloaded(ChunkEvent.Unload event)
-    {
-        ChunkAccess chunk = event.getChunk();
-        // Remove all paths in this chunk
-        if (chunk instanceof LevelChunk levelChunk)
-        {
-            for (BlockEntity te : levelChunk.getBlockEntities().values())
-            {
-                if (te instanceof HearthBlockEntity hearth)
-                {   hearth.cleanup();
-                }
-            }
-        }
-    }
-
     /**
      * Range of the Hearth starting from an exit point
      */
@@ -937,6 +913,13 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
         this.registerLocation();
         this.checkForSmokestack();
         this.checkInputSignal();
+        this.level.getLightEngine().checkBlock(this.getBlockPos());
+    }
+
+    @Override
+    public void setRemoved()
+    {   super.setRemoved();
+        this.cleanup();
     }
 
     private void registerLocation()
@@ -1361,6 +1344,8 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
 
     protected void cleanup()
     {
+        hotFuelHolder.invalidate();
+        coldFuelHolder.invalidate();
         HearthSaveDataHandler.HEARTH_POSITIONS.remove(Pair.of(this.getBlockPos(), this.getLevel().dimension().location()));
         MinecraftForge.EVENT_BUS.unregister(this);
         if (this.level.isClientSide)
