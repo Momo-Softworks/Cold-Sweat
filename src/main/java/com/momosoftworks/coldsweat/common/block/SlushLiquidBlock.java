1635efb72114b9de9df69cfe3f02cc325906b874
MikulDev <gamer3mc@gmail.com>
2025-12-14 22:24:02 -0500

    Fix slush interactions with water

--- src/main/java/com/momosoftworks/coldsweat/common/block/SlushLiquidBlock.java
+++ src/main/java/com/momosoftworks/coldsweat/common/block/SlushLiquidBlock.java
@@ -66,9 +66,18 @@ public class SlushLiquidBlock extends LiquidBlock
 
                 if (neighborFluid.is(FluidTags.WATER))
                 {
-                    Block resultBlock = slushState.isSource() ? Blocks.ICE : Blocks.SNOW_BLOCK;
-                    level.setBlockAndUpdate(pos, net.minecraftforge.event.ForgeEventFactory.fireFluidPlaceBlockEvent(level, pos, pos, resultBlock.defaultBlockState()));
-                    SlushFluid.fizz(level, pos);
+                    if (direction == Direction.DOWN)
+                    {
+                        Block resultBlock = Blocks.SNOW_BLOCK;
+                        level.setBlockAndUpdate(pos.below(), net.minecraftforge.event.ForgeEventFactory.fireFluidPlaceBlockEvent(level, pos.below(), pos, resultBlock.defaultBlockState()));
+                        SlushFluid.fizz(level, pos);
+                    }
+                    else
+                    {
+                        Block resultBlock = slushState.isSource() ? Blocks.ICE : Blocks.SNOW_BLOCK;
+                        level.setBlockAndUpdate(pos, net.minecraftforge.event.ForgeEventFactory.fireFluidPlaceBlockEvent(level, pos, pos, resultBlock.defaultBlockState()));
+                        SlushFluid.fizz(level, pos);
+                    }
                     return true;
                 }
             }
