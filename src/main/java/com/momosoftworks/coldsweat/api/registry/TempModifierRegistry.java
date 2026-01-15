b55b79d85ce5196abda03c6b25e5c381915171de
MikulDev <gamer3mc@gmail.com>
2026-01-15 17:48:16 -0500

    Annotate getKey() with @Nullable in TempModifierRegistry

--- src/main/java/com/momosoftworks/coldsweat/api/registry/TempModifierRegistry.java
+++ src/main/java/com/momosoftworks/coldsweat/api/registry/TempModifierRegistry.java
@@ -49,6 +49,7 @@ public class TempModifierRegistry
         return Optional.ofNullable(TEMP_MODIFIERS.get(id)).map(TempModifierHolder::get);
     }
 
+    @Nullable
     public static ResourceLocation getKey(TempModifier modifier)
     {   return CSMath.getIfNotNull(getHolder(modifier), TempModifierHolder::getId, null);
     }
