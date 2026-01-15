c0213cc2997bb9389465e0729605404acc4b44c3
MikulDev <gamer3mc@gmail.com>
2026-01-15 17:48:34 -0500

    Don't render water droplets if player is fully immune to water

--- src/main/java/com/momosoftworks/coldsweat/client/event/WetnessRenderer.java
+++ src/main/java/com/momosoftworks/coldsweat/client/event/WetnessRenderer.java
@@ -98,7 +98,8 @@ public class WetnessRenderer
 
         // Get the player's wetness level
         double wetness = Temperature.getModifier(mc.player, Temperature.Trait.WORLD, WaterTempModifier.class).map(mod ->
-                         {  return CSMath.blend(0, 1, Math.abs(mod.getTemperature()), 0, 0.2);
+                         {   if (Temperature.getImmunityToModifier(player, mod) == 1) return 0d;
+                             else return CSMath.blend(0, 1, Math.abs(mod.getTemperature()), 0, 0.2);
                          }).orElse(0d);
 
         // Spawn a bunch of droplets when the player exits the water
