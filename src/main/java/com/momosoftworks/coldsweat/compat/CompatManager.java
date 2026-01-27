ff23ef74c7611366402b2eade96039596d602f06
MikulDev <gamer3mc@gmail.com>
2026-01-27 11:41:19 -0500

    drainCreateBacktank(): Don't execute code if player isn't wearing a backtank

--- src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
@@ -411,6 +411,9 @@ public class CompatManager
         Player player = event.player;
         if (!CompatManager.isCreateLoaded()) return;
 
+        ItemStack backTank = player.getItemBySlot(EquipmentSlot.CHEST);
+        if (!(backTank.getItem() instanceof BacktankItem)) return;
+
         double worldTemp = Temperature.get(player, Temperature.Trait.WORLD);
         double minTemp = Temperature.get(player, Temperature.Trait.FREEZING_POINT);
         double maxTemp = Temperature.get(player, Temperature.Trait.BURNING_POINT);
@@ -418,8 +421,6 @@ public class CompatManager
         if (CSMath.betweenExclusive(worldTemp, minTemp, maxTemp)) return;
         if (worldTemp < minTemp && !ConfigSettings.COLD_DRAINS_BACKTANK.get()) return;
         if (worldTemp > maxTemp && !ConfigSettings.HEAT_DRAINS_BACKTANK.get()) return;
-
-        ItemStack backTank = player.getItemBySlot(EquipmentSlot.CHEST);
 
         if (USING_BACKTANK && player.level.isClientSide)
         {   player.getPersistentData().putInt("VisualBacktankAir", Math.round(BacktankUtil.getAir(backTank)) - 1);
