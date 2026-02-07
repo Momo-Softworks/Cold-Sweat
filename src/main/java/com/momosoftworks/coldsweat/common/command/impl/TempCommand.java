1871a749d0a87d1b3740653a84e55ba4905e9fff
MikulDev <gamer3mc@gmail.com>
2026-02-06 19:46:40 -0500

    Store actual "Units" enum in config instead of boolean for Celsius

--- src/main/java/com/momosoftworks/coldsweat/common/command/impl/TempCommand.java
+++ src/main/java/com/momosoftworks/coldsweat/common/command/impl/TempCommand.java
@@ -302,7 +302,7 @@ public class TempCommand extends BaseCommand
         Temperature.Units preferredUnits = CSMath.getIfNotNull(source.getPlayer(),
                                                                player -> Preference.getOrDefault(player, Preference.UNITS, Temperature.Units.F),
                                                                Temperature.Units.F);
-        String unitsName = trait.isForWorld() ? " " + preferredUnits.getFormattedName() : "";
+        String unitsName = trait.isForWorld() ? " " + preferredUnits.getFormattedName().getString() : "";
         double convertedTemp = Temperature.convertIfNeeded(temp, trait, preferredUnits, true);
 
         //Compose & send message
@@ -335,7 +335,7 @@ public class TempCommand extends BaseCommand
         Temperature.Units preferredUnits = CSMath.getIfNotNull(source.getPlayer(),
                                                                player -> Preference.getOrDefault(player, Preference.UNITS, Temperature.Units.F),
                                                                Temperature.Units.F);
-        String unitsName = trait.isForWorld() ? " " + preferredUnits.getFormattedName() : "";
+        String unitsName = trait.isForWorld() ? " " + preferredUnits.getFormattedName().getString() : "";
         double convertedTemp = Temperature.convertIfNeeded(temp, trait, preferredUnits, true);
 
         //Compose & send message
@@ -361,7 +361,7 @@ public class TempCommand extends BaseCommand
         {   //Compose & send message
             Temperature.Units preferredUnits = CSMath.getIfNotNull(source.getPlayer(), player -> Preference.getOrDefault(player, Preference.UNITS, Temperature.Units.F), Temperature.Units.F);
             double temp = CSMath.truncate(Temperature.convertIfNeeded(Temperature.get((LivingEntity) target, trait), trait, preferredUnits, true), 2);
-            String unitsName = trait.isForWorld() ? " " + preferredUnits.getFormattedName() : "";
+            String unitsName = trait.isForWorld() ? " " + preferredUnits.getFormattedName().getString() : "";
             source.sendSuccess(() -> Component.translatable("commands.cold_sweat.temperature.get.result", target.getName().getString(),
                                                             trait.getSerializedName(), temp + unitsName),
                                false);
@@ -579,7 +579,7 @@ public class TempCommand extends BaseCommand
     {
         return new HoverEvent(
                 HoverEvent.Action.SHOW_TEXT,
-                Component.literal(CSMath.truncate(getFormattedTraitValue(trait, value, units), 1) + " " + units.getFormattedName()));
+                Component.literal(CSMath.truncate(getFormattedTraitValue(trait, value, units), 1) + " " + units.getFormattedName().getString()));
     }
 
     private int executeModifyEntityTemp(CommandSourceStack source, Collection<? extends Entity> entities, Temperature.Trait attribute,
