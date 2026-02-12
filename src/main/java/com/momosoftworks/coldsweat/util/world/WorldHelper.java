0f749a2689b7bb0fc41d007b4b885b72f18bee6b
MikulDev <gamer3mc@gmail.com>
2026-02-12 15:51:25 -0500

    Fix incorrect water temp calculations

--- src/main/java/com/momosoftworks/coldsweat/util/world/WorldHelper.java
+++ src/main/java/com/momosoftworks/coldsweat/util/world/WorldHelper.java
@@ -809,7 +809,7 @@ public abstract class WorldHelper
             if (surroundedByBlock(levelReader, pos, Blocks.ICE))
             {   return true;
             }
-            Lazy<Boolean> freezingTemp = Lazy.of(() -> getRoughTemperatureAt((ServerWorld) levelReader, pos) + getWaterTemperatureAt(serverLevel, pos) <= 0f);
+            Lazy<Boolean> freezingTemp = Lazy.of(() -> getRoughTemperatureAt((ServerWorld) levelReader, pos) + getWaterTemperatureAt(serverLevel, pos) - ConfigSettings.DEFAULT_WATER_TEMPERATURE.get() <= 0f);
 
             if (!mustBeAtEdge)
             {   return freezingTemp.get();
@@ -828,7 +828,7 @@ public abstract class WorldHelper
             if (mustBeAtEdge && surroundedByBlock(levelReader, pos, Blocks.ICE))
             {   return false;
             }
-            double waterTemp = getWaterTemperatureAt(serverLevel, pos);
+            double waterTemp = getWaterTemperatureAt(serverLevel, pos) - ConfigSettings.DEFAULT_WATER_TEMPERATURE.get();
             double temperature = getRoughTemperatureAt(serverLevel, pos) + waterTemp;
             return temperature > 0f;
         }
