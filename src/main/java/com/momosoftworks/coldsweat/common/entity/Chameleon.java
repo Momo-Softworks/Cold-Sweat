4a37108bb926ac09bec837930c091fad2eebc1a2
MikulDev <gamer3mc@gmail.com>
2026-04-06 13:47:45 -0400

    Fix chameleons shedding immediately upon spawn

--- src/main/java/com/momosoftworks/coldsweat/common/entity/Chameleon.java
+++ src/main/java/com/momosoftworks/coldsweat/common/entity/Chameleon.java
@@ -141,7 +141,7 @@ public class Chameleon extends Animal
     protected void defineSynchedData()
     {
         super.defineSynchedData();
-        this.entityData.define(SHED_TIME, 0);
+        this.entityData.define(SHED_TIME, -1);
         this.entityData.define(LAST_SHED, 0);
         this.entityData.define(HURT_TIMESTAMP, 0);
         this.entityData.define(TRUSTED_PLAYERS, new CompoundTag());
