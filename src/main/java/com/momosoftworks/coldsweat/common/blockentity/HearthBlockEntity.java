3e501d76ea0861aeef8fcee38c3e23e88b2a52fb
MikulDev <gamer3mc@gmail.com>
2026-04-17 15:04:31 -0400

    Fix HearthBlockEntity.getAirParticle throwing exception when not using hot or cold fuel

--- src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
+++ src/main/java/com/momosoftworks/coldsweat/common/blockentity/HearthBlockEntity.java
@@ -1187,6 +1187,7 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
         }
     }
 
+    @Nullable
     public ParticleOptions getAirParticle()
     {
         List<ParticleOptions> options = new ArrayList<>();
@@ -1196,6 +1197,7 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
         if (this.usingHotFuel)
         {   options.add(ParticleTypesInit.WARM_AIR.get());
         }
+        if (options.isEmpty()) return null;
         return options.get(this.level.random.nextInt(options.size()));
     }
 
@@ -1214,7 +1216,9 @@ public class HearthBlockEntity extends RandomizableContainerBlockEntity implemen
         float xm = rand.nextFloat() / 20 - 0.025f;
         float zm = rand.nextFloat() / 20 - 0.025f;
 
-        level.addParticle(this.getAirParticle(), false, x + xr, y + yr, z + zr, xm, 0, zm);
+        ParticleOptions particle = this.getAirParticle();
+        if (particle == null) return;
+        level.addParticle(particle, false, x + xr, y + yr, z + zr, xm, 0, zm);
     }
 
     @Override
