47a401ce8dcd0c3adb00030e88536a5c0cd1fa2e
MikulDev <gamer3mc@gmail.com>
2026-05-06 10:20:21 -0400

    Use neutral temp if BiomeTempModifier doesn't find any biomes

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/BiomeTempModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/BiomeTempModifier.java
@@ -66,7 +66,7 @@ public class BiomeTempModifier extends TempModifier
             if (holder.unwrapKey().isEmpty()) continue;
 
             BiomeTempData biomeTempData = ConfigSettings.BIOME_TEMPS.get(level.registryAccess()).get(holder);
-            if (CSMath.getIfNotNull(biomeTempData, BiomeTempData::isDisabled, false))
+            if (biomeTempData != null && biomeTempData.isDisabled())
             {   continue;
             }
             // Biome temp with time of day
@@ -86,6 +86,9 @@ public class BiomeTempModifier extends TempModifier
 
             // Tally number of biomes
             biomeCount++;
+        }
+        if (biomeCount == 0)
+        {   worldTemp = CSMath.average(ConfigSettings.MIN_TEMP.get(), ConfigSettings.MAX_TEMP.get());
         }
 
         worldTemp /= Math.max(1, biomeCount);
