43fb272d42ad4baa22a10ac20aaadb9fff8df195
MikulDev <gamer3mc@gmail.com>
2026-05-10 23:32:53 -0400

    Fix overflow insulation tooltip text rendering behind background

--- src/main/java/com/momosoftworks/coldsweat/client/gui/tooltip/ClientInsulationTooltip.java
+++ src/main/java/com/momosoftworks/coldsweat/client/gui/tooltip/ClientInsulationTooltip.java
@@ -437,7 +437,9 @@ public class ClientInsulationTooltip implements ClientTooltipComponent
         renderCellBorder(poseStack, x, y, BorderSegment.HEAD, BorderType.OVERFLOW);
         renderCellBorder(poseStack, x, y, BorderSegment.BODY, BorderType.OVERFLOW);
         renderCellBorder(poseStack, x, y, BorderSegment.TAIL, BorderType.OVERFLOW);
+        poseStack.translate(0, 0, 401);
         Screen.drawString(poseStack, font, text, x + 8, y - 2, textColor);
+        poseStack.translate(0, 0, -401);
         // Return the width of the cell and text
         return 12 + font.width(text);
     }
