757e8b278434567a45535e8ed69605b31b26daf7
MikulDev <gamer3mc@gmail.com>
2026-05-13 00:31:35 -0400

    Refine item temp tooltips & make waterskin TOML-based

--- src/main/java/com/momosoftworks/coldsweat/common/item/FilledWaterskinItem.java
+++ src/main/java/com/momosoftworks/coldsweat/common/item/FilledWaterskinItem.java
@@ -17,7 +17,6 @@ import com.momosoftworks.coldsweat.core.init.ModItemComponents;
 import com.momosoftworks.coldsweat.core.init.ModItems;
 import com.momosoftworks.coldsweat.core.init.ModSounds;
 import com.momosoftworks.coldsweat.core.network.message.ParticleBatchMessage;
-import com.momosoftworks.coldsweat.data.tag.ModItemTempTags;
 import com.momosoftworks.coldsweat.util.math.CSMath;
 import com.momosoftworks.coldsweat.util.serialization.ConfigHelper;
 import com.momosoftworks.coldsweat.util.world.WorldHelper;
@@ -101,7 +100,7 @@ public class FilledWaterskinItem extends Item
         if (EntityTempManager.isTemperatureEnabled(entity) && entity.tickCount % 5 == 0)
         {
             double itemTemp = stack.get(ModItemComponents.WATER_TEMPERATURE);
-            boolean shouldDrain = ConfigHelper.getTaggedConfigsFor(stack.getItem(), ModItemTempTags.DRAINS_WATERSKIN, ConfigSettings.ITEM_TEMPERATURES.get())
+            boolean shouldDrain = ConfigSettings.ITEM_TEMPERATURES.get().get(stack.getItem())
                                   .stream().anyMatch(c -> c.test(entity, stack, slot, null));
             if (shouldDrain)
             {
