d0c2e1da942a37a237e2f5ff29b92a2dd2812d67
MikulDev <gamer3mc@gmail.com>
2026-05-13 00:40:29 -0400

    Remove unused chameleon/tempting tag reference

--- src/main/java/com/momosoftworks/coldsweat/data/tag/ModItemTags.java
+++ src/main/java/com/momosoftworks/coldsweat/data/tag/ModItemTags.java
@@ -14,7 +14,6 @@ public class ModItemTags
     public static final ITag.INamedTag<Item> NOT_INSULATABLE = createTag("not_insulatable");
 
     public static final ITag.INamedTag<Item> CHAMELEON_TAMING = createTag("chameleon/taming");
-    public static final ITag.INamedTag<Item> CHAMELEON_TEMPTING = createTag("chameleon/tempting");
     public static final ITag.INamedTag<Item> CHAMELEON_HOT = createTag("chameleon/find_hot_biomes");
     public static final ITag.INamedTag<Item> CHAMELEON_COLD = createTag("chameleon/find_cold_biomes");
     public static final ITag.INamedTag<Item> CHAMELEON_HUMID = createTag("chameleon/find_humid_biomes");
