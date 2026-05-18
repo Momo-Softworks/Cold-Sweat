dd54ede626828f2439c2153213d90e79223d78e1
MikulDev <gamer3mc@gmail.com>
2026-05-18 17:39:11 -0400

    Add new waterskin configs to auto-updater

--- src/main/java/com/momosoftworks/coldsweat/config/ModUpdater.java
+++ src/main/java/com/momosoftworks/coldsweat/config/ModUpdater.java
@@ -250,6 +250,13 @@ public class ModUpdater
             });
         }
 
+        if (isBehind(configVersion, "2.4.1"))
+        {
+            addConfigSetting(ItemSettingsConfig.ITEM_TEMPERATURES, List.of("cold_sweat:filled_waterskin",  0.025, "hand,hotbar", "core", "{'Temperature':'0.1:'}", 999, true));
+            addConfigSetting(ItemSettingsConfig.ITEM_TEMPERATURES, List.of("cold_sweat:filled_waterskin", -0.025, "hand,hotbar", "core", "{'Temperature':':-0.1'}", 999, true));
+            addConfigSetting(ItemSettingsConfig.FOOD_TEMPERATURES, List.of("cold_sweat:filled_waterskin", "{item:Temperature}"));
+        }
+
         // Update config version
         MainSettingsConfig.VERSION.set(version);
 
