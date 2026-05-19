07dd75610daaf36901646d913974dcc112c66e2e
MikulDev <gamer3mc@gmail.com>
2026-05-19 19:27:26 -0400

    Fix ModUpdater to use 1.21 components in waterskin config

--- src/main/java/com/momosoftworks/coldsweat/config/ModUpdater.java
+++ src/main/java/com/momosoftworks/coldsweat/config/ModUpdater.java
@@ -252,9 +252,9 @@ public class ModUpdater
 
         if (isBehind(configVersion, "2.4.1"))
         {
-            addConfigSetting(ItemSettingsConfig.ITEM_TEMPERATURES, List.of("cold_sweat:filled_waterskin",  0.025, "hand,hotbar", "core", "{'Temperature':'0.1:'}", 999, true));
-            addConfigSetting(ItemSettingsConfig.ITEM_TEMPERATURES, List.of("cold_sweat:filled_waterskin", -0.025, "hand,hotbar", "core", "{'Temperature':':-0.1'}", 999, true));
-            addConfigSetting(ItemSettingsConfig.FOOD_TEMPERATURES, List.of("cold_sweat:filled_waterskin", "{item:Temperature}"));
+            addConfigSetting(ItemSettingsConfig.ITEM_TEMPERATURES, List.of("cold_sweat:filled_waterskin",  0.025, "hand,hotbar", "core", "{'cold_sweat:temperature':'0.1:'}", 999, true));
+            addConfigSetting(ItemSettingsConfig.ITEM_TEMPERATURES, List.of("cold_sweat:filled_waterskin", -0.025, "hand,hotbar", "core", "{'cold_sweat:temperature':':-0.1'}", 999, true));
+            addConfigSetting(ItemSettingsConfig.FOOD_TEMPERATURES, List.of("cold_sweat:filled_waterskin", "{item:cold_sweat:temperature}"));
         }
 
         // Update config version
