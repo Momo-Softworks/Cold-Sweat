8decd3f01263675b65d51ed7d6645a4bd1f75c1d
MikulDev <gamer3mc@gmail.com>
2026-05-23 20:54:12 -0400

    Use Codec to serialize TempModifier

--- src/main/java/com/momosoftworks/coldsweat/util/serialization/NBTHelper.java
+++ src/main/java/com/momosoftworks/coldsweat/util/serialization/NBTHelper.java
@@ -30,6 +30,10 @@ public class NBTHelper
 {
     private NBTHelper() {}
 
+    /**
+     * DEPRECATED. use {@link TempModifier#FULL_CODEC} instead.
+     */
+    @Deprecated(since = "2.4.1", forRemoval = true)
     public static CompoundTag modifierToTag(TempModifier modifier)
     {
         // Write the modifier's data to a CompoundTag
@@ -61,6 +65,10 @@ public class NBTHelper
         return modifierTag;
     }
 
+    /**
+     * DEPRECATED. use {@link TempModifier#FULL_CODEC} instead.
+     */
+    @Deprecated(since = "2.4.1", forRemoval = true)
     public static Optional<TempModifier> tagToModifier(CompoundTag modifierTag)
     {
         // Create a new modifier from the CompoundTag
@@ -120,7 +128,7 @@ public class NBTHelper
     }
 
     /**
-     * Gets an item's tag, without creating a new one if it is not present.<br>
+     * Gets an item's tag, without instantiating one on the item if it isn't present.<br>
      * An empty {@link CompoundTag} will be returned in that case, so a null check will not be necessary.<br>
      * <br>
      * Use {@link ItemStack#getOrCreateTag()} if you need to write to the tag.<br>
@@ -159,7 +167,7 @@ public class NBTHelper
 
     @SubscribeEvent
     public static void convertTagsInContainer(PlayerContainerEvent.Open event)
-    {   updateItemTags(event.getContainer().slots.stream().map(Slot::getItem).toList());
+    {   updateItemTags(event.getContainer().getItems());
     }
 
     private static void updateItemTags(Collection<ItemStack> items)
