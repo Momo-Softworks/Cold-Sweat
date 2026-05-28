532771980b85c2d25dbe3a02a6a231db0a21a9e0
MikulDev <gamer3mc@gmail.com>
2026-05-27 22:44:20 -0400

    Fix tooltip flickering

--- src/main/java/com/momosoftworks/coldsweat/client/event/TooltipHandler.java
+++ src/main/java/com/momosoftworks/coldsweat/client/event/TooltipHandler.java
@@ -437,7 +437,8 @@ public class TooltipHandler
         if (ConfigSettings.ENABLE_HINTS.get())
         {   addUnmetRequirementHints(elements, unmetLabelIndex, allUnmetInsulation);
         }
-        LAST_TOOLTIP = elements;
+        LAST_TOOLTIP.clear();
+        LAST_TOOLTIP.addAll(elements);
     }
 
     private static void addInsulationTooltips(List<ITextComponent> elements, int tooltipStartIndex,
