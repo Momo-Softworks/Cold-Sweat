9173c71615661ab163355f0dc0c7398f8c0cd626
MikulDev <gamer3mc@gmail.com>
2026-05-28 17:22:35 -0400

    Fix temp rate 4x higher than normal

--- src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/handler/EntityTempManager.java
@@ -267,11 +267,7 @@ public class EntityTempManager
             {
                 // Tick modifiers serverside
                 if (isServer)
-                {
-                    // Tick modifiers 1/4 as much for entities
-                    if (entity instanceof Player || entity.tickCount % 5 == 0)
-                    {   cap.tick(entity);
-                    }
+                {   cap.tick(entity);
                 }
 
                 // Tick modifiers & removed expired
