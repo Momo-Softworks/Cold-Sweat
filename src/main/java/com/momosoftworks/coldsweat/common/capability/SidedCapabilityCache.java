99e17772e37ae9b68e9025c34de36fa91575aee6
MikulDev <gamer3mc@gmail.com>
2026-05-28 17:22:17 -0400

    Fix port issues

--- src/main/java/com/momosoftworks/coldsweat/common/capability/SidedCapabilityCache.java
+++ src/main/java/com/momosoftworks/coldsweat/common/capability/SidedCapabilityCache.java
@@ -27,7 +27,7 @@ public class SidedCapabilityCache<C, K extends IAttachmentHolder> extends Capabi
     }
 
     @Override
-    public Optional<C> get(K key)
+    public C get(K key)
     {   return EffectiveSide.get().isClient() ? clientCache.get(key) : super.get(key);
     }
 
@@ -58,7 +58,7 @@ public class SidedCapabilityCache<C, K extends IAttachmentHolder> extends Capabi
     }
 
     @Override
-    public void ifPresent(K key, Consumer<Optional<C>> consumer)
+    public void ifPresent(K key, Consumer<C> consumer)
     {
         if (EffectiveSide.get().isClient())
         {   clientCache.ifPresent(key, consumer);
