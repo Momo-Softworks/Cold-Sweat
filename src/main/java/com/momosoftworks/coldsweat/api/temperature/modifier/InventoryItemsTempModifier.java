7419d8b5afa1ce8723f931a2393bc14196f1a1ff
MikulDev <gamer3mc@gmail.com>
2026-05-29 10:03:04 -0400

    Fix port issues

--- src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/InventoryItemsTempModifier.java
+++ src/main/java/com/momosoftworks/coldsweat/api/temperature/modifier/InventoryItemsTempModifier.java
@@ -33,7 +33,7 @@ public class InventoryItemsTempModifier extends TempModifier
     @Override
     public void tick(LivingEntity entity)
     {
-        if (entity.level.isClientSide) return;
+        if (entity.level().isClientSide) return;
         effectsPerTrait.clear();
 
         Map<ItemTempData, Double> effectsPerItemTemp = new HashMap<>();
