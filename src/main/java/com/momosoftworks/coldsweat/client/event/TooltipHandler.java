c95c443291815bb11b3ec70c151c376aa7139f1d
MikulDev <gamer3mc@gmail.com>
2026-05-30 00:38:12 -0400

    Fix tooltip flickering again

--- src/main/java/com/momosoftworks/coldsweat/client/event/TooltipHandler.java
+++ src/main/java/com/momosoftworks/coldsweat/client/event/TooltipHandler.java
@@ -253,10 +253,7 @@ public class TooltipHandler
     @SubscribeEvent
     public static void updateHoveredItem(RenderTooltipEvent.Pre event)
     {
-        if (!SHOWING_TOOLTIP)
-        {   LAST_TOOLTIP.clear();
-            SHOWING_TOOLTIP = true;
-        }
+        SHOWING_TOOLTIP = true;
         ItemStack stack = event.getItemStack();
 
         if (!HOVERED_STACK.equals(stack))
@@ -289,8 +286,8 @@ public class TooltipHandler
                     if (slotIndex != HOVERED_SLOT)
                     {   FETCHING_TOOLTIP = true;
                         HOVERED_SLOT = slotIndex;
+                        ColdSweatPacketHandler.INSTANCE.sendToServer(SyncItemPredicatesMessage.fromClient(slotIndex, equipmentSlot, stack));
                     }
-                    ColdSweatPacketHandler.INSTANCE.sendToServer(SyncItemPredicatesMessage.fromClient(slotIndex, equipmentSlot, stack));
                 }
             }
         }
@@ -742,9 +739,12 @@ public class TooltipHandler
     @SubscribeEvent
     public static void tickSoulLampInsertTooltip(TickEvent.ClientTickEvent event)
     {
-        if (event.phase == TickEvent.Phase.END)
+        if (event.phase == TickEvent.Phase.START)
         {   FUEL_FADE_TIMER++;
             SHOWING_TOOLTIP = false;
+        }
+        else if (!SHOWING_TOOLTIP)
+        {   LAST_TOOLTIP.clear();
         }
     }
 }
