125782cd95c03fe17f96b0ee8fac467cab47cedc
manhtuan28 <buimanhtuan2k4@gmail.com>
2026-06-10 23:16:58 -0400

    Add Immersive Engineering External Heater support for Hearth & Boiler

--- src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
@@ -1,11 +1,13 @@
 package com.momosoftworks.coldsweat.compat;
 
+import blusunrize.immersiveengineering.api.tool.ExternalHeaterHandler;
 import com.anthonyhilyard.iceberg.util.Tooltips;
 import com.mojang.datafixers.util.Either;
 import com.momosoftworks.coldsweat.ColdSweat;
 import com.momosoftworks.coldsweat.api.event.core.init.FetchSeasonsModsEvent;
 import com.momosoftworks.coldsweat.api.insulation.Insulation;
 import com.momosoftworks.coldsweat.api.util.Temperature;
+import com.momosoftworks.coldsweat.common.blockentity.HearthBlockEntity;
 import com.momosoftworks.coldsweat.common.capability.handler.EntityTempManager;
 import com.momosoftworks.coldsweat.common.item.ThermometerItem;
 import com.momosoftworks.coldsweat.compat.create.ColdSweatDisplayBehaviors;
@@ -44,6 +46,7 @@ import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -94,6 +97,7 @@ public class CompatManager
     private static final boolean AETHER_LOADED = modLoaded("aether");
     private static final boolean REGIONS_UNEXPLORED_LOADED = modLoaded("regions_unexplored");
     private static final boolean AD_ASTRA_LOADED = modLoaded("ad_astra");
+    private static final boolean IMMERSIVE_ENGINEERING_LOADED = modLoaded("immersiveengineering");
 
     private static final List<String> SEASONS_MODS = new ArrayList<>();
 
@@ -222,6 +226,9 @@ public class CompatManager
     }
     public static boolean isAdAstraLoaded()
     {   return AD_ASTRA_LOADED;
+    }
+    public static boolean isImmersiveEngineeringLoaded()
+    {   return IMMERSIVE_ENGINEERING_LOADED;
     }
 
     public static abstract class Curios
@@ -376,6 +383,40 @@ public class CompatManager
             Vector3d translated = new Vector3d(pos.getX(), pos.getY(), pos.getZ());
             translated = ship.getWorldToShip().transformPosition(translated);
             return new BlockPos(VectorConversionsMCKt.toMinecraft(translated));
+        }
+    }
+
+    public static abstract class ImmersiveEngineering
+    {
+        public static final int ENERGY_PER_FUEL = 256;
+        private static final Map<HearthBlockEntity, LazyOptional<ExternalHeaterHandler.IExternalHeatable>> HEATER_CAPS = new HashMap<>();
+
+        public static LazyOptional<ExternalHeaterHandler.IExternalHeatable> getHeaterCap(HearthBlockEntity hearthLike)
+        {
+            LazyOptional<ExternalHeaterHandler.IExternalHeatable> heaterCap = HEATER_CAPS.computeIfAbsent(hearthLike, hearth ->
+                LazyOptional.of(() -> new ExternalHeaterHandler.IExternalHeatable()
+                {
+                    @Override
+                    public int doHeatTick(int energyAvailable, boolean redstone)
+                    {
+                        if (hearth.getFuel(HearthBlockEntity.FuelType.HOT).get() < hearth.getMaxFuel())
+                        {
+                            if (energyAvailable >= ENERGY_PER_FUEL)
+                            {
+                                hearth.addHotFuel(1, true);
+                                return ENERGY_PER_FUEL;
+                            }
+                        }
+                        return 0;
+                    }
+                }));
+            heaterCap.addListener(cap -> HEATER_CAPS.remove(hearthLike));
+            return heaterCap;
+        }
+
+        public static void invalidateHeaterCap(HearthBlockEntity hearthLike)
+        {   LazyOptional<ExternalHeaterHandler.IExternalHeatable> cap = HEATER_CAPS.remove(hearthLike);
+            if (cap != null) cap.invalidate();
         }
     }
 
