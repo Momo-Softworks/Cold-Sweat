d3791bfb165cfb6faa46f4e573c66ec2d39a6e33
MikulDev <gamer3mc@gmail.com>
2026-06-17 18:37:39 -0400

    Increase minimum required TWR version

--- src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
@@ -67,7 +67,6 @@ import top.theillusivec4.curios.common.capability.CurioItemCapability;
 import top.theillusivec4.curios.common.capability.ItemizedCurioCapability;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
 
 @Mod.EventBusSubscriber
 public class CompatManager
@@ -85,7 +84,7 @@ public class CompatManager
     private static final boolean WYTHERS_LOADED = modLoaded("wwoo");
     private static final boolean TOOLTIPS_LOADED = modLoaded("legendarytooltips");
     private static final boolean PRIMAL_WINTER_LOADED = modLoaded("primalwinter");
-    private static final boolean THIRST_LOADED = modLoaded("thirst", "1.19.2-1.3.11");
+    private static final boolean THIRST_LOADED = modLoaded("thirst", "1.19.2-3.0.0");
     private static final boolean ICEBERG_LOADED = modLoaded("iceberg");
     private static final boolean SPOILED_LOADED = modLoaded("spoiled");
     private static final boolean SUPPLEMENTARIES_LOADED = modLoaded("supplementaries");
