978da65f151be8c14a4c26a646f29b41cd1c28bf
MikulDev <gamer3mc@gmail.com>
2026-06-17 18:37:39 -0400

    Increase minimum required TWR version

--- src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
+++ src/main/java/com/momosoftworks/coldsweat/compat/CompatManager.java
@@ -92,7 +92,7 @@ public class CompatManager
     private static final boolean WYTHERS_LOADED = modLoaded("wwoo");
     private static final boolean TOOLTIPS_LOADED = modLoaded("legendarytooltips");
     private static final boolean PRIMAL_WINTER_LOADED = modLoaded("primalwinter");
-    private static final boolean THIRST_LOADED = modLoaded("thirst", "1.21.1-2.1.0");
+    private static final boolean THIRST_LOADED = modLoaded("thirst", "1.21.1-3.0.0");
     private static final boolean ICEBERG_LOADED = modLoaded("iceberg", "1.3.0");
     private static final boolean SPOILED_LOADED = modLoaded("spoiled", "6.2.0");
     private static final boolean SUPPLEMENTARIES_LOADED = modLoaded("supplementaries");
