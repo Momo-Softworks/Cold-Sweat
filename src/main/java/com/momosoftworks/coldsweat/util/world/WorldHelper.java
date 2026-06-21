27bdd36ab4ba113abf64b2c1cfce5efc9b083240
MikulDev <gamer3mc@gmail.com>
2026-06-20 23:10:51 -0400

    Simplify WorldHelper.getStructureAt

--- src/main/java/com/momosoftworks/coldsweat/util/world/WorldHelper.java
+++ src/main/java/com/momosoftworks/coldsweat/util/world/WorldHelper.java
@@ -26,7 +26,6 @@ import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -47,7 +46,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.LevelChunkSection;
-import net.minecraft.world.level.chunk.status.ChunkStatus;
+
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureStart;
@@ -348,33 +347,14 @@ public abstract class WorldHelper
         if (!(level instanceof ServerLevel serverLevel) || !level.isLoaded(pos)) return Optional.empty();
 
         StructureManager structureManager = serverLevel.structureManager();
-        Registry<Structure> structureRegistry = serverLevel.registryAccess().registryOrThrow(Registries.STRUCTURE);
-
-        // Iterate over all structures at the position (ignores Y level)
+        Registry<Structure> registry = serverLevel.registryAccess().registryOrThrow(Registries.STRUCTURE);
         for (Map.Entry<Structure, LongSet> entry : structureManager.getAllStructuresAt(pos).entrySet())
         {
             Structure structure = entry.getKey();
-            LongSet strucCoordinates = entry.getValue();
-
-            // Iterate over all chunk coordinates within the structures
-            for (long coordinate : strucCoordinates)
+            StructureStart start = structureManager.getStructureWithPieceAt(pos, structure);
+            if (start.isValid())
             {
-                SectionPos sectionpos = SectionPos.of(new ChunkPos(coordinate), level.getMinSection());
-                // Get the structure start
-                StructureStart structurestart = structureManager.getStartForStructure(sectionpos, structure, level.getChunk(sectionpos.x(), sectionpos.z(), ChunkStatus.STRUCTURE_STARTS));
-
-                if (structurestart != null && structurestart.isValid())
-                {
-                    // If the structure has a piece at the position, get the structure's holder
-                    if (structureManager.structureHasPieceAt(pos, structurestart))
-                    {
-                        ResourceLocation structureId = structureRegistry.getKey(structure);
-                        if (structureId == null)
-                        {   return Optional.empty();
-                        }
-                        return (Optional<Holder<Structure>>) (Object) structureRegistry.getHolder(ResourceKey.create(Registries.STRUCTURE, structureId));
-                    }
-                }
+                return registry.getHolder(registry.getResourceKey(structure).orElseThrow()).map(holder -> holder);
             }
         }
         return Optional.empty();
