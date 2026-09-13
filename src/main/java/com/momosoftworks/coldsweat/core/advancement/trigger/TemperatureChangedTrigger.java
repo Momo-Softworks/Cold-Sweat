c924a4e825dcc6964fd920a914e5f1423d0a6e23
MikulDev <gamer3mc@gmail.com>
2026-09-12 20:16:13 -0400

    Make advancement trigger fields consistent

--- src/main/java/com/momosoftworks/coldsweat/core/advancement/trigger/TemperatureChangedTrigger.java
+++ src/main/java/com/momosoftworks/coldsweat/core/advancement/trigger/TemperatureChangedTrigger.java
@@ -28,7 +28,7 @@ public class TemperatureChangedTrigger extends SimpleCriterionTrigger<Temperatur
         {
             JsonObject entry = element.getAsJsonObject();
 
-            Temperature.Trait trait = Temperature.Trait.fromID(entry.get("type").getAsString());
+            Temperature.Trait trait = Temperature.Trait.fromID(entry.get("trait").getAsString());
 
             TriggerHelper.getTempValueOrRange(entry)
                  .ifLeft(either -> conditions.add(new TriggerHelper.TempCondition(trait, either, either)))
